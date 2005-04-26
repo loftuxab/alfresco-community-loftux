27f4aad4b7883288dff13f949a99268504808177
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-04-26 09:22:44 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/company-name/HEAD@191 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/core/source/java/com/activiti/util/Conversion.java
+++ projects/core/source/java/com/activiti/util/Conversion.java
@@ -76,4 +76,30 @@ public final class Conversion
       
       return buf.toString();
    }
+   
+   /**
+    * Convert an XML format date of the form <pre>yyyy-mm-ddThh:mm:ss</pre> to a Date object
+    * 
+    * @param xml
+    * 
+    * @return Date object or null if no XML date was provided
+    */
+   public static Date dateFromXmlDate(String xml)
+   {
+      if (xml == null || xml.length() != 19)
+      {
+         return null;
+      }
+      
+      int year    = Integer.parseInt(xml.substring(0, 4));
+      int month   = Integer.parseInt(xml.substring(5, 7));
+      int day     = Integer.parseInt(xml.substring(8, 10));
+      int hours   = Integer.parseInt(xml.substring(11, 13));
+      int mins    = Integer.parseInt(xml.substring(14, 16));
+      int secs    = Integer.parseInt(xml.substring(17, 19));
+      
+      Date date = new Date(year - 1900, month - 1, day, hours, mins, secs);
+      
+      return date;
+   }
 }
