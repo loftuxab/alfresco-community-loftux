be22d48b5bd3a2046c6c8eb932008a1a0607d2c7
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-05-03 16:45:11 +0000

    "Browse Items" view mode in the main browse screen now displays the first few immediate children of a node.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/company-name/HEAD@224 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ projects/web-client/source/java/org/alfresco/web/ui/repo/component/UINodeDescendants.java
@@ -0,0 +1,135 @@
+/*
+ * Created on 03-May-2005
+ */
+package org.alfresco.web.ui.repo.component;
+
+import javax.faces.component.UICommand;
+import javax.faces.component.UIComponent;
+import javax.faces.el.ValueBinding;
+import javax.faces.event.ActionEvent;
+
+import com.activiti.repo.ref.NodeRef;
+
+/**
+ * @author Kevin Roast
+ */
+public class UINodeDescendants extends UICommand
+{
+   // ------------------------------------------------------------------------------
+   // Construction
+   
+   /**
+    * Default constructor
+    */
+   public UINodeDescendants()
+   {
+      setRendererType("awc.repo.NodeDescendantsLinkRenderer");
+   }
+   
+   
+   // ------------------------------------------------------------------------------
+   // Component implementation
+   
+   /**
+    * @see javax.faces.component.UIComponent#getFamily()
+    */
+   public String getFamily()
+   {
+      return "awc.repo.NodeDescendants";
+   }
+   
+   
+   // ------------------------------------------------------------------------------
+   // Strongly typed component property accessors
+   
+   /**
+    * @return the maximum number of child descendants to be displayed, default maximum is 4. 
+    */
+   public int getMaxChildren()
+   {
+      ValueBinding vb = getValueBinding("maxChildren");
+      if (vb != null)
+      {
+         this.maxChildren = (Integer)vb.getValue(getFacesContext());
+      }
+      
+      if (this.maxChildren != null)
+      {
+         return this.maxChildren.intValue();
+      }
+      else
+      {
+         // return default
+         return 4;
+      }
+   }
+   
+   /**
+    * @param value      The maximum allowed before the no more links are shown
+    */
+   public void setMaxChildren(int value)
+   {
+      if (value > 0 && value <= 256)
+      {
+         this.maxChildren = Integer.valueOf(value);
+      }
+   }
+   
+   /**
+    * @return whether to show ellipses "..." if more descendants than the maxChildren value are found
+    */
+   public boolean getShowEllipses()
+   {
+      ValueBinding vb = getValueBinding("showEllipses");
+      if (vb != null)
+      {
+         this.showEllipses = (Boolean)vb.getValue(getFacesContext());
+      }
+      
+      if (this.showEllipses != null)
+      {
+         return this.showEllipses.booleanValue();
+      }
+      else
+      {
+         // return default
+         return true;
+      }
+   }
+   
+   /**
+    * @param showLink      True to show ellipses "..." if more descendants than maxChildren are found
+    */
+   public void setShowEllipses(boolean showEllipses)
+   {
+      this.showEllipses = Boolean.valueOf(showEllipses);
+   }
+   
+   
+   // ------------------------------------------------------------------------------
+   // Inner classes
+   
+   /**
+    * Class representing the clicking of a node descendant element.
+    */
+   public static class NodeSelectedEvent extends ActionEvent
+   {
+      public NodeSelectedEvent(UIComponent component, NodeRef nodeRef)
+      {
+         super(component);
+         NodeReference = nodeRef;
+      }
+      
+      public NodeRef NodeReference = null;
+   }
+   
+   
+   // ------------------------------------------------------------------------------
+   // Private data
+   
+   /** maximum number of child descendants to display */
+   private Integer maxChildren = null;
+   
+   /** whether to show ellipses if more descendants than the maxChildren are found */
+   private Boolean showEllipses = null;
+}
---
+++ projects/web-client/source/java/org/alfresco/web/ui/repo/renderer/NodeDescendantsLinkRenderer.java
@@ -0,0 +1,199 @@
+/*
+ * Created on 03-May-2005
+ */
+package org.alfresco.web.ui.repo.renderer;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.component.NamingContainer;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import org.alfresco.web.ui.repo.component.UINodeDescendants;
+import org.springframework.web.jsf.FacesContextUtils;
+
+import com.activiti.repo.node.NodeService;
+import com.activiti.repo.ref.ChildAssocRef;
+import com.activiti.repo.ref.NodeRef;
+import com.activiti.web.bean.repository.Node;
+import com.activiti.web.bean.repository.Repository;
+import com.activiti.web.jsf.Utils;
+import com.activiti.web.jsf.renderer.BaseRenderer;
+
+/**
+ * @author Kevin Roast
+ */
+public class NodeDescendantsLinkRenderer extends BaseRenderer
+{
+   // ------------------------------------------------------------------------------
+   // Renderer implementation
+   
+   /**
+    * @see javax.faces.render.Renderer#decode(javax.faces.context.FacesContext, javax.faces.component.UIComponent)
+    */
+   public void decode(FacesContext context, UIComponent component)
+   {
+      Map requestMap = context.getExternalContext().getRequestParameterMap();
+      String fieldId = getHiddenFieldName(context, component);
+      String value = (String)requestMap.get(fieldId);
+      
+      // we encoded the value to start with our Id
+      if (value != null && value.startsWith(component.getClientId(context) + NamingContainer.SEPARATOR_CHAR))
+      {
+         // found a new selected value for this component
+         // queue an event to represent the change
+         String selectedNodeId = value.substring(component.getClientId(context).length() + 1);
+         NodeService service = getNodeService(context);
+         NodeRef ref = new NodeRef(Repository.getStoreRef(), selectedNodeId);
+         
+         UINodeDescendants.NodeSelectedEvent event = new UINodeDescendants.NodeSelectedEvent(component, ref); 
+         component.queueEvent(event);
+      }
+   }
+   
+   /**
+    * @see javax.faces.render.Renderer#encodeEnd(javax.faces.context.FacesContext, javax.faces.component.UIComponent)
+    */
+   public void encodeEnd(FacesContext context, UIComponent component) throws IOException
+   {
+      // always check for this flag - as per the spec
+      if (component.isRendered() == true)
+      {
+         Writer out = context.getResponseWriter();
+         
+         UINodeDescendants control = (UINodeDescendants)component;
+         
+         // make sure we have a NodeRef from the 'value' property ValueBinding
+         Object val = control.getValue();
+         if (val instanceof NodeRef == false)
+         {
+            throw new IllegalArgumentException("UINodeDescendants component 'value' property must resolve to a NodeRef!");
+         }
+         NodeRef parentRef = (NodeRef)val;
+         
+         // TODO: check we have a "container" node here?!
+         
+         // use Spring JSF integration to get the node service bean
+         NodeService service = getNodeService(context);
+         List<ChildAssocRef> childRefs = service.getChildAssocs(parentRef);
+         
+         // TODO: need a comparator to sort node refs (based on childref qname)
+         //       as currently the list is returned in a random order per request
+         
+         // walk each child ref and output a descendant link control for each item
+         String separator = (String)component.getAttributes().get("separator");
+         if (separator == null)
+         {
+            separator = DEFAULT_SEPARATOR;
+         }
+         int maximum = childRefs.size() > control.getMaxChildren() ? control.getMaxChildren() : childRefs.size();
+         for (int index=0; index<maximum; index++)
+         {
+            ChildAssocRef ref = childRefs.get(index);
+            out.write(renderDescendant(context, control, ref, false));
+            
+            // output separator if appropriate
+            if (index < maximum - 1)
+            {
+               out.write( separator );
+            }
+         }
+         
+         // do we need to render ellipses to indicate more items than the maximum
+         if (control.getShowEllipses() == true && childRefs.size() > control.getMaxChildren())
+         {
+            out.write( separator );
+            // TODO: is this the correct way to get the information we need?
+            //       e.g. primary parent may not be the correct path? how do we make sure we find
+            //       the correct parent and more importantly the correct Display Name value!
+            out.write( renderDescendant(context, control, service.getPrimaryParent(parentRef), true) );
+         }
+      }
+   }
+   
+   /**
+    * Render a descendant as a clickable link
+    * 
+    * @param context    FacesContext
+    * @param control    UINodeDescendants to get attributes from
+    * @param childRef   The ChildAssocRef of the child to render an HTML link for
+    * @param ellipses   Whether to render the label of this descendant as a ellipses i.e. "..."
+    *  
+    * @return HTML for a descendant link
+    */
+   private String renderDescendant(FacesContext context, UINodeDescendants control, ChildAssocRef childRef, boolean ellipses)
+   {
+      StringBuilder buf = new StringBuilder(256);
+      
+      buf.append("<a href='#' onclick=\"");
+      // build an HTML param that contains the client Id of this control, followed by the node Id
+      String param = control.getClientId(context) + NamingContainer.SEPARATOR_CHAR + childRef.getChildRef().getId();
+      buf.append(Utils.generateFormSubmit(context, control, getHiddenFieldName(context, control), param));
+      buf.append('"');
+      Map attrs = control.getAttributes();
+      if (attrs.get("style") != null)
+      {
+         buf.append(" style=\"")
+            .append(attrs.get("style"))
+            .append('"');
+      }
+      if (attrs.get("styleClass") != null)
+      {
+         buf.append(" class=")
+            .append(attrs.get("styleClass"));
+      }
+      buf.append('>');
+      
+      if (ellipses == false)
+      {
+         // label is the name of the child node
+         buf.append(Utils.encode(childRef.getQName().getLocalName()));
+      }
+      else
+      {
+         buf.append("...");
+      }
+      
+      buf.append("</a>");
+      
+      return buf.toString();
+   }
+   
+   
+   // ------------------------------------------------------------------------------
+   // Private helpers
+
+   /**
+    * Get the hidden field name for this node descendant component.
+    * Build a shared field name from the parent form name and the string "ndec".
+    * 
+    * @return hidden field name shared by all node descendant components within the Form.
+    */
+   private static String getHiddenFieldName(FacesContext context, UIComponent component)
+   {
+      return Utils.getParentForm(context, component).getClientId(context) + NamingContainer.SEPARATOR_CHAR + "ndec";
+   }
+   
+   /**
+    * Use Spring JSF integration to return the node service bean instance
+    * 
+    * @param context    FacesContext
+    * 
+    * @return node service bean instance or throws runtime exception if not found
+    */
+   private static NodeService getNodeService(FacesContext context)
+   {
+      NodeService service = (NodeService)FacesContextUtils.getRequiredWebApplicationContext(context).getBean(Repository.NODE_SERVICE);
+      if (service == null)
+      {
+         throw new IllegalStateException("Unable to obtain NodeService bean reference.");
+      }
+      
+      return service;
+   }
+   
+   private static final String DEFAULT_SEPARATOR = " | ";
+}
