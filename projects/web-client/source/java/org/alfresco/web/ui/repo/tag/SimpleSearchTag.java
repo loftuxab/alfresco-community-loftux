9ff44a1463b3277565730d5dca57406760b9dca4
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-05-09 15:40:01 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@269 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/web-client/source/java/org/alfresco/web/ui/repo/tag/SimpleSearchTag.java
+++ projects/web-client/source/java/org/alfresco/web/ui/repo/tag/SimpleSearchTag.java
@@ -3,6 +3,9 @@
  */
 package org.alfresco.web.ui.repo.tag;
 
+import javax.faces.component.UICommand;
+import javax.faces.component.UIComponent;
+
 import org.alfresco.web.ui.common.tag.HtmlComponentTag;
 
 /**
@@ -11,19 +14,11 @@ import org.alfresco.web.ui.common.tag.HtmlComponentTag;
 public class SimpleSearchTag extends HtmlComponentTag
 {
    /**
-    * 
-    */
-   public SimpleSearchTag()
-   {
-      super();
-   }
-
-   /**
     * @see javax.faces.webapp.UIComponentTag#getComponentType()
     */
    public String getComponentType()
    {
-      return null;
+      return "org.alfresco.faces.SimpleSearch";
    }
 
    /**
@@ -31,6 +26,56 @@ public class SimpleSearchTag extends HtmlComponentTag
     */
    public String getRendererType()
    {
+      // self rendering component
       return null;
    }
+
+   /**
+    * @see javax.faces.webapp.UIComponentTag#setProperties(javax.faces.component.UIComponent)
+    */
+   protected void setProperties(UIComponent component)
+   {
+      super.setProperties(component);
+      
+      setActionProperty((UICommand)component, this.action);
+      setActionListenerProperty((UICommand)component, this.actionListener);
+   }
+   
+   /**
+    * @see org.alfresco.web.ui.common.tag.HtmlComponentTag#release()
+    */
+   public void release()
+   {
+      super.release();
+      
+      this.action = null;
+      this.actionListener = null;
+   }
+   
+   /**
+    * Set the action
+    *
+    * @param action     the action
+    */
+   public void setAction(String action)
+   {
+      this.action = action;
+   }
+
+   /**
+    * Set the actionListener
+    *
+    * @param actionListener     the actionListener
+    */
+   public void setActionListener(String actionListener)
+   {
+      this.actionListener = actionListener;
+   }
+
+
+   /** the action */
+   private String action;
+
+   /** the actionListener */
+   private String actionListener;
 }
