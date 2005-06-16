48ebc3fdd1835a37e7445ee0d15b36d4df8623e6
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-06-16 10:28:19 +0000

    Fixed formatting
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@527 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/repository/source/java/org/alfresco/service/cmr/rule/RuleService.java
+++ projects/repository/source/java/org/alfresco/service/cmr/rule/RuleService.java
@@ -3,9 +3,7 @@
  */
 package org.alfresco.service.cmr.rule;
 
-import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
 import org.alfresco.service.cmr.repository.NodeRef;
 
@@ -14,112 +12,113 @@ import org.alfresco.service.cmr.repository.NodeRef;
  * 
  * @author Roy Wetherall
  */
-public interface RuleService 
+public interface RuleService
 {
-	/**
-	 * Get the rule types currently defined in the repository.
-	 * 
-	 * @return	a list of rule types
-	 */
-	public List<RuleType> getRuleTypes();
-    
+    /**
+     * Get the rule types currently defined in the repository.
+     * 
+     * @return a list of rule types
+     */
+    public List<RuleType> getRuleTypes();
+
     /**
      * Gets a rule type by name.
      * 
-     * @param name      the name of the rule type
-     * @return          the rule type, null if not found
+     * @param name the name of the rule type
+     * @return the rule type, null if not found
      */
     public RuleType getRuleType(String name);
-	
-	/**
-	 * Get the condition definitions currently defined in the repository.
-	 * 
-	 * @return	a list of condition definitions
-	 */
-	public List<RuleConditionDefinition> getConditionDefinitions();
-	
+
+    /**
+     * Get the condition definitions currently defined in the repository.
+     * 
+     * @return a list of condition definitions
+     */
+    public List<RuleConditionDefinition> getConditionDefinitions();
+
     /**
      * Get a condition defintion by name.
      * 
-     * @param name  the name of the condition definition
-     * @return      the condition definition, null if not found
+     * @param name the name of the condition definition
+     * @return the condition definition, null if not found
      */
     public RuleConditionDefinition getConditionDefintion(String name);
-    
-	/**
-	 * Get the action definitions currently defined in the repository.
-	 * 
-	 * @return	a list of action definitions
-	 */
-	public List<RuleActionDefinition> getActionDefinitions();
-	
+
+    /**
+     * Get the action definitions currently defined in the repository.
+     * 
+     * @return a list of action definitions
+     */
+    public List<RuleActionDefinition> getActionDefinitions();
+
     /**
      * Get an action definition by name
      * 
-     * @param name  the name of the action definition
-     * @return      the action definition, null if not found
+     * @param name the name of the action definition
+     * @return the action definition, null if not found
      */
     public RuleActionDefinition getActionDefinition(String name);
-    
-	/**
-	 * Makes a specified node Actionable.
-	 * 
-	 * @param nodeRef	the node reference
-	 */
-	public void makeActionable(NodeRef nodeRef);
-	
-	/**
-	 * Indicates whether a node is actionable.
-	 * 
-	 * @param nodeRef   the node reference
-	 * @return			true if the node is actionable, false otherwise
-	 */
-	public boolean isActionable(NodeRef nodeRef);
-	
+
+    /**
+     * Makes a specified node Actionable.
+     * 
+     * @param nodeRef the node reference
+     */
+    public void makeActionable(NodeRef nodeRef);
+
+    /**
+     * Indicates whether a node is actionable.
+     * 
+     * @param nodeRef the node reference
+     * @return true if the node is actionable, false otherwise
+     */
+    public boolean isActionable(NodeRef nodeRef);
+
     /**
      * Indicates whether the node in question has any rules associated with it.
      * 
-     * @param nodeRef   the node reference
-     * @return          true if the node has rules associated, false otherwise
+     * @param nodeRef the node reference
+     * @return true if the node has rules associated, false otherwise
      */
     public boolean hasRules(NodeRef nodeRef);
-    
-	/**
-	 * Get all the rules associated with an actionable node, including those
-	 * inherited from parents.
-	 * <p>
-	 * An exception is raised if the actionable aspect is not present on the 
-	 * passed node.
-	 * 
-	 * @param nodeRef	the node reference
-	 * @return			a list of the rules associated with the node 
-	 */
-	public List<Rule> getRules(NodeRef nodeRef);
-	
-	/**
-	 * Get the rules associated with an actionable node.
-	 * <p>
-	 * Optionally this list includes rules inherited from its parents.
-	 * <p>
-	 * An exception is raised if the actionable aspect is not present on the 
-	 * passed node.
-	 * 
-	 * @param nodeRef			the node reference
-	 * @param includeInhertied  indicates whether the inherited rules should be included
-	 * 						    in the result list or not
-	 * @return					a list of the rules associated with the node 
-	 */
-	public List<Rule> getRules(NodeRef nodeRef, boolean includeInhertied);
-	
-	/**
-	 * Get the rule given its id.
-	 * 
-	 * @param nodeRef	the node reference
-	 * @param ruleId	the rule id
-	 * @return			the rule corresponding ot the id
-	 */
-	public Rule getRule(NodeRef nodeRef, String ruleId);
-    
+
+    /**
+     * Get all the rules associated with an actionable node, including those
+     * inherited from parents.
+     * <p>
+     * An exception is raised if the actionable aspect is not present on the
+     * passed node.
+     * 
+     * @param nodeRef the node reference
+     * @return a list of the rules associated with the node
+     */
+    public List<Rule> getRules(NodeRef nodeRef);
+
+    /**
+     * Get the rules associated with an actionable node.
+     * <p>
+     * Optionally this list includes rules inherited from its parents.
+     * <p>
+     * An exception is raised if the actionable aspect is not present on the
+     * passed node.
+     * 
+     * @param nodeRef the node reference
+     * @param includeInhertied
+     *            indicates whether the inherited rules should be included in
+     *            the result list or not
+     * @return a list of the rules associated with the node
+     */
+    public List<Rule> getRules(NodeRef nodeRef, boolean includeInhertied);
+
+    /**
+     * Get the rule given its id.
+     * 
+     * @param nodeRef the node reference
+     * @param ruleId the rule id
+     * @return the rule corresponding ot the id
+     */
+    public Rule getRule(NodeRef nodeRef, String ruleId);
+
     /**
      * 
      * @param nodeRef
@@ -127,54 +126,58 @@ public interface RuleService
      * @return
      */
     public List<Rule> getRulesByRuleType(NodeRef nodeRef, RuleType ruleType);
-	
-	/**
-	 * Get the rules associated with an actionable node that would be executed for a 
-	 * specified execution context.
-	 * <p>
-	 * An exception is raised if the actionable aspect is not present on the 
-	 * passed node.
-	 * 
-	 * @param nodeRef			the node reference
-	 * @param ruleType			the type of rule to be executed
-	 * @param executionContext	the execution context
-	 * @return					a list of the rules that would be executed
-	 */
-    // TODO decide whether we need this or not
-	//public List<Rule> previewExecutingRules(NodeRef nodeRef, RuleType ruleType, Map<String, Serializable> executionContext);	
-	
-	/**
+
+//    // TODO decide whether we need this or not
+//    /**
+//     * Get the rules associated with an actionable node that would be executed
+//     * for a specified execution context.
+//     * <p>
+//     * An exception is raised if the actionable aspect is not present on the
+//     * passed node.
+//     * 
+//     * @param nodeRef
+//     *            the node reference
+//     * @param ruleType
+//     *            the type of rule to be executed
+//     * @param executionContext
+//     *            the execution context
+//     * @return a list of the rules that would be executed
+//     */
+//    // public List<Rule> previewExecutingRules(NodeRef nodeRef, RuleType
+//    // ruleType, Map<String, Serializable> executionContext);
+
+    /**
      * Helper method to create a new rule.
      * <p>
-     * Call add rule once the details of the rule have been specified in
-     * order to associate the rule with a node reference.
+     * Call add rule once the details of the rule have been specified in order
+     * to associate the rule with a node reference.
      * 
-     * @param   ruleType    the type of rule to create
-     * @return              the created rule
-	 */
-	public Rule createRule(RuleType ruleType);
-	
-	/**
-	 * Adds the details of the rule to the specified node reference.
+     * @param ruleType the type of rule to create
+     * @return the created rule
+     */
+    public Rule createRule(RuleType ruleType);
+
+    /**
+     * Adds the details of the rule to the specified node reference.
      * <p>
-     * If the rule is already associated with the node, the details are 
-     * updated with those specified.
-	 * 
-	 * @param nodeRef
-	 * @param rule
-	 */
-	public void addRule(NodeRef nodeRef, Rule rule);
-	
-	/**
-	 * Removes a rule associated with a node
-	 * 
-	 * @param nodeRef
-	 * @param rule
-	 */
-	public void removeRule(NodeRef nodeRef, Rule rule);	
-	
-	public void addRulePendingExecution(NodeRef actionableNodeRef, NodeRef actionedUponNodeRef, Rule rule);
-	
-	public void executePendingRules();
-	
+     * If the rule is already associated with the node, the details are updated
+     * with those specified.
+     * 
+     * @param nodeRef
+     * @param rule
+     */
+    public void addRule(NodeRef nodeRef, Rule rule);
+
+    /**
+     * Removes a rule associated with a node
+     * 
+     * @param nodeRef
+     * @param rule
+     */
+    public void removeRule(NodeRef nodeRef, Rule rule);
+
+    public void addRulePendingExecution(NodeRef actionableNodeRef, NodeRef actionedUponNodeRef, Rule rule);
+
+    public void executePendingRules();
+
 }
