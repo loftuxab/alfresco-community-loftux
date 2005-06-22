bb96e97d6e1c6fb172051af250c1a4b26a40e2a5
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-06-22 16:36:47 +0000

    removed non-mysql elements
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@593 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/installer/tomcat-bundle/db_setup.bat
+++ projects/installer/tomcat-bundle/db_setup.bat
@@ -7,10 +7,4 @@ c:\mysql\bin\mysqladmin -u root create alfresco
 c:\mysql\bin\mysql -u root -e "grant all on alfresco.* to 'alfresco'@'localhost' identified by 'alfresco' with grant option;"
 c:\mysql\bin\mysqladmin -u root shutdown
 
-rem ---------------------------------------
-rem Postgres create DB command
-rem ---------------------------------------
-rem c:\alfresco\postgresql\bin\initdb -D c:\alfresco\data
-rem c:\alfresco\postgresql\bin\createdb alfresco
-
 echo Alfresco database configured.
--- projects/installer/tomcat-bundle/startup.bat
+++ projects/installer/tomcat-bundle/startup.bat
@@ -12,19 +12,6 @@ start "MySQL Server" /min cmd /c c:\mysql\bin\mysqld-nt --console
 sleep 5
 
 rem ---------------------------------------
-rem Start DB (Postgres)
-rem ---------------------------------------
-rem cd \alfresco\tomcat
-rem c:\alfresco\postgres\bin\pg_ctl start -D c:\alfresco\data -l c:\alfresco\logfile
-
-rem ---------------------------------------
-rem Start DB (HSQLDB) in a minimised console
-rem ---------------------------------------
-rem start "HSQL Server" /min cmd /c java -cp tomcat/lib/hsqldb.jar org.hsqldb.Server -database.0 alfresco -dbname.0 alfresco
-
-echo Starting Tomcat...
-
-rem ---------------------------------------
 rem Start Tomcat
 rem ---------------------------------------
 cd \alfresco\tomcat
