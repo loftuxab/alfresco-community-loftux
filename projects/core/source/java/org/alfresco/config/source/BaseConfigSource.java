264b2e591bc38d670e61bd73561c6dde1a735c8f
dcaruana <dcaruana@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-06-23 18:50:39 +0000

    Addition of source code header to java files
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@623 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/core/source/java/org/alfresco/config/source/BaseConfigSource.java
+++ projects/core/source/java/org/alfresco/config/source/BaseConfigSource.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2005 Alfresco, Inc.
+ *
+ * Licensed under the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/lgpl.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
 package org.alfresco.config.source;
 
 import java.io.InputStream;
--- projects/repository/source/java/org/alfresco/repo/node/ConcurrentNodeServiceTest.java
+++ projects/repository/source/java/org/alfresco/repo/node/ConcurrentNodeServiceTest.java
@@ -1,8 +1,19 @@
 /*
- * Created on 17-Jun-2005
+ * Copyright (C) 2005 Alfresco, Inc.
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Licensed under the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/lgpl.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
  */
 package org.alfresco.repo.node;
 
