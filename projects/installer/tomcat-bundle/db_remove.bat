06e8be68ab096e98180459e3c6a4822525e83af9
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-06-24 11:37:54 +0000

    updates
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@646 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/installer/tomcat-bundle/db_remove.bat
+++ projects/installer/tomcat-bundle/db_remove.bat
@@ -3,6 +3,7 @@ rem ---------------------------------------
 rem MySQL create DB command
 rem ---------------------------------------
 start "MySQL Server" /min cmd /c c:\mysql\bin\mysqld-nt --console
+sleep 3
 c:\mysql\bin\mysqladmin -u root drop alfresco
 c:\mysql\bin\mysqladmin -u root shutdown
 
