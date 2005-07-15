5e6e7b19ea3f59b27900d09d68ce38c4b092695b
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-07-15 12:29:23 +0000

    Minor formatting
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@824 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/repository/source/java/org/alfresco/filesys/smb/server/repo/ContentDiskDriver.java
+++ projects/repository/source/java/org/alfresco/filesys/smb/server/repo/ContentDiskDriver.java
@@ -30,7 +30,6 @@ import org.alfresco.filesys.server.core.DeviceContext;
 import org.alfresco.filesys.server.core.DeviceContextException;
 import org.alfresco.filesys.server.filesys.AccessDeniedException;
 import org.alfresco.filesys.server.filesys.DiskDeviceContext;
-import org.alfresco.filesys.server.filesys.DiskInfo;
 import org.alfresco.filesys.server.filesys.DiskInterface;
 import org.alfresco.filesys.server.filesys.FileInfo;
 import org.alfresco.filesys.server.filesys.FileName;
@@ -145,7 +144,6 @@ public class ContentDiskDriver implements DiskInterface
         DiskDeviceContext context = new DiskDeviceContext(rootNodeRef.toString());
 
         //  Default the filesystem to look like an 80Gb sized disk with 90% free space
-        
         context.setDiskInformation(new SrvDiskInfo(2560, 64, 512, 2304));
         
         // set parameters
