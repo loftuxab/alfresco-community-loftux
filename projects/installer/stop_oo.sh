fa0883288c1d91b5ac29ffd44f053fa96685be08
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-07-19 10:50:36 +0000

    added shell scripts back
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@833 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ projects/installer/stop_oo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Start script for the OpenOffice transform service
+# ---------------------------------------------------------------------------
+
+echo "Stopping OpenOffice service..."
+
+killall soffice.bin
+
