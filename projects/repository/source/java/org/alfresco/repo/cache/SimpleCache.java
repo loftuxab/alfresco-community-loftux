398d36bf3d6dde97354cfae34b7643ecfdfea776
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-08-12 09:41:06 +0000

    Alfresco cache interface
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@994 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ projects/repository/source/java/org/alfresco/repo/cache/SimpleCache.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2005 Alfresco, Inc.
+ *
+ * Licensed under the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/lgpl.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.alfresco.repo.cache;
+
+import java.io.Serializable;
+
+/**
+ * Basic caching interface.
+ * <p>
+ * All implementations <b>must</b> be thread-safe.  Additionally, the use of the
+ * <tt>Serializable</tt> for both keys and values ensures that the underlying
+ * cache implementations can support both clustered caches as well as persistent
+ * caches.
+ * 
+ * @author Derek Hulley
+ */
+public interface SimpleCache
+{
+    public boolean contains(Serializable key);
+    
+    public Serializable get(Serializable key);
+    
+    public void put(Serializable key, Serializable value);
+    
+    public void remove(Serializable key);
+}
