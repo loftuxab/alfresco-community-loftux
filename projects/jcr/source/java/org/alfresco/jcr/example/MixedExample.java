0bc28585e494a085500c4ea313c2e34d5946f665
dcaruana <dcaruana@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-09-23 14:40:00 +0000

    Examples and integration with Alfresco Java Service API
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@1320 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ projects/jcr/source/java/org/alfresco/jcr/example/MixedExample.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2005 Alfresco, Inc.
+ *
+ * Licensed under the Mozilla Public License version 1.1 
+ * with a permitted attribution clause. You may obtain a
+ * copy of the License at
+ *
+ *   http://www.alfresco.org/legal/license.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.alfresco.jcr.example;
+
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.Repository;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.transaction.UserTransaction;
+
+import org.alfresco.jcr.api.JCRNodeRef;
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.transaction.TransactionService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+/**
+ * Example that demonstrate use of JCR and Alfresco API calls.
+ * 
+ * @author David Caruana
+ */
+public class MixedExample
+{
+
+    public static void main(String[] args)
+        throws Exception
+    {
+        // Setup Spring and Transaction Service
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:org/alfresco/jcr/test/test-context.xml");
+        ServiceRegistry registry = (ServiceRegistry)context.getBean(ServiceRegistry.SERVICE_REGISTRY);
+        TransactionService trxService = (TransactionService)registry.getTransactionService();
+        NodeService nodeService = (NodeService)registry.getNodeService();
+        
+        // Retrieve Repository
+        Repository repository = (Repository)context.getBean("JCR.Repository");
+
+        // Login to workspace
+        // Note: Default workspace is the one used by Alfresco Web Client which contains all the Spaces
+        //       and their documents
+        Session session = repository.login(new SimpleCredentials("superuser", "".toCharArray()));
+
+        // Start a Transaction
+        UserTransaction trx = trxService.getUserTransaction();
+        trx.begin();
+
+        try
+        {
+            // Retrieve Company Home
+            Node root = session.getRootNode();
+            Node companyHome = root.getNode("app:Company_Home");
+
+            // Read Company Home Name
+            Property name = companyHome.getProperty("cm:name");
+            System.out.println("Name = " + name.getString());
+            
+            // Update Node via Alfresco Node Service API
+            NodeRef companyHomeRef = JCRNodeRef.getNodeRef(companyHome);
+            nodeService.setProperty(companyHomeRef, ContentModel.PROP_NAME, "Updated Company Home Name");
+            
+            // Re-read via JCR
+            System.out.println("Updated name = " + name.getString());
+        }
+        finally
+        {
+            trx.rollback();
+            session.logout();
+        }
+    }
+    
+}
