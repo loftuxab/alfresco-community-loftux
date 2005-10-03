ba169b5814c2dd0ad050ca0a72e639ea85186915
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-10-03 22:30:49 +0000

    generic install
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@1423 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/installer/stop_oo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem ---------------------------------------------------------------------------
-rem Start script for the OpenOffice transform service
-rem ---------------------------------------------------------------------------
-
-echo Stopping OpenOffice service...
-
-c:\windows\system32\taskkill /f /im soffice.exe
-
