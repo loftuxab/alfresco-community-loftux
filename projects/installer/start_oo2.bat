942f37386d43dd02df968b61329e86febe5c4e4a
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2005-10-10 09:21:20 +0000

    no longer need 2nd bat
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@1508 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/installer/start_oo2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem ---------------------------------------------------------------------------
-rem Start script for the OpenOffice transform service
-rem ---------------------------------------------------------------------------
-
-echo Starting OpenOffice service...
-"C:\Program Files\OpenOffice.org1.1.5\program\soffice" "-accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" -nologo -headless
-
-
--- projects/installer/zstart_oo.bat
+++ projects/installer/zstart_oo.bat
@@ -4,6 +4,6 @@ rem Start script for the OpenOffice transform service
 rem ---------------------------------------------------------------------------
 
 echo Starting OpenOffice service...
-start "OO Server" /min cmd /c start_oo2.bat
+"C:\Program Files\OpenOffice.org1.1.5\program\soffice" "-accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" -nologo -headless
 
 
