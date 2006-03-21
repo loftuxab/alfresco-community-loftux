3eddfb099e049a708ef42262f508d043fa031c9b
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2006-03-21 21:46:15 +0000

    Added sys:incomplete aspect. Extended <mandatory> definition in the DD. The "mandatory" properties in our system have, until now, been optional, i.e. the integrity has not been enforced. It is possible to have    <mandatory enforced="true">true</mandatory>, which means "mandatory and enforced", but    <mandatory enforced="false">true</mandatory>, which means "mandatory but not enforced". Our system properties have been marked as "mandatory". Dublin core has had the properties marked as "required".  Currently, if the Dublin Core is added, the node is tagged with sys:incomplete.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@2562 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/core/source/java/org/alfresco/util/ParameterCheck.java
+++ root/projects/core/source/java/org/alfresco/util/ParameterCheck.java
@@ -25,65 +25,65 @@ import java.util.Collection;
  */
 public class ParameterCheck
 {
-   /**
-    * Checks that the parameter with the given name has content i.e. it is not null
-    * 
-    * @param strParamName Name of parameter to check
-    * @param object Value of the parameter to check
-    */
-   public static void mandatory(String strParamName, Object object)
-   {
-      if (strParamName == null || strParamName.length() == 0)
-      {
-         throw new IllegalArgumentException("Parameter name is mandatory");
-      }
-      
-      // check that the given string value has content
-      if (object == null)
-      {
-         throw new IllegalArgumentException(strParamName + " is a mandatory parameter");
-      }
-   }
-   
-   /**
-    * Checks that the string parameter with the given name has content 
-    * i.e. it is not null and not zero length
-    * 
-    * @param strParamName Name of parameter to check
-    * @param strParamValue Value of the parameter to check
-    */
-   public static void mandatoryString(String strParamName, String strParamValue)
-   {
-      if (strParamName == null || strParamName.length() == 0)
-      {
-         throw new IllegalArgumentException("Parameter name is mandatory");
-      }
-      
-      // check that the given string value has content
-      if (strParamValue == null || strParamValue.length() == 0)
-      {
-         throw new IllegalArgumentException(strParamName + " is a mandatory parameter");
-      }
-   }
-   
-   
-   /**
-    * Checks that the collection parameter contains at least one item.
-    * 
-    * @param strParamName   Name of parameter to check
-    * @param coll  collection to check
-    */
-   public static void mandatoryCollection(String strParamName, Collection coll)
-   {
-      if (strParamName == null || strParamName.length() == 0)
-      {
-         throw new IllegalArgumentException("Parameter name is mandatory");
-      }
-      
-      if (coll == null || coll.size() == 0)
-      {
-         throw new IllegalArgumentException(strParamName + " collection must contain at least one item");
-      }
-   }
+    /**
+     * Checks that the parameter with the given name has content i.e. it is not
+     * null
+     * 
+     * @param strParamName Name of parameter to check
+     * @param object Value of the parameter to check
+     */
+    public static void mandatory(String strParamName, Object object)
+    {
+        if (strParamName == null || strParamName.length() == 0)
+        {
+            throw new IllegalArgumentException("Parameter name is mandatory");
+        }
+
+        // check that the given string value has content
+        if (object == null)
+        {
+            throw new IllegalArgumentException(strParamName + " is a mandatory parameter");
+        }
+    }
+
+    /**
+     * Checks that the string parameter with the given name has content i.e. it
+     * is not null and not zero length
+     * 
+     * @param strParamName Name of parameter to check
+     * @param strParamValue Value of the parameter to check
+     */
+    public static void mandatoryString(String strParamName, String strParamValue)
+    {
+        if (strParamName == null || strParamName.length() == 0)
+        {
+            throw new IllegalArgumentException("Parameter name is mandatory");
+        }
+
+        // check that the given string value has content
+        if (strParamValue == null || strParamValue.length() == 0)
+        {
+            throw new IllegalArgumentException(strParamName + " is a mandatory parameter");
+        }
+    }
+
+    /**
+     * Checks that the collection parameter contains at least one item.
+     * 
+     * @param strParamName Name of parameter to check
+     * @param coll collection to check
+     */
+    public static void mandatoryCollection(String strParamName, Collection coll)
+    {
+        if (strParamName == null || strParamName.length() == 0)
+        {
+            throw new IllegalArgumentException("Parameter name is mandatory");
+        }
+
+        if (coll == null || coll.size() == 0)
+        {
+            throw new IllegalArgumentException(strParamName + " collection must contain at least one item");
+        }
+    }
 
 }
