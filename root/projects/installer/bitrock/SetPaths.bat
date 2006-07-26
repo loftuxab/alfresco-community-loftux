70a02f6c28040d24561b7a1e5e59dcaf5a67a813
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2006-07-26 09:45:10 +0000

    removed old bitrock data
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@3403 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/installer/bitrock/SetPaths.bat
+++
@@ -1,3 +0,0 @@
-REM Set Paths Utility
-SET JAVA_HOME=@@BITROCK_ALFRESCO_INSTALLDIR@@\java
-SET OPENOFFICE_PATH=@@BITROCK_ALFRESCO_INSTALLDIR@@\openoffice\openoffice\program	    	    
--- root/projects/installer/bitrock/bin/start_oo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem ---------------------------------------------------------------------------
-rem Start script for the OpenOffice transform service
-rem ---------------------------------------------------------------------------
-
-echo Starting OpenOffice service...
-"@@BITROCK_ALFRESCO_INSTALLDIR@@\openoffice\openoffice\program\soffice" "-accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" -nologo -headless
-
-
