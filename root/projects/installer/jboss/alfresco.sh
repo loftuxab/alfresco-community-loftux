34439feca1f5769a1292a5163604bbd921e00b00
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2006-12-18 16:09:42 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@4640 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/installer/jboss/alfresco.sh
+++ root/projects/installer/jboss/alfresco.sh
@@ -3,7 +3,9 @@
 # Set the following to where JBoss is installed
 APPSERVER=/opt/alfresco/jboss
 # Set any default JVM values
-export JAVA_OPTS='-Xms128m -Xmx512m -server -XX:CompileCommand=exclude,org/apache/lucene/index/IndexReader\$1,doBody'
+export JAVA_OPTS='-Xms128m -Xmx512m -server -XX:+CMSPermGenSweepingEnabled -XX:MaxPermSize=128m'
+# Following only needed for Sun JVMs before to 1.5 update 8
+export JAVA_OPTS=$JAVA_OPTS '-XX:CompileCommand=exclude,org/apache/lucene/index/IndexReader\$1,doBody -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeIndexes -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeDeletions'
 #
 if [ "$1" = "start" ]; then
   "$APPSERVER"/bin/run.sh
--- root/projects/installer/tomcat/alfresco.sh
+++ root/projects/installer/tomcat/alfresco.sh
@@ -3,7 +3,9 @@
 # Set the following to where Tomcat is installed
 APPSERVER=/opt/alfresco/tomcat
 # Set any default JVM values
-export JAVA_OPTS='-Xms128m -Xmx512m -server -XX:CompileCommand=exclude,org/apache/lucene/index/IndexReader\$1,doBody'
+export JAVA_OPTS='-Xms128m -Xmx512m -server'
+# Following only needed for Sun JVMs before to 1.5 update 8
+export JAVA_OPTS=$JAVA_OPTS '-XX:CompileCommand=exclude,org/apache/lucene/index/IndexReader\$1,doBody -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeIndexes -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeDeletions'
 #
 if [ "$1" = "start" ]; then
   "$APPSERVER"/bin/startup.sh
