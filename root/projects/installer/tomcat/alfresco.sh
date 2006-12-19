1d52063d1195eef6df9d59ae9213c2b75010377b
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2006-12-19 16:15:44 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@4661 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/installer/tomcat/alfresco.sh
+++ root/projects/installer/tomcat/alfresco.sh
@@ -5,7 +5,7 @@ APPSERVER=/opt/alfresco/tomcat
 # Set any default JVM values
 export JAVA_OPTS='-Xms128m -Xmx512m -server'
 # Following only needed for Sun JVMs before to 1.5 update 8
-export JAVA_OPTS=$JAVA_OPTS '-XX:CompileCommand=exclude,org/apache/lucene/index/IndexReader\$1,doBody -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeIndexes -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeDeletions'
+export JAVA_OPTS="${JAVA_OPTS} -XX:CompileCommand=exclude,org/apache/lucene/index/IndexReader\$1,doBody -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeIndexes -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeDeletions"
 #
 if [ "$1" = "start" ]; then
   "$APPSERVER"/bin/startup.sh
