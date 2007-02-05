7c606199946af287b837ccac429f8d6168774d2a
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2007-02-05 14:26:54 +0000

    Added sample SDK for creating AMP files
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@5035 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ root/projects/sdk/samples/BasicAmpSample/source/java/org/alfresco/module/sdkdemoamp/Demo.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2007 Alfresco, Inc.
+ *
+ * Licensed under the Mozilla Public License version 1.1 
+ * with a permitted attribution clause. You may obtain a
+ * copy of the License at
+ *
+ *   http://www.alfresco.org/legal/license.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.alfresco.module.sdkdemoamp;
+
+/**
+ * This class does nothing except dump some output to <i>system.out</i>.
+ * 
+ * @author Derek Hulley
+ */
+public class Demo
+{
+	public void init()
+	{
+		System.out.println("SDK Demo AMP class has been loaded");
+	}
+}
