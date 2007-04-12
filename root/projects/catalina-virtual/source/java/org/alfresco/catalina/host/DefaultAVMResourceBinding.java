eee63be206e5e4b1242b5107cf5a7628a10bc678
jonc <jonc@7033f55c-6c29-4ad8-afba-765cb616e738>
2007-04-12 05:23:18 +0000

    Replacing headers that were introduced by an  automated tool with the original non-ugly ones;  the actual text is the same.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@5490 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/catalina-virtual/source/java/org/alfresco/catalina/host/DefaultAVMResourceBinding.java
+++ root/projects/catalina-virtual/source/java/org/alfresco/catalina/host/DefaultAVMResourceBinding.java
@@ -1,29 +1,31 @@
 /*-----------------------------------------------------------------------------
-*  Copyright 2006 Alfresco Inc.
+*  Copyright 2007 Alfresco Inc.
 *  
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * As a special exception to the terms and conditions of version 2.0 of
- * the GPL, you may redistribute this Program in connection with Free/Libre
- * and Open Source Software ("FLOSS") applications as described in Alfresco's
- * FLOSS exception.  You should have recieved a copy of the text describing
- * the FLOSS exception, and it is also available here:
- * http://www.alfresco.com/legal/licensing"*  
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*  
+*  This program is distributed in the hope that it will be useful, but
+*  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+*  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+*  for more details.
+*  
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  As a special
+*  exception to the terms and conditions of version 2.0 of the GPL, you may
+*  redistribute this Program in connection with Free/Libre and Open Source
+*  Software ("FLOSS") applications as described in Alfresco's FLOSS exception.
+*  You should have recieved a copy of the text describing the FLOSS exception,
+*  and it is also available here:   http://www.alfresco.com/legal/licensing
+*  
 *  
 *  Author  Jon Cox  <jcox@alfresco.com>
 *  File    DefaultAVMResourceBinding.java
 *----------------------------------------------------------------------------*/
+
+
 package org.alfresco.catalina.host;
 import java.util.Map;
 import java.util.regex.Matcher;
