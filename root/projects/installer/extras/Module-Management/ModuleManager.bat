fe45fb05ccc7a137af43124cecca1af787e38ee3
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2007-04-12 02:52:48 +0000

    Merged V2.0 to HEAD    5466, 5471-5475: ModuleManagementTool enhancements
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@5487 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/installer/extras/Module-Management/ModuleManager.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Install an AMP file into an Alfresco WAR
-
-rem set the LIB_DIR value to point to the Alfresco lib directory
-SET LIB_DIR=C:\work\jakarta-tomcat-5.5.9\webapps\alfresco\WEB-INF\lib
-
-SET TEMP_CLASS_PATH=%LIB_DIR%\alfresco-repository.jar;%LIB_DIR%\alfresco-core.jar;%LIB_DIR%\truezip.jar;%LIB_DIR%\log4j-1.2.8.jar;%LIB_DIR%\spring.jar;%LIB_DIR%\commons-logging.jar
-
-java -cp "%TEMP_CLASS_PATH%" org.alfresco.repo.module.tool.ModuleManagementTool "%1" "%2" "%3" "%4" "%5" "%6" "%7"
