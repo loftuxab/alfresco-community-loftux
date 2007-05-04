78963b303a2cb456df59f65f85cd86c9ce5668b0
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2007-05-04 11:27:46 +0000

    My Forms template now opens website virtualisation URL when icon clicked. New Template AVM API methods added to generate staging, user sandbox and path asset URLs for virtualisation server. Some refactoring of constants used in AVMConstants into JNDIConstants to allow repository classes to also use them.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@5624 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/core/source/java/org/alfresco/config/JNDIConstants.java
+++ root/projects/core/source/java/org/alfresco/config/JNDIConstants.java
@@ -1,6 +1,6 @@
-/*-----------------------------------------------------------------------------
-*  Copyright 2006 Alfresco Inc.
-*  
+/*
+ * Copyright (C) 2005-2007 Alfresco Software Limited.
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -14,39 +14,59 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * As a special exception to the terms and conditions of version 2.0 of
- * the GPL, you may redistribute this Program in connection with Free/Libre
- * and Open Source Software ("FLOSS") applications as described in Alfresco's
- * FLOSS exception.  You should have recieved a copy of the text describing
- * the FLOSS exception, and it is also available here:
- * http://www.alfresco.com/legal/licensing"*  
-*  
-*  Author  Jon Cox  <jcox@alfresco.com>
-*  File    JNDIConstants.java
-*----------------------------------------------------------------------------*/
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have recieved a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing"
+ *
+ *  Author  Jon Cox  <jcox@alfresco.com>
+ *  File    JNDIConstants.java
+ *----------------------------------------------------------------------------*/
 
 package org.alfresco.config;
 
 /**
-*  Constants to create proper JNDI names for the directories
-*  that contain www content.
-*  <p>
-*  Ultimately, the constants in this fill will go away entirely.
-*  This is a stop-gap until we have support multiple virtual AVMHost
-*  instances, and a full Spring config (with associated sync to virt server).
-*  
-*/
+ * Constants to create proper JNDI names for the directories
+ * that contain www content.
+ * <p>
+ * Ultimately, the constants in this fill will go away entirely.
+ * This is a stop-gap until we have support multiple virtual AVMHost
+ * instances, and a full Spring config (with associated sync to virt server).
+ */
 public final class JNDIConstants 
 {
     /**
-    *  Directory used for virtualized web content.
-    *  Typically, this directory is a transparent overlay 
-    *  on a shared staging area.
-    */
+     * Directory used for virtualized web content.
+     * Typically, this directory is a transparent overlay 
+     * on a shared staging area.
+     */
     public final static String  DIR_DEFAULT_WWW     = "www";
 
     /**
-    *  Directory in which virtualized webapps reside (e.g.: "ROOT").
-    */
+     * Directory in which virtualized webapps reside (e.g.: "ROOT").
+     */
     public final static String  DIR_DEFAULT_APPBASE  = "avm_webapps";
+    
+    /**
+     * Default virtualization server IP address 
+     */
+    public final static String DEFAULT_VSERVER_IP = "127-0-0-1.ip.alfrescodemo.net";
+    
+    /**
+     * Default virtualization server port number
+     */
+    public final static int DEFAULT_VSERVER_PORT = 8180;
+    
+    /**
+     * Virtualization server sandbox URL pattern
+     */
+    public final static String PREVIEW_SANDBOX_URL = "http://{0}.www--sandbox.{1}:{2}";
+    
+    /**
+     * Virtualization server asset URL pattern
+     */
+    public final static String PREVIEW_ASSET_URL   = "http://{0}.www--sandbox.{1}:{2}{3}";
 }
