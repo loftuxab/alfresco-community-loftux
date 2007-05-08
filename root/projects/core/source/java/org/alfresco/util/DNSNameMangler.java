d1013f7136e6a006dae382e1ebf853afb12d0de2
jonc <jonc@7033f55c-6c29-4ad8-afba-765cb616e738>
2007-05-08 02:33:18 +0000

    Point checkin. Reorg to allow sandbox-related constants to be accessed more easily from other compilation units.   AVMConstants had become a dumping ground for things that had nothing to do with const values.   Therefore, this is now AVMUtil, and sandbox-related constants have been moved to org.alfresco.sandbox.SandboxConstants, which builds in repository.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@5637 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/core/source/java/org/alfresco/util/DNSNameMangler.java
+++ root/projects/core/source/java/org/alfresco/util/DNSNameMangler.java
@@ -1,30 +1,42 @@
-/*
- * Copyright (C) 2005-2007 Alfresco Software Limited.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+/*-----------------------------------------------------------------------------
+*  Copyright 2007 Alfresco Inc.
+*  
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*  
+*  This program is distributed in the hope that it will be useful, but
+*  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+*  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+*  for more details.
+*  
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  As a special
+*  exception to the terms and conditions of version 2.0 of the GPL, you may
+*  redistribute this Program in connection with Free/Libre and Open Source
+*  Software ("FLOSS") applications as described in Alfresco's FLOSS exception.
+*  You should have received a copy of the text describing the FLOSS exception,
+*  and it is also available here:   http://www.alfresco.com/legal/licensing
+*  
+*  
+*  Author  Jon Cox     <jcox@alfresco.com>
+*  Author  Britt Park  <britt.park@alfresco.com>
+*  File    DNSNameMangler.java
+*
+*
+*  NOTE:
+*       If virtualization servers registered with a central service,
+*       this library could access that service and then hand out URLs
+*       that correspond to those different servers based on criteria such
+*       as load balancing, mime type of asset corresponding to URL, 
+*       portion of AVM repository being accessed, etc.
+*----------------------------------------------------------------------------*/
 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * As a special exception to the terms and conditions of version 2.0 of
- * the GPL, you may redistribute this Program in connection with Free/Libre
- * and Open Source Software ("FLOSS") applications as described in Alfresco's
- * FLOSS exception.  You should have recieved a copy of the text describing
- * the FLOSS exception, and it is also available here:
- * http://www.alfresco.com/legal/licensing" */
-
-package org.alfresco.web.bean.wcm;
-
+package org.alfresco.util;
 import java.util.regex.Pattern;
-
 import org.alfresco.util.GUID;
 
 /**
@@ -55,7 +67,7 @@ import org.alfresco.util.GUID;
  * @author Britt Park
  *
  */
-class DNSNameMangler
+public class DNSNameMangler
 {
     // Component Separator.
     private static final String SEPARATOR = ".";
@@ -97,7 +109,7 @@ class DNSNameMangler
      * @param components The Strings from which to synthesize the DNS name.
      * @return A Valid DNS name.
      */
-    static String MakeDNSName(String... components)
+    static public String MakeDNSName(String... components)
     {
         StringBuilder builder = new StringBuilder();
 
