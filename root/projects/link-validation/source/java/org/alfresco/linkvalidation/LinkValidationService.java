10360adf3864a7257c456f138d90f66e389670d8
jonc <jonc@7033f55c-6c29-4ad8-afba-765cb616e738>
2007-06-02 23:33:31 +0000

    Point checkin. Added APIs for link validation of links in staging and fetching the results.   Still only mocking things up by using HEAD (rather than last snapshot), not doing incremental updates yet, and ignoring the multi-threading arg to the update.   However, you can poll for progress via UpdateHrefInfoStatus & the APIs for checking link status are probably usable as-is for some GUI integration work.  More to come.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@5834 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/link-validation/source/java/org/alfresco/linkvalidation/LinkValidationService.java
+++ root/projects/link-validation/source/java/org/alfresco/linkvalidation/LinkValidationService.java
@@ -26,17 +26,157 @@
 *----------------------------------------------------------------------------*/
 
 package org.alfresco.linkvalidation;
+
+import java.util.List;
+
 import org.alfresco.service.cmr.avm.AVMNotFoundException;
 
 public interface LinkValidationService
 {
-    public void updateHrefInfo(String  store_name_or_webapp_path,
-                               boolean incremental,
-                               int     connectTimeout,
-                               int     readTimeout) 
+    /**
+    * Updates href status and href file dependencies for path.
+    * 
+    *
+    * @param storeNameOrWebappPath 
+    *            The store name or path to webapp
+    *                        
+    * @param incremental     
+    *            If true, updates information incrementally, based on the 
+    *            files that have changed and prior calculations regarding 
+    *            url-to-file dependencies.  If false, first deletes all URL
+    *            info associated with the store/webapp (if any), then does 
+    *            a full rescan to update info.
+    * 
+    * @param connectTimeout  
+    *            Amount of time in milliseconds that this function will wait
+    *            before declaring that the connection has failed 
+    *            (e.g.: 10000 ms).
+    *
+    * @param readTimeout     
+    *            time in milliseconds that this function will wait before
+    *            declaring that a read on the connection has failed
+    *            (e.g.:  30000 ms).
+    * 
+    * @param nthreads
+    *             Number of threads to use when fetching URLs (e.g.: 5)
+    *
+    * @param status
+    *             While updateHrefInfo() is a synchronous function, 
+    *             'status' may be polled in a separate thread to 
+    *             observe its progress.
+    */
+    public void updateHrefInfo( String  storeNameOrWebappPath,
+                                boolean incremental,
+                                int     connectTimeout,
+                                int     readTimeout,
+                                int     nthreads,
+                                UpdateHrefInfoStatus status
+                              ) 
                 throws AVMNotFoundException;
 
-    public void getBrokenHrefs(String  store_name_or_webapp_path)
-                throws AVMNotFoundException;
+
+    /**
+    *  Fetches information on broken hrefs within a store name or path 
+    *  to a webapp.  This function is just a convenience wrapper for calling  
+    *  getHrefConcordance with statusGTE=400 and statusLTE=599.
+    */
+    public List<HrefConcordanceEntry> getBrokenHrefConcordance( 
+                                          String  storeNameOrWebappPath 
+                                      ) throws AVMNotFoundException;
+
+
+    /**
+    *  Returns information regarding the hrefs within storeNameOrWebappPath
+    *  whose return status is greater than or equal to 'statusGTE', and 
+    *  less than or equal to 'statusLTE'.  The List<HrefConcordanceEntry>
+    *  is sorted in increasing lexicographic order by href.  Within each
+    *  HrefConcordanceEntry, the files retrieved via getLocations()
+    *  are also sorted in increasing lexicographic order.
+    * 
+    *  <p>
+    *  Example 1:<br>
+    *  The following parameters will fetch all the broken links
+    *  within the ROOT webapp in the staging area of the 'mysite' web project:
+    *  <ul>
+    *    <li> storeNameOrWebappPath="mysite:/www/avm_webapps/ROOT"
+    *    <li> statusGTE=400
+    *    <li> statusLTE=599
+    *  <ul>
+    *  <p>
+    *  Example 2:<br>
+    *  The following parameters will fetch all the links whose return status
+    *  is "successful" (2xx) for all webapps contained by the staging area of
+    *  the 'mysite' web project:
+    *  <ul>
+    *    <li> storeNameOrWebappPath="mysite"
+    *    <li> statusGTE=200
+    *    <li> statusLTE=299
+    *  <ul>
+    *  Example 3:<br>
+    *  The following parameters will fetch all the links whose return status
+    *  is 200 (OK) within the ROOT webapp in the staging area of the 'mysite' 
+    *  web project
+    *  <ul>
+    *    <li> storeNameOrWebappPath="mysite:/www/avm_webapps/ROOT"
+    *    <li> statusGTE=200
+    *    <li> statusLTE=200
+    *  <ul>
+    *  <p>
+    *  For details regarding HTTP status codes, see:
+    *  http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+    *
+    */
+    public List<HrefConcordanceEntry> getHrefConcordance( 
+                                         String  storeNameOrWebappPath,
+                                         int     statusGTE,
+                                         int     statusLTE
+                                      )  throws AVMNotFoundException;
+
+
+    /**
+    *  This function is just a convenience wrapper for calling  
+    *  getHrefManifests with statusGTE=400 and statusLTE=599.
+    */
+    public List<HrefManifest> getBrokenHrefManifests( 
+                                 String storeNameOrWebappPath
+                              )  throws AVMNotFoundException;
+
+    /**
+    *  Returns a manifest consisting of just the broken hrefs 
+    *  within each file containing one or more broken href. 
+    *  The HrefManifest list is sorted in increasing lexicographic 
+    *  order by file name.  The hrefs within each HrefManifest
+    *  are also sorted in increasing lexicographic order.
+    */
+    public List<HrefManifest> getHrefManifests( 
+                                  String storeNameOrWebappPath,
+                                  int    statusGTE,
+                                  int    statusLTE) throws 
+                                  AVMNotFoundException;
+
+    /**
+    *  This function is just a convenience wrapper for calling  
+    *  getHrefManifest with statusGTE=400 and statusLTE=599.
+    *  <p>
+    *  Note:  If you want to get the broken links in every file in 
+    *  a webapp or store, it's much more efficient to use 
+    *  getBrokenHrefManifests instead of this function.
+    */
+    public HrefManifest getBrokenHrefManifest( String path) 
+                                               throws AVMNotFoundException;
+
+    /**
+    *  Returns a manifest of all the hrefs within the file specified by 'path'
+    *  whose response status is greater than or equal to statusGTE, 
+    *  and less than or equal to statusLTE.
+    *  <p>
+    *  Note:  If you want to get a list of manifests of every file in a
+    *  webapp or store, it's much more efficient to use getHrefManifests
+    *  instead of this function.
+    */
+    public HrefManifest getHrefManifest( String path,
+                                         int    statusGTE,
+                                         int    statusLTE) throws 
+                                         AVMNotFoundException;
 }
 
