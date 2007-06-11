a08a20bdbd8903ac61a13d6272f6509020bc4d9d
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2007-06-11 13:01:47 +0000

    All ContentService methods all use read-only transactions. The write streams are closed outside of the call to the service, so these will depend on the outer transaction's read/write state. The ContentHitsAspect was firing in-transaction.  This now queues the count increment to processing after the transaction completes.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@5909 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/sdk/samples/CustomAspect/source/org/alfresco/sample/ContentHitsAspect.java
+++ root/projects/sdk/samples/CustomAspect/source/org/alfresco/sample/ContentHitsAspect.java
@@ -1,40 +1,73 @@
 package org.alfresco.sample;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.alfresco.repo.content.ContentServicePolicies;
 import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.BehaviourFilter;
 import org.alfresco.repo.policy.JavaBehaviour;
 import org.alfresco.repo.policy.PolicyComponent;
 import org.alfresco.repo.policy.Behaviour.NotificationFrequency;
+import org.alfresco.repo.transaction.AlfrescoTransactionSupport;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
+import org.alfresco.repo.transaction.TransactionListener;
+import org.alfresco.repo.transaction.TransactionListenerAdapter;
+import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
 import org.alfresco.service.cmr.repository.InvalidNodeRefException;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
+import org.alfresco.service.transaction.TransactionService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * This class contains the behaviour behind the 'my:contentHits' aspect.
+ * <p>
+ * Access to content readers and writers does not necessarily have to be done in
+ * writable transactions.  This aspect behaviour demonstrates how the hit count
+ * incrementing is done after the transaction.
  * 
  * @author Roy Wetherall
+ * @author Derek Hulley
  */
 public class ContentHitsAspect implements ContentServicePolicies.OnContentReadPolicy,
                                           ContentServicePolicies.OnContentUpdatePolicy,
                                           NodeServicePolicies.OnAddAspectPolicy
 {
-    /** Aspect name */
+    /** A key that keeps track of nodes that need read count increments */
+    private static final String KEY_CONTENT_HITS_READS = ContentHitsAspect.class.getName() + ".reads";
+    /** A key that keeps track of nodes that need write count increments */
+    private static final String KEY_CONTENT_HITS_WRITES = ContentHitsAspect.class.getName() + ".writes";
+    
+    /* Aspect names */
     public static final QName ASPECT_CONTENT_HITS = QName.createQName("extension.contenthits", "contentHits");
     
-    /** Property names */
+    /* Property names */
     public static final QName PROP_COUNT_STARTED_DATE = QName.createQName("extension.contenthits", "countStartedDate");
     public static final QName PROP_UPDATE_COUNT = QName.createQName("extension.contenthits", "updateCount");
     public static final QName PROP_READ_COUNT = QName.createQName("extension.contenthits", "readCount");
     
-    /** The policy component */
-    private PolicyComponent policyComponent;
+    private static Log logger = LogFactory.getLog(ContentHitsAspect.class);
     
-    /** The node service */
+    private PolicyComponent policyComponent;
+    private BehaviourFilter policyFilter;
     private NodeService nodeService;
+    private TransactionService transactionService;
+    private ThreadPoolExecutor threadExecuter;
+    private TransactionListener transactionListener;
+
+    /**
+     * Default constructor for bean construction
+     */
+    public ContentHitsAspect()
+    {
+        this.transactionListener = new ContentHitsTransactionListener();
+    }
     
     /**
      * Sets the policy component
@@ -45,7 +78,17 @@ public class ContentHitsAspect implements ContentServicePolicies.OnContentReadPo
     {
         this.policyComponent = policyComponent;
     }
-    
+
+    /**
+     * Set the component to filter out behaviour
+     * 
+     * @param policyFilter      the policy behaviour filter
+     */
+    public void setPolicyFilter(BehaviourFilter policyFilter)
+    {
+        this.policyFilter = policyFilter;
+    }
+
     /** 
      * Sets the node service 
      * 
@@ -55,7 +98,27 @@ public class ContentHitsAspect implements ContentServicePolicies.OnContentReadPo
     {
         this.nodeService = nodeService;
     }
-    
+
+    /**
+     * Set the service that allows new transactions
+     * 
+     * @param transactionService    the transaction service 
+     */
+    public void setTransactionService(TransactionService transactionService)
+    {
+        this.transactionService = transactionService;
+    }
+
+    /**
+     * Set the thread pool that will handle the post-commit write transactions.
+     * 
+     * @param threadExecuter        an <i>executor</i>
+     */
+    public void setThreadExecuter(ThreadPoolExecutor threadExecuter)
+    {
+        this.threadExecuter = threadExecuter;
+    }
+
     /**
      * Spring initilaise method used to register the policy behaviours
      */
@@ -78,7 +141,7 @@ public class ContentHitsAspect implements ContentServicePolicies.OnContentReadPo
 
     /**
      * onAddAspect policy behaviour.
-     * 
+     * <p>
      * Sets the count started date to the date/time at which the contentHits aspect was
      * first applied.
      * 
@@ -93,45 +156,208 @@ public class ContentHitsAspect implements ContentServicePolicies.OnContentReadPo
     
     /**
      * onContentRead policy behaviour.
+     * <p>
+     * This adds the node to the list of nodes that require read count incremets after the transaction completes.
      * 
      * Increments the aspect's read count property by one.
-     * 
-     * @see org.alfresco.repo.content.ContentServicePolicies.OnContentReadPolicy#onContentRead(org.alfresco.service.cmr.repository.NodeRef)
      */
     public void onContentRead(NodeRef nodeRef)
     {
-        try
+        // Bind the listener to the transaction
+        AlfrescoTransactionSupport.bindListener(transactionListener);
+        // Get the set of nodes read
+        @SuppressWarnings("unchecked")
+        Set<NodeRef> readNodeRefs = (Set<NodeRef>) AlfrescoTransactionSupport.getResource(KEY_CONTENT_HITS_READS);
+        if (readNodeRefs == null)
         {
-            // Increment the read count property value
-            Integer currentValue = (Integer)this.nodeService.getProperty(nodeRef, PROP_READ_COUNT);
-            int newValue = currentValue.intValue() + 1;
-            this.nodeService.setProperty(nodeRef, PROP_READ_COUNT, newValue);
+            readNodeRefs = new HashSet<NodeRef>(5);
+            AlfrescoTransactionSupport.bindResource(KEY_CONTENT_HITS_READS, readNodeRefs);
         }
-        catch (Exception exception)
-        {
-            // Do nothing since we do not want failure here to effect the transaction
-        }
+        readNodeRefs.add(nodeRef);
     }
 
     /**
      * onContentUpdate policy behaviour.
-     * 
-     * Increments the aspect's update count property by one.
-     * 
-     * @see org.alfresco.repo.content.ContentServicePolicies.OnContentUpdatePolicy#onContentUpdate(org.alfresco.service.cmr.repository.NodeRef, boolean)
+     * <p>
+     * This adds the node to the list of nodes that require write count incremets after the transaction completes.
      */
     public void onContentUpdate(NodeRef nodeRef, boolean newContent)
     {
-        // Increment the update count property value
-        try
+        // Bind the listener to the transaction
+        AlfrescoTransactionSupport.bindListener(transactionListener);
+        // Get the set of nodes written
+        @SuppressWarnings("unchecked")
+        Set<NodeRef> writeNodeRefs = (Set<NodeRef>) AlfrescoTransactionSupport.getResource(KEY_CONTENT_HITS_WRITES);
+        if (writeNodeRefs == null)
         {
-            Integer currentValue = (Integer)this.nodeService.getProperty(nodeRef, PROP_UPDATE_COUNT);
-            int newValue = currentValue.intValue() + 1;
-            this.nodeService.setProperty(nodeRef, PROP_UPDATE_COUNT, newValue);
+            writeNodeRefs = new HashSet<NodeRef>(5);
+            AlfrescoTransactionSupport.bindResource(KEY_CONTENT_HITS_WRITES, writeNodeRefs);
         }
-        catch(InvalidNodeRefException e)
+        writeNodeRefs.add(nodeRef);
+    }
+    
+    private class ContentHitsTransactionListener extends TransactionListenerAdapter
+    {
+        @Override
+        public void afterCommit()
         {
-            // Note: just in case node has been deleted
+            // Get all the nodes that need their read counts incremented
+            @SuppressWarnings("unchecked")
+            Set<NodeRef> readNodeRefs = (Set<NodeRef>) AlfrescoTransactionSupport.getResource(KEY_CONTENT_HITS_READS);
+            if (readNodeRefs != null)
+            {
+                for (NodeRef nodeRef : readNodeRefs)
+                {
+                    Runnable runnable = new ContentHitsReadCountIncrementer(nodeRef);
+                    threadExecuter.execute(runnable);
+                }
+            }
+            @SuppressWarnings("unchecked")
+            Set<NodeRef> writeNodeRefs = (Set<NodeRef>) AlfrescoTransactionSupport.getResource(KEY_CONTENT_HITS_WRITES);
+            if (writeNodeRefs != null)
+            {
+                for (NodeRef nodeRef : readNodeRefs)
+                {
+                    Runnable runnable = new ContentHitsWriteCountIncrementer(nodeRef);
+                    threadExecuter.execute(runnable);
+                }
+            }
         }
-    }    
+    }
+    
+    /**
+     * The worker that will increment a node's content read count.
+     * 
+     * @author Derek Hulley
+     */
+    private class ContentHitsReadCountIncrementer implements Runnable
+    {
+        private NodeRef nodeRef;
+        private ContentHitsReadCountIncrementer(NodeRef nodeRef)
+        {
+            this.nodeRef = nodeRef;
+        }
+        /**
+         * Increments the read count on the node
+         */
+        public void run()
+        {
+            RetryingTransactionHelper txnHelper = transactionService.getRetryingTransactionHelper();
+            RetryingTransactionCallback<Integer> callback = new RetryingTransactionCallback<Integer>()
+            {
+                public Integer execute() throws Throwable
+                {
+                    // Increment the read count property value
+                    Integer currentValue = (Integer) nodeService.getProperty(nodeRef, PROP_READ_COUNT);
+                    int newValue = currentValue.intValue() + 1;
+                    nodeService.setProperty(nodeRef, PROP_READ_COUNT, newValue);
+                    return (Integer) newValue;
+                }
+            };
+            try
+            {
+                // Ensure that the policy doesn't refire for this node on this thread
+                // This won't prevent background processes from refiring, though
+                policyFilter.disableBehaviour(nodeRef, ASPECT_CONTENT_HITS);
+                
+                Integer newCount = txnHelper.doInTransaction(callback, false, true);
+                // Done
+                if (logger.isDebugEnabled())
+                {
+                    logger.debug(
+                            "Incremented content read count on node: \n" +
+                            "   Node:      " + nodeRef + "\n" +
+                            "   New Count: " + newCount);
+                }
+            }
+            catch (InvalidNodeRefException e)
+            {
+                if (logger.isDebugEnabled())
+                {
+                    logger.debug("Unable to increment content read count on missing node: " + nodeRef);
+                }
+            }
+            catch (Throwable e)
+            {
+                if (logger.isDebugEnabled())
+                {
+                    logger.debug(e);
+                }
+                logger.error("Failed to increment content read count on node: " + nodeRef);
+                // We are the last call on the thread
+            }
+            finally
+            {
+                policyFilter.enableBehaviour(ASPECT_CONTENT_HITS);
+            }
+        }
+    }
+    
+    /**
+     * The worker that will increment a node's content write count.
+     * 
+     * @author Derek Hulley
+     */
+    private class ContentHitsWriteCountIncrementer implements Runnable
+    {
+        private NodeRef nodeRef;
+        private ContentHitsWriteCountIncrementer(NodeRef nodeRef)
+        {
+            this.nodeRef = nodeRef;
+        }
+        /**
+         * Increments the write count on the node
+         */
+        public void run()
+        {
+            RetryingTransactionHelper txnHelper = transactionService.getRetryingTransactionHelper();
+            RetryingTransactionCallback<Integer> callback = new RetryingTransactionCallback<Integer>()
+            {
+                public Integer execute() throws Throwable
+                {
+                    // Increment the read count property value
+                    Integer currentValue = (Integer) nodeService.getProperty(nodeRef, PROP_UPDATE_COUNT);
+                    int newValue = currentValue.intValue() + 1;
+                    nodeService.setProperty(nodeRef, PROP_UPDATE_COUNT, newValue);
+                    return (Integer) newValue;
+                }
+            };
+            try
+            {
+                // Ensure that the policy doesn't refire for this node on this thread
+                // This won't prevent background processes from refiring, though
+                policyFilter.disableBehaviour(nodeRef, ASPECT_CONTENT_HITS);
+                
+                Integer newCount = txnHelper.doInTransaction(callback, false, true);
+                // Done
+                if (logger.isDebugEnabled())
+                {
+                    logger.debug(
+                            "Incremented content write count on node: \n" +
+                            "   Node:      " + nodeRef + "\n" +
+                            "   New Count: " + newCount);
+                }
+            }
+            catch (InvalidNodeRefException e)
+            {
+                if (logger.isDebugEnabled())
+                {
+                    logger.debug("Unable to increment content write count on missing node: " + nodeRef);
+                }
+            }
+            catch (Throwable e)
+            {
+                if (logger.isDebugEnabled())
+                {
+                    logger.debug(e);
+                }
+                logger.error("Failed to increment content write count on node: " + nodeRef);
+                // We are the last call on the thread
+            }
+            finally
+            {
+                policyFilter.enableBehaviour(ASPECT_CONTENT_HITS);
+            }
+        }
+    }
 }
