238d704feea8f5d0e387061ab303a85588b4a345
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2007-09-10 15:21:52 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@6726 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/installer/bin-lin/clean_tomcat.sh
+++ root/projects/installer/bin-lin/clean_tomcat.sh
@@ -2,6 +2,5 @@
 # ---------------------------------
 # Script to clean Tomcat temp files
 # ---------------------------------
-
 echo "Cleaning temporary Alfresco files from Tomcat..."
 rm -rf tomcat/temp/Alfresco tomcat/work/Catalina/localhost/alfresco
