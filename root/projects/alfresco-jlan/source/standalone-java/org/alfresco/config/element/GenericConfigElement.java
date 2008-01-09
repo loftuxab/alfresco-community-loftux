daa6b932933d16b02d471f99eac4eb960e19a78f
gkspencer <gkspencer@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-01-09 15:22:12 +0000

    Moved source for standalone build to a seperate area.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@7958 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ root/projects/alfresco-jlan/source/standalone-java/org/alfresco/config/element/GenericConfigElement.java
@@ -0,0 +1,26 @@
+package org.alfresco.config.element;
+
+/*
+ * GenericConfigElement.java
+ *
+ * Copyright (c) 2007 Starlasoft. All rights reserved.
+ */
+
+import org.alfresco.config.ConfigElement;
+
+/**
+ * Generic Config Element Class
+ * 
+ * <P>This class needs to match the Alfresco version of the class.
+ */
+public class GenericConfigElement extends ConfigElement {
+
+  /**
+   * Class constructor
+   * 
+   * @param name String
+   */
+  public GenericConfigElement(String name) {
+    super( name, "");
+  }
+}
