a0d0f64c033a4e42da3b88df2c270a87cec9d849
saravanan <saravanan@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-01-30 11:25:48 +0000

    Bug fix for Stopping Alfresco from linux AR-1980 Bug fix for "Error with Linux Installer in silent mode"  AWC-1148
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@8122 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/installer/linux/installer/start_oo.sh
+++ root/projects/installer/linux/installer/start_oo.sh
@@ -4,9 +4,10 @@
 # ---------------------------------------------------------------------------
 
 echo "Starting OpenOffice service..."
+ALF_HOME=`pwd`
 
 # Comment or uncomment the appropriate location using #
-@@ALFRESCO_DIR@@/openoffice.org2.1/program/soffice "-accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" "-env:UserInstallation=file://@@ALFRESCO_DIR@@/oouser" -nologo -headless -nofirststartwizard &
+"$ALF_HOME"/openoffice.org2.1/program/soffice "-accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" "-env:UserInstallation=file://$ALF_HOME/oouser" -nologo -headless -nofirststartwizard &
 
 # If NeoOffice on Mac OS X
 #/Applications/NeoOfficeJ.app/Contents/program/soffice "-accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" -nologo -headless &
