41dd06000e61f293ea5698a930418dff9bb7309a
uzi <uzi@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-04-19 18:16:59 +0000

    Cleanup for Dynamic Website file structure, part 1
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@8851 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/dynamic-website/source/web/ui/builder/wizard-adapter-extjs.js
+++
@@ -1,8 +0,0 @@
-/***********************************************************************
- *
- * ExtJS Wizard Adapter
- * For Alfresco Dynamic Website
- *
- * This includes an inline wizard implementation that works with ExtJS
- *
- ***********************************************************************/ 
