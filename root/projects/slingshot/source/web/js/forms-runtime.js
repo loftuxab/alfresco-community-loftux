aaef69f59c89a8845153aef61b86afa10846c9e8
gcornwell <gcornwell@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-05-07 16:37:40 +0000

    Updates to forms runtime, added all methods (but with no impl) and implemented all basic validation methods
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@9023 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/source/web/js/forms-runtime.js
+++ root/projects/slingshot/source/web/js/forms-runtime.js
@@ -1,37 +1,17 @@
-/* Ensure Alfresco.forms and validation objects exist */
+// Ensure Alfresco.forms and validation objects exist
 Alfresco.forms = Alfresco.forms || {};
 Alfresco.forms.validation = Alfresco.forms.validation || {};
 
-/*
- *** Alfresco.forms.Form
-*/
 (function()
 {
-   /* Form Definition */
    
    Alfresco.forms.Form = function(formId)
    {
       this.formId = formId;
       this.submitIds = null;
       this.validateOnSubmit = true;
-      this.errorPlacement = "alert";
       this.ajaxSubmit = false;
       this.validations = [];
-      
-      var form = document.getElementById(this.formId);
-      if (form != null)
-      {
-         // add the event to the form and make the scope of the handler this form.
-         YAHOO.util.Event.addListener(form, "submit", this.submitInvoked, this, true);
-         
-         if (Alfresco.logger.isDebugEnabled())
-            Alfresco.logger.debug("Added submit handler for form: ", this.formId);
-      }
-      else
-      {
-         this.showInternalError("form with id of '" + this.formId + 
-               "' could not be located, ensure the form is created after the form element is available.");
-      }
       
       return this;
    };
@@ -41,20 +21,51 @@ Alfresco.forms.validation = Alfresco.forms.validation || {};
       formId: null,
       submitIds: null,
       validateOnSubmit: null,
-      errorPlacement: null,
       ajaxSubmit: null,
       validations: null,
-
-      // ****************
-      //  Public methods
-      // ****************
-        
+      
+      init: function()
+      {
+         // TODO: determine what event handlers need to be setup depending on the
+         //       current state of the form object.
+      
+         var form = document.getElementById(this.formId);
+         if (form != null)
+         {
+            // add the event to the form and make the scope of the handler this form.
+            YAHOO.util.Event.addListener(form, "submit", this._submitInvoked, this, true);
+            
+            if (Alfresco.logger.isDebugEnabled())
+               Alfresco.logger.debug("Added submit handler for form: ", this.formId);
+         }
+         else
+         {
+            this._showInternalError("form with id of '" + this.formId + 
+                  "' could not be located, ensure the form is created after the form element is available.");
+         }
+      },
+      
+      validateOnSubmit: function(validate)
+      {
+         alert("not implemented yet");
+      },
+      
+      setSubmitIds: function(submitIds)
+      {
+         alert("not implemented yet");
+      },
+      
+      enableAJAXSubmit: function(callbacks)
+      {
+         alert("not implemented yet");
+      },
+      
       addValidation: function(fieldId, validationHandler, validationArgs, when)
       {
          var field = document.getElementById(fieldId);
          if (field == null)
          {
-            this.showInternalError("element with id of '" + fieldId + "' could not be located.");
+            this._showInternalError("element with id of '" + fieldId + "' could not be located.");
             return;
          }
          
@@ -76,18 +87,25 @@ Alfresco.forms.validation = Alfresco.forms.validation || {};
          {
             // add the event to the field, pass the validation as a paramter 
             // to the handler and make the scope of the handler this form.
-            YAHOO.util.Event.addListener(field, when, this.validationEventFired, validation, this);
+            YAHOO.util.Event.addListener(field, when, this._validationEventFired, validation, this);
             
             if (Alfresco.logger.isDebugEnabled())
                Alfresco.logger.debug("Added validation for field: ", validation);
          }
       },
       
-      // ******************
-      //  Private methods
-      // ******************
+      addError: function(msg, field, showNow)
+      {
+         // TODO: Add the error next to the field if supplied otherwise
+         //       at the top of the form
+         
+         if (showNow)
+         {
+            alert(msg);
+         }
+      },
       
-      validationEventFired: function(event, validation)
+      _validationEventFired: function(event, validation)
       {
          if (Alfresco.logger.isDebugEnabled())
             Alfresco.logger.debug("Validation event has been fired for field: " + validation.fieldId);
@@ -98,7 +116,7 @@ Alfresco.forms.validation = Alfresco.forms.validation || {};
          validation.handler(YAHOO.util.Event.getTarget(event), validation.args, this);
       },
       
-      submitInvoked: function(event)
+      _submitInvoked: function(event)
       {
          if (Alfresco.logger.isDebugEnabled())
             Alfresco.logger.debug("Submit invoked on form: ", this);
@@ -114,40 +132,68 @@ Alfresco.forms.validation = Alfresco.forms.validation || {};
             var field = document.getElementById(val.fieldId);
             if (field != null)
             {
-               // TODO: revisit this as we will want to collect all
-               //       the errors when errorPlacement is not 'alert'
                if (val.handler(field, val.args, this) == false)
                {
                   // stop the event from continuing and sending the form.
                   YAHOO.util.Event.stopEvent(event);
+                  break;
                }
             }
          }
       },
       
-      showInternalError: function(msg, field)
+      _showInternalError: function(msg, field)
       {
          this.addError("Internal Form Error: " + msg, field, true);
-      },
-      
-      addError: function(msg, field, showNow)
-      {
-         // TODO: Put the error in the appropriate place, according
-         //       to the 'errorPlacement' setting
-         
-         if (showNow)
-         {
-            alert(msg);
-         }
       }
    };
    
    /* Validation Handlers */
    
+   Alfresco.forms.validation.mandatory = function(field, args, form)
+   {
+      if (Alfresco.logger.isDebugEnabled())
+         Alfresco.logger.debug("Validating mandatory state of field '" + field.id + "'");
+      
+      var valid = true; 
+         
+      if (field.type && field.type == "radio")
+      {
+         // TODO: Do we actually need to support this scenario?
+         //       wouldn't a radio button normally have a default
+         //       'checked' option?
+         
+         var formElem = document.getElementById(form.formId);
+         var radios = formElem[field.name];
+         var anyChecked = false;
+         for (var x = 0; x < radios.length; x++)
+         {
+            if (radios[x].checked)
+            {
+               anyChecked = true;
+               break;
+            }
+         }
+         
+         valid = anyChecked;
+      }
+      else
+      {
+         valid = field.value.length != 0;
+      }
+      
+      if (valid == false)
+      {
+         form.addError(field.id + " is mandatory.", field, true);
+      }
+      
+      return valid; 
+   };
+   
    Alfresco.forms.validation.length = function(field, args, form)
    {
       if (Alfresco.logger.isDebugEnabled())
-         Alfresco.logger.debug("Validating length of field with id '" + field.id +
+         Alfresco.logger.debug("Validating length of field '" + field.id +
                                "' using args: " + YAHOO.lang.dump(args));
       
       var valid = true;
@@ -179,6 +225,94 @@ Alfresco.forms.validation = Alfresco.forms.validation || {};
       if (valid == false)
       {
          form.addError(field.id + " is not the correct length.", field, true);
+      }
+      
+      return valid;
+   };
+   
+   Alfresco.forms.validation.number = function(field, args, form)
+   {
+      if (Alfresco.logger.isDebugEnabled())
+         Alfresco.logger.debug("Validating field '" + field.id + "' is a number");
+      
+      var valid = (isNaN(field.value) == false);
+      
+      if (valid == false)
+      {
+         form.addError(field.id + " is not a number.", field, true);
+      }
+      
+      return valid;
+   };
+   
+   Alfresco.forms.validation.numberRange = function(field, args, form)
+   {
+      if (Alfresco.logger.isDebugEnabled())
+         Alfresco.logger.debug("Validating number range of field '" + field.id +
+                               "' using args: " + YAHOO.lang.dump(args));
+      
+      var valid = true;
+      var value = field.value;
+      
+      if (value.length > 0)
+      {
+         if (isNaN(value))
+         {
+            valid = false;
+            form.addError(field.id + " is not a number.", field, true);
+         }
+         else
+         {
+            var min = -1;
+            var max = -1;
+            
+            if (args.min)
+            {
+               min = args.min;
+            }
+            
+            if (args.max)
+            {
+               max = args.max;
+            }
+            
+            if (min != -1 && value < min)
+            {
+               valid = false;
+            }
+            
+            if (max != -1 && length > max)
+            {
+               valid = false;
+            }
+            
+            if (valid == false)
+            {
+               form.addError(field.id + " is not within the allowable range.", field, true);
+            }
+         }
+      }
+      
+      return valid;
+   };
+   
+   Alfresco.forms.validation.regexMatch = function(field, args, form)
+   {
+      if (Alfresco.logger.isDebugEnabled())
+         Alfresco.logger.debug("Validating regular expression of field '" + field.id +
+                               "' using args: " + YAHOO.lang.dump(args));
+      
+      var valid = true;
+      
+      if (field.value.length > 0)
+      {
+         var pattern = new RegExp(args.pattern);
+         valid = pattern.test(field.value);
+         
+         if (valid == false)
+         {
+            form.addError(field.id + " is invalid.", field, true);
+         }
       }
       
       return valid;
