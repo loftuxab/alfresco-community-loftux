30360e30f58756b59c577726d15f0cd7b4ee32b2
ewinlof <ewinlof@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-06-12 15:37:57 +0000

    Created dashboard layouts (now used by user & site dashboards) - 1 column - 2 columns - 2 columns wide left - 2 columns wide right - 3 columns - 4 columns
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@9463 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/templates/org/alfresco/dashboard.js
+++ root/projects/slingshot/config/alfresco/templates/org/alfresco/dashboard.js
@@ -1,1 +1,22 @@
-model.regions = parseInt(template.properties.regions);
+// 1 - ""
+// 2 - "yui-g" "yui-gc" "yui-gd" "yui-ge" "yui-gf"
+// 3 - "yui-gb"
+// 4 - "yui-g"
+// 5 - "yui-gb" // note, will leave an empty column to the right
+// 6 - "yui-gb"
+
+model.gridClass = template.properties.gridClass;
+//[{components: 3}, {components: 3}, {components: 3}, {components: 3}];//parseInt(template.properties.column);
+var columns = [];
+for(var i = 0; true; i++)
+{
+   var noOfComponents = template.properties["gridColumn" + (i + 1)];
+   if(noOfComponents)
+   {
+      columns[i] = {components: parseInt(noOfComponents)};
+   }
+   else{
+      break;
+   }
+}
+model.gridColumns = columns;
