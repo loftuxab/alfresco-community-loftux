5ecf196e326db4cef94d2fe4271c95847610504f
saravanan <saravanan@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-06-13 13:02:39 +0000

    Updates to Knowledge-base search component
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@9470 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/extranet/config/alfresco/templates/ask.ftl
+++ root/projects/extranet/config/alfresco/templates/ask.ftl
@@ -6,84 +6,28 @@
 	<title><@pageTitle/></title>
 	<@global.header/>
 	${head}
+
+<script type="text/javascript" src="yui/yahoo-dom-event/yahoo-dom-event.js"></script>
+<script type="text/javascript" src="yui/dragdrop/dragdrop-min.js"></script>
+<script type="text/javascript" src="yui/container/container-min.js"></script>
+<script type="text/javascript" src="scripts/swfobject.js"></script>
 	
-  <script type="text/javascript">
+<script type="text/javascript" src="scripts/ask-kbsearch.js"></script>
+<link rel="stylesheet" type="text/css" href="yui/container/assets/container.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+
    
-     function ajaxRead()
-     {
-		file = "/extranet/proxy/alfresco/kb/search.atom?q=" + document.kbsearch.search.value ;
- 		
-		var xmlObj = null;
-      	if(window.XMLHttpRequest)
-      	{
-          xmlObj = new XMLHttpRequest();
-      	}
-      	else if(window.ActiveXObject)
-      	{
-          		xmlObj = new ActiveXObject("Microsoft.XMLHTTP");
-      	} else
-		alert("not supported");
-
-		//prepare the xmlhttprequest object
-		xmlObj.open("GET",file,true);
-		xmlObj.setRequestHeader("Cache-Control", "no-cache");
-		xmlObj.setRequestHeader("Pragma", "no-cache");
-		xmlObj.onreadystatechange = function() {
-		if (xmlObj.readyState == 4)
-		{
-			if (xmlObj.status == 200)
-			{
-				if (xmlObj.responseText != null)
-					processXML(xmlObj.responseXML);
-				else
-				{
-					alert("Failed to receive RSS file from the server - file not found.");
-					return false;
-				}
-			}
-			else
-				alert("Error code " + xmlObj.status + " received: " + xmlObj.statusText);
-		}
-	}
-
-	//send the request
-	xmlObj.send(null);
-    }
-    
-    
-    function processXML(obj)
-    {
-    	document.getElementById ('dataArea').visible = true;
-    	
-     	var dataArray = obj.getElementsByTagName('entry');
-		var dataArrayLen = dataArray.length;
-		var insertData = '<table style="width:833px; border: solid 3px #cde"><tr><th>' + 'Search Results for the Keyword : '+ document.kbsearch.search.value +'</th></tr>';
-     	 for (var i=0; i<dataArrayLen; i++)
-     	 {
-			 var summary = dataArray[i].getElementsByTagName('summary');
-	 		 var updated = dataArray[i].getElementsByTagName('updated');
-	 		 var title = dataArray[i].getElementsByTagName('title');	
-	 		 var category = dataArray[i].getElementsByTagName('category');
-         	 insertData += '<tr><td style="color:red; font-size: 1.5em;"><a href=#>' + title[0].firstChild.data + '</a></td></tr>';
-         	 insertData += '<tr><td>' + summary[0].firstChild.data + '</td></tr>';
-	 		 insertData += '<tr><td>' + updated[0].firstChild.data + '--- Category : '+ category[0].firstChild.data + '</td></tr>';
-	 		 insertData += '<tr><td><hr color:red;background:red;height:1px;border:0;/></td></tr>';
-     	}
-      insertData += '</table><br/><br/>';
-      
-      document.getElementById ('dataArea').innerHTML = insertData;
-    }
 	
-	function clearData()
+<style type="text/css">
+	.mask 
 	{
-		//document.getElementById ('dataArea').innerHTML = '';
-		//document.getElementById ('search').value ='';
-		document.getElementById ('dataArea').visible = false;
-		return false;
+		-moz-opacity: 0.8;
+		opacity:.80;
+		filter: alpha(opacity=80);
+		background-color:#2f2f2f;
 	}
-	
-    </script>
-	
+</style>
+
 </head>
 
 <body>
---
+++ root/projects/extranet/source/web/scripts/ask-kbsearch.js
@@ -0,0 +1,157 @@
+	//Javascript for ask search
+	
+	//Global variables	
+	var strObj = new Array;
+	var selectedVar = '';
+	
+	//namespace	
+	YAHOO.namespace('com.alfresco');
+	function init() 
+	{
+		var handleCancel = function(o)
+		{
+			this.cancel();
+		}
+	
+		YAHOO.com.alfresco.dialog6 = new YAHOO.widget.Dialog("lightbox_display",
+		{ 
+			width : "550px",
+			fixedcenter : false,
+			visible : false,
+			close: true,
+			draggable: true,
+			modal: true,
+			y: 25,
+			constraintoviewport : true
+		});
+			
+		YAHOO.util.Event.addListener('closeImg', 'click', function(o){YAHOO.com.alfresco.dialog6.hide()});
+		YAHOO.com.alfresco.dialog6.render();
+
+		var onDialog6Show = function(e, args, o)
+		{
+			o.body.id = 'videoCon';
+	
+			//flash embed script, more information: http://blog.deconcept.com/swfobject/#download
+			var so = new SWFObject(strObj[selectedVar], "sotester", "500", "680", "9", "#000000");
+	
+			so.write("videoCon");
+		};
+
+		YAHOO.com.alfresco.dialog6.showEvent.subscribe(onDialog6Show, YAHOO.com.alfresco.dialog6);
+
+		var onDialog6Hide = function(e, args, o)
+		{
+			o.setBody('');
+		};
+		YAHOO.com.alfresco.dialog6.hideEvent.subscribe(onDialog6Hide, YAHOO.com.alfresco.dialog6);
+
+	
+		YAHOO.util.Dom.setStyle(['lightbox_display'], 'display', 'block');
+	};
+
+	YAHOO.util.Event.addListener(window, "load", init);
+
+     function ajaxRead()
+     {
+		file = "/extranet/proxy/alfresco/kb/search.atom?q=" + document.kbsearch.search.value ;
+ 		
+		var xmlObj = null;
+      	if(window.XMLHttpRequest)
+      	{
+          xmlObj = new XMLHttpRequest();
+      	}
+      	else if(window.ActiveXObject)
+      	{
+          		xmlObj = new ActiveXObject("Microsoft.XMLHTTP");
+      	} else
+		alert("not supported");
+
+		//prepare the xmlhttprequest object
+		xmlObj.open("GET",file,true);
+		xmlObj.setRequestHeader("Cache-Control", "no-cache");
+		xmlObj.setRequestHeader("Pragma", "no-cache");
+		xmlObj.onreadystatechange = function() {
+		if (xmlObj.readyState == 4)
+		{
+			if (xmlObj.status == 200)
+			{
+				if (xmlObj.responseText != null)
+					processXML(xmlObj.responseXML);
+				else
+				{
+					alert("Failed to receive RSS file from the server - file not found.");
+					return false;
+				}
+			}
+			else
+				alert("Error code " + xmlObj.status + " received: " + xmlObj.statusText);
+		}
+	}
+
+	//send the request
+	xmlObj.send(null);
+    }
+    
+    function explodeDomainName(link)
+	{
+		var domain = new Array();
+		domain = link.split('/');
+		var url ='';
+		for(var i=0; i<domain.length; i++)
+		{
+			if(i>=3) url +=  '/' + domain[i] ;
+		}
+		return 	url;
+	}
+	
+    function setSelectedVar(i)
+	{
+		selectedVar = i;
+	}
+    
+    function processXML(obj)
+    {
+    	document.getElementById ('dataArea').visible = true;
+    	 
+     	var dataArray = obj.getElementsByTagName('entry');
+		var dataArrayLen = dataArray.length;
+		var insertData = '<table style=\"width:833px; align:centre;\"><tr><th><strong>' + 'Search Results for the Keyword : '+ document.kbsearch.search.value +'</strong></th></tr>';
+     	for (var i=0; i<dataArrayLen; i++)
+	     	 {
+			 var summary = dataArray[i].getElementsByTagName('summary');
+	 		 var updated = dataArray[i].getElementsByTagName('updated');
+	 		 var title = dataArray[i].getElementsByTagName('title');	
+	 		 var category = dataArray[i].getElementsByTagName('category');
+	 		 var link = dataArray[i].getElementsByTagName('link');
+			 var originallink = dataArray[i].getElementsByTagName('originallink');
+			 var icon = dataArray[i].getElementsByTagName('icon');
+			 var url = 'http://hosted13.alfresco.com';
+			
+			 swfHyperLink = link[0].attributes.getNamedItem("href").nodeValue;
+			 swfHyperLink = 'http://hosted13.alfresco.com' + explodeDomainName(swfHyperLink);
+			 
+			pdfHyperLink = originallink[0].attributes.getNamedItem("href").nodeValue;
+			pdfHyperLink = 'http://hosted13.alfresco.com' + explodeDomainName(pdfHyperLink);
+
+			iconLink = icon[0].firstChild.data;
+			iconLink = 'http://hosted13.alfresco.com' + explodeDomainName(iconLink)
+
+			strObj[i] = swfHyperLink;
+			insertData += "<tr><td><a id=\""+ i +"\" href=\"#void\" onclick=\"setSelectedVar("+i+");YAHOO.com.alfresco.dialog6.show();\"><img src=\"images/preview.gif\" style=\"border-width:0px;vertical-align:middle;\"></img></a>&nbsp;"
+			insertData += "<a href=\""+ pdfHyperLink+ "\">" + title[0].firstChild.data + "<img src=\""+ iconLink + "\" style=\"border-width:0px;vertical-align:right;\" alt=\"Download\"></img> </a></td></tr>";
+			insertData += "<tr><td>" + summary[0].firstChild.data + "</td></tr>";
+	 		insertData += "<tr><td>" + updated[0].firstChild.data + "--- Category : "+ category[0].firstChild.data + "</td></tr>";
+	 		insertData += "<tr><td><hr style=\"color:red;background:red;height:1px;border:0;\"/></td></tr>";
+     		}
+		//end of for loop
+
+	      insertData += "</table><br/><br/>";
+   
+		document.getElementById ('dataArea').innerHTML = insertData;
+    }
+	
+	function clearData()
+	{
+		document.getElementById ('dataArea').innerHTML = "";
+	}
