378d20773432e5b881d27a64a3451aa228b7a5af
simonb <simonb@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-07-03 14:26:12 +0000

    Initial version of wiki page delete scripts. Added delete button to toolbar.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@9637 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/source/web/components/wiki/toolbar.js
+++ root/projects/slingshot/source/web/components/wiki/toolbar.js
@@ -27,6 +27,18 @@
    		this.siteId = siteId;
    		return this;
    	},
+   	
+   	/**
+   	  * The title of the current page.
+   	  * 
+   	  * @property siteId
+   	  * @type string
+   	  */
+   	setTitle: function(title)
+   	{
+   	   this.title = title;
+   	   return this;
+   	},
    		
 	   /**
 		 * Fired by YUILoaderHelper when required component script files have
@@ -55,18 +67,103 @@
 	      	type: "push"
 	      });
 	      
+	      // Create button
 	      var createButton = Alfresco.util.createYUIButton(this, "create-button", this.onCreateClick,
 	      {
 	      	type: "push"
 	      });
 	      
+	      // Delete button
+	      var deleteButton = Alfresco.util.createYUIButton(this, "delete-button", this.onDeleteClick,
+	      {
+	      	type: "push"
+	      });
+	      
+	      this.deleteDialog = new YAHOO.widget.SimpleDialog("deleteDialog", 
+	      {
+	         width: "300px",
+            fixedcenter: true,
+            visible: false,
+            draggable: false,
+            close: true,
+            text: "Are you sure you want to delete this page?",
+            icon: YAHOO.widget.SimpleDialog.ICON_HELP,
+            constraintoviewport: true,
+            buttons: [ { text:"Yes", handler: { fn: this.onConfirm, scope: this }, isDefault: true },
+            			{ text:"No",  handler: { fn: this.onCancel, scope: this } }]
+	      });
+	      this.deleteDialog.setHeader("Are you sure?");
+	      this.deleteDialog.render(this.id + "-body");
 	   },
 	   
+	   /**
+		 * Fired when the user confirms that they want to delete a page. 
+		 * Kicks off a DELETE request to the Alfresco repo to remove an event.
+		 *
+		 * @method onConfirm
+		 * @param e {object} DomEvent
+		 */
+	   onConfirm: function(e)
+	   {
+	      Alfresco.util.Ajax.request(
+   		{
+   		   method: Alfresco.util.Ajax.DELETE,
+   		   url: Alfresco.constants.PROXY_URI + "slingshot/wiki/page/" + this.siteId + "/" + this.title,
+   		   successCallback:
+   		   {
+   			   fn: this.onPageDeleted,
+   				scope: this
+   			},
+   		   failureMessage: "Could not delete page"
+   		});
+	   },
+	   
+      /**
+   	 * Fired when the user decides not to delete a page.
+   	 * Hides the confirmation dialog.
+   	 *
+   	 * @method onCancel
+   	 * @param e {object} DomEvent
+   	 */
+	   onCancel: function(e)
+	   {
+	      this.deleteDialog.hide();
+	   },
+	   
+	   /**
+       * Callback handler then gets invoked when a page is 
+   	 * successfully deleted.
+   	 * 
+   	 * @method onPageDeleted
+   	 * @param e {object} DomEvent
+   	 */
+	   onPageDeleted: function(e)
+	   {
+	      // TODO: redirect to wiki landing page, when there is one
+	      alert("Page deleted");
+	      this.deleteDialog.hide();
+	   },
+	   
+	   /**
+   	 * Event handler for when the user hits the 'New Page' button.
+   	 * Opens up the create page panel.
+   	 *
+   	 * @method onCreateClick
+   	 * @param e {object} DomEvent
+   	 */
 	   onCreateClick: function(e)
 	   {
 	      this.panel.show();
 	   },
 	   
+	   /**
+   	 * Event handler for save button on the page creation panel.
+   	 * Modifies the URL to redirect the user to the page with the title they
+   	 * specified in the text field.
+   	 *
+   	 * @method onSaveClick
+   	 * @param e {object} DomEvent
+   	 */
 	   onSaveClick: function(e)
 	   {
 	      var elem = YAHOO.util.Dom.get(this.id + "-title");
@@ -78,9 +175,20 @@
 	            title = title.replace(/\s+/g, "_");
 	            // Change the location bar
    	         window.location = Alfresco.constants.URL_CONTEXT + "page/wiki?site=" + this.siteId + "&title=" + title;
-	         }
-	         
+	         } 
 	      } 
+	   },
+	   
+      /**
+       * Event handler for the delete button in the toolbar.
+       * Pops up the delete confirmation dialog.
+   	 *
+   	 * @method onDeleteClick
+   	 * @param e {object} DomEvent
+   	 */
+	   onDeleteClick: function(e)
+	   {
+	      this.deleteDialog.show();
 	   }
    };
 
