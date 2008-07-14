145a92d3146fb97340c827464158715228146668
simonb <simonb@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-07-14 09:35:48 +0000

    Added event handler to browse tags component. When a link is clicked it now fires an 'onTagSelected' event with the name of the tag that was selected.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@9834 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/source/web/components/tags/tags.js
+++ root/projects/slingshot/source/web/components/tags/tags.js
@@ -19,19 +19,41 @@
    
    Alfresco.TagComponent.prototype =
    {
+      /**
+       * Fired by YUILoaderHelper when required component script files have
+       * been loaded into the browser.
+       *
+       * @method onComponentsLoaded
+       */
       componentsLoaded: function TagComponent_componentsLoaded()
       {
          YAHOO.util.Event.onContentReady(this.id, this.onReady, this, true);
       },
    
+      /**
+       * Fired by YUI when parent element is available for scripting.
+       * Registers a default action listener on <em>all</em> of the tag links in the 
+       * component. Fires 'onTagSelected' event with the name of the tag that was selected.
+       *
+       * To register for the event, interested components should do something like this:
+       * YAHOO.Bubbling.on("onTagSelected", this.onTagSelected, this); 
+       *
+       * @method onReady
+       */   
       onReady: function TagComponent_onReady()
       {
-         //Alfresco.util.registerDefaultActionHandler(this.id, "tag-link", "li", this);
-      },
-
-      onTagSelection: function TagComponent_onTagSelection(htmlId, ownerId, param)
-      {
-         YAHOO.Bubbling.fire('onSetBlogPostListParams', {tag : param});
+         YAHOO.Bubbling.addDefaultAction('tag-link', function(layer, args)
+         {
+            var link = args[1].target;
+            if (link)
+            {
+               var tagName = link.firstChild.nodeValue;
+               YAHOO.Bubbling.fire('onTagSelected', {
+                  "tagname": tagName
+               });
+            }
+            return true;
+         });
       }
    }
 })();
