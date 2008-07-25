063dd92dd681d39e6fcc13b89956fbbb240d8be4
simonb <simonb@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-07-25 10:37:47 +0000

    Modified site dashboard navigation to allow arbitary URLs to pages. Pages are now specified by the id attribute in web-framework-config-application.xml whereas the value is now the URL that is used to generate the link to the page. Modified the title of the wiki page definition.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@10023 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/navigation/collaboration-navigation.get.html.ftl
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/navigation/collaboration-navigation.get.html.ftl
@@ -2,7 +2,7 @@
    <span class="navigation-item"><a href="${url.context}/page/site/${page.url.templateArgs.site!""}/dashboard">${msg("link.siteDashboard")}</a></span>
 <#list pages as p>
    <span class="navigation-separator">|</span>
-   <span class="navigation-item"><a href="${url.context}/page/site/${page.url.templateArgs.site!""}/${p.pageId}">${p.title}</a></span>
+   <span class="navigation-item"><a href="${url.context}/page/site/${page.url.templateArgs.site!""}/<#if p.pageUrl??>${p.pageUrl}<#else>${p.pageId}</#if>">${p.title}</a></span>
 </#list>
 <span class="navigation-separator">|</span>
 <span class="navigation-item"><a href="${url.context}/page/site/${page.url.templateArgs.site!""}/invite">${msg("link.members")}</a></span>
--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/navigation/collaboration-navigation.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/navigation/collaboration-navigation.get.js
@@ -7,12 +7,31 @@ if(usedPages == null)
    usedPages = [];
 }
 
+var availablePages = config.scoped["SitePages"]["pages"].childrenMap["page"];
+var urlMap = {};
+for (i = 0; i < availablePages.size(); i++)
+{
+   // Get page id from config file
+   pageId = availablePages.get(i).attributes["id"];
+   if (pageId)
+   {
+      pageUrl = availablePages.get(i).value;
+      urlMap[pageId] = pageUrl;
+   }
+}
+
 // Find the label for each page
 for(var i = 0; i < usedPages.length; i++)
 {
    var usedPage = usedPages[i];
    var p = sitedata.getPage(usedPage.pageId);
    usedPage.title = p.title;
+   var pageUrl = urlMap[usedPage.pageId];
+   if (pageUrl)
+   {
+      // Overwrite the stored pageUrl with the latest one from config file
+      usedPage.pageUrl = pageUrl;  
+   }
 }
 
 // Prepare template model
