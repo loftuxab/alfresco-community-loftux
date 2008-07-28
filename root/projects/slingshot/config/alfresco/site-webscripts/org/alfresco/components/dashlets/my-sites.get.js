807c8f204901e1d517f99d6acc104b79c1feaf09
rwetherall <rwetherall@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-07-28 11:11:50 +0000

    My sites dashlet now uses the MySites web script
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@10063 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/dashlets/my-sites.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/dashlets/my-sites.get.js
@@ -1,5 +1,5 @@
 // Call the repo for sites the user is a member of
-var result = remote.call("/api/sites?size=5&pos=0");
+var result = remote.call("/api/people/" + user.name + "/sites");
 
 // Create javascript objects from the server response
 var sites = eval('(' + result + ')');
