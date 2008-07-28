4df6e57070264125f46cb5d502cd9a6b831b85aa
saravanan <saravanan@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-07-28 16:23:38 +0000

    Updates to KB search
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@10075 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/extranet/config/alfresco/site-webscripts/org/alfresco/extranet/components/kbsearch/kbsearch.get.html.ftl
+++ root/projects/extranet/config/alfresco/site-webscripts/org/alfresco/extranet/components/kbsearch/kbsearch.get.html.ftl
@@ -1,6 +1,37 @@
-<form name="kbsearch" id="kbsearch" onSubmit="ajaxRead(); return false">
-Search the Knowledge Base: 
-<input name="search" type="text"/>
-<input value="Search" type="button" onclick="ajaxRead(); return false" />
-<input value="Clear" type="button" onclick="clearData()" />
-</form>
+ <p><h1>Alfresco Knowledge Base Search</h1></p>
+    <input id="searchText" type="text" onKeyPress="{if (event.keyCode==13) textsearch();}">     
+    <input type="Submit" onClick="textsearch()"/>
+    <input onclick="resetSearchArticles()" type="button" value="Reset"/>
+<div><a id="toggleadvanced" href="javascript:categorydisplay();">Advanced&gt;&gt;</a></div>
+<div id="categorydisplay"  style="display:none">
+<!-- TODO Hardcoded KB categories, which has to be updated-->
+<table border="0">
+<tr>
+	<td valign="top">
+		<label for="alfresco_version">Alfresco Version(s)</label>
+		<select name="alfresco_version" id="alfresco_version"  onchange="javascript:setAlfrescoversion('');">
+		<option value="Any" selected=selected>Any</option>
+		<option value="workspace://SpacesStore/kb:version-1-2">1.2</option>
+		<option value="workspace://SpacesStore/kb:version-1-4">1.4</option>
+		<option value="workspace://SpacesStore/kb:version1.4.3">1.4.3</option>
+		<option value="workspace://SpacesStore/kb:version2-0">2.0</option>
+		<option value="workspace://SpacesStore/kb:version-2-0-1">2.2</option>
+		</select>
+	</td>
+			
+	<td valign="top">&nbsp;| Type&nbsp;</td><td valign="top">
+		<select name="article_type" id="article_type" onchange="javascript:setArticletype('');">
+		<option value="workspace://SpacesStore/kb:type-any">Any</option>
+		<option value="workspace://SpacesStore/kb:type-article">Article</option>
+		<option value="workspace://SpacesStore/kb:type-faq">FAQ</option>
+		<option value="workspace://SpacesStore/kb:type-white-paper">White Paper</option>
+		</select>
+	</td>
+
+</tr>
+<tr></td></tr>
+</table>
+
+</div>
+ 
+   
---
+++ root/projects/extranet/source/web/scripts/kbadvancedsearch.js
@@ -0,0 +1,102 @@
+var article_type     = '';
+var searchText       = '';
+var alfresco_version = '';
+
+function explodeDomainName(link)
+{
+    var domain = new Array();
+    domain = link.split('/');
+    var url ='';
+    for(var i=0; i<domain.length; i++)
+    {
+        if(i>=3) url +=  '/' + domain[i] ;
+    }
+    return     url;
+}
+
+function categorydisplay() {
+	vista = (document.getElementById("categorydisplay").style.display == 'none') ? 'block' : 'none';
+	document.getElementById("categorydisplay").style.display = vista;
+	toggle = (document.getElementById("toggleadvanced").innerHTML == 'Advanced&gt;&gt;') ? '&lt;&lt;Basic' : 'Advanced&gt;&gt;';
+	document.getElementById("toggleadvanced").innerHTML = toggle;
+	if (toggle == "Advanced&gt;&gt;") {
+			resetCategories();
+	}
+}
+
+function resetCategories() {
+	document.getElementById("alfresco_version").options[0].selected = true;
+	document.getElementById("article_type").options[0].selected = true;
+}
+
+function resetSearchArticles() {
+	document.getElementById("alfresco_version").options[0].selected = true;
+	document.getElementById("article_type").options[0].selected = true;
+	resetCategories('');
+	document.getElementById("categorydisplay").style.display = 'none'
+	document.getElementById("toggleadvanced").innerHTML = 'Advanced&gt;&gt;';
+	document.getElementById("searchText").value = "";
+	article_type     = '';
+    searchText  	 = '';
+    alfresco_version = '';
+}
+
+function setAlfrescoversion(){
+	alfresco_version= document.getElementById("alfresco_version").value;
+	textsearch();
+}
+
+function setArticletype(){
+	article_type  =  document.getElementById("article_type").value;
+	textsearch();
+}
+
+function textsearch()
+{
+YAHOO.util.Event.onDOMReady(function() {
+    YAHOO.example.ClientPagination = new function() {
+       	this.formatUrl = function(elCell, oRecord, oColumn, sData) {
+	    var x = oRecord.getData("originallink").indexOf("/d/a");
+        if(x > -1)
+        {
+           dataLink = '/extranet/proxy/alfresco/api/node/content' + oRecord.getData("originallink").substring(x+4);
+        }
+        
+        elCell.innerHTML = "<a href='" + dataLink + "'>" + sData + "&nbsp;<img src=\""+oRecord.getData("icon")+"\" alt=\"download\" style=\"border-width:0px;vertical-align:right;\"></img></a>";
+        };
+	    var myColumnDefs = [
+	    {key:"id", label:"S.No",sortable:true},
+	    {key:"title", label:"Title",sortable:true, formatter:this.formatUrl},
+        {key:"description", label:"Description",sortable:true},
+        {key:"authorname", label:"Creator",sortable:true},
+	    {key:"modifier", label:"Modified",sortable:true},
+        {key:"type", label:"Type",sortable:true},
+        {key:"updated", label:"updated",sortable:true},
+        {key:"version", label:"Version",sortable:true},
+        {key:"tags", label:"Tags",sortable:true},
+         ];
+
+    
+        this.myDataSource = new YAHOO.util.DataSource("/extranet/proxy/alfresco/kb/advancedsearchresults.json?searchText="+document.getElementById('searchText').value+"&article_type="+document.getElementById('article_type').value+"&alfresco_version="+alfresco_version+"&");
+        this.myDataSource.responseType = YAHOO.util.DataSource.TYPE_JSON;
+        this.myDataSource.responseSchema = {
+            resultsList: "records",
+            fields: ["id","title","description","authorname","modifier","type","updated","version","tags","swflink","originallink","icon"],
+	        metaFields : {
+            totalRecords: 'totalRecords' // The totalRecords meta field is
+                                         // a "magic" meta, and will be passed
+                                         // to the Paginator.
+            }
+        };
+
+        var oConfigs = {
+                paginator: new YAHOO.widget.Paginator({
+                    rowsPerPage: 15
+                }),
+                //initialRequest: "results=10"
+        };
+        this.myDataTable = new YAHOO.widget.DataTable("paginated", myColumnDefs,
+                this.myDataSource, oConfigs);
+    };
+});
+}
