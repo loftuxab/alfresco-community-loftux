ab3dbbebbfc66ca9b87a3e78346f32ae7aa3c72d
mhatfield <mhatfield@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-09-05 13:10:08 +0000

    Messages JavaScript included before Alfresco.js so support utility i18n. DateFormat refactored and i18n'ed
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@10784 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 352f936e6d6bd1366088b43e08a9404d80010de1

--- root/projects/slingshot/source/web/js/alfresco.js
+++ root/projects/slingshot/source/web/js/alfresco.js
@@ -1978,109 +1978,114 @@ Alfresco.thirdparty.dateFormat = function()
    	The date defaults to the current date/time.
    	The mask defaults ``"ddd mmm d yyyy HH:MM:ss"``.
    */
-   var DAY_NAMES = (Alfresco.util.message("days.medium") + "," + Alfresco.util.message("days.long")).split(",");
-   var MONTH_NAMES = (Alfresco.util.message("months.short") + "," + Alfresco.util.message("months.long")).split(",");
-   
-   var dateFormat = function () {
+   var dateFormat = function()
+   {
    	var   token        = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloZ]|"[^"]*"|'[^']*'/g,
       		timezone     = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
       		timezoneClip = /[^-+\dA-Z]/g,
-      		pad = function (value, length) {
+      		pad = function (value, length)
+      		{
       			value = String(value);
       			length = parseInt(length) || 2;
       			while (value.length < length)
+      			{
       				value = "0" + value;
+      			}
       			return value;
       		};
 
    	// Regexes and supporting functions are cached through closure
-   	return function (date, mask) {
+   	return function (date, mask)
+   	{
    		// Treat the first argument as a mask if it doesn't contain any numbers
-   		if (
-   			arguments.length == 1 &&
-   			(typeof date == "string" || date instanceof String) &&
-   			!/\d/.test(date)
-   		) {
+   		if (arguments.length == 1 &&
+   			   (typeof date == "string" || date instanceof String) &&
+   			   !/\d/.test(date))
+   		{
    			mask = date;
    			date = undefined;
    		}
 
    		date = date ? new Date(date) : new Date();
    		if (isNaN(date))
+   		{
    			throw "invalid date";
+   		}
 
-   		var dF = dateFormat;
-   		mask   = String(dF.masks[mask] || mask || dF.masks["default"]);
+   		mask = String(this.masks[mask] || mask || this.masks["default"]);
 
-   		var	d = date.getDate(),
-   			D = date.getDay(),
-   			m = date.getMonth(),
-   			y = date.getFullYear(),
-   			H = date.getHours(),
-   			M = date.getMinutes(),
-   			s = date.getSeconds(),
-   			L = date.getMilliseconds(),
-   			o = date.getTimezoneOffset(),
-   			flags = {
-   				d:    d,
-   				dd:   pad(d),
-   				ddd:  dF.i18n.dayNames[D],
-   				dddd: dF.i18n.dayNames[D + 7],
-   				m:    m + 1,
-   				mm:   pad(m + 1),
-   				mmm:  dF.i18n.monthNames[m],
-   				mmmm: dF.i18n.monthNames[m + 12],
-   				yy:   String(y).slice(2),
-   				yyyy: y,
-   				h:    H % 12 || 12,
-   				hh:   pad(H % 12 || 12),
-   				H:    H,
-   				HH:   pad(H),
-   				M:    M,
-   				MM:   pad(M),
-   				s:    s,
-   				ss:   pad(s),
-   				l:    pad(L, 3),
-   				L:    pad(L > 99 ? Math.round(L / 10) : L),
-   				t:    H < 12 ? "a"  : "p",
-   				tt:   H < 12 ? "am" : "pm",
-   				T:    H < 12 ? "A"  : "P",
-   				TT:   H < 12 ? "AM" : "PM",
-   				Z:    (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
-   				o:    (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4)
-   			};
+   		var d = date.getDate(),
+   			 D = date.getDay(),
+   			 m = date.getMonth(),
+   			 y = date.getFullYear(),
+   			 H = date.getHours(),
+   			 M = date.getMinutes(),
+   			 s = date.getSeconds(),
+   			 L = date.getMilliseconds(),
+   			 o = date.getTimezoneOffset(),
+   			 flags =
+   			 {
+   				 d:    d,
+   				 dd:   pad(d),
+   				 ddd:  this.i18n.dayNames[D],
+   				 dddd: this.i18n.dayNames[D + 7],
+   				 m:    m + 1,
+   				 mm:   pad(m + 1),
+   				 mmm:  this.i18n.monthNames[m],
+   				 mmmm: this.i18n.monthNames[m + 12],
+   				 yy:   String(y).slice(2),
+   				 yyyy: y,
+   				 h:    H % 12 || 12,
+   				 hh:   pad(H % 12 || 12),
+   				 H:    H,
+   				 HH:   pad(H),
+   				 M:    M,
+   				 MM:   pad(M),
+   				 s:    s,
+   				 ss:   pad(s),
+   				 l:    pad(L, 3),
+   				 L:    pad(L > 99 ? Math.round(L / 10) : L),
+   				 t:    H < 12 ? this.TIME_AM[0] : this.TIME_PM[0],
+   				 tt:   H < 12 ? this.TIME_AM : this.TIME_PM,
+   				 T:    H < 12 ? this.TIME_AM[0].toUpperCase() : this.TIME_PM[0].toUpperCase(),
+   				 TT:   H < 12 ? this.TIME_AM.toUpperCase() : this.TIME_PM.toUpperCase(),
+   				 Z:    (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
+   				 o:    (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4)
+   			 };
 
-   		return mask.replace(token, function ($0) {
+   		return mask.replace(token, function ($0)
+   		{
    			return ($0 in flags) ? flags[$0] : $0.slice(1, $0.length - 1);
    		});
    	};
    }();
 
-   // Some common format strings
-   dateFormat.masks = {
-   	"default":       "ddd d mmm yyyy HH:MM:ss",
-   	shortDate:       "d/m/yy",
-   	mediumDate:      "d mmm, yyyy",
-   	longDate:        "d mmmm, yyyy",
-   	fullDate:        "dddd, d mmmm, yyyy",
-   	shortTime:       "h:MM TT",
-   	mediumTime:      "h:MM:ss TT",
-   	longTime:        "h:MM:ss TT Z",
-   	isoDate:         "yyyy-mm-dd",
-   	isoTime:         "HH:MM:ss",
-   	isoDateTime:     "yyyy-mm-dd'T'HH:MM:ss",
-   	isoFullDateTime: "yyyy-mm-dd'T'HH:MM:ss.lo"
-   };
-
-   // Internationalization strings
-   dateFormat.i18n = {
-   	dayNames: DAY_NAMES,
-   	monthNames: MONTH_NAMES
-   };
- 
    /**
     * Alfresco wrapper: delegate to wrapped code
     */
-   return dateFormat.apply(this, arguments);
+   return dateFormat.apply(arguments.callee, arguments);
 }
-
+Alfresco.thirdparty.dateFormat.DAY_NAMES = (Alfresco.util.message("days.medium") + "," + Alfresco.util.message("days.long")).split(",");
+Alfresco.thirdparty.dateFormat.MONTH_NAMES = (Alfresco.util.message("months.short") + "," + Alfresco.util.message("months.long")).split(",");
+Alfresco.thirdparty.dateFormat.TIME_AM = Alfresco.util.message("date-format.am");
+Alfresco.thirdparty.dateFormat.TIME_PM = Alfresco.util.message("date-format.pm");
+Alfresco.thirdparty.dateFormat.masks =
+{
+	"default":       Alfresco.util.message("date-format.default"),
+	shortDate:       Alfresco.util.message("date-format.shortDate"),
+	mediumDate:      Alfresco.util.message("date-format.mediumDate"),
+	longDate:        Alfresco.util.message("date-format.longDate"),
+	fullDate:        Alfresco.util.message("date-format.fullDate"),
+	shortTime:       Alfresco.util.message("date-format.shortTime"),
+	mediumTime:      Alfresco.util.message("date-format.mediumTime"),
+	longTime:        Alfresco.util.message("date-format.longTime"),
+	isoDate:         "yyyy-mm-dd",
+	isoTime:         "HH:MM:ss",
+	isoDateTime:     "yyyy-mm-dd'T'HH:MM:ss",
+	isoFullDateTime: "yyyy-mm-dd'T'HH:MM:ss.lo"
+};
+Alfresco.thirdparty.dateFormat.i18n =
+{
+	dayNames: Alfresco.thirdparty.dateFormat.DAY_NAMES,
+	monthNames: Alfresco.thirdparty.dateFormat.MONTH_NAMES
+};
