8d1056518215cdd360e31279920826371f5bdb06
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-09-05 13:00:38 +0000

    Updated Linux startup script
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@10780 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: a8d070a6073ec3addf376fe29919cd7c82df912a

--- root/projects/installer/linux/alfresco.sh
+++ root/projects/installer/linux/alfresco.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 # Start or stop Alfresco server
 # Set the following to where Tomcat is installed
-APPSERVER=/opt/alfresco/tomcat
+ALF_HOME=.
+cd "$ALF_HOME"
+APPSERVER="${ALF_HOME}/tomcat"
 # Set any default JVM values
 export JAVA_OPTS='-Xms128m -Xmx512m -XX:MaxPermSize=128m -server'
 # Following only needed for Sun JVMs before to 1.5 update 8
 export JAVA_OPTS="${JAVA_OPTS} -XX:CompileCommand=exclude,org/apache/lucene/index/IndexReader\$1,doBody -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeIndexes -XX:CompileCommand=exclude,org/alfresco/repo/search/impl/lucene/index/IndexInfo\$Merger,mergeDeletions"
-export PATH="$ALF_HOME/bin;${PATH}"
 #
 if [ "$1" = "start" ]; then
-  "$APPSERVER"/bin/startup.sh
+  "${APPSERVER}/bin/startup.sh"
 #  if [ -r ./virtual_start.sh ]; then
 #    sh ./virtual_start.sh
 #  fi
@@ -17,7 +18,7 @@ if [ "$1" = "start" ]; then
 #    sh ./start_oo.sh
 #  fi
 elif [ "$1" = "stop" ]; then
-  "$APPSERVER"/bin/shutdown.sh
+  "${APPSERVER}/bin/shutdown.sh"
 #  if [ -r ./virtual_start.sh ]; then
 #    sh ./virtual_stop.sh
 #  fi
