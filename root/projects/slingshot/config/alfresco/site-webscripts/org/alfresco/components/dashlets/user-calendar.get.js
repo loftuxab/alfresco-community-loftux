33e6adcfc3825e6c4f5f45e6d34716be0077e18a
mhatfield <mhatfield@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-09-05 13:08:58 +0000

    User calendar dashlet only shows future events
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@10782 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 20cb4fcc21bcf2556b740e78ef7678d48dc41535

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/dashlets/user-calendar.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/dashlets/user-calendar.get.js
@@ -1,4 +1,6 @@
 <import resource="classpath:alfresco/site-webscripts/org/alfresco/callutils.js">
 
-var uri = "/calendar/events/user";
+var now = new Date();
+var fromDate = now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDay();
+var uri = "/calendar/events/user?from=" + encodeURIComponent(fromDate);
 model.eventList = doGetCall(uri).events;
--- root/projects/slingshot/source/web/components/dashlets/mini-calendar.js
+++ root/projects/slingshot/source/web/components/dashlets/mini-calendar.js
@@ -113,7 +113,7 @@
    		var html = "", item;
    		if (events && events.length > 0)
    		{
-   			var title = Alfresco.util.formatDate(date, "fullDate");
+   			var title = Alfresco.util.formatDate(date, "ddd, d mmm yyyy");
 				var url = Alfresco.constants.URL_CONTEXT + "page/site/" + this.siteId + "/calendar?date=" + theDate;
    			html += '<div class="detail-list-item">'
    			html += '<div class="icon"><img src="' + Alfresco.constants.URL_CONTEXT + '/components/calendar/images/calendar-16.png" alt="day" /></div>'
