44c38891927be1e6a48d89b0e3365baf65d7ac76
mhatfield <mhatfield@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-09-07 18:19:20 +0000

    SLNG-1153: Calendar agenda view does not include today's events
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@10843 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: dd871ba5cd0c81f527428e6a903c63a94fc25e38

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/dashlets/user-calendar.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/dashlets/user-calendar.get.js
@@ -1,6 +1,6 @@
 <import resource="classpath:alfresco/site-webscripts/org/alfresco/callutils.js">
 
 var now = new Date();
-var fromDate = now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDay();
+var fromDate = now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDate();
 var uri = "/calendar/events/user?from=" + encodeURIComponent(fromDate);
 model.eventList = doGetCall(uri).events;
