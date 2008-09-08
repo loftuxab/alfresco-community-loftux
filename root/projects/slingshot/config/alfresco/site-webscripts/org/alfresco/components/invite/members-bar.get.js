33517518eead1b7b87f81023388058bfb59d9e45
gcornwell <gcornwell@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-09-08 20:26:37 +0000

    Fix for SLNG-1148: Site Members list exception when a non-site member accesses the page
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@10860 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 47b05c156dfc150643adc3b16a8b5409eedf7a33

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/invite/members-bar.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/invite/members-bar.get.js
@@ -3,11 +3,23 @@
 model.isManager = false;
     
 // Check the role of the user - only SiteManagers are allowed to invite people/view invites
-var obj = doGetCall("/api/sites/" + page.url.templateArgs.site + "/memberships/" + user.name);
-if (obj)
+var obj = doGetCall("/api/sites/" + page.url.templateArgs.site + "/memberships");
+
+var currentUser = user.name;
+var role = null;
+
+// iterate round the members of the site and see if current user is present
+// TODO: when we have a pattern for doing isXXX checks this should be refined
+for (var x = 0; x < obj.length; x++)
 {
-   if (obj.role == 'SiteManager')
+   if (obj[x].person.userName == currentUser)
    {
-      model.isManager = true;
+      role = obj[x].role;
    }
+}
+
+// set the isManager flag if appropriate
+if (role !== null && role == 'SiteManager')
+{
+   model.isManager = true;
 }
