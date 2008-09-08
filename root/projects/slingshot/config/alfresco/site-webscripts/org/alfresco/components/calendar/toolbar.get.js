b123ba5811cc0b1927a992da38c6e66b9571b16c
gcornwell <gcornwell@7033f55c-6c29-4ad8-afba-765cb616e738>
2008-09-08 14:56:41 +0000

    Fix for SLNG-1147: Calendar component exception when a non-site member accesses the Calendar page of a public site
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@10854 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: e98bae2795f3f6384e2df10da47e2d44495039e4

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/calendar/toolbar.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/calendar/toolbar.get.js
@@ -1,11 +1,28 @@
 <import resource="classpath:alfresco/site-webscripts/org/alfresco/callutils.js">
 
-// Check the role of the user - until there is a method of doing this check
-// on the web tier we have to make a call back to the repo to get this information.
-var obj = doGetCall("/api/sites/" + page.url.templateArgs.site + "/memberships/" + user.name);
-if (obj)
+// Check whether the current user is a member of the site first and then if they are
+// the role of the user - until there is a method of doing this check on the web tier 
+// we have to make a call back to the repo to get this information.
+
+var obj = doGetCall("/api/sites/" + page.url.templateArgs.site + "/memberships");
+
+var currentUser = user.name;
+var role = null;
+
+// iterate round the members of the site and see if current user is present
+// TODO: when we have a pattern for doing isXXX checks this should be refined
+for (var x = 0; x < obj.length; x++)
 {
-   model.role = obj.role;
+   if (obj[x].person.userName == currentUser)
+   {
+      role = obj[x].role;
+   }
+}
+
+// set role appropriately
+if (role !== null)
+{
+   model.role = role;
 }
 else
 {
