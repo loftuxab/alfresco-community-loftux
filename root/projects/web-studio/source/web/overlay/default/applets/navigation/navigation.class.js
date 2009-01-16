526df2ca9da278bd03f1a90f05e2f6e8df3ddf0e
uzi <uzi@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-01-16 15:17:21 +0000

    Fix for ALFCOM-2196 (Child pages are removed if user remove parent page) - found a case where the 'remove' button can remain active (when it should be disabled)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@12833 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/web-studio/source/web/overlay/default/applets/navigation/navigation.class.js
+++ root/projects/web-studio/source/web/overlay/default/applets/navigation/navigation.class.js
@@ -97,6 +97,13 @@ WebStudio.Applets.Navigation.prototype.bindSliderControl = function(container)
 					childDiv.ondblclick = _this.nodeDoubleClickHandler.bind(childNode);
 				}
 			}
+			
+			// if this has child nodes, block the "remove" button
+			_this.treeView.menu.setDisabled('roots', _this.INDEX_BUTTON_DELETE);
+			if(node.children.length == 0)
+			{
+				_this.treeView.menu.setEnabled('roots', _this.INDEX_BUTTON_DELETE);
+			}
 		});
 		
 		// set up a handler for changes to active node state
