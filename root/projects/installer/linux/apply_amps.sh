3494fdf5fba779315ef55b28324d9fc885f9a1fe
mrogers <mrogers@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-03-16 12:26:40 +0000

    ALFCOM-2622 - Use of "read -p" in apply_amps.sh fails on some Unix flavors
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@13635 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/installer/linux/apply_amps.sh
+++ root/projects/installer/linux/apply_amps.sh
@@ -5,11 +5,13 @@
 export CATALINA_HOME=tomcat
 echo "This script will apply all the AMPs in ./amps to the alfresco.war file in $CATALINA_HOME\webapps"
 echo "Press control-c to stop this script . . ."
-read -p "Press any other key to continue . . ."
+echo "Press any other key to continue . . ."
+read RESP
 java -jar bin/alfresco-mmt.jar install ./amps $CATALINA_HOME/webapps/alfresco.war -directory
 java -jar bin/alfresco-mmt.jar list $CATALINA_HOME/webapps/alfresco.war
 echo "About to clean out tomcat/webapps/alfresco directory and temporary files..."
 echo "Press control-c to stop this script . . ."
-read -p "Press any other key to continue . . ."
+echo "Press any other key to continue . . ."
+read DUMMY
 rm -rf tomcat/webapps/alfresco
 sh ./bin/clean_tomcat.sh
