e27142276b20bfa7f202b8e3490e4ac4e963893b
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-04-03 14:03:07 +0000

    MOB-622 - View User Details in Users Admin Console.  - everything apart from account enabled/disabled and Groups displayed. Consolidation of some common cm:content and cm:person labels into slingshot messages bundle. YUI history manager used to allow Back+Forward navigation in Users Admin Console tool. Improvements to Person Get REST API to retrieve additional user properties. Some CSS fixes to Admin Console and User Profile.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@13826 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/source/web/components/console/users.js
+++ root/projects/slingshot/source/web/components/console/users.js
@@ -59,7 +59,18 @@
       Alfresco.util.ComponentManager.register(this);
       
       /* Load YUI Components */
-      Alfresco.util.YUILoaderHelper.require(["button", "container", "datasource", "datatable", "json"], this.onComponentsLoaded, this);
+      Alfresco.util.YUILoaderHelper.require(["button", "container", "datasource", "datatable", "json", "history"], this.onComponentsLoaded, this);
+      
+      /* Decoupled event listeners */
+      YAHOO.Bubbling.on("viewUserClick", this.onViewUserClick, this);
+      
+      /* History navigation events */
+      YAHOO.Bubbling.on("searchChanged", this.onSearchChanged, this);
+      YAHOO.Bubbling.on("panelChanged", this.onPanelChanged, this);
+      YAHOO.Bubbling.on("userIdChanged", this.onUserIdChanged, this);
+      
+      /* UI panels - keep up-to-date with top level div IDs */
+      this.panels = ["search", "view"];
       
       return this;
    }
@@ -102,13 +113,37 @@
       widgets: {},
       
       /**
+       * List of the available UI panels; such as Search, View, Edit etc.
+       * 
+       * @property panels
+       * @type array
+       */
+      panels: [],
+      
+      /**
+       * The current UI panel on display
+       * 
+       * @property currentPanel
+       * @type string
+       */
+      currentPanel: "",
+      
+      /**
+       * Current user id for an action.
+       * 
+       * @property currentUserId
+       * @type string
+       */
+      currentUserId: "",
+      
+      /**
        * Current search term, obtained from form input field.
        * 
        * @property searchTerm
        * @type string
        */
       searchTerm: "",
-
+      
       /**
        * Set multiple initialization options at once.
        *
@@ -154,12 +189,73 @@
        */
       onReady: function ConsoleUsers_onReady()
       {  
+         // YUI History
+         var bookmarkedSearch = YAHOO.util.History.getBookmarkedState("search") || "";
+         var bookmarkedPanel = YAHOO.util.History.getBookmarkedState("panel") || "search";
+         var bookmarkedUserId = YAHOO.util.History.getBookmarkedState("userid") || "";
+         
+         // init from the history
+         // note that the current panel is initialised and stored later
+         this.searchTerm = bookmarkedSearch;
+         this.currentUserId = bookmarkedUserId;
+         
+         // Register History Manager callbacks
+         YAHOO.util.History.register("panel", bookmarkedPanel, function CU_onHistoryManagerPanelChanged(newPanel)
+         {
+            YAHOO.Bubbling.fire("panelChanged",
+            {
+               panel: newPanel
+            });
+         });
+         YAHOO.util.History.register("search", bookmarkedSearch, function CU_onHistoryManagerSearchChanged(newTerm)
+         {
+            YAHOO.Bubbling.fire("searchChanged",
+            {
+               search: newTerm
+            });
+         });
+         YAHOO.util.History.register("userid", bookmarkedUserId, function CU_onHistoryManagerUserIdChanged(newUserId)
+         {
+            YAHOO.Bubbling.fire("userIdChanged",
+            {
+               userid: newUserId
+            });
+         });
+         
+         // Continue only when History Manager fires its onReady event
+         YAHOO.util.History.onReady(this.onHistoryManagerReady, this, true);
+         
+         // Initialize the browser history management library
+         try
+         {
+             YAHOO.util.History.initialize("yui-history-field", "yui-history-iframe");
+         }
+         catch(e)
+         {
+            /*
+             * The only exception that gets thrown here is when the browser is
+             * not supported (Opera, or not A-grade)
+             */
+            Alfresco.logger.debug("Alfresco.ConsoleUsers: Couldn't initialize HistoryManager.", e.toString());
+            this.onHistoryManagerReady();
+         }
+      },
+   
+      /**
+       * Fired by YUI when History Manager is initialised and available for scripting.
+       * Component initialisation, including instantiation of YUI widgets and event listener binding.
+       *
+       * @method onHistoryManagerReady
+       */
+      onHistoryManagerReady: function ConsoleUsers_onHistoryManagerReady()
+      {
          var me = this;
          
-         // Search button
+         // Buttons
          this.widgets.searchButton = Alfresco.util.createYUIButton(this, "search-button", this.onSearchClick);
-         // New User button
          this.widgets.newuserButton = Alfresco.util.createYUIButton(this, "newuser-button", this.onNewUserClick);
+         this.widgets.deleteuserButton = Alfresco.util.createYUIButton(this, "deleteuser-button", this.onDeleteUserClick);
+         this.widgets.edituserButton = Alfresco.util.createYUIButton(this, "edituser-button", this.onEditUserClick);
          
          // DataTable and DataSource setup
          this.widgets.dataSource = new YAHOO.util.DataSource(Alfresco.constants.PROXY_URI + "api/people");
@@ -240,8 +336,30 @@
             correctScope: true
          }, "keydown").enable();
          
-         // Set initial focus
-         searchText.focus();
+         // display the initial panel based on history or default (which is "search")
+         var bookmarkedPanel = YAHOO.util.History.getBookmarkedState("panel") || "search";
+         YAHOO.Bubbling.fire("panelChanged",
+         {
+            panel: bookmarkedPanel
+         });
+         
+         //
+         // TODO: add panel handlers and this section can be removed... should happen in panelChanged?
+         //
+         if (bookmarkedPanel === "search" && this.searchTerm !== "")
+         {
+            YAHOO.Bubbling.fire("searchChanged",
+            {
+               search: this.searchTerm
+            });
+         }
+         if (bookmarkedPanel === "view" && this.currentUserId !== "")
+         {
+            YAHOO.Bubbling.fire("userIdChanged",
+            {
+               userid: this.currentUserId
+            });
+         }
       },
       
       /**
@@ -291,7 +409,8 @@
             var lastName = oRecord.getData("lastName");
             var name = firstName + ' ' + (lastName ? lastName : "");
             
-            elCell.innerHTML = $html(name);
+            // fire the 'viewUserClick' event when the selected user in the list has changed
+            elCell.innerHTML = "<a href='#' onclick=\"YAHOO.Bubbling.fire('viewUserClick', {username: '" + oRecord.getData("userName") + "'}); return false;\">" + $html(name) + "</a>";
          };
          
          /**
@@ -384,19 +503,137 @@
        * YUI WIDGET EVENT HANDLERS
        * Handlers for standard events fired from YUI widgets, e.g. "click"
        */
-
+      
+      /**
+       * Panel changed history manager event handler
+       *
+       * @method onPanelChanged
+       * @param e {object} DomEvent
+       * @param args {array} Event parameters (depends on event type)
+       */
+      onPanelChanged: function ConsoleUsers_onPanelChanged(e, args)
+      {
+         var panel = args[1].panel;
+         
+         this._showPanel(panel);
+      },
+      
+      /**
+       * Search changed history manager event handler
+       *
+       * @method onSearchChanged
+       * @param e {object} DomEvent
+       * @param args {array} Event parameters (depends on event type)
+       */
+      onSearchChanged: function ConsoleUsers_onSearchChanged(e, args)
+      {
+         var searchTerm = args[1].search;
+         
+         // update the text field - as this event could come from bookmark, navigation or a search button click
+         var searchTermElem = Dom.get(this.id + "-search-text");
+         searchTermElem.value = searchTerm;
+         
+         this._performSearch(searchTerm);
+      },
+      
+      /**
+       * UserId changed history manager event handler
+       *
+       * @method onUserIdChanged
+       * @param e {object} DomEvent
+       * @param args {array} Event parameters (depends on event type)
+       */
+      onUserIdChanged: function ConsoleUsers_onUserIdChanged(e, args)
+      {
+         var userid = args[1].userid;
+         
+         // keep track of current user id
+         this.currentUserId = userid;
+         
+         //
+         // TODO: panel handlers!
+         //
+         if (this.currentPanel === "view")
+         {
+            var me = this;
+            var success = function(res)
+            {
+               var fnSetter = function(id, val)
+               {
+                  Dom.get(me.id + id).innerHTML = val ? $html(val) : "";
+               };
+               
+               var person = YAHOO.lang.JSON.parse(res.serverResponse.responseText);
+               
+               // apply avatar image URL
+               var photos = Dom.getElementsByClassName("photoimg", "img");
+               for (i in photos)
+               {
+                  photos[i].src = person.avatar ?
+                        Alfresco.constants.PROXY_URI + person.avatar + "?c=force" :
+                        Alfresco.constants.URL_CONTEXT + "components/images/no-user-photo-64.png";
+               }
+               
+               // apply to person fields to view
+               var firstName = person.firstName;
+               var lastName = person.lastName;
+               var fullName = firstName + ' ' + (lastName ? lastName : "");
+               fnSetter("-view-title", fullName);
+               fnSetter("-view-name", fullName);
+               fnSetter("-view-jobtitle", person.jobtitle);
+               fnSetter("-view-organization", person.organization);
+               fnSetter("-view-location", person.location);
+               fnSetter("-view-email", person.email);
+               fnSetter("-view-telephone", person.telephone);
+               fnSetter("-view-mobile", person.mobile);
+               fnSetter("-view-companyname", person.organization);
+               // build the company address up and set manually - encoding each value
+               var addr = "";
+               addr += person.companyaddress1 ? ($html(person.companyaddress1) + "<br/>") : "";
+               addr += person.companyaddress2 ? ($html(person.companyaddress2) + "<br/>") : "";
+               addr += person.companyaddress3 ? ($html(person.companyaddress3) + "<br/>") : "";
+               addr += person.companypostcode ? ($html(person.companypostcode) + "<br/>") : "";
+               Dom.get(me.id + "-view-companyaddress").innerHTML = addr;
+               fnSetter("-view-companytelephone", person.companytelephone);
+               fnSetter("-view-companyfax", person.companyfax);
+               fnSetter("-view-companyemail", person.companyemail);
+               fnSetter("-view-skype", person.skype);
+               fnSetter("-view-instantmsg", person.instantmsg);
+               fnSetter("-view-quota", (person.quota > 0 ? Alfresco.util.formatFileSize(person.quota) : ""));
+               fnSetter("-view-usage", Alfresco.util.formatFileSize(person.sizeCurrent));
+               // biography is a special html field
+               var bio = person.persondescription ? person.persondescription : "";
+               Dom.get(me.id + "-view-bio").innerHTML = Alfresco.util.stripUnsafeHTMLTags(bio).replace(/\n/g, "<br/>");;
+            };
+            
+            // make an ajax call to get user details
+            Alfresco.util.Ajax.request(
+            {
+               url: Alfresco.constants.PROXY_URI + "api/people/" + encodeURIComponent(userid),
+               method: Alfresco.util.Ajax.GET,
+               successCallback:
+               {
+                  fn: success,
+                  scope: this
+               },
+               failureMessage: this._msg("message.getuser-failure", userid)   
+            });
+         }
+      },
+      
       /**
        * Search button click event handler
        *
        * @method onSearchClick
        * @param e {object} DomEvent
-       * @param p_obj {object} Object passed back from addListener method
+       * @param args {array} Event parameters (depends on event type)
        */
-      onSearchClick: function ConsoleUsers_onSearchClick(e, p_obj)
+      onSearchClick: function ConsoleUsers_onSearchClick(e, args)
       {
          var searchTermElem = Dom.get(this.id + "-search-text");
          var searchTerm = searchTermElem.value;
          
+         // inform the user if the search term entered is too small
          if (searchTerm.length < this.options.minSearchTermLength)
          {
             Alfresco.util.PopupManager.displayMessage(
@@ -406,7 +643,7 @@
             return;
          }
          
-         this._performSearch(searchTerm);
+         YAHOO.util.History.multiNavigate({panel: "search", "search": searchTerm});
       },
       
       /**
@@ -414,11 +651,49 @@
        *
        * @method onNewUserClick
        * @param e {object} DomEvent
-       * @param p_obj {object} Object passed back from addListener method
+       * @param args {array} Event parameters (depends on event type)
        */
-      onNewUserClick: function ConsoleUsers_onNewUserClick(e, p_obj)
+      onNewUserClick: function ConsoleUsers_onNewUserClick(e, args)
       {
          
+      },
+      
+      /**
+       * Delete User button click event handler
+       *
+       * @method onDeleteUserClick
+       * @param e {object} DomEvent
+       * @param args {array} Event parameters (depends on event type)
+       */
+      onDeleteUserClick: function ConsoleUsers_onDeleteUserClick(e, args)
+      {
+         
+      },
+      
+      /**
+       * Edit User button click event handler
+       *
+       * @method onEditUserClick
+       * @param e {object} DomEvent
+       * @param args {array} Event parameters (depends on event type)
+       */
+      onEditUserClick: function ConsoleUsers_onEditUserClick(e, args)
+      {
+         
+      },
+      
+      /**
+       * View User event handler
+       *
+       * @method onViewUserClick
+       * @param e {object} DomEvent
+       * @param args {array} Event parameters (depends on event type)
+       */
+      onViewUserClick: function ConsoleUsers_onViewUserClick(e, args)
+      {
+         var userid = args[1].username;
+         
+         YAHOO.util.History.multiNavigate({panel: "view", userid: userid});
       },
 
       /**
@@ -452,50 +727,54 @@
          // keep track of the last search performed
          this.searchTerm = searchTerm;
          
-         // Reset the custom error messages
-         this._setDefaultDataTableErrors(this.widgets.dataTable);
-         
-         // Don't display any message
-         this.widgets.dataTable.set("MSG_EMPTY", this._msg("message.searching"));
-         
-         // Empty results table
-         this.widgets.dataTable.deleteRows(0, this.widgets.dataTable.getRecordSet().getLength());
-         
-         var successHandler = function ConsoleUsers__ps_successHandler(sRequest, oResponse, oPayload)
+         // check search length again as we may have got here via history navigation
+         if (searchTerm.length >= this.options.minSearchTermLength)
          {
+            // Reset the custom error messages
             this._setDefaultDataTableErrors(this.widgets.dataTable);
-            this.widgets.dataTable.onDataReturnInitializeTable.call(this.widgets.dataTable, sRequest, oResponse, oPayload);
-         }
-         
-         var failureHandler = function ConsoleUsers__ps_failureHandler(sRequest, oResponse)
-         {
-            if (oResponse.status == 401)
+            
+            // Don't display any message
+            this.widgets.dataTable.set("MSG_EMPTY", this._msg("message.searching"));
+            
+            // Empty results table
+            this.widgets.dataTable.deleteRows(0, this.widgets.dataTable.getRecordSet().getLength());
+            
+            var successHandler = function ConsoleUsers__ps_successHandler(sRequest, oResponse, oPayload)
             {
-               // Our session has likely timed-out, so refresh to offer the login page
-               window.location.reload();
+               this._setDefaultDataTableErrors(this.widgets.dataTable);
+               this.widgets.dataTable.onDataReturnInitializeTable.call(this.widgets.dataTable, sRequest, oResponse, oPayload);
             }
-            else
+            
+            var failureHandler = function ConsoleUsers__ps_failureHandler(sRequest, oResponse)
             {
-               try
+               if (oResponse.status == 401)
                {
-                  var response = YAHOO.lang.JSON.parse(oResponse.responseText);
-                  this.widgets.dataTable.set("MSG_ERROR", response.message);
-                  this.widgets.dataTable.showTableMessage(response.message, YAHOO.widget.DataTable.CLASS_ERROR);
-                  this._setResultsMessage("message.noresults");
+                  // Our session has likely timed-out, so refresh to offer the login page
+                  window.location.reload();
                }
-               catch(e)
+               else
                {
-                  this._setDefaultDataTableErrors(this.widgets.dataTable);
+                  try
+                  {
+                     var response = YAHOO.lang.JSON.parse(oResponse.responseText);
+                     this.widgets.dataTable.set("MSG_ERROR", response.message);
+                     this.widgets.dataTable.showTableMessage(response.message, YAHOO.widget.DataTable.CLASS_ERROR);
+                     this._setResultsMessage("message.noresults");
+                  }
+                  catch(e)
+                  {
+                     this._setDefaultDataTableErrors(this.widgets.dataTable);
+                  }
                }
             }
-         }
-         
-         this.widgets.dataSource.sendRequest(this._buildSearchParams(searchTerm),
-         {
+            
+            this.widgets.dataSource.sendRequest(this._buildSearchParams(searchTerm),
+            {
                success: successHandler,
                failure: failureHandler,
                scope: this
-         });
+            });
+         }
       },
 
       /**
@@ -521,6 +800,38 @@
       {
          var resultsDiv = Dom.get(this.id + "-search-bar");
          resultsDiv.innerHTML = this._msg(messageId, arg1, arg2);
+      },
+      
+      /**
+       * Make the specified panel visible - hiding any others
+       * 
+       * @method _showPanel
+       * @param panel {string} The panel to make visible
+       * @private
+       */
+      _showPanel: function ConsoleUsers__showPanel(panel)
+      {
+         if (this.currentPanel !== panel)
+         {
+            this.currentPanel = panel;
+            for (var index in this.panels)
+            {
+               var _panel = this.panels[index];
+               if (_panel !== panel)
+               {
+                  Dom.setStyle(this.id + "-" + _panel, "display", "none");
+               }
+            }
+            Dom.setStyle(this.id + "-" + panel, "display", "block");
+         }
+         
+         //
+         // TODO: add panel handlers and this can be removed
+         //
+         if ("search" === panel)
+         {
+            Dom.get(this.id + "-search-text").focus();
+         }
       },
       
       /**
