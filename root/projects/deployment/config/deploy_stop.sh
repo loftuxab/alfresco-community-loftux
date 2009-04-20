9d10942738f291facade995684138e685bd4a4ff
mrogers <mrogers@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-04-20 09:24:50 +0000

    ETHREEOH-1870
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@14004 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 7561f5dae089579075ebe1c7bb040075cc257e10

--- root/projects/deployment/config/deploy_stop.sh
+++ root/projects/deployment/config/deploy_stop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -server -cp alfresco-deployment.jar:spring-2.0.2.jar:commons-logging-1.0.4.jar:alfresco-core.jar:jug.jar:. org.alfresco.deployment.Main shutdown-context.xml
+java -server -Djava.ext.dirs=. org.alfresco.deployment.Main shutdown-context.xml
