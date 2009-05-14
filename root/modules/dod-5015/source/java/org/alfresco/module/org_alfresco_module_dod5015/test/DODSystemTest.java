f5d5c456f1a9148bd8b3ea19717c13a6fb356838
nmcerlean <nmcerlean@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-05-14 12:56:19 +0000

    Added test code for RM and renamed a RMModel field.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@14318 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/test/DODSystemTest.java
+++ root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/test/DODSystemTest.java
@@ -27,6 +27,7 @@ package org.alfresco.module.org_alfresco_module_dod5015.test;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.alfresco.model.ContentModel;
@@ -93,22 +94,6 @@ public class DODSystemTest extends BaseSpringTest
         filePlan = TestUtilities.loadFilePlanData(null, this.nodeService, this.importService);
 	}
 	
-	private NodeRef getRecordCategory(String seriesName, String categoryName)
-	{
-	    SearchParameters searchParameters = new SearchParameters();
-	    searchParameters.addStore(SPACES_STORE);
-	    String query = "PATH:\"rma:filePlan/cm:" + ISO9075.encode(seriesName) + "/cm:" + ISO9075.encode(categoryName) + "\"";
-	    System.out.println("Query: " + query);
-	    searchParameters.setQuery(query);
-	    searchParameters.setLanguage(SearchService.LANGUAGE_LUCENE);
-	    ResultSet rs = this.searchService.query(searchParameters);
-	    
-        //setComplete();
-        //endTransaction();
-	    
-	    return rs.getNodeRef(0);
-	}
-	
 	public void testBasicFileingTest()
 	{
 	    // Get a record category to file into
@@ -155,7 +140,131 @@ public class DODSystemTest extends BaseSpringTest
 	    // TODO commit to check that the filed record can be created correctly
 	    
 	}
-        
-        
+	
+	/**
+	 * This test case reads several sample Record Folders from the filePlan to ensure
+	 * that they have been imported correctly.
+	 */
+	public void testReadRecordFolders()
+	{
+	    //TODO May delete this test once things are up and running.
+        NodeRef recordFolder = getRecordFolder("Reports", "AIS Audit Records", "January AIS Audit Records");     
+        assertNotNull(recordFolder);
 
+        // Include this as it has brackets in its name.
+        recordFolder = getRecordFolder("Miscellaneous Files", "Civilian Employee Training Program Records", "Chuck Stevens Training Records (2008)");
+        assertNotNull(recordFolder);
+        
+        // Include this as it has a slash in its name.
+        recordFolder = getRecordFolder("Miscellaneous Files", "Monthly Cockpit Crew Training", "January Cockpit/Crew Training");     
+        assertNotNull(recordFolder);
+	}
+	
+	/**
+	 * This test case reads all the record categories under the spaces store and asserts
+	 * that each has a cm:description and that it is non-null and has non-whitespace
+	 * content.
+	 */
+    public void testRecordCategoryDescriptions()
+    {
+        // See Table 2-1.8 in DoD 5015.02-STD v3 Baseline RMA Compliance Test Procedures
+        List<NodeRef> recordCategories = this.getAllRecordCategories();
+        assertNotNull(recordCategories);
+        
+        for (NodeRef recordCategory : recordCategories)
+        {
+            Map<QName, Serializable> props = nodeService.getProperties(recordCategory);
+            final Serializable recCatDescription = props.get(ContentModel.PROP_DESCRIPTION);
+            assertNotNull(recCatDescription);
+            assertTrue(recCatDescription.toString().trim().length() > 0);
+        }
+        
+        // This test formerly tested a single RecordCategory like so:
+        // NodeRef recordCategory = this.getRecordCategory("Miscellaneous Files", "Civilian Employee Training Program Records");
+    }
+    
+    public void testFileRecordInAFolder()
+    {
+        // The below recordFolder is in a category with a review-period.
+        NodeRef recordFolder = getRecordFolder("Reports", "AIS Audit Records", "January AIS Audit Records");     
+        assertNotNull(recordFolder);
+        
+        // Create the document
+        NodeRef newRecord = this.nodeService.createNode(recordFolder, 
+                                                        ContentModel.ASSOC_CONTAINS, 
+                                                        QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "TestRecord.txt"), 
+                                                        ContentModel.TYPE_CONTENT).getChildRef();
+        
+        // Set the content
+        ContentWriter writer = this.contentService.getWriter(newRecord, ContentModel.PROP_CONTENT, true);
+        writer.setMimetype(MimetypeMap.MIMETYPE_TEXT_PLAIN);
+        writer.setEncoding("UTF-8");
+        writer.putContent("There is some content in this record");
+        
+        // Checked that the document has been marked as incomplete
+        //assertTrue(this.nodeService.hasAspect(recordOne, RecordsManagementModel.ASPECT_INCOMPLETE_RECORD));       
+        
+        // File a document in the record category
+        Map<String, Serializable> stateContext = new HashMap<String, Serializable>(5);
+        stateContext.put("fileableNode", recordFolder);
+        Map<String, Serializable> propValues = new HashMap<String, Serializable>(5);
+        propValues.put(RecordsManagementModel.PROP_PUBLICATION_DATE.toString(), new Date());
+        propValues.put(RecordsManagementModel.PROP_SUPPLEMENTAL_MARKING_LIST.toString(), "markingListValue");
+        propValues.put(RecordsManagementModel.PROP_MEDIA_TYPE.toString(), "mediaTypeValue"); 
+        propValues.put(RecordsManagementModel.PROP_FORMAT.toString(), "formatValue"); 
+        propValues.put(RecordsManagementModel.PROP_DATE_RECEIVED.toString(), new Date());
+        stateContext.put("recordProperties", (Serializable)propValues);
+        rmService.addRecordState(newRecord, "filed", stateContext);
+        
+        // Check the values of the filed record
+        assertNotNull(this.nodeService.getProperty(newRecord, RecordsManagementModel.PROP_REVIEW_AS_OF));        
+    }
+
+    /**
+     * Gets all Record Categories under the SPACES_STORE.
+     * @return
+     */
+    private List<NodeRef> getAllRecordCategories()
+    {
+        String typeQuery = "TYPE:\"" + RecordsManagementModel.TYPE_RECORD_CATEGORY + "\"";
+        ResultSet types = this.searchService.query(SPACES_STORE, SearchService.LANGUAGE_LUCENE, typeQuery);
+        
+        return types.getNodeRefs();
+    }
+
+    private NodeRef getRecordCategory(String seriesName, String categoryName)
+    {
+        SearchParameters searchParameters = new SearchParameters();
+        searchParameters.addStore(SPACES_STORE);
+        
+        String query = "PATH:\"rma:filePlan/cm:" + ISO9075.encode(seriesName) + "/cm:" + ISO9075.encode(categoryName) + "\"";
+
+        searchParameters.setQuery(query);
+        searchParameters.setLanguage(SearchService.LANGUAGE_LUCENE);
+        ResultSet rs = this.searchService.query(searchParameters);
+        
+        //setComplete();
+        //endTransaction();
+        
+        final NodeRef result = rs.getNodeRef(0);
+        return result;
+    }
+    
+    private NodeRef getRecordFolder(String seriesName, String categoryName, String folderName)
+    {
+        SearchParameters searchParameters = new SearchParameters();
+        searchParameters.addStore(SPACES_STORE);
+        String query = "PATH:\"rma:filePlan/cm:" + ISO9075.encode(seriesName)
+            + "/cm:" + ISO9075.encode(categoryName)
+            + "/cm:" + ISO9075.encode(folderName) + "\"";
+        System.out.println("Query: " + query);
+        searchParameters.setQuery(query);
+        searchParameters.setLanguage(SearchService.LANGUAGE_LUCENE);
+        ResultSet rs = this.searchService.query(searchParameters);
+        
+        //setComplete();
+        //endTransaction();
+        
+        return rs.getNodeRef(0);
+    }
 }
