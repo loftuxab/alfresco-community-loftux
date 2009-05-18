b22931b84128b809333bb54688bcc61fce902fa0
jvonka <jvonka@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-05-18 14:57:17 +0000

    WCM - minor: remove deprecated warnings (and cleanup imports)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@14349 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/link-validation/source/java/org/alfresco/linkvalidation/LinkValidationServiceImpl.java
+++ root/projects/link-validation/source/java/org/alfresco/linkvalidation/LinkValidationServiceImpl.java
@@ -1,27 +1,29 @@
-/*-----------------------------------------------------------------------------
-*  Copyright 2007 Alfresco Inc.
-*
-*  This program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful, but
-*  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-*  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-*  for more details.
-*
-*  You should have received a copy of the GNU General Public License along
-*  with this program; if not, write to the Free Software Foundation, Inc.,
-*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  As a special
-*  exception to the terms and conditions of version 2.0 of the GPL, you may
-*  redistribute this Program in connection with Free/Libre and Open Source
-*  Software ("FLOSS") applications as described in Alfresco's FLOSS exception.
-*  You should have received a copy of the text describing the FLOSS exception,
-*  and it is also available here:   http://www.alfresco.com/legal/licensing
-*
-*
-*  Author  Jon Cox  <jcox@alfresco.com>
+/*
+ * Copyright (C) 2005-2009 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have recieved a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing"
+ */
+/*---------------------------------------------------------------------------
+* Author  Jon Cox  <jcox@alfresco.com>
 *  File    LinkValidationServiceImpl.java
 *----------------------------------------------------------------------------*/
 
@@ -65,7 +67,6 @@ import org.alfresco.repo.domain.PropertyValue;
 import org.alfresco.repo.security.authentication.AuthenticationContext;
 import org.alfresco.repo.transaction.RetryingTransactionHelper;
 import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
-import org.alfresco.sandbox.SandboxConstants;
 import org.alfresco.service.cmr.attributes.AttrAndQuery;
 import org.alfresco.service.cmr.attributes.AttrQueryGTE;
 import org.alfresco.service.cmr.attributes.AttrQueryLTE;
@@ -81,6 +82,7 @@ import org.alfresco.util.MD5;
 import org.alfresco.util.NameMatcher;
 import org.alfresco.util.Pair;
 import org.alfresco.util.VmShutdownListener;
+import org.alfresco.wcm.sandbox.SandboxConstants;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
