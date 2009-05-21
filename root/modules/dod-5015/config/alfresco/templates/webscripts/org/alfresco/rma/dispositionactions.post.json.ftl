a1e4668ae5d8d35ed3564e866a88043e806dd8d1
gcornwell <gcornwell@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-05-21 12:32:10 +0000

    - Fixed fallout of RM model change including the addition of a specialised control to represent the disposition actions of a folder category - Some minor changes to RM model (labels and typos)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@14390 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 2d7231543f589cc23d6f86e35934e7d3868e88ea

---
+++ root/modules/dod-5015/config/alfresco/templates/webscripts/org/alfresco/rma/dispositionactions.post.json.ftl
@@ -0,0 +1,19 @@
+{
+   "data":
+   {
+      "items":
+      [
+      <#list results as row>
+         {
+            "action": "${row.item.properties["rma:dispositionAction"]!""}",
+            "description": "${row.item.properties["rma:dispositionDescription"]!""}",
+            "period": "${row.item.properties["rma:dispositionPeriod"]!""}",
+            "periodproperty": "${row.item.properties["rma:dispositionPeriodProperty"]!""}",
+            "event": "${row.item.properties["rma:dispositionEvent"]!""}",
+            "trigger": "${row.item.properties["rma:dispositionEventTrigger"]!""}",
+            "nodeRef": "${row.item.nodeRef}"
+         }<#if row_has_next>,</#if>
+      </#list>
+      ]
+   }
+}
---
+++ root/modules/dod-5015/config/alfresco/templates/webscripts/org/alfresco/rma/dispositionactions.post.json.js
@@ -0,0 +1,41 @@
+function main()
+{
+   var count = 0,
+      items = [],
+      results = [];
+
+   // extract mandatory data from request body
+   if (!json.has("items"))
+   {
+       status.setCode(status.STATUS_BAD_REQUEST, "items parameter is not present");
+       return;
+   }
+   
+   // convert the JSONArray object into a native JavaScript array
+   var jsonItems = json.get("items"),
+      numItems = jsonItems.length(),
+      item, result, resultObj;
+   
+   for (count = 0; count < numItems; count++)
+   {
+      item = jsonItems.get(count);
+      if (item != "")
+      {
+         result = search.findNode(item);
+         if (result != null)
+         {
+            results.push(
+            {
+               item: result
+            });
+         }
+      }
+   }
+
+   if (logger.isLoggingEnabled())
+       logger.log("#disposition action items = " + count + ", #results = " + results.length);
+
+   model.results = results;
+}
+
+main();
