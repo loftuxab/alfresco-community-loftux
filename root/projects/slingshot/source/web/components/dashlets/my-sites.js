1a00481c17399e9e2db4808688ccf9a4017cd731
lawrencec <lawrencec@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-06-02 14:15:52 +0000

    MOB-490 - delete action now inline on hover
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@14503 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: bc68d58061c0f4d94cb23a3bc27eeb0f646ed947

--- root/projects/slingshot/source/web/components/dashlets/my-sites.js
+++ root/projects/slingshot/source/web/components/dashlets/my-sites.js
@@ -136,7 +136,7 @@
       {
          // Listen on clicks for the create site link
          Event.addListener(this.id + "-createSite-button", "click", this.onCreateSiteLinkClick, this, true);
-
+         
          // Listen on clicks for delete site icons
          var sites = this.options.sites, i, j;
          for (i = 0, j = sites.length; i < j; i++)
@@ -148,6 +148,10 @@
             this._addFavouriteHandling(i);
             this._addImapFavouriteHandling(i);
          }
+         // initialize the mouse over listener
+         Alfresco.util.rollover.registerHandlerFunctions(this.id, this.onSiteElementMouseEntered, this.onSiteElementMouseExited, this);
+         // init mouse over
+         Alfresco.util.rollover.registerListenersByClassName(this.id, 'detail-list-item', 'div');         
       },
 
       /**
@@ -377,6 +381,125 @@
          // Hide the site in this component
          var site = args[1].site;
          Dom.setStyle(this.id + "-site-div-" + site.shortName, "display", "none");
-      }      
+      },
+      /** Called when the mouse enters into a list item. */
+      onSiteElementMouseEntered: function SiteList_onCommentElementMouseEntered(layer, args)
+      {
+         var elem = args[1].target;
+         Dom.addClass(elem, 'over');
+      },
+      
+      /** Called whenever the mouse exits a list item. */
+      onSiteElementMouseExited: function SiteList_onCommentElementMouseExited(layer, args)
+      {
+         var elem = args[1].target;
+         Dom.removeClass(elem, 'over');
+      }
    };
 })();
+Alfresco.util.rollover = {};
+
+/**
+ * Attaches mouseover/exit event listener to the passed element.
+ * 
+ * @method Alfresco.util.rollover._attachRolloverListener
+ * @param elem the element to which to add the listeners
+ * @param mouseOverEventName the bubble event name to fire for mouse enter events
+ * @param mouseOutEventName the bubble event name to fire for mouse out events
+ */
+Alfresco.util.rollover._attachRolloverListener = function(elem, mouseOverEventName, mouseOutEventName)
+{  
+   var eventElem = elem, relTarg;
+     
+   var mouseOverHandler = function(e)
+   {
+      // find the correct target element and check whether we only moved between
+      // subelements of the hovered element
+      if (!e)
+      {
+         e = window.event;
+      }
+      relTarg = (e.relatedTarget !== undefined) ? e.relatedTarget : e.fromElement;
+      while (relTarg && relTarg != eventElem && relTarg.nodeName != 'BODY')
+      {
+         relTarg = relTarg.parentNode;
+      }
+      if (relTarg == eventElem)
+      {
+         return;
+      }
+    
+      // the mouse entered the element, fire an event to inform about it
+      YAHOO.Bubbling.fire(mouseOverEventName,
+      {
+         event: e,
+         target: eventElem
+      });
+   };
+ 
+   var mouseOutHandler = function(e)
+   {
+      // find the correct target element and check whether we only moved between
+      // subelements of the hovered element
+      if (!e)
+      {
+         e = window.event;         
+      }
+      relTarg = (e.relatedTarget !== undefined) ? e.relatedTarget : e.toElement;
+      while (relTarg !== null && relTarg != eventElem && relTarg.nodeName != 'BODY')
+      {
+         relTarg = relTarg.parentNode;
+      }
+      if (relTarg == eventElem)
+      {
+         return;
+      }
+     
+      // the mouse exited the element, fire an event to inform about it
+      YAHOO.Bubbling.fire(mouseOutEventName,
+      {
+         event: e,
+         target: eventElem
+      });
+   };
+ 
+   YAHOO.util.Event.addListener(elem, 'mouseover', mouseOverHandler);
+   YAHOO.util.Event.addListener(elem, 'mouseout', mouseOutHandler);
+};
+
+/**
+ * Register rollover listeners to elements identified by a class and tag name.
+ * 
+ * @param htmlId the id of the component for which the listeners get registered.
+ *        This id is used to distinguish events from different components.
+ * @param className the class name of elements to add the listener to
+ * @param tagName the tag name of elements to add the listener to.
+ */
+Alfresco.util.rollover.registerListenersByClassName = function(htmlId, className, tagName)
+{
+   var mouseEnteredBubbleEventName = 'onRolloverMouseEntered-' + htmlId;
+   var mouseExitedBubbleEventName = 'onRolloverMouseExited-' + htmlId;
+   var elems = YAHOO.util.Dom.getElementsByClassName(className, tagName, htmlId);
+   for (var x = 0; x < elems.length; x++)
+   {
+      Alfresco.util.rollover._attachRolloverListener(elems[x], mouseEnteredBubbleEventName, mouseExitedBubbleEventName);
+   }
+};
+
+
+/**
+ * Register handle functions that handle the mouse enter/exit events
+ * 
+ * @param htmlId the id of the component for which the listeners got registered
+ * @param mouseEnteredFn the function to call for mouse entered events
+ * @param mouseExitedFunction the function to call for mouse exited events
+ * @param scope the object which is used as scope for the function execution
+ */
+Alfresco.util.rollover.registerHandlerFunctions = function(htmlId, mouseEnteredFn, mouseExitedFn, scope)
+{
+   // register bubble events
+   var mouseEnteredBubbleEventName = 'onRolloverMouseEntered-' + htmlId;
+   var mouseExitedBubbleEventName = 'onRolloverMouseExited-' + htmlId;
+   YAHOO.Bubbling.on(mouseEnteredBubbleEventName, mouseEnteredFn, scope);
+   YAHOO.Bubbling.on(mouseExitedBubbleEventName, mouseExitedFn, scope);
+};
