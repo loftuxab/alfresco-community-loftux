b76b47d080bf911e5069c29e9e30eaf50795bc20
gcornwell <gcornwell@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-06-09 15:00:51 +0000

    First cut of MOB-837 (type support in forms)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@14606 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 092a79351cdccebb5d08eb83738645484beebe1c

---
+++ root/projects/web-framework-commons/source/java/org/alfresco/web/config/forms/ModelTypeEvaluator.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2005-2009 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have received a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing"
+ */
+package org.alfresco.web.config.forms;
+
+import org.alfresco.config.evaluator.Evaluator;
+
+/**
+ * Evaluator to determine whether the condition matches the content
+ * model type provided.
+ * 
+ * @author Gavin Cornwell
+ */
+public class ModelTypeEvaluator implements Evaluator
+{
+    /**
+     * Determines whether the given condition matches the type provided
+     * by the object.
+     * 
+     * @see org.alfresco.config.evaluator.Evaluator#applies(java.lang.Object,
+     *      java.lang.String)
+     */
+    public boolean applies(Object obj, String condition)
+    {
+        boolean result = false;
+
+        if (obj instanceof String)
+        {
+            if (condition.equalsIgnoreCase((String)obj))
+            {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+}
