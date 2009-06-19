0241260cfa897dea3b089ba8c75db16ca8c7caad
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-06-19 10:51:49 +0000

    Fix so that items to display in Profile User Content is configurable. Fix up user content data webscript so that maxItems to display is passed directly to Lucene query. Cleanup of related javascript and templates.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@14800 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/profile/user-content.get.js
+++ root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/profile/user-content.get.js
@@ -1,36 +1,42 @@
-
-
-var result = remote.call("/slingshot/profile/usercontents?user=" + stringUtils.urlEncode(page.url.templateArgs["userid"])+"&maxResults=3");
-
 model.addedContent = [];
 model.modifiedContent = [];
-var numContentToShow = 3;
+
+// read config - use default values if not found
+var maxItems = 3;
+var conf = new XML(config.script);
+if (conf["max-items"] != null)
+{
+   maxItems = parseInt(conf["max-items"]);
+}
+
+var result = remote.call("/slingshot/profile/usercontents?user=" + stringUtils.urlEncode(page.url.templateArgs["userid"]) + "&maxResults=" + maxItems);
+
 if (result.status == 200)
 {
    // Create javascript objects from the server response
    var data = eval('(' + result + ')');
-   ['created','modified'].forEach(function(type){
+   
+   ['created','modified'].forEach(function(type)
+   {
       var store = (type==='created') ? model.addedContent : model.modifiedContent;
       var content = data[type].items;
-
       
-      for (var i=0,len=content.length;i<len;i++)
+      for (var i=0,len=content.length; i<len; i++)
       {
          var c = content[i];
-         if (store.length < numContentToShow)
+         if (store.length < maxItems)
          {
-            //convert createdOn and modifiedOn fields to date
-            if (c[type+'On'])
+            // convert createdOn and modifiedOn fields to date
+            if (c[type + 'On'])
             {
-               c[type+'On'] = new Date(c[type+'On']);
+               c[type + 'On'] = new Date(c[type + 'On']);
             }
             store.push(c);
          }
       }
       (type==='created') ? model.addedContent = store: model.modifiedContent = store;
    });
-   
 }
+
 model.numAddedContent = model.addedContent.length;
 model.numModifiedContent = model.modifiedContent.length;
-
