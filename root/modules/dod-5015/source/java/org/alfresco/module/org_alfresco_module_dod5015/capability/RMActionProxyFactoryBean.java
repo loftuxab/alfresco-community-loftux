9f974caba65c875a979fab58cad9d3570787e779
ahind <ahind@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-07-14 20:05:58 +0000

    RM capabilities: no op impl for RM_QUERY; First cut of Action security wrappers; Tidy up for new access API and config changes to support it across all public services (except AVM)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@15186 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: d8d5038a62f054343d9013cd10635d45140f2b68

---
+++ root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/capability/RMActionProxyFactoryBean.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2005-2007 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's
+ * FLOSS exception.  You should have recieved a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * http://www.alfresco.com/legal/licensing
+ */
+package org.alfresco.module.org_alfresco_module_dod5015.capability;
+
+import org.alfresco.module.org_alfresco_module_dod5015.action.RecordsManagementAction;
+import org.alfresco.module.org_alfresco_module_dod5015.action.RecordsManagementActionService;
+import org.alfresco.repo.action.RuntimeActionService;
+import org.alfresco.repo.action.executer.ActionExecuter;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
+public class RMActionProxyFactoryBean extends ProxyFactoryBean
+{
+    protected RuntimeActionService runtimeActionService;
+
+    private RecordsManagementActionService recordsManagementActionService;
+
+    /**
+     * Set action service
+     * 
+     * @param actionService
+     */
+    public void setRuntimeActionService(RuntimeActionService runtimeActionService)
+    {
+        this.runtimeActionService = runtimeActionService;
+    }
+
+    /**
+     * Set records management service
+     * 
+     * @param recordsManagementActionService
+     */
+    public void setRecordsManagementActionService(RecordsManagementActionService recordsManagementActionService)
+    {
+        this.recordsManagementActionService = recordsManagementActionService;
+    }
+
+    public void registerAction()
+    {
+        runtimeActionService.registerActionExecuter((ActionExecuter) getObject());
+        recordsManagementActionService.register((RecordsManagementAction) getObject());
+    }
+}
