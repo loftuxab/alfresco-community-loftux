e8aae17829a7a9dec86b5f05a4312b76da12ad7b
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-07-25 02:53:04 +0000

    Brought java.util.Pair into line with coding standards  - Copywrite notice  - File format  - Member naming  - Fixed NPEs on toString(), equals() and hashCode()  - Backward-compatible serialization
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@15404 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 3b69b28d0af71583866fed9186ad3968bd055560

--- root/projects/core/source/java/org/alfresco/util/Pair.java
+++ root/projects/core/source/java/org/alfresco/util/Pair.java
@@ -1,27 +1,59 @@
-/**
- * 
+/*
+ * Copyright (C) 2005-2009 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have recieved a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing"
  */
 package org.alfresco.util;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
+import java.io.ObjectInputStream.GetField;
 
 /**
  * Utility class for containing two things that aren't like each other
- * @author britt
  */
 public final class Pair<F, S> implements Serializable
 {
-    private static final long serialVersionUID = -7406248421185630612L;
+    @SuppressWarnings("unchecked")
+    public static final Pair NULL_PAIR = new Pair(null, null);
     
+    @SuppressWarnings("unchecked")
+    public static final <X, Y> Pair<X, Y> nullPair()
+    {
+        return NULL_PAIR;
+    }
+    
+    private static final long serialVersionUID = -7406248421185630612L;
+
     /**
      * The first member of the pair.
      */
-    private F fFirst;
+    private F first;
     
     /**
      * The second member of the pair.
      */
-    private S fSecond;
+    private S second;
     
     /**
      * Make a new one.
@@ -31,8 +63,8 @@ public final class Pair<F, S> implements Serializable
      */
     public Pair(F first, S second)
     {
-        fFirst = first;
-        fSecond = second;
+        this.first = first;
+        this.second = second;
     }
     
     /**
@@ -41,7 +73,7 @@ public final class Pair<F, S> implements Serializable
      */
     public F getFirst()
     {
-        return fFirst;
+        return first;
     }
     
     /**
@@ -50,24 +82,19 @@ public final class Pair<F, S> implements Serializable
      */
     public S getSecond()
     {
-        return fSecond;
+        return second;
     }
     
     public void setFirst(F first)
     {
-        fFirst = first;
+        this.first = first;
     }
     
     public void setSecond(S second)
     {
-        fSecond = second;
+        this.second = second;
     }
     
-    /**
-     * Override of equals.
-     * @param other The thing to compare to.
-     * @return equality.
-     */
     @Override
     public boolean equals(Object other)
     {
@@ -75,29 +102,44 @@ public final class Pair<F, S> implements Serializable
         {
             return true;
         }
-        if (!(other instanceof Pair))
+        if (other == null || !(other instanceof Pair<?, ?>))
         {
             return false;
         }
-        Pair o = (Pair)other;
-        return fFirst.equals(o.getFirst()) && fSecond.equals(o.getSecond());
+        Pair<?, ?> o = (Pair<?, ?>)other;
+        return EqualsHelper.nullSafeEquals(this.first, o.first) &&
+               EqualsHelper.nullSafeEquals(this.second, o.second);
     }
     
-    /**
-     * Override of hashCode.
-     */
     @Override
     public int hashCode()
     {
-        return fFirst.hashCode() + fSecond.hashCode();
+        return (first == null ? 0 : first.hashCode()) + (second == null ? 0 : second.hashCode());
     }
 
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
     @Override
     public String toString()
     {
-        return "(" + fFirst + ", " + fSecond + ")";
+        return "(" + first + ", " + second + ")";
+    }
+
+    /**
+     * Ensure that previously-serialized instances don't fail due to the member name change.
+     */
+    @SuppressWarnings("unchecked")
+    private void readObject(ObjectInputStream is) throws ClassNotFoundException, IOException
+    {
+        GetField fields = is.readFields();
+        if (fields.defaulted("first"))
+        {
+            // This is a pre-V3.3
+            this.first = (F) fields.get("fFirst", null);
+            this.second = (S) fields.get("fSecond", null);
+        }
+        else
+        {
+            this.first = (F) fields.get("first", null);
+            this.second = (S) fields.get("second", null);
+        }
     }
 }
