1e4f629359dcda959f4a5cf3dc99dd276be1f998
ahind <ahind@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-07-30 20:17:29 +0000

    RM capabilities: Files missed from previous check in ....
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@15496 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: ca4879b79b1bcc173913383fd508d753947229c3

---
+++ root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/capability/group/UpdateCapability.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2005-2007 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have recieved a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing"
+ */
+package org.alfresco.module.org_alfresco_module_dod5015.capability.group;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import net.sf.acegisecurity.vote.AccessDecisionVoter;
+
+import org.alfresco.module.org_alfresco_module_dod5015.capability.impl.AbstractCapability;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.namespace.QName;
+
+/**
+ * @author andyh
+ */
+public class UpdateCapability extends AbstractCapability
+{
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.alfresco.module.org_alfresco_module_dod5015.capability.impl.AbstractCapability#hasPermissionImpl(org.alfresco.service.cmr.repository.NodeRef)
+     */
+    @Override
+    protected int hasPermissionImpl(NodeRef nodeRef)
+    {
+        return AccessDecisionVoter.ACCESS_ABSTAIN;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.alfresco.module.org_alfresco_module_dod5015.capability.Capability#getName()
+     */
+    public String getName()
+    {
+        return "Update";
+    }
+
+    public int evaluate(NodeRef nodeRef, QName aspectQName, Map<QName, Serializable> properties)
+    {
+        if ((aspectQName != null) && (voter.isProtectedAspect(aspectQName)))
+        {
+            return AccessDecisionVoter.ACCESS_DENIED;
+        }
+        if ((properties != null) && (voter.includesProtectedProperties(properties)))
+        {
+            return AccessDecisionVoter.ACCESS_DENIED;
+        }
+        if (voter.getFileRecordsCapability().evaluate(nodeRef) == AccessDecisionVoter.ACCESS_GRANTED)
+        {
+            return AccessDecisionVoter.ACCESS_GRANTED;
+        }
+        if (voter.getCreateModifyDestroyFoldersCapability().evaluate(nodeRef, null) == AccessDecisionVoter.ACCESS_GRANTED)
+        {
+            return AccessDecisionVoter.ACCESS_GRANTED;
+        }
+        if (voter.getCreateModifyDestroyFileplanMetadataCapability().evaluate(nodeRef) == AccessDecisionVoter.ACCESS_GRANTED)
+        {
+            return AccessDecisionVoter.ACCESS_GRANTED;
+        }
+        if (voter.getEditDeclaredRecordMetadataCapability().evaluate(nodeRef) == AccessDecisionVoter.ACCESS_GRANTED)
+        {
+            return AccessDecisionVoter.ACCESS_GRANTED;
+        }
+        if (voter.getEditNonRecordMetadataCapability().evaluate(nodeRef) == AccessDecisionVoter.ACCESS_GRANTED)
+        {
+            return AccessDecisionVoter.ACCESS_GRANTED;
+        }
+        if (voter.getEditRecordMetadataCapability().evaluate(nodeRef) == AccessDecisionVoter.ACCESS_GRANTED)
+        {
+            return AccessDecisionVoter.ACCESS_GRANTED;
+        }
+        return AccessDecisionVoter.ACCESS_DENIED;
+    }
+}
