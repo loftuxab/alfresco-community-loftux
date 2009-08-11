092b9d437331307605cd2dcd61a2844997123455
gcornwell <gcornwell@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-08-11 22:12:48 +0000

    - Changed generation of unique identifier, no longer appears as a field on the create screens, instead gets generated after the node is persisted, the identifier is then generated using the same algorithm as used for records i.e. year-<pad0><node-dbid>. The idenifier still appears on the edit screen so can be changed if desired. - Moved the StreamArchive webscript implementation to remote API project so it can be re-used by other non RM webscripts - Added skeleton for transfer REST API
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@15687 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 9be6a408748738010e327f890e45eb66af220be3

--- root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/forms/RecordsManagementTypeFormFilter.java
+++ root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/forms/RecordsManagementTypeFormFilter.java
@@ -1,13 +1,16 @@
 package org.alfresco.module.org_alfresco_module_dod5015.forms;
 
+import java.util.Calendar;
 import java.util.List;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.module.org_alfresco_module_dod5015.CustomisableRmElement;
-import org.alfresco.repo.forms.FieldDefinition;
+import org.alfresco.module.org_alfresco_module_dod5015.RecordsManagementModel;
 import org.alfresco.repo.forms.Form;
+import org.alfresco.repo.forms.FormData;
 import org.alfresco.service.cmr.dictionary.TypeDefinition;
+import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
-import org.alfresco.util.GUID;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -37,6 +40,7 @@ public class RecordsManagementTypeFormFilter extends RecordsManagementFormFilter
         TypeDefinition type = (TypeDefinition)item;
         QName typeName = type.getName();
         
+        /*
         if (TYPE_RECORD_SERIES.equals(typeName) || 
             TYPE_RECORD_CATEGORY.equals(typeName) ||
             TYPE_RECORD_FOLDER.equals(typeName))
@@ -54,7 +58,7 @@ public class RecordsManagementTypeFormFilter extends RecordsManagementFormFilter
                     fieldDef.setDefaultValue(GUID.generate());
                     break;
                 }
-            }
+            }*/
             
             // add any custom properties for the type being created (we don't need to deal with
             // the record type in here as records are typically uploaded and then their metadata
@@ -71,6 +75,65 @@ public class RecordsManagementTypeFormFilter extends RecordsManagementFormFilter
             {
                 addCustomRMProperties(CustomisableRmElement.RECORD_FOLDER, form);
             }
+        //}
+    }
+    
+    /*
+     * @see org.alfresco.repo.forms.processor.Filter#afterPersist(java.lang.Object, org.alfresco.repo.forms.FormData, java.lang.Object)
+     */
+    public void afterPersist(Object item, FormData data, Object persistedObject)
+    {
+        // we know the persisted object is a NodeRef so cast
+        NodeRef nodeRef = (NodeRef)persistedObject;
+        
+        // Once an RM container type has been persisted generate a default identifer for it.
+        if (this.nodeService.hasAspect(nodeRef, ASPECT_FILE_PLAN_COMPONENT))
+        {
+            if (logger.isDebugEnabled())
+                logger.debug("Generating unique identifier for " + 
+                            this.nodeService.getType(nodeRef).toPrefixString(this.namespaceService));
+            
+            this.nodeService.setProperty(nodeRef, RecordsManagementModel.PROP_IDENTIFIER,
+                        generateIdentifier(nodeRef));
         }
+    }
+    
+    /**
+     * Generates a unique identifier for the given node (based on the dbid).
+     * 
+     * @param nodeRef The NodeRef to generate a unique id for
+     * @return The identifier
+     */
+    protected String generateIdentifier(NodeRef nodeRef)
+    {
+        Calendar fileCalendar = Calendar.getInstance();
+        String year = Integer.toString(fileCalendar.get(Calendar.YEAR));
+        Long dbId = (Long)this.nodeService.getProperty(nodeRef, ContentModel.PROP_NODE_DBID);
+        String identifier = year + "-" + padString(dbId.toString(), 10);
+        
+        if (logger.isDebugEnabled())
+            logger.debug("Generated '" + identifier + "' for unique identifier");
+        
+        return identifier;
+    }
+    
+    /**
+     * Function to pad a string with zero '0' characters to the required length
+     * 
+     * @param s     String to pad with leading zero '0' characters
+     * @param len   Length to pad to
+     * 
+     * @return padded string or the original if already at >=len characters 
+     */
+    protected String padString(String s, int len)
+    {
+       String result = s;
+       
+       for (int i = 0; i < (len - s.length()); i++)
+       {
+           result = "0" + result;
+       }
+       
+       return result;
     }
 }
