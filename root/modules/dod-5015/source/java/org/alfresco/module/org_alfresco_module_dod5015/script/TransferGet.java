62bf56f021f364637351a2ef449ddd4994276960
gcornwell <gcornwell@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-08-12 14:38:22 +0000

    Transfer REST API implementation, used to download the contents of a transfer object to an end users local machine.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@15703 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 7ce946cc9264557ad46b4a7cc67a3fc7af8ccbbb

--- root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/script/TransferGet.java
+++ root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/script/TransferGet.java
@@ -24,11 +24,27 @@
  */
 package org.alfresco.module.org_alfresco_module_dod5015.script;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.alfresco.module.org_alfresco_module_dod5015.DOD5015Model;
+import org.alfresco.module.org_alfresco_module_dod5015.RecordsManagementModel;
 import org.alfresco.repo.web.scripts.content.StreamArchive;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.alfresco.service.namespace.RegexQNamePattern;
+import org.alfresco.web.scripts.Cache;
+import org.alfresco.web.scripts.Status;
 import org.alfresco.web.scripts.WebScriptRequest;
 import org.alfresco.web.scripts.WebScriptResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Streams the nodes of a transfer object to the client in the form of an
@@ -38,11 +54,113 @@ import org.alfresco.web.scripts.WebScriptResponse;
  */
 public class TransferGet extends StreamArchive
 {
+    /** Logger */
+    private static Log logger = LogFactory.getLog(TransferGet.class);
+    
     /**
      * @see org.alfresco.web.scripts.WebScript#execute(org.alfresco.web.scripts.WebScriptRequest, org.alfresco.web.scripts.WebScriptResponse)
      */
     public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException
     {
-        super.execute(req, res);
+        File tempArchiveFile = null;
+        try
+        {
+            // retrieve requested format
+            String format = req.getFormat();
+            
+            // construct model for template
+            Status status = new Status();
+            Cache cache = new Cache(getDescription().getRequiredCache());
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("status", status);
+            model.put("cache", cache);
+            
+            // get the parameters that represent the NodeRef, we know they are present
+            // otherwise this webscript would not have matched
+            Map<String, String> templateVars = req.getServiceMatch().getTemplateVars();
+            String storeType = templateVars.get("store_type");
+            String storeId = templateVars.get("store_id");
+            String nodeId = templateVars.get("id");
+            String transferId = templateVars.get("transfer_id");
+            
+            // create the NodeRef and ensure it is valid
+            StoreRef storeRef = new StoreRef(storeType, storeId);
+            NodeRef nodeRef = new NodeRef(storeRef, nodeId);
+            
+            if (logger.isDebugEnabled())
+                logger.debug("Retrieving transfer '" + transferId + "' from file plan: " + nodeRef);
+            
+            if (!this.nodeService.exists(nodeRef))
+            {
+                status.setCode(HttpServletResponse.SC_NOT_FOUND, 
+                            "Node " + nodeRef.toString() + " does not exist");
+                Map<String, Object> templateModel = createTemplateParameters(req, res, model);
+                sendStatus(req, res, status, cache, format, templateModel);
+                return;
+            }
+            
+            // ensure the node is a filePlan object
+            if (!DOD5015Model.TYPE_FILE_PLAN.equals(this.nodeService.getType(nodeRef)))
+            {
+                status.setCode(HttpServletResponse.SC_BAD_REQUEST, 
+                            "Node " + nodeRef.toString() + " is not a file plan");
+                Map<String, Object> templateModel = createTemplateParameters(req, res, model);
+                sendStatus(req, res, status, cache, format, templateModel);
+                return;
+            }
+            
+            // get all the transfer nodes and find the one we need
+            List<ChildAssociationRef> assocs = this.nodeService.getChildAssocs(nodeRef, 
+                        RecordsManagementModel.ASSOC_TRANSFERS, RegexQNamePattern.MATCH_ALL);
+            NodeRef transferNodeRef = null;
+            for (ChildAssociationRef child : assocs)
+            {
+                if (child.getChildRef().getId().equals(transferId))
+                {
+                    transferNodeRef = child.getChildRef();
+                    break;
+                }
+            }
+            
+            // send 404 if the transfer is not found
+            if (transferNodeRef == null)
+            {
+                status.setCode(HttpServletResponse.SC_NOT_FOUND, 
+                            "Could not locate transfer with id: " + transferId);
+                Map<String, Object> templateModel = createTemplateParameters(req, res, model);
+                sendStatus(req, res, status, cache, format, templateModel);
+                return;
+            }
+            
+            // get all 'transferred' nodes and create as an array of NodeRefs
+            assocs = this.nodeService.getChildAssocs(transferNodeRef, 
+                        RecordsManagementModel.ASSOC_TRANSFERRED, RegexQNamePattern.MATCH_ALL);
+            NodeRef[] itemsToTransfer = new NodeRef[assocs.size()];
+            for (int idx = 0; idx < assocs.size(); idx++)
+            {
+                itemsToTransfer[idx] = assocs.get(idx).getChildRef();
+            }
+            
+            // create an archive of all the nodes to transfer
+            tempArchiveFile = createArchive(itemsToTransfer);
+            
+            // stream the archive back to the client as an attachment (forcing save as)
+            streamContent(req, res, tempArchiveFile, true, tempArchiveFile.getName());
+        }
+        catch (Throwable e)
+        {
+            throw createStatusException(e, req, res);
+        }
+        finally
+        {
+           // try and delete the temporary file
+           if (tempArchiveFile != null)
+           {
+               tempArchiveFile.delete();
+               
+               if (logger.isDebugEnabled())
+                   logger.debug("Deleted temporary archive: " + tempArchiveFile.getAbsolutePath());
+           }
+        }
     }
 }
