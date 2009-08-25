e7b413f2684581f4897ae6a83c5edbfde0d5d29a
mrogers <mrogers@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-08-25 08:35:55 +0000

    Missed check in from previous commit (15873)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@15878 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/caveat/ScriptAuthority.java
@@ -0,0 +1,33 @@
+package org.alfresco.module.org_alfresco_module_dod5015.caveat;
+
+import java.io.Serializable;
+
+
+public class ScriptAuthority implements Serializable
+{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private String authorityTitle;
+    private String authorityName;
+    
+    public void setAuthorityName(String authorityName)
+    {
+        this.authorityName = authorityName;
+    }
+    public String getAuthorityName()
+    {
+        return authorityName;
+    }
+    public void setAuthorityTitle(String authorityName)
+    {
+        this.authorityTitle = authorityName;
+    }
+    public String getAuthorityTitle()
+    {
+        return authorityTitle;
+    }
+
+                                     
+}
