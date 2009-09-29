45d2d67cd9c2aa3ca3fb13beef13d2e04b4719af
mrogers <mrogers@7033f55c-6c29-4ad8-afba-765cb616e738>
2009-09-29 14:00:37 +0000

    MOB-1615 -  Cannot file email messages as a typical user (user)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@16593 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/capability/RMEntryVoter.java
+++ root/modules/dod-5015/source/java/org/alfresco/module/org_alfresco_module_dod5015/capability/RMEntryVoter.java
@@ -1251,7 +1251,7 @@ public class RMEntryVoter implements AccessDecisionVoter, InitializingBean
             }
         }
 
-        throw new ACLEntryVoterException("Unknown type");
+        return null;
     }
 
     private static QName getQName(MethodInvocation invocation, Class[] params, int position)
