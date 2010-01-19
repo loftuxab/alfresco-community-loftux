ce71a5ff3f9eb0ae9cfa9df4a8d0fa3cc541ab1f
dward <dward@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-01-19 13:35:06 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@18139 7033f55c-6c29-4ad8-afba-765cb616e738
    Former-commit-id: ffdbdb8b4bb54f0bcbee3cc9314513505d13429a

---
+++ root/projects/slingshot/source/java/org/alfresco/web/portlet/LazyDashboardFilter.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2005-2009 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have received a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing"
+ */
+package org.alfresco.web.portlet;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.alfresco.web.site.FrameworkHelper;
+import org.alfresco.web.site.RequestContext;
+import org.alfresco.web.site.RequestUtil;
+import org.alfresco.web.site.exception.RequestContextException;
+
+/**
+ * Performs lazy creation of dashboard pages when they are requested without requiring redirects, thus making them
+ * addressable from a portlet.
+ * 
+ * @author dward
+ */
+public class LazyDashboardFilter implements Filter
+{
+    private static final Pattern PATTERN_DASHBOARD_PATH = Pattern.compile("/user/([^/]*)/dashboard");
+
+    /*
+     * (non-Javadoc)
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse,
+     * javax.servlet.FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException
+    {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        // If this is a request for the current user's dashboard page, create it if it doesn't exist
+        String pathInfo = httpServletRequest.getPathInfo();
+        Matcher matcher;
+        if (pathInfo != null && (matcher = PATTERN_DASHBOARD_PATH.matcher(pathInfo)).matches())
+        {
+            // Get hold of the context
+            RequestContext context;
+            try
+            {
+                context = RequestUtil.getRequestContext(request);
+            }
+            catch (RequestContextException e)
+            {
+                throw new ServletException(e);
+            }
+
+            String userid = context.getUserId();
+
+            // test user dashboard page exists?
+            if (userid != null && userid.equals(URLDecoder.decode(matcher.group(1), "UTF-8")))
+            {
+                if (context.getModel().getPage("user/" + userid + "/dashboard") == null)
+                {
+                    // no site found! create initial dashboard for this user...
+                    Map<String, String> tokens = new HashMap<String, String>();
+                    tokens.put("userid", userid);
+                    FrameworkHelper.getPresetsManager().constructPreset(context.getModel(), "user-dashboard", tokens);
+                }
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    public void init(FilterConfig config) throws ServletException
+    {
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see javax.servlet.Filter#destroy()
+     */
+    public void destroy()
+    {
+    }
+
+}
