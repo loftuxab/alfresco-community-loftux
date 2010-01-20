1b195a5e3b196969e34401e9354c27522723c4c2
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-01-20 11:14:52 +0000

    Refactored LazyDashboardFilter against SpringSurf libraries
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@18164 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: b31dbc7c582ea22000b92bb245d551b34a728e08

--- root/projects/slingshot/source/java/org/alfresco/web/portlet/LazyDashboardFilter.java
+++ root/projects/slingshot/source/java/org/alfresco/web/portlet/LazyDashboardFilter.java
@@ -25,7 +25,6 @@
 package org.alfresco.web.portlet;
 
 import java.io.IOException;
-import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -34,15 +33,16 @@ import java.util.regex.Pattern;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import org.alfresco.web.site.FrameworkHelper;
-import org.alfresco.web.site.RequestContext;
-import org.alfresco.web.site.RequestUtil;
-import org.alfresco.web.site.exception.RequestContextException;
+import org.springframework.extensions.surf.RequestContext;
+import org.springframework.extensions.surf.WebFrameworkServiceRegistry;
+import org.springframework.extensions.surf.exception.RequestContextException;
+import org.springframework.extensions.surf.util.URLDecoder;
 
 /**
  * Performs lazy creation of dashboard pages when they are requested without requiring redirects, thus making them
@@ -53,7 +53,10 @@ import org.alfresco.web.site.exception.RequestContextException;
 public class LazyDashboardFilter implements Filter
 {
     private static final Pattern PATTERN_DASHBOARD_PATH = Pattern.compile("/user/([^/]*)/dashboard");
-
+    
+    private ServletContext context;
+    
+    
     /*
      * (non-Javadoc)
      * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse,
@@ -63,38 +66,39 @@ public class LazyDashboardFilter implements Filter
             ServletException
     {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-
+        
         // If this is a request for the current user's dashboard page, create it if it doesn't exist
         String pathInfo = httpServletRequest.getPathInfo();
         Matcher matcher;
         if (pathInfo != null && (matcher = PATTERN_DASHBOARD_PATH.matcher(pathInfo)).matches())
         {
+            WebFrameworkServiceRegistry serviceRegistry = WebFrameworkServiceRegistry.getInstance(this.context);
             // Get hold of the context
             RequestContext context;
             try
             {
-                context = RequestUtil.getRequestContext(request);
+                context = serviceRegistry.getRequestContextFactory().newInstance(request);
             }
             catch (RequestContextException e)
             {
                 throw new ServletException(e);
             }
-
+            
             String userid = context.getUserId();
-
+            
             // test user dashboard page exists?
-            if (userid != null && userid.equals(URLDecoder.decode(matcher.group(1), "UTF-8")))
+            if (userid != null && userid.equals(URLDecoder.decode(matcher.group(1))))
             {
-                if (context.getModel().getPage("user/" + userid + "/dashboard") == null)
+                if (serviceRegistry.getModelObjectService().getPage("user/" + userid + "/dashboard") == null)
                 {
                     // no site found! create initial dashboard for this user...
                     Map<String, String> tokens = new HashMap<String, String>();
                     tokens.put("userid", userid);
-                    FrameworkHelper.getPresetsManager().constructPreset(context.getModel(), "user-dashboard", tokens);
+                    serviceRegistry.getPresetsManager().constructPreset("user-dashboard", tokens);
                 }
             }
         }
-
+        
         chain.doFilter(request, response);
     }
 
@@ -104,6 +108,8 @@ public class LazyDashboardFilter implements Filter
      */
     public void init(FilterConfig config) throws ServletException
     {
+        // get reference to our ServletContext
+        this.context = config.getServletContext();
     }
 
     /*
@@ -113,5 +119,4 @@ public class LazyDashboardFilter implements Filter
     public void destroy()
     {
     }
-
 }
