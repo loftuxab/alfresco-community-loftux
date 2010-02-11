95869a9d4491245172ac33eb40754a8c34779550
kroast <kroast@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-02-11 15:41:14 +0000

    . Application Settings page for Share Admin Console:  - Currently only contains a single rather lonely option - Current Theme  - Surf Theme model objects added for the three available Share themes  - Available themes now detected by Share and displayed as options for admin user  - NOTE does not persist across sessions yet! . Fxes to existing CSS for themes . Latest SpringSurf libraries with related fixes and enhancement to allow setting of current theme via web-tier JavaScript.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@18591 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ root/projects/slingshot/source/web/components/console/application.js
@@ -0,0 +1,185 @@
+/**
+ * Copyright (C) 2005-2009 Alfresco Software Limited.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have recieved a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing
+ */
+
+/**
+ * ConsoleApplication tool component.
+ * 
+ * @namespace Alfresco
+ * @class Alfresco.ConsoleApplication
+ */
+(function()
+{
+   /**
+    * YUI Library aliases
+    */
+   var Dom = YAHOO.util.Dom,
+       Event = YAHOO.util.Event,
+       Element = YAHOO.util.Element;
+   
+   /**
+    * Alfresco Slingshot aliases
+    */
+   var $html = Alfresco.util.encodeHTML;
+
+   /**
+    * ConsoleApplication constructor.
+    * 
+    * @param {String} htmlId The HTML id üof the parent element
+    * @return {Alfresco.ConsoleApplication} The new ConsoleApplication instance
+    * @constructor
+    */
+   Alfresco.ConsoleApplication = function(htmlId)
+   {
+      this.name = "Alfresco.ConsoleApplication";
+      Alfresco.ConsoleApplication.superclass.constructor.call(this, htmlId);
+      
+      /* Register this component */
+      Alfresco.util.ComponentManager.register(this);
+      
+      /* Load YUI Components */
+      Alfresco.util.YUILoaderHelper.require(["button", "container", "json", "history"], this.onComponentsLoaded, this);
+      
+      /* Define panel handlers */
+      var parent = this;
+      
+      // NOTE: the panel registered first is considered the "default" view and is displayed first
+      
+      /* Options Panel Handler */
+      OptionsPanelHandler = function OptionsPanelHandler_constructor()
+      {
+         OptionsPanelHandler.superclass.constructor.call(this, "options");
+      };
+      
+      YAHOO.extend(OptionsPanelHandler, Alfresco.ConsolePanelHandler,
+      {
+         /**
+          * Called by the ConsolePanelHandler when this panel shall be loaded
+          *
+          * @method onLoad
+          */
+         onLoad: function onLoad()
+         {
+            // Buttons
+            parent.widgets.applyButton = Alfresco.util.createYUIButton(parent, "apply-button", null,
+            {
+               type: "submit"
+            });
+            
+            // Form definition
+            var form = new Alfresco.forms.Form(parent.id + "-options-form");
+            form.setSubmitElements([parent.widgets.applyButton]);
+            form.setSubmitAsJSON(true);
+            form.setAJAXSubmit(true,
+            {
+               successCallback:
+               {
+                  fn: this.onSuccess,
+                  scope: this
+               }
+            });
+            form.init();
+         },
+         
+         /**
+          * Successfully applied options event handler
+          *
+          * @method onSuccess
+          * @param response {object} Server response object
+          */
+         onSuccess: function OptionsPanel_onSuccess(response)
+         {
+            if (response && response.json)
+            {
+               if (response.json.success)
+               {
+                  // refresh the browser to force the themed components to reload
+                  window.location.reload(true);
+               }
+               else if (response.json.message)
+               {
+                  Alfresco.util.PopupManager.displayPrompt(
+                  {
+                     text: response.json.message
+                  });
+               }
+            }
+            else
+            {
+               Alfresco.util.PopupManager.displayPrompt(
+               {
+                  text: Alfresco.util.message("message.failure")
+               });
+            }
+         }
+      });
+      new OptionsPanelHandler();
+      
+      return this;
+   };
+   
+   YAHOO.extend(Alfresco.ConsoleApplication, Alfresco.ConsoleTool,
+   {
+      /**
+       * Fired by YUILoaderHelper when required component script files have
+       * been loaded into the browser.
+       *
+       * @method onComponentsLoaded
+       */
+      onComponentsLoaded: function ConsoleApplication_onComponentsLoaded()
+      {
+         Event.onContentReady(this.id, this.onReady, this, true);
+      },
+      
+      /**
+       * Fired by YUI when parent element is available for scripting.
+       * Component initialisation, including instantiation of YUI widgets and event listener binding.
+       *
+       * @method onReady
+       */
+      onReady: function ConsoleApplication_onReady()
+      {
+         // Call super-class onReady() method
+         Alfresco.ConsoleApplication.superclass.onReady.call(this);
+      },
+      
+      /**
+       * YUI WIDGET EVENT HANDLERS
+       * Handlers for standard events fired from YUI widgets, e.g. "click"
+       */
+      
+      /**
+       * Gets a custom message
+       *
+       * @method _msg
+       * @param messageId {string} The messageId to retrieve
+       * @return {string} The custom message
+       * @private
+       */
+      _msg: function ConsoleApplication__msg(messageId)
+      {
+         return Alfresco.util.message.call(this, messageId, "Alfresco.ConsoleApplication", Array.prototype.slice.call(arguments).slice(1));
+      }
+   });
+})();
