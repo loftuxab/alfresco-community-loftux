433338b919a1428356605d987c1eecd1495832c7
gcornwell <gcornwell@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-02-21 22:46:04 +0000

    Added remaining resource dependencies in spring config and return them in /wef/resources webscript. Also updated unit test to reflect new dependencies.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@18742 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 7e19a4521ca89447623a1808b4f340bfaa022b50

--- root/projects/web-editor/source/java/org/alfresco/wef/WEFTest.java
+++ root/projects/web-editor/source/java/org/alfresco/wef/WEFTest.java
@@ -69,11 +69,11 @@ public class WEFTest extends TestCase
         assertEquals("application", app.getType());
         assertNull(app.getContainer());
         List<WEFResource> appDependencies = app.getDependencies();
-        assertEquals(2, appDependencies.size());
+        assertEquals(5, appDependencies.size());
         
         // get and check the forms plugin and it's dependencies
         WEFPlugin formsPlugin = plugins.get(1);
-        assertEquals("com.alfresco.forms", formsPlugin.getName());
+        assertEquals("org.alfresco.forms", formsPlugin.getName());
         assertEquals("Plugin providing forms capabilities", formsPlugin.getDescription());
         assertEquals("/components/form/form-min.js", formsPlugin.getPath());
         assertEquals("plugin", formsPlugin.getType());
@@ -83,24 +83,24 @@ public class WEFTest extends TestCase
         
         // get and check the AWE plugin and it's dependencies
         WEFPlugin awePlugin = plugins.get(2);
-        assertEquals("com.alfresco.awe", awePlugin.getName());
+        assertEquals("org.alfresco.awe", awePlugin.getName());
         assertEquals("Plugin providing in-context editing capabilities", awePlugin.getDescription());
         assertEquals("/js/awe.js", awePlugin.getPath());
         assertEquals("plugin", awePlugin.getType());
         assertNull(awePlugin.getContainer());
         List<WEFResource> aweDependencies = awePlugin.getDependencies();
-        assertEquals(2, aweDependencies.size());
+        assertEquals(6, aweDependencies.size());
         
         // test retrieval of individual plugins
-        WEFPlugin plugin = pluginRegistry.getPlugin("com.alfresco.forms");
+        WEFPlugin plugin = pluginRegistry.getPlugin("org.alfresco.forms");
         assertNotNull(plugin);
         assertTrue(plugin instanceof WEFPlugin);
-        assertEquals("com.alfresco.forms", plugin.getName());
+        assertEquals("org.alfresco.forms", plugin.getName());
         
-        plugin = pluginRegistry.getPlugin("com.alfresco.awe");
+        plugin = pluginRegistry.getPlugin("org.alfresco.awe");
         assertNotNull(plugin);
         assertTrue(plugin instanceof WEFPlugin);
-        assertEquals("com.alfresco.awe", plugin.getName());
+        assertEquals("org.alfresco.awe", plugin.getName());
         
         plugin = pluginRegistry.getPlugin("application.wef");
         assertNotNull(plugin);
@@ -118,7 +118,7 @@ public class WEFTest extends TestCase
         assertEquals("application", app.getType());
         assertNull(app.getContainer());
         appDependencies = app.getDependencies();
-        assertEquals(2, appDependencies.size());
+        assertEquals(5, appDependencies.size());
     }
     
     /**
@@ -129,29 +129,48 @@ public class WEFTest extends TestCase
         // get list of all resources
         List<WEFResource> resources = this.pluginRegistry.getPluginResources();
         assertNotNull(resources);
-        assertEquals(14, resources.size());
-        assertEquals("com.alfresco.forms.runtime", resources.get(0).getName());
-        assertEquals("com.alfresco.forms.css", resources.get(1).getName());
-        assertEquals("com.alfresco.forms.control.date-picker", resources.get(2).getName());
-        assertEquals("com.alfresco.forms.control.period", resources.get(3).getName());
-        assertEquals("com.alfresco.forms.control.object-finder", resources.get(4).getName());
-        assertEquals("com.alfresco.forms.control.object-finder-css", resources.get(5).getName());
-        assertEquals("com.moxiecode.tinymce", resources.get(6).getName());
-        assertEquals("com.alfresco.editors.tinymce", resources.get(7).getName());
-        assertEquals("com.alfresco.forms.control.rich-text", resources.get(8).getName());
-        assertEquals("com.alfresco.forms.control.content", resources.get(9).getName());
-        assertEquals("com.alfresco.forms", resources.get(10).getName());
-        assertEquals("com.alfresco.awe.reset-css", resources.get(11).getName());
-        assertEquals("com.alfresco.awe", resources.get(12).getName());
-        assertEquals("application.wef", resources.get(13).getName());
+        assertEquals(30, resources.size());
+        assertEquals("org.alfresco.wef", resources.get(0).getName());
+        assertEquals("org.alfresco.wef.ribbon-css", resources.get(1).getName());
+        assertEquals("org.alfresco.wef.ribbon", resources.get(2).getName());
+        assertEquals("org.alfresco.wef.toolbar", resources.get(3).getName());
+        assertEquals("utilities", resources.get(4).getName());
+        assertEquals("animation", resources.get(5).getName());
+        assertEquals("selector", resources.get(6).getName());
+        assertEquals("cookie", resources.get(7).getName());
+        assertEquals("menu", resources.get(8).getName());
+        assertEquals("container", resources.get(9).getName());
+        assertEquals("button", resources.get(10).getName());
+        assertEquals("com.yahoo.bubbling", resources.get(11).getName());
+        assertEquals("org.alfresco.messages", resources.get(12).getName());
+        assertEquals("org.alfresco.utils", resources.get(13).getName());
+        assertEquals("org.alfresco.forms.runtime", resources.get(14).getName());
+        assertEquals("org.alfresco.forms.css", resources.get(15).getName());
+        assertEquals("org.alfresco.forms.control.date-picker", resources.get(16).getName());
+        assertEquals("org.alfresco.forms.control.period", resources.get(17).getName());
+        assertEquals("org.alfresco.forms.control.object-finder", resources.get(18).getName());
+        assertEquals("org.alfresco.forms.control.object-finder-css", resources.get(19).getName());
+        assertEquals("com.moxiecode.tinymce", resources.get(20).getName());
+        assertEquals("org.alfresco.editors.tinymce", resources.get(21).getName());
+        assertEquals("org.alfresco.forms.control.rich-text", resources.get(22).getName());
+        assertEquals("org.alfresco.forms.control.content", resources.get(23).getName());
+        assertEquals("org.alfresco.forms", resources.get(24).getName());
+        assertEquals("org.alfresco.awe.reset-css", resources.get(25).getName());
+        assertEquals("org.alfresco.awe.css", resources.get(26).getName());
+        assertEquals("com.yahoo.skin", resources.get(27).getName());
+        assertEquals("org.alfresco.awe", resources.get(28).getName());
+        assertEquals("application.wef", resources.get(29).getName());
         
         // get the application itself and ensure the dependencies are the plugins
         WEFApplication app = this.pluginRegistry.getApplication("application.wef");
         assertNotNull(app);
         List<WEFResource> plugins = app.getDependencies();
         assertNotNull(plugins);
-        assertEquals(2, plugins.size());
-        assertEquals("com.alfresco.forms", plugins.get(0).getName());
-        assertEquals("com.alfresco.awe", plugins.get(1).getName());
+        assertEquals(5, plugins.size());
+        assertEquals("org.alfresco.wef", plugins.get(0).getName());
+        assertEquals("org.alfresco.wef.ribbon", plugins.get(1).getName());
+        assertEquals("org.alfresco.wef.toolbar", plugins.get(2).getName());
+        assertEquals("org.alfresco.forms", plugins.get(3).getName());
+        assertEquals("org.alfresco.awe", plugins.get(4).getName());
     }
 }
