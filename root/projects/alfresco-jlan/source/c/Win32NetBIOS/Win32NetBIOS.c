6b36bf8e60a667c77a9c2a136030fc51fbe33fb2
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-03-02 23:07:53 +0000

    Updated files to LGPL
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@18966 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: eba088e31cddc36ac1cd844510936c65524b169a

--- root/projects/alfresco-jlan/source/c/Win32NetBIOS/Win32NetBIOS.c
+++ root/projects/alfresco-jlan/source/c/Win32NetBIOS/Win32NetBIOS.c
@@ -1,20 +1,21 @@
 /*
-* Copyright (C) 2007 Alfresco, Inc.
-*
-* Licensed under the GNU Lesser General Public License as
-* published by the Free Software Foundation; either version
-* 2.1 of the License, or (at your option) any later version.
-* You may obtain a copy of the License at
-*
-*     http://www.gnu.org/licenses/lgpl.txt
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-* either express or implied. See the License for the specific
-* language governing permissions and limitations under the
-* License.
-*/
+ * Copyright (C) 2005-2010 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef UNICODE
  #define UNICODE
