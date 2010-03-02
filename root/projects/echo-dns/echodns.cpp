6b5d523b97bc8993f293ef0f2d73b71975c42614
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-03-02 18:33:11 +0000

    Updated files to LGPL
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@18956 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: bd7d5b45dc59ba876a3684fd7c17fd817741ccd8

--- root/projects/echo-dns/echodns.cpp
+++ root/projects/echo-dns/echodns.cpp
@@ -1,19 +1,20 @@
 //-----------------------------------------------------------------------------
-// Copyright 2006 Alfresco Inc.
+// Copyright (C) 2005-2010 Alfresco Software Limited.
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// This file is part of Alfresco
 //
-// This program is distributed in the hope that it will be useful,
+// Alfresco is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Alfresco is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// You should have received a copy of the GNU Lesser General Public License
+// along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
 //
 //
 // Author  Jon Cox  <jcox@alfresco.com>
