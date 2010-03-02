6e65ec73ce044b49a0aacc3dea6f28847282330b
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-03-02 21:36:59 +0000

    Updated files to LGPL
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@18963 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/sdk/samples/WebServiceSamples/source/org/alfresco/sample/webservice/Query2.java
+++ root/projects/sdk/samples/WebServiceSamples/source/org/alfresco/sample/webservice/Query2.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2005-2010 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
 // NOTE: if you change the package location of this class you will need to update the WS_SEURITY_INFO XML as for this example this class
 //       doubles as the passwordCAllbackClass
 package org.alfresco.sample.webservice;
