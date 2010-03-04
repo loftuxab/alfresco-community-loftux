c05ad7928188f503acdd9eca870dd8f0626ee782
mhatfield <mhatfield@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-03-04 08:21:41 +0000

    DocLib: Move common.lib.js into parse-args.lib.js
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@19050 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/modules/dod-5015/config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/dod5015-node.get.js
+++ root/modules/dod-5015/config/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/dod5015-node.get.js
@@ -1,5 +1,4 @@
 <import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/dod5015-evaluator.lib.js">
 <import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/dod5015-filters.lib.js">
 <import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/parse-args.lib.js">
-<import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/common.lib.js">
 <import resource="classpath:/alfresco/templates/webscripts/org/alfresco/slingshot/documentlibrary/dod5015-doclist.get.js">
