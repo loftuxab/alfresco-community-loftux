c86e3a05eaa2f1221085d5c7bc7f2e8d263553e4
mrogers <mrogers@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-03-16 17:18:10 +0000

    Missed change to PropertyCheck
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@19317 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: dc13821693194109a042ea88c2539c36bd92f047

--- root/projects/wdr-deployment/source/java/org/alfresco/deployment/impl/asr/AVMDeploymentTarget.java
+++ root/projects/wdr-deployment/source/java/org/alfresco/deployment/impl/asr/AVMDeploymentTarget.java
@@ -58,7 +58,7 @@ import org.alfresco.service.cmr.repository.ContentWriter;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.service.transaction.TransactionService;
 import org.alfresco.util.GUID;
-import org.springframework.extensions.surf.util.PropertyCheck;
+import org.alfresco.util.PropertyCheck;
 import org.alfresco.util.TempFileProvider;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
