9a33047f18a40782433bc50e362aec1b08509332
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-03-19 12:27:29 +0000

    Updated JVM options in start up scripts
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@19393 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 2cb2cf263d4f92d063113118ef472dee38d63050

--- root/projects/installer/linux/alfresco.sh
+++ root/projects/installer/linux/alfresco.sh
@@ -5,7 +5,7 @@ ALF_HOME=.
 cd "$ALF_HOME"
 APPSERVER="${ALF_HOME}/tomcat"
 # Set any default JVM values
-export JAVA_OPTS='-Xms128m -Xmx512m -XX:MaxPermSize=160m -server'
+export JAVA_OPTS='-Xms512m -Xmx1024m -Xss1024k -XX:MaxPermSize=256m -XX:NewSize=256m -server'
 export JAVA_OPTS="${JAVA_OPTS} -Dalfresco.home=${ALF_HOME} -Dcom.sun.management.jmxremote"
 #
 if [ "$1" = "start" ]; then
