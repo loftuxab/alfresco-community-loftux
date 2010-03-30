ae7a592d82147df1184a5d2b4a1ad02880d64dbb
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-03-30 16:10:32 +0000

    Updated script to apply share amps as well as repo amps
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@19674 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/installer/linux/apply_amps.sh
+++ root/projects/installer/linux/apply_amps.sh
@@ -3,13 +3,15 @@
 # Script for apply AMPs to installed WAR
 # -------
 export CATALINA_HOME=tomcat
-echo "This script will apply all the AMPs in ./amps to the alfresco.war file in $CATALINA_HOME\webapps"
+echo "This script will apply all the AMPs in ./amps to the alfresco.war and share.war files in $CATALINA_HOME\webapps"
 echo "Press control-c to stop this script . . ."
 echo "Press any other key to continue . . ."
 read RESP
 java -jar bin/alfresco-mmt.jar install ./amps $CATALINA_HOME/webapps/alfresco.war -directory
 java -jar bin/alfresco-mmt.jar list $CATALINA_HOME/webapps/alfresco.war
-echo "About to clean out tomcat/webapps/alfresco directory and temporary files..."
+java -jar bin/alfresco-mmt.jar install ./amps-share $CATALINA_HOME/webapps/share.war -directory
+java -jar bin/alfresco-mmt.jar list $CATALINA_HOME/webapps/share.war
+echo "About to clean out tomcat/webapps/alfresco and share directories and temporary files..."
 echo "Press control-c to stop this script . . ."
 echo "Press any other key to continue . . ."
 read DUMMY
--- root/projects/installer/linux/installer/apply_amps.sh
+++ root/projects/installer/linux/installer/apply_amps.sh
@@ -7,13 +7,15 @@ export JAVA_HOME="@@JAVA_HOME@@"
 export PATH=$PATH:$JAVA_HOME/bin
 cd "$ALF_HOME"
 export CATALINA_HOME="$ALF_HOME"/tomcat
-echo "This script will apply all the AMPs in ./amps to the alfresco.war file in $CATALINA_HOME\webapps"
+echo "This script will apply all the AMPs in ./amps to the alfresco.war and share.war files in $CATALINA_HOME\webapps"
 echo "Press control-c to stop this script . . ."
 echo "Press any other key to continue . . ."
 read RESP
 java -jar bin/alfresco-mmt.jar install ./amps $APPSERVER/webapps/alfresco.war -directory
 java -jar bin/alfresco-mmt.jar list $APPSERVER/webapps/alfresco.war
-echo "About to clean out tomcat/webapps/alfresco directory and temporary files..."
+java -jar bin/alfresco-mmt.jar install ./amps-share $APPSERVER/webapps/share.war -directory
+java -jar bin/alfresco-mmt.jar list $APPSERVER/webapps/share.war
+echo "About to clean out tomcat/webapps/alfresco and share directories and temporary files..."
 echo "Press control-c to stop this script . . ."
 echo "Press any other key to continue . . ."
 read DUMMY
