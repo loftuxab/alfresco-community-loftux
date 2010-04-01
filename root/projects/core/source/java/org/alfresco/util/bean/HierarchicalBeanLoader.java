3956ce3fad3b226b244d496395037af292d159da
dhulley <dhulley@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-04-01 01:23:31 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@19720 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/core/source/java/org/alfresco/util/bean/HierarchicalBeanLoader.java
+++ root/projects/core/source/java/org/alfresco/util/bean/HierarchicalBeanLoader.java
@@ -1,43 +1,50 @@
 /*
- * Copyright (C) 2005-2010 Alfresco Software Limited.
+ * Copyright (C) 2005-2009 Alfresco Software Limited.
  *
- * This file is part of Alfresco
- *
- * Alfresco is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Alfresco is distributed in the hope that it will be useful,
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ * As a special exception to the terms and conditions of version 2.0 of 
+ * the GPL, you may redistribute this Program in connection with Free/Libre 
+ * and Open Source Software ("FLOSS") applications as described in Alfresco's 
+ * FLOSS exception.  You should have recieved a copy of the text describing 
+ * the FLOSS exception, and it is also available here: 
+ * http://www.alfresco.com/legal/licensing"
  */
-package org.alfresco.util.resource;
+package org.alfresco.util.bean;
 
 import org.alfresco.util.PropertyCheck;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
- * Locate resources by using a class hierarchy to drive the search.  The well-known
+ * Factory bean to find beans using a class hierarchy to drive the lookup. The well-known
  * placeholder {@link #DEFAULT_DIALECT_PLACEHOLDER} is replaced with successive class
  * names starting from the {@link #setDialectClass(String) dialect class} and
  * progressing up the hierarchy until the {@link #setDialectBaseClass(String) base class}
- * is reached.  A full resource search using Spring's {@link DefaultResourceLoader} is
- * done at each point until the resource is found or the base of the class hierarchy is
- * reached.
+ * is reached.  The bean is looked up in the context at each point until the
+ * bean is found or the base of the class hierarchy is reached.
  * <p/>
- * For example assume classpath resources:<br/>
+ * For example assume bean names:<br/>
  * <pre>
- *    RESOURCE 1: config/ibatis/org.hibernate.dialect.Dialect/SqlMap-DOG.xml
- *    RESOURCE 2: config/ibatis/org.hibernate.dialect.MySQLInnoDBDialect/SqlMap-DOG.xml
- *    RESOURCE 3: config/ibatis/org.hibernate.dialect.Dialect/SqlMap-CAT.xml
- *    RESOURCE 4: config/ibatis/org.hibernate.dialect.MySQLDialect/SqlMap-CAT.xml
+ *    BEAN 1: contentDAO.org.hibernate.dialect.Dialect
+ *    BEAN 2: contentDAO.org.hibernate.dialect.MySQLInnoDBDialect
+ *    BEAN 3: propertyValueDAO.org.hibernate.dialect.Dialect
+ *    BEAN 4: propertyValueDAO.org.hibernate.dialect.MySQLDialect
  * </pre>
  * and<br/>
  * <pre>
@@ -45,37 +52,69 @@ import org.springframework.core.io.Resource;
  * </pre>
  * For dialect <b>org.hibernate.dialect.MySQLInnoDBDialect</b> the following will be returned:<br>
  * <pre>
- *    config/ibatis/#resource.dialect#/SqlMap-DOG.xml == RESOURCE 2
- *    config/ibatis/#resource.dialect#/SqlMap-CAT.xml == RESOURCE 4
+ *    contentDAO.bean.dialect == BEAN 2
+ *    propertyValueDAO.bean.dialect == BEAN 4
  * </pre>
  * For dialect<b>org.hibernate.dialect.MySQLDBDialect</b> the following will be returned:<br>
  * <pre>
- *    config/ibatis/#resource.dialect#/SqlMap-DOG.xml == RESOURCE 1
- *    config/ibatis/#resource.dialect#/SqlMap-CAT.xml == RESOURCE 4
+ *    contentDAO.bean.dialect == BEAN 1
+ *    propertyValueDAO.bean.dialect == BEAN 4
  * </pre>
  * For dialect<b>org.hibernate.dialect.Dialect</b> the following will be returned:<br>
  * <pre>
- *    config/ibatis/#resource.dialect#/SqlMap-DOG.xml == RESOURCE 1
- *    config/ibatis/#resource.dialect#/SqlMap-CAT.xml == RESOURCE 3
+ *    contentDAO.bean.dialect == BEAN 1
+ *    propertyValueDAO.bean.dialect == BEAN 3
  * </pre>
  * 
  * @author Derek Hulley
- * @since 3.2 (Mobile)
+ * @since 3.2SP1
  */
-public class HierarchicalResourceLoader extends DefaultResourceLoader implements InitializingBean
+public class HierarchicalBeanLoader
+    implements InitializingBean, FactoryBean, ApplicationContextAware
 {
-    public static final String DEFAULT_DIALECT_PLACEHOLDER = "#resource.dialect#";
-    public static final String DEFAULT_DIALECT_REGEX = "\\#resource\\.dialect\\#";
+    public static final String DEFAULT_DIALECT_PLACEHOLDER = "#bean.dialect#";
+    public static final String DEFAULT_DIALECT_REGEX = "\\#bean\\.dialect\\#";
     
+    private ApplicationContext ctx;
+    private String targetBeanName;
+    private Class<?> targetClass;
     private String dialectBaseClass;
     private String dialectClass;
     
     /**
      * Create a new HierarchicalResourceLoader.
      */
-    public HierarchicalResourceLoader()
+    public HierarchicalBeanLoader()
     {
         super();
+    }
+
+    /**
+     * The application context that this bean factory serves.
+     */
+    public void setApplicationContext(ApplicationContext ctx)
+    {
+        this.ctx = ctx;
+    }
+
+    /**
+     * @param targetBeanName        the name of the target bean to return,
+     *                              including the {@link #DEFAULT_DIALECT_PLACEHOLDER}
+     *                              where the specific dialect must be replaced.
+     */
+    public void setTargetBeanName(String targetBeanName)
+    {
+        this.targetBeanName = targetBeanName;
+    }
+
+    /**
+     * Set the target class that will be returned by {@link #getObjectType()}
+     * 
+     * @param targetClass           the type that this factory returns
+     */
+    public void setTargetClass(Class<?> targetClass)
+    {
+        this.targetClass = targetClass;
     }
 
     /**
@@ -93,27 +132,39 @@ public class HierarchicalResourceLoader extends DefaultResourceLoader implements
     {
         this.dialectClass = className;
     }
-
+    
     public void afterPropertiesSet() throws Exception
     {
+        PropertyCheck.mandatory(this, "targetBeanName", targetBeanName);
+        PropertyCheck.mandatory(this, "targetClass", targetClass);
         PropertyCheck.mandatory(this, "dialectBaseClass", dialectBaseClass);
         PropertyCheck.mandatory(this, "dialectClass", dialectClass);
     }
     
     /**
-     * Get a resource using the defined class hierarchy as a search path.
-     * 
-     * @param location          the location including a {@link #DEFAULT_DIALECT_PLACEHOLDER placeholder}
-     * @return                  a resource found by successive searches using class name replacement, or
-     *                          <tt>null</tt> if not found.
+     * @return          Returns {@link #setTargetClass(Class) target class}
      */
-    @SuppressWarnings("unchecked")
-    @Override
-    public Resource getResource(String location)
+    public Class<?> getObjectType()
+    {
+        return targetClass;
+    }
+
+    /**
+     * @return          Returns <tt>true</tt> always
+     */
+    public boolean isSingleton()
+    {
+        return true;
+    }
+
+    /**
+     * Replaces the 
+     */
+    public Object getObject() throws Exception
     {
         if (dialectClass == null || dialectBaseClass == null)
         {
-            return super.getResource(location);
+            ctx.getBean(targetBeanName);
         }
         
         // If a property value has not been substituted, extract the property name and load from system
@@ -130,7 +181,7 @@ public class HierarchicalResourceLoader extends DefaultResourceLoader implements
             dialectClassStr = System.getProperty(prop, dialectClass);
         }
 
-        Class dialectBaseClazz;
+        Class<?> dialectBaseClazz;
         try
         {
             dialectBaseClazz = Class.forName(dialectBaseClassStr);
@@ -139,7 +190,7 @@ public class HierarchicalResourceLoader extends DefaultResourceLoader implements
         {
             throw new RuntimeException("Dialect base class not found: " + dialectBaseClassStr);
         }
-        Class dialectClazz;
+        Class<?> dialectClazz;
         try
         {
             dialectClazz = Class.forName(dialectClassStr);
@@ -165,24 +216,27 @@ public class HierarchicalResourceLoader extends DefaultResourceLoader implements
         if (!dialectBaseClazz.isAssignableFrom(dialectClazz))
         {
             throw new RuntimeException(
-                    "Non-existent HierarchicalResourceLoader hierarchy: " +
+                    "Non-existent HierarchicalBeanLoader hierarchy: " +
                     dialectBaseClazz.getName() + " is not a superclass of " + dialectClazz);
         }
         
         Class<? extends Object> clazz = dialectClazz;
-        Resource resource = null;
-        while (resource == null)
+        Object bean = null;
+        while (bean == null)
         {
             // Do replacement
-            String newLocation = location.replaceAll(DEFAULT_DIALECT_REGEX, clazz.getName());
-            resource = super.getResource(newLocation);
-            if (resource != null && resource.exists())
+            String newBeanName = targetBeanName.replaceAll(DEFAULT_DIALECT_REGEX, clazz.getName());
+            try
             {
-                // Found
+                bean = ctx.getBean(newBeanName);
+                // Found it
                 break;
             }
+            catch (NoSuchBeanDefinitionException e)
+            {
+            }
             // Not found
-            resource = null;
+            bean = null;
             // Are we at the base class?
             if (clazz.equals(dialectBaseClazz))
             {
@@ -194,10 +248,10 @@ public class HierarchicalResourceLoader extends DefaultResourceLoader implements
             if (clazz == null)
             {
                 throw new RuntimeException(
-                        "Non-existent HierarchicalResourceLoaderBean hierarchy: " +
+                        "Non-existent HierarchicalBeanLoaderBean hierarchy: " +
                         dialectBaseClazz.getName() + " is not a superclass of " + dialectClazz);
             }
         }
-        return resource;
+        return bean;
     }
 }
