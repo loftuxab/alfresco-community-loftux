276032a0a3592c657bead9aba950f19cc8a017fe
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-04-20 16:19:44 +0000

    Added amps-share dir in zip/tar bundles (fix ALF-2501)
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@19921 7033f55c-6c29-4ad8-afba-765cb616e738

--- root/projects/installer/linux/installer/apply_amps.sh
+++ root/projects/installer/linux/installer/apply_amps.sh
@@ -7,7 +7,7 @@ export JAVA_HOME="@@JAVA_HOME@@"
 export PATH=$PATH:$JAVA_HOME/bin
 cd "$ALF_HOME"
 export CATALINA_HOME="$ALF_HOME"/tomcat
-echo "This script will apply all the AMPs in ./amps to the alfresco.war and share.war files in $CATALINA_HOME\webapps"
+echo "This script will apply all the AMPs in ./amps to the alfresco.war and share.war files in $CATALINA_HOME/webapps"
 echo "Press control-c to stop this script . . ."
 echo "Press any other key to continue . . ."
 read RESP
