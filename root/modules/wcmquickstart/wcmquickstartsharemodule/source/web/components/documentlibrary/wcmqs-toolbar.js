68e3284c491c7400c20487fd77fd5be2fa34aacc
mhatfield <mhatfield@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-09-07 00:38:45 +0000

    WCM-QS: Refactoring of custom Document Library "Preview" and toolbar "New Article" actions. Populated missing component bindings. Reinstated Document Library container custom type casting from r22113. Fix to core action code when Replication data not supplied.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@22281 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: f6ae16790915053bfd24c129767399be0d618b32

--- root/modules/wcmquickstart/wcmquickstartsharemodule/source/web/components/documentlibrary/wcmqs-toolbar.js
+++ root/modules/wcmquickstart/wcmquickstartsharemodule/source/web/components/documentlibrary/wcmqs-toolbar.js
@@ -1,82 +1,41 @@
 (function()
 {
-	Alfresco.DocumentList.prototype.onReady = function WCMQS_toolbar_onReady()
-    {
-       // New Content menu button
-       this.widgets.createContent = Alfresco.util.createYUIButton(this, "createContent-button", this.onCreateContent,
-       {
-          type: "menu", 
-          menu: "createContent-menu",
-          lazyloadmenu: false,
-          disabled: true,
-          value: "create"
-       });
+   /**
+    * Store reference to onReady() function to allow extension.
+    *
+    * @method onReady_WCMQS
+    */
+   Alfresco.DocListToolbar.prototype.onReady_WCMQS = Alfresco.DocListToolbar.prototype.onReady;
 
-       this.widgets.newArticleButton = Alfresco.util.createYUIButton(this, "newArticle-button", this.onNewArticle,
-       {
-          disabled: true,
-          value : "create"
-       });
-       
-       // New Folder button: user needs "create" access
-       this.widgets.newFolder = Alfresco.util.createYUIButton(this, "newFolder-button", this.onNewFolder,
-       {
-          disabled: true,
-          value: "create"
-       });
-       
-       // File Upload button: user needs  "create" access
-       this.widgets.fileUpload = Alfresco.util.createYUIButton(this, "fileUpload-button", this.onFileUpload,
-       {
-          disabled: true,
-          value: "create"
-       });
+   /**
+    * Extend prototype onReady() function to allow creation of "New Article" button.
+    *
+    * @method onReady
+    */
+   Alfresco.DocListToolbar.prototype.onReady = function WCMQS_toolbar_onReady()
+   {
+      // New Article button: user needs "create" access
+      this.widgets.newArticleButton = Alfresco.util.createYUIButton(this, "newArticle-button", this.onNewArticle,
+      {
+         disabled: true,
+         value : "create"
+      });
 
-       // Selected Items menu button
-       this.widgets.selectedItems = Alfresco.util.createYUIButton(this, "selectedItems-button", this.onSelectedItems,
-       {
-          type: "menu", 
-          menu: "selectedItems-menu",
-          lazyloadmenu: false,
-          disabled: true
-       });
-
-       // Customize button
-       this.widgets.customize = Alfresco.util.createYUIButton(this, "customize-button", this.onCustomize);
-
-       // Hide/Show NavBar button
-       this.widgets.hideNavBar = Alfresco.util.createYUIButton(this, "hideNavBar-button", this.onHideNavBar);
-       this.widgets.hideNavBar.set("label", this.msg(this.options.hideNavBar ? "button.navbar.show" : "button.navbar.hide"));
-       Dom.setStyle(this.id + "-navBar", "display", this.options.hideNavBar ? "none" : "block");
-       
-       // RSS Feed link button
-       this.widgets.rssFeed = Alfresco.util.createYUIButton(this, "rssFeed-button", null, 
-       {
-          type: "link"
-       });
-
-       // Folder Up Navigation button
-       this.widgets.folderUp =  Alfresco.util.createYUIButton(this, "folderUp-button", this.onFolderUp,
-       {
-          disabled: true
-       });
-
-       // DocLib Actions module
-       this.modules.actions = new Alfresco.module.DoclibActions(this.options.workingMode);
-       
-       // Reference to Document List component
-       this.modules.docList = Alfresco.util.ComponentManager.findFirst("Alfresco.DocumentList");
-
-       // Preferences service
-       this.services.preferences = new Alfresco.service.Preferences();
-
-       // Finally show the component body here to prevent UI artifacts on YUI button decoration
-       Dom.setStyle(this.id + "-body", "visibility", "visible");
-    },
-	
-	Alfresco.DocumentList.prototype.onNewArticle = function WCMQS_toolbar_onNewArticle(e, p_obj)
-	{
-    	alert("Hello World!");
-	   
-	};
+      this.onReady_WCMQS.apply(this, arguments);
+   },
+   
+   /**
+    * New Article button click handler
+    *
+    * @method onNewArticle
+    * @param e {object} DomEvent
+    * @param p_obj {object} Object passed back from addListener method
+    */
+   Alfresco.DocListToolbar.prototype.onNewArticle = function WCMQS_toolbar_onNewArticle(e, p_obj)
+   {
+      Alfresco.util.PopupManager.displayMessage(
+      {
+         text: "Not implemented"
+      });
+   };
 })();
