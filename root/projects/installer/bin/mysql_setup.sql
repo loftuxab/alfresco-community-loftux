e23373c18970125baefecdaae01c20d60629f267
pholmeshiggin <pholmeshiggin@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-09-08 10:47:15 +0000

    Added apply_amps scripts and other minor cleanups
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@22324 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: d496c91fc67870daa9910d5ba2730fb19f50b89c

--- root/projects/installer/bin/mysql_setup.sql
+++
@@ -1,3 +0,0 @@
-create database alfresco default character set utf8 collate utf8_bin;
-grant all on alfresco.* to 'alfresco'@'localhost' identified by 'alfresco' with grant option;
-grant all on alfresco.* to 'alfresco'@'localhost.localdomain' identified by 'alfresco' with grant option;
