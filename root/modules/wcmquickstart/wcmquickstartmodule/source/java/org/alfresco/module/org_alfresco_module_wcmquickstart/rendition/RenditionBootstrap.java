6a67a9280995d253cbaf6e16085e38b8daf564ae
bremmington <bremmington@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-09-17 14:05:30 +0000

    ALF-4833 (WCM-QS): - RenditionBootstrap now wraps initialisation inside a transaction and is sensitive to the repo being read-only. - Added Quick Start components to the clean-modules build target
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@22617 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 0b262aa25c6425390e312f0a7171de57e8aec0c0

--- root/modules/wcmquickstart/wcmquickstartmodule/source/java/org/alfresco/module/org_alfresco_module_wcmquickstart/rendition/RenditionBootstrap.java
+++ root/modules/wcmquickstart/wcmquickstartmodule/source/java/org/alfresco/module/org_alfresco_module_wcmquickstart/rendition/RenditionBootstrap.java
@@ -27,6 +27,7 @@ import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.module.org_alfresco_module_wcmquickstart.model.WebSiteModel;
 import org.alfresco.repo.rendition.RenditionDefinitionImpl;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
 import org.alfresco.service.cmr.action.ParameterDefinition;
 import org.alfresco.service.cmr.dictionary.DataTypeDefinition;
 import org.alfresco.service.cmr.dictionary.DictionaryService;
@@ -37,6 +38,7 @@ import org.alfresco.service.cmr.rendition.RenditionService;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.repository.datatype.DefaultTypeConverter;
 import org.alfresco.service.namespace.QName;
+import org.alfresco.service.transaction.TransactionService;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -50,177 +52,198 @@ import org.springframework.extensions.surf.util.AbstractLifecycleBean;
  * 
  * @author Roy Wetherall
  */
-public class RenditionBootstrap implements ApplicationContextAware, 
-										   ApplicationListener<ApplicationContextEvent>,
-										   WebSiteModel
+public class RenditionBootstrap implements ApplicationContextAware, ApplicationListener<ApplicationContextEvent>,
+        WebSiteModel
 {
-	/** Bean lifecycle object */
-	private Lifecycle lifecycle = new Lifecycle();
-	
-	/** Node service */
-	private NodeService nodeService;
-	
-	/** Rendition service */
-	private RenditionService renditionService;
-	
-	/** Dictionary service */
-	private DictionaryService dictionaryService;
-	
-	/** List of bootstrap rendition definitions */
-	private List<BootstrapRenditionDefinition> definitions;
-	
-	/**
-	 * Set the node service
-	 * @param nodeService	node service
-	 */
-	public void setNodeService(NodeService nodeService)
+    /** Bean lifecycle object */
+    private Lifecycle lifecycle = new Lifecycle();
+
+    /** Node service */
+    private NodeService nodeService;
+
+    /** Rendition service */
+    private RenditionService renditionService;
+
+    /** Dictionary service */
+    private DictionaryService dictionaryService;
+
+    /** Transaction service */
+    private TransactionService transactionService;
+
+    /** List of bootstrap rendition definitions */
+    private List<BootstrapRenditionDefinition> definitions;
+
+    /**
+     * Set the node service
+     * 
+     * @param nodeService
+     *            node service
+     */
+    public void setNodeService(NodeService nodeService)
     {
-	    this.nodeService = nodeService;
+        this.nodeService = nodeService;
     }
-	
-	/**
-	 * Sets the rendition service
-	 * @param renditionService	rendition service
-	 */
-	public void setRenditionService(RenditionService renditionService)
+
+    /**
+     * Sets the rendition service
+     * 
+     * @param renditionService
+     *            rendition service
+     */
+    public void setRenditionService(RenditionService renditionService)
     {
-	    this.renditionService = renditionService;
+        this.renditionService = renditionService;
     }
-	
-	/**
-	 * Sets the dictionary service
-	 * @param dictionaryService	dictionary service
-	 */
-	public void setDictionaryService(DictionaryService dictionaryService)
+
+    /**
+     * Sets the dictionary service
+     * 
+     * @param dictionaryService
+     *            dictionary service
+     */
+    public void setDictionaryService(DictionaryService dictionaryService)
     {
-	    this.dictionaryService = dictionaryService;
+        this.dictionaryService = dictionaryService;
     }
-	
-	/**
-	 * Set the list of bootstrap rendition definitions
-	 * @param definitions	list of bootstrap rendition definitions
-	 */
-	public void setDefinitions(List<BootstrapRenditionDefinition> definitions)
+
+    public void setTransactionService(TransactionService transactionService)
     {
-	    this.definitions = definitions;
+        this.transactionService = transactionService;
     }
-	
-	/**
-	 * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
-	 */
-	@Override
+
+    /**
+     * Set the list of bootstrap rendition definitions
+     * 
+     * @param definitions
+     *            list of bootstrap rendition definitions
+     */
+    public void setDefinitions(List<BootstrapRenditionDefinition> definitions)
+    {
+        this.definitions = definitions;
+    }
+
+    /**
+     * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
+     */
+    @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
     {
-		lifecycle.setApplicationContext(applicationContext);
+        lifecycle.setApplicationContext(applicationContext);
     }
-	
-	/**
-	 * Initialises the rendition definitions into the repository.
-	 */
-	private void initRenditions()
-	{
-		if (definitions != null)
-		{			
-			for (BootstrapRenditionDefinition definition : definitions)
-            {	
-				// Clear any existing defintions with the same name
-				RenditionDefinition existingRenDef = renditionService.loadRenditionDefinition(definition.getQName());
-				if (existingRenDef != null)
-				{
-					nodeService.deleteNode(existingRenDef.getNodeRef());
-				}
-				
-				// Get the rendition definition
-				RenditionDefinition renDef = getRenditionDefinition(definition);
 
-				// Save the rendition
-				renditionService.saveRenditionDefinition(renDef);
-            }
-		}
-	}
-	
-	/**
-	 * 
-	 * @param definition
-	 * @return
-	 */
-	private RenditionDefinition getRenditionDefinition(BootstrapRenditionDefinition definition)
-	{
-		RenditionDefinition renDef = null;
-		
-		// Get the rendering engine
-		RenderingEngineDefinition engine = renditionService.getRenderingEngineDefinition(definition.getRenderingEngineName());
-		if (engine == null)
-		{
-			throw new AlfrescoRuntimeException("Rendering engine " + definition.getRenderingEngineName() + " does not exist.");
-		}
-		
-		// Set the child rendition definitions
-		if (definition instanceof BootstrapCompositeRenditionDefinition)
-		{
-			renDef = renditionService.createCompositeRenditionDefinition(definition.getQName());
-			
-			List<BootstrapRenditionDefinition> childDefs = ((BootstrapCompositeRenditionDefinition)definition).getDefinitions();
-			for (BootstrapRenditionDefinition childDef : childDefs)
-            {
-				RenditionDefinition childRenDef = getRenditionDefinition(childDef);
-				((CompositeRenditionDefinition)renDef).addAction(childRenDef);
-            }
-		}
-		else
-		{
-			renDef = renditionService.createRenditionDefinition(definition.getQName(), definition.getRenderingEngineName());
-		}
-		
-		// Set async 				
-		renDef.setExecuteAsynchronously(definition.isExecuteAsynchronously());
-		
-		// Set parameters
-		Map<String, Serializable> convertedValues = new HashMap<String, Serializable>(definition.getParameters().size());
-		convertedValues.put(RenditionDefinitionImpl.RENDITION_DEFINITION_NAME, definition.getQName());
-		for (Map.Entry<String, Serializable> entry : definition.getParameters().entrySet())
+    /**
+     * Initialises the rendition definitions into the repository.
+     */
+    private void initRenditions()
+    {
+        if (definitions != null)
         {
-			String key = entry.getKey();
-			Serializable value = entry.getValue();
-			
-			// Get the parameter definition
-			ParameterDefinition paramDef = engine.getParameterDefintion(key);
-			if (paramDef == null)
-			{
-				throw new AlfrescoRuntimeException("No parameter definition for " + key + " found for rendering engine " + definition.getRenderingEngineName());
-			}
-			
-			// Get the parameter type definition
-			QName paramType = paramDef.getType();
-			DataTypeDefinition paramTypeDef = dictionaryService.getDataType(paramType);
-			if (paramTypeDef == null)
-			{
-				throw new AlfrescoRuntimeException("No type definition for parameter " + key + " with type " + paramType.toString() + " and rendering engine " + definition.getRenderingEngineName());
-			}
-			// Convert the parameter value
-			Serializable convertedValue = (Serializable)DefaultTypeConverter.INSTANCE.convert(paramTypeDef, value);
-			
-			// Set the parameter value
-            convertedValues.put(entry.getKey(), convertedValue);					
-		} 
-		renDef.setParameterValues(convertedValues);
-		
-		return renDef;
-	}
+            for (BootstrapRenditionDefinition definition : definitions)
+            {
+                // Clear any existing defintions with the same name
+                RenditionDefinition existingRenDef = renditionService.loadRenditionDefinition(definition.getQName());
+                if (existingRenDef != null)
+                {
+                    nodeService.deleteNode(existingRenDef.getNodeRef());
+                }
 
-	/**
-	 * @see org.springframework.context.ApplicationListener#onApplicationEvent(org.springframework.context.ApplicationEvent)
-	 */
-	@Override
+                // Get the rendition definition
+                RenditionDefinition renDef = getRenditionDefinition(definition);
+
+                // Save the rendition
+                renditionService.saveRenditionDefinition(renDef);
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param definition
+     * @return
+     */
+    private RenditionDefinition getRenditionDefinition(BootstrapRenditionDefinition definition)
+    {
+        RenditionDefinition renDef = null;
+
+        // Get the rendering engine
+        RenderingEngineDefinition engine = renditionService.getRenderingEngineDefinition(definition
+                .getRenderingEngineName());
+        if (engine == null)
+        {
+            throw new AlfrescoRuntimeException("Rendering engine " + definition.getRenderingEngineName()
+                    + " does not exist.");
+        }
+
+        // Set the child rendition definitions
+        if (definition instanceof BootstrapCompositeRenditionDefinition)
+        {
+            renDef = renditionService.createCompositeRenditionDefinition(definition.getQName());
+
+            List<BootstrapRenditionDefinition> childDefs = ((BootstrapCompositeRenditionDefinition) definition)
+                    .getDefinitions();
+            for (BootstrapRenditionDefinition childDef : childDefs)
+            {
+                RenditionDefinition childRenDef = getRenditionDefinition(childDef);
+                ((CompositeRenditionDefinition) renDef).addAction(childRenDef);
+            }
+        }
+        else
+        {
+            renDef = renditionService.createRenditionDefinition(definition.getQName(), definition
+                    .getRenderingEngineName());
+        }
+
+        // Set async
+        renDef.setExecuteAsynchronously(definition.isExecuteAsynchronously());
+
+        // Set parameters
+        Map<String, Serializable> convertedValues = new HashMap<String, Serializable>(definition.getParameters().size());
+        convertedValues.put(RenditionDefinitionImpl.RENDITION_DEFINITION_NAME, definition.getQName());
+        for (Map.Entry<String, Serializable> entry : definition.getParameters().entrySet())
+        {
+            String key = entry.getKey();
+            Serializable value = entry.getValue();
+
+            // Get the parameter definition
+            ParameterDefinition paramDef = engine.getParameterDefintion(key);
+            if (paramDef == null)
+            {
+                throw new AlfrescoRuntimeException("No parameter definition for " + key
+                        + " found for rendering engine " + definition.getRenderingEngineName());
+            }
+
+            // Get the parameter type definition
+            QName paramType = paramDef.getType();
+            DataTypeDefinition paramTypeDef = dictionaryService.getDataType(paramType);
+            if (paramTypeDef == null)
+            {
+                throw new AlfrescoRuntimeException("No type definition for parameter " + key + " with type "
+                        + paramType.toString() + " and rendering engine " + definition.getRenderingEngineName());
+            }
+            // Convert the parameter value
+            Serializable convertedValue = (Serializable) DefaultTypeConverter.INSTANCE.convert(paramTypeDef, value);
+
+            // Set the parameter value
+            convertedValues.put(entry.getKey(), convertedValue);
+        }
+        renDef.setParameterValues(convertedValues);
+
+        return renDef;
+    }
+
+    /**
+     * @see org.springframework.context.ApplicationListener#onApplicationEvent(org.springframework.context.ApplicationEvent)
+     */
+    @Override
     public void onApplicationEvent(ApplicationContextEvent event)
     {
-		lifecycle.onApplicationEvent(event);
+        lifecycle.onApplicationEvent(event);
     }
-	
-	/**
-	 * Lifecycle implementation
-	 */
-	private class Lifecycle extends AbstractLifecycleBean
+
+    /**
+     * Lifecycle implementation
+     */
+    private class Lifecycle extends AbstractLifecycleBean
     {
         /**
          * @see org.alfresco.util.AbstractLifecycleBean#onBootstrap(org.springframework.context.ApplicationEvent)
@@ -228,17 +251,28 @@ public class RenditionBootstrap implements ApplicationContextAware,
         @Override
         protected void onBootstrap(ApplicationEvent event)
         {
-        	AuthenticationUtil.runAs(new AuthenticationUtil.RunAsWork<Object>()
-			{
-				@Override
-                public Object doWork() throws Exception
+            if (!transactionService.isReadOnly())
+            {
+                AuthenticationUtil.runAs(new AuthenticationUtil.RunAsWork<Object>()
                 {
-					initRenditions();
-	                return null;
-                }
-			}, AuthenticationUtil.getSystemUserName());            
+                    @Override
+                    public Object doWork() throws Exception
+                    {
+                        return transactionService.getRetryingTransactionHelper().doInTransaction(
+                                new RetryingTransactionCallback<Object>()
+                        {
+                            @Override
+                            public Object execute() throws Throwable
+                            {
+                                initRenditions();
+                                return null;
+                            }
+                        });
+                    }
+                }, AuthenticationUtil.getSystemUserName());
+            }
         }
-    
+
         /**
          * @see org.alfresco.util.AbstractLifecycleBean#onShutdown(org.springframework.context.ApplicationEvent)
          */
