a2776daf6804b65d50f39df1fa0d17d530b885da
bremmington <bremmington@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-10-05 12:21:01 +0000

    WebQS: - ALF-5085  Web Editor "sticking" on or off irrespective of which site (editorial or live) is being accessed
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@22885 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: e1960bccc6061a94e2d08984294bfe6df755aba7

--- root/modules/wcmquickstart/wcmquickstartwebsite/source/java/org/alfresco/wcm/client/directive/AbstractTemplateDirective.java
+++ root/modules/wcmquickstart/wcmquickstartwebsite/source/java/org/alfresco/wcm/client/directive/AbstractTemplateDirective.java
@@ -33,60 +33,45 @@ import freemarker.template.TemplateModelException;
  */
 public abstract class AbstractTemplateDirective implements TemplateDirectiveModel
 {
-	public static final long serialVersionUID = 4251970922970982753L;
+    public static final long serialVersionUID = 4251970922970982753L;
 
     protected String urlPrefix = null;
-    protected Boolean editingEnabled = null;
-    protected Boolean debugEnabled = null;
-    
+
     protected HttpServletRequest getRequest(Environment env) throws TemplateModelException
     {
-    	return ((HttpRequestHashModel)env.getDataModel().get("Request")).getRequest();    	
+        return ((HttpRequestHashModel) env.getDataModel().get("Request")).getRequest();
     }
-        
+
     /**
      * Determines whether editing is currently enabled
      * 
      * @return true if editing is enabled
-     * @throws TemplateModelException 
+     * @throws TemplateModelException
      */
     protected boolean isEditingEnabled(Environment env) throws TemplateModelException
     {
-        if (this.editingEnabled == null)
+        boolean editingEnabled = false;
+        HttpServletRequest request = getRequest(env);
+        Object enabledKey = request.getAttribute(TemplateConstants.REQUEST_ATTR_KEY_WEF_ENABLED);
+        if (enabledKey != null && (enabledKey instanceof Boolean))
         {
-        	HttpServletRequest request = getRequest(env);
-            Object enabledKey = request.getAttribute(TemplateConstants.REQUEST_ATTR_KEY_WEF_ENABLED);
-            if (enabledKey != null && enabledKey instanceof Boolean)
-            {
-                this.editingEnabled = (Boolean) enabledKey;
-            }
-            else
-            {
-                // try to load from properties file
-                this.editingEnabled = TemplateProps.isEditingEnabled();
-                if (this.editingEnabled == null)
-                {
-                    // set default value
-                    this.editingEnabled = Boolean.FALSE;
-                }
-            }
+            editingEnabled = (Boolean) enabledKey;
         }
-
-        return this.editingEnabled;
+        return editingEnabled;
     }
 
     /**
      * Returns the URL prefix for the web editor application.
      * 
      * @return The WEF prefix URL
-     * @throws TemplateModelException 
+     * @throws TemplateModelException
      */
     protected String getWebEditorUrlPrefix(Environment env) throws TemplateModelException
     {
         if (this.urlPrefix == null)
         {
-        	HttpServletRequest request = getRequest(env);
-            String prefix = (String)request.getAttribute(TemplateConstants.REQUEST_ATTR_KEY_URL_PREFIX);
+            HttpServletRequest request = getRequest(env);
+            String prefix = (String) request.getAttribute(TemplateConstants.REQUEST_ATTR_KEY_URL_PREFIX);
             if (prefix != null && prefix.length() > 0)
             {
                 this.urlPrefix = prefix;
@@ -109,56 +94,52 @@ public abstract class AbstractTemplateDirective implements TemplateDirectiveMode
     /**
      * Determines whether debug is enabled for the web editor application
      * <p>
-     * This is the value of the <code>debug</code> init
-     * parameter of the Web Editor filter definition in web.xml.
-     * If the init parameter is not present false will be returned.
+     * This is the value of the <code>debug</code> init parameter of the Web
+     * Editor filter definition in web.xml. If the init parameter is not present
+     * false will be returned.
      * </p>
      * 
      * @return true if debug is enabled
-     * @throws TemplateModelException 
+     * @throws TemplateModelException
      */
     protected boolean isDebugEnabled(Environment env) throws TemplateModelException
     {
-        if (this.debugEnabled == null)
+        boolean debugEnabled = false;
+        HttpServletRequest request = getRequest(env);
+        Object debug = request.getAttribute(TemplateConstants.REQUEST_ATTR_KEY_DEBUG_ENABLED);
+        if ((debug != null) && (debug instanceof Boolean))
         {
-        	HttpServletRequest request = getRequest(env);
-            Object debug = request.getAttribute(TemplateConstants.REQUEST_ATTR_KEY_DEBUG_ENABLED);
-            if (debug != null && debug instanceof Boolean)
-            {
-                this.debugEnabled = (Boolean)debug;
-            }
-            else
-            {
-                // load from properties file
-                this.debugEnabled = TemplateProps.isDebugEnabled();
-                if (this.debugEnabled == null)
-                {
-                    // set default value
-                    this.debugEnabled = Boolean.FALSE;
-                }
-            }
-
+            debugEnabled = (Boolean) debug;
         }
-
-        return this.debugEnabled.booleanValue();
+        else
+        {
+            // load from properties file
+            Boolean prop = TemplateProps.isDebugEnabled();
+            if (prop != null)
+            {
+                debugEnabled = prop;
+            }
+        }
+        return debugEnabled;
     }
 
     /**
      * Determins the location of the toolbar
      * 
      * @return string location
-     * @throws TemplateModelException 
+     * @throws TemplateModelException
      */
     protected String getToolbarLocation(Environment env) throws TemplateModelException
     {
-    	HttpServletRequest request = getRequest(env);
+        HttpServletRequest request = getRequest(env);
         return (String) request.getAttribute(TemplateConstants.REQUEST_ATTR_KEY_TOOLBAR_LOCATION);
     }
-        
+
     /**
      * Encodes the given string, so that it can be used within an HTML page.
      * 
-     * @param string     the String to convert
+     * @param string
+     *            the String to convert
      */
     protected String encode(String string)
     {
@@ -167,7 +148,7 @@ public abstract class AbstractTemplateDirective implements TemplateDirectiveMode
             return "";
         }
 
-        StringBuilder sb = null;      // create on demand
+        StringBuilder sb = null; // create on demand
         String enc;
         char c;
         for (int i = 0; i < string.length(); i++)
@@ -176,23 +157,39 @@ public abstract class AbstractTemplateDirective implements TemplateDirectiveMode
             c = string.charAt(i);
             switch (c)
             {
-                case '"': enc = "&quot;"; break;    //"
-                case '&': enc = "&amp;"; break;     //&
-                case '<': enc = "&lt;"; break;      //<
-                case '>': enc = "&gt;"; break;      //>
+            case '"':
+                enc = "&quot;";
+                break; // "
+            case '&':
+                enc = "&amp;";
+                break; // &
+            case '<':
+                enc = "&lt;";
+                break; // <
+            case '>':
+                enc = "&gt;";
+                break; // >
 
-                case '\u20AC': enc = "&euro;";  break;
-                case '\u00AB': enc = "&laquo;"; break;
-                case '\u00BB': enc = "&raquo;"; break;
-                case '\u00A0': enc = "&nbsp;"; break;
+            case '\u20AC':
+                enc = "&euro;";
+                break;
+            case '\u00AB':
+                enc = "&laquo;";
+                break;
+            case '\u00BB':
+                enc = "&raquo;";
+                break;
+            case '\u00A0':
+                enc = "&nbsp;";
+                break;
 
-                default:
-                    if (((int)c) >= 0x80)
-                    {
-                        //encode all non basic latin characters
-                        enc = "&#" + ((int)c) + ";";
-                    }
-                    break;
+            default:
+                if (((int) c) >= 0x80)
+                {
+                    // encode all non basic latin characters
+                    enc = "&#" + ((int) c) + ";";
+                }
+                break;
             }
 
             if (enc != null)
