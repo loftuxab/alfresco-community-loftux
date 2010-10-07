fe4dad617ab96444e7ffe182f676f2a2b579234d
bremmington <bremmington@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-10-07 07:40:46 +0000

    WebQS: - Tidying of tests
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@22936 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: cb595fa816983c7256c17a780b2800dd2c46e6dc

--- root/modules/wcmquickstart/wcmquickstartclientapi/source/test/java/org/alfresco/wcm/client/DictionaryServiceTest.java
+++ root/modules/wcmquickstart/wcmquickstartclientapi/source/test/java/org/alfresco/wcm/client/DictionaryServiceTest.java
@@ -18,49 +18,38 @@
  */
 package org.alfresco.wcm.client;
 
-import junit.framework.TestCase;
-
-import org.alfresco.wcm.client.impl.DictionaryServiceImpl;
-import org.alfresco.wcm.client.impl.SectionFactoryCmisImpl;
-import org.alfresco.wcm.client.util.CmisSessionHelper;
-import org.alfresco.wcm.client.util.CmisSessionPool;
-import org.alfresco.wcm.client.util.impl.CmisSessionPoolImpl;
-import org.alfresco.wcm.client.util.impl.GuestSessionFactoryImpl;
-import org.apache.chemistry.opencmis.client.api.Session;
-import org.apache.commons.pool.impl.GenericObjectPool;
-
 /**
  * Dictionary service test
  * 
  * @author Roy Wetherall
  */
 public class DictionaryServiceTest extends BaseTest
-{	
-	public void testDictionaryService()
-	{
-		assertTrue(dictionaryService.isDocumentSubType(DictionaryService.TYPE_CMIS_DOCUMENT));
-		assertTrue(dictionaryService.isDocumentSubType(DictionaryService.FULL_TYPE_CMIS_DOCUMENT));
-		assertFalse(dictionaryService.isDocumentSubType(DictionaryService.TYPE_CMIS_FOLDER));
-		assertFalse(dictionaryService.isDocumentSubType(DictionaryService.FULL_TYPE_CMIS_FOLDER));
-		assertTrue(dictionaryService.isDocumentSubType("ws:indexPage"));
-		assertTrue(dictionaryService.isDocumentSubType("D:ws:indexPage"));
-		assertFalse(dictionaryService.isDocumentSubType("ws:section"));
-		assertFalse(dictionaryService.isDocumentSubType("F:ws:section"));
-		assertFalse(dictionaryService.isDocumentSubType("jk:junk"));
-		
-		assertFalse(dictionaryService.isFolderSubType(DictionaryService.TYPE_CMIS_DOCUMENT));
-		assertFalse(dictionaryService.isFolderSubType(DictionaryService.FULL_TYPE_CMIS_DOCUMENT));
-		assertTrue(dictionaryService.isFolderSubType(DictionaryService.TYPE_CMIS_FOLDER));
-		assertTrue(dictionaryService.isFolderSubType(DictionaryService.FULL_TYPE_CMIS_FOLDER));
-		assertFalse(dictionaryService.isFolderSubType("ws:indexPage"));
-		assertFalse(dictionaryService.isFolderSubType("D:ws:indexPage"));
-		assertTrue(dictionaryService.isFolderSubType("ws:section"));
-		assertTrue(dictionaryService.isFolderSubType("F:ws:section"));
-		assertFalse(dictionaryService.isFolderSubType("jk:junk"));
-		
-		assertEquals("D:cmis:document", dictionaryService.getParentType("ws:indexPage"));
-		assertEquals("D:cmis:document", dictionaryService.getParentType("D:ws:indexPage"));
-		assertEquals("cmis:document", dictionaryService.getParentType("ws:indexPage", true));
-		assertEquals("cmis:document", dictionaryService.getParentType("D:ws:indexPage", true));
-	}
+{
+    public void testDictionaryService()
+    {
+        assertTrue(dictionaryService.isDocumentSubType(DictionaryService.TYPE_CMIS_DOCUMENT));
+        assertTrue(dictionaryService.isDocumentSubType(DictionaryService.FULL_TYPE_CMIS_DOCUMENT));
+        assertFalse(dictionaryService.isDocumentSubType(DictionaryService.TYPE_CMIS_FOLDER));
+        assertFalse(dictionaryService.isDocumentSubType(DictionaryService.FULL_TYPE_CMIS_FOLDER));
+        assertTrue(dictionaryService.isDocumentSubType("ws:indexPage"));
+        assertTrue(dictionaryService.isDocumentSubType("D:ws:indexPage"));
+        assertFalse(dictionaryService.isDocumentSubType("ws:section"));
+        assertFalse(dictionaryService.isDocumentSubType("F:ws:section"));
+        assertFalse(dictionaryService.isDocumentSubType("jk:junk"));
+
+        assertFalse(dictionaryService.isFolderSubType(DictionaryService.TYPE_CMIS_DOCUMENT));
+        assertFalse(dictionaryService.isFolderSubType(DictionaryService.FULL_TYPE_CMIS_DOCUMENT));
+        assertTrue(dictionaryService.isFolderSubType(DictionaryService.TYPE_CMIS_FOLDER));
+        assertTrue(dictionaryService.isFolderSubType(DictionaryService.FULL_TYPE_CMIS_FOLDER));
+        assertFalse(dictionaryService.isFolderSubType("ws:indexPage"));
+        assertFalse(dictionaryService.isFolderSubType("D:ws:indexPage"));
+        assertTrue(dictionaryService.isFolderSubType("ws:section"));
+        assertTrue(dictionaryService.isFolderSubType("F:ws:section"));
+        assertFalse(dictionaryService.isFolderSubType("jk:junk"));
+
+        assertEquals("D:cmis:document", dictionaryService.getParentType("ws:indexPage"));
+        assertEquals("D:cmis:document", dictionaryService.getParentType("D:ws:indexPage"));
+        assertEquals("cmis:document", dictionaryService.getParentType("ws:indexPage", true));
+        assertEquals("cmis:document", dictionaryService.getParentType("D:ws:indexPage", true));
+    }
 }
