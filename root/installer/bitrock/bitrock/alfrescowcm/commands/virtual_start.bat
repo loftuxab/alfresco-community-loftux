a37915a64c086ec70e726130cc440628e4de7eea
bremmington <bremmington@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-11-17 16:15:03 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@23740 7033f55c-6c29-4ad8-afba-765cb616e738
    Former-commit-id: 9ec1cf62dbf047cb0a2c38b86a533139efbd8865

--- root/installer/bitrock/bitrock/alfrescowcm/commands/virtual_start.bat
+++ root/installer/bitrock/bitrock/alfrescowcm/commands/virtual_start.bat
@@ -3,4 +3,4 @@ rem ---------------------------------------------------------------------------
 rem Start script for the Alfresco Server
 rem ---------------------------------------------------------------------------
 
-call "%~dp0bin\virtual_alf.bat" start
+call "%~dp0virtual_alf.bat" start
