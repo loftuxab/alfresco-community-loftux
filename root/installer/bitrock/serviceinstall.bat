f6ec68801dbfe69c44a622d9c40805ab9f662ca8
ddraper <ddraper@7033f55c-6c29-4ad8-afba-765cb616e738>
2010-12-02 12:47:04 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@24157 7033f55c-6c29-4ad8-afba-765cb616e738
    Former-commit-id: ac74262a4c246e04cba7b98717f38cb8b31cbe7b

--- root/installer/bitrock/serviceinstall.bat
+++ root/installer/bitrock/serviceinstall.bat
@@ -17,13 +17,13 @@ goto end
 
 :remove
 
-if exist @@BITROCK_INSTALLDIR@@\mysql\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\mysql\scripts\serviceinstall.bat)
-if exist @@BITROCK_INSTALLDIR@@\postgresql\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\postgresql\scripts\serviceinstall.bat)
-if exist @@BITROCK_INSTALLDIR@@\apache2\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\apache2\scripts\serviceinstall.bat)
-if exist @@BITROCK_INSTALLDIR@@\tomcat\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\tomcat\scripts\serviceinstall.bat)
-if exist @@BITROCK_INSTALLDIR@@\openoffice\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\openoffice\scripts\serviceinstall.bat)
-if exist @@BITROCK_INSTALLDIR@@\subversion\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\subversion\scripts\serviceinstall.bat)
-rem RUBY_APPLICATION_REMOVE
-if exist @@BITROCK_INSTALLDIR@@\lucene\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\lucene\scripts\serviceinstall.bat)
 if exist @@BITROCK_INSTALLDIR@@\third_application\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\third_application\scripts\serviceinstall.bat)
+if exist @@BITROCK_INSTALLDIR@@\lucene\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\lucene\scripts\serviceinstall.bat)
+rem RUBY_APPLICATION_REMOVE
+if exist @@BITROCK_INSTALLDIR@@\subversion\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\subversion\scripts\serviceinstall.bat)
+if exist @@BITROCK_INSTALLDIR@@\openoffice\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\openoffice\scripts\serviceinstall.bat)
+if exist @@BITROCK_INSTALLDIR@@\tomcat\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\tomcat\scripts\serviceinstall.bat)
+if exist @@BITROCK_INSTALLDIR@@\apache2\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\apache2\scripts\serviceinstall.bat)
+if exist @@BITROCK_INSTALLDIR@@\postgresql\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\postgresql\scripts\serviceinstall.bat)
+if exist @@BITROCK_INSTALLDIR@@\mysql\scripts\serviceinstall.bat (start /MIN @@BITROCK_INSTALLDIR@@\mysql\scripts\serviceinstall.bat)
 :end
