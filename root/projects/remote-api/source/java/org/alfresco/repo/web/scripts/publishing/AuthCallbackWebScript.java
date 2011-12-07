loftuxab/alfresco-community-loftux

commit b78cd3dd83529480e50cb26c2c9a99be4418b0ab

ALF-11550: Added service provider's redirect URL into JSON response from channels.POST and channel-reauth.POST webscripts. Adjusted the ChannelType interface as necessary to make this possible.
git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@32575 7033f55c-6c29-4ad8-afba-765cb616e738

Former-commit-id: dc3764728d08ea80752d9a3b45d80c3c73c60656

root/projects/remote-api/source/java/org/alfresco/repo/web/scripts/publishing/AuthCallbackWebScript.java
@@ -90,7 +90,7 @@ public class AuthCallbackWebScript extends DeclarativeWebScript
         
         if (ChannelType.AuthStatus.RETRY.equals(authStatus))
         {
-            String authoriseUrl = channel.getChannelType().getAuthorisationUrl(channel, channelAuthHelper.getAuthoriseCallbackUrl(channelNodeRef));
+            String authoriseUrl = channel.getChannelType().getAuthorisationUrls(channel, channelAuthHelper.getAuthoriseCallbackUrl(channelNodeRef));
             if (authoriseUrl == null)
             {
                 authoriseUrl = channelAuthHelper.getDefaultAuthoriseUrl(channelNodeRef);

root/projects/remote-api/config/alfresco/templates/webscripts/org/alfresco/repository/publishing/channel-reauth.post.desc.xml
@@ -15,6 +15,7 @@
    <dt>channelId</dt><dd>the identifier of the publishing channel</dd>
    <dt>authoriseUrl</dt> <dd>The URL to send the user to in order for them to authorise access to the channel</dd>
    <dt>authCallbackUrl</dt> <dd>The URL to return the channel authorisation details to</dd>
+   <dt>authRedirectUrl</dt> <dd>The URL that the channel service provider will try to redirect the user to</dd>
    </dl>
    ]]></description>
    <url>/api/publishing/channels/{store_protocol}/{store_id}/{node_id}/reauthorise</url>
@@ -47,6 +48,7 @@
                   "channelId": string,
                   "authoriseUrl": string
                   "authCallbackUrl": string
+                  "authRedirectUrl": string
                }
             }
             ]]>

root/projects/remote-api/config/alfresco/templates/webscripts/org/alfresco/repository/publishing/channels.post.desc.xml
@@ -15,6 +15,7 @@
    <dt>pollUrl</dt> <dd>The URL to poll to discover whether the channel has been authorised</dd>
    <dt>authoriseUrl</dt> <dd>The URL to send the user to in order for them to authorise access to the channel</dd>
    <dt>authCallbackUrl</dt> <dd>The URL to return the channel authorisation details to</dd>
+   <dt>authRedirectUrl</dt> <dd>The URL to which the channel service provider will redirect the user upon authorisation</dd>
    </dl>
    
    ]]></description>
@@ -49,6 +50,7 @@
                   "channelId": string,
                   "authoriseUrl": string
                   "authCallbackUrl": string
+                  "authRedirectUrl": string
                }
             }
             ]]>

root/projects/remote-api/config/alfresco/templates/webscripts/org/alfresco/repository/publishing/publishing.lib.ftl
@@ -153,6 +153,7 @@
       "channelId" : "${channelId}",
       "authoriseUrl": "${authoriseUrl}",
       "authCallbackUrl": "${authCallbackUrl}"
+      "authRedirectUrl": "${authRedirectUrl}"
    }
 </#escape>
 </#macro>

root/projects/remote-api/source/java/org/alfresco/repo/web/scripts/publishing/ChannelAuthHelper.java
@@ -23,6 +23,7 @@ import java.util.TreeMap;
 
 import org.alfresco.repo.admin.SysAdminParams;
 import org.alfresco.service.cmr.publishing.channels.Channel;
+import org.alfresco.service.cmr.publishing.channels.ChannelType;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.util.UrlUtil;
 
@@ -71,8 +72,9 @@ public class ChannelAuthHelper
     
     public Map<String, Object> buildAuthorisationModel(Channel channel)
     {
-        String callbackUrl = getAuthoriseCallbackUrl(channel.getNodeRef());
-        String authoriseUrl = channel.getChannelType().getAuthorisationUrl(channel, callbackUrl);
+        String alfrescoCallbackUrl = getAuthoriseCallbackUrl(channel.getNodeRef());
+        ChannelType.AuthUrlPair authUrlPair = channel.getChannelType().getAuthorisationUrls(channel, alfrescoCallbackUrl); 
+        String authoriseUrl = authUrlPair.authorisationRequestUrl;
         if (authoriseUrl == null)
         {
             // If a channel type returns null as the authorise URL then we
@@ -84,7 +86,8 @@ public class ChannelAuthHelper
         Map<String, Object> model = new TreeMap<String, Object>();
         model.put("authoriseUrl", authoriseUrl);
         model.put("channelId", channel.getId());
-        model.put("authCallbackUrl", callbackUrl);
+        model.put("authCallbackUrl", alfrescoCallbackUrl);
+        model.put("authRedirectUrl", authUrlPair.authorisationRedirectUrl);
         return model;
     }
 }

root/projects/repository/source/java/org/alfresco/repo/publishing/AbstractChannelType.java
@@ -95,11 +95,11 @@ public abstract class AbstractChannelType implements ChannelType, ChannelTypePub
     }
 
     @Override
-    public String getAuthorisationUrl(Channel channel, String callbackUrl)
+    public AuthUrlPair getAuthorisationUrls(Channel channel, String callbackUrl)
     {
-        // Returning a null here to indicate that we should use our own
+        // Returning a null as the authorisation request URL here to indicate that we should use our own
         // credential-gathering mechanism.
-        return null;
+        return new AuthUrlPair(null, callbackUrl);
     }
 
     @Override

root/projects/repository/source/java/org/alfresco/repo/publishing/AbstractOAuth1ChannelType.java
@@ -68,7 +68,7 @@ public abstract class AbstractOAuth1ChannelType<A> extends AbstractChannelType
     }
     
     @Override
-    public String getAuthorisationUrl(Channel channel, String callbackUrl)
+    public AuthUrlPair getAuthorisationUrls(Channel channel, String callbackUrl)
     {
         ParameterCheck.mandatory("channel", channel);
         ParameterCheck.mandatory("callbackUrl", callbackUrl);
@@ -87,7 +87,8 @@ public abstract class AbstractOAuth1ChannelType<A> extends AbstractChannelType
         nodeService.setProperty(channelNodeRef, PublishingModel.PROP_OAUTH1_TOKEN_VALUE, 
                 getEncryptor().encrypt(PublishingModel.PROP_OAUTH1_TOKEN_VALUE, requestToken.getValue()));
 
-        return oauthOperations.buildAuthorizeUrl(requestToken.getValue(), getOAuth1Parameters(callbackUrl));
+        String authUrl = oauthOperations.buildAuthorizeUrl(requestToken.getValue(), getOAuth1Parameters(callbackUrl));
+        return new AuthUrlPair(authUrl, callbackUrl);
     }
     
     @Override

root/projects/repository/source/java/org/alfresco/repo/publishing/MockChannelType.java
@@ -151,8 +151,8 @@ public class MockChannelType extends AbstractChannelType
     }
 
     @Override
-    public String getAuthorisationUrl(Channel channel, String callbackUrl)
+    public AuthUrlPair getAuthorisationUrls(Channel channel, String callbackUrl)
     {
-        return "";
+        return new AuthUrlPair("", "");
     }
 }

commit 28febd451bf08d1cb11e37ebbc3e932bd792a0ce

Try to fix the compile error fallout from r32575 - Auth URLs are now a pair
git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@32590 7033f55c-6c29-4ad8-afba-765cb616e738

Former-commit-id: a0f426973c0e2e668737a72a9dcafbfdd922e470

root/projects/remote-api/source/java/org/alfresco/repo/web/scripts/publishing/AuthCallbackWebScript.java
@@ -27,6 +27,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.alfresco.service.cmr.publishing.channels.Channel;
 import org.alfresco.service.cmr.publishing.channels.ChannelService;
 import org.alfresco.service.cmr.publishing.channels.ChannelType;
+import org.alfresco.service.cmr.publishing.channels.ChannelType.AuthUrlPair;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -90,13 +91,14 @@ public class AuthCallbackWebScript extends DeclarativeWebScript
         
         if (ChannelType.AuthStatus.RETRY.equals(authStatus))
         {
-            String authoriseUrl = channel.getChannelType().getAuthorisationUrls(channel, channelAuthHelper.getAuthoriseCallbackUrl(channelNodeRef));
-            if (authoriseUrl == null)
+            AuthUrlPair authoriseUrls = channel.getChannelType().getAuthorisationUrls(channel, channelAuthHelper.getAuthoriseCallbackUrl(channelNodeRef));
+            String authRequestUrl = authoriseUrls.authorisationRequestUrl;
+            if (authRequestUrl == null)
             {
-                authoriseUrl = channelAuthHelper.getDefaultAuthoriseUrl(channelNodeRef);
+               authRequestUrl = channelAuthHelper.getDefaultAuthoriseUrl(channelNodeRef);
             }
             status.setCode(HttpServletResponse.SC_MOVED_TEMPORARILY);
-            status.setLocation(authoriseUrl);
+            status.setLocation(authRequestUrl);
         }
         Map<String,Object> model = new TreeMap<String, Object>();
         model.put("authStatus", authStatus.name());