loftuxab/alfresco-community-loftux

commit 19feb05193b5646527315e0a87916a0186eb8a60

Fixed ALF-11627 "Collaborator could not revert previous version"
git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@32649 7033f55c-6c29-4ad8-afba-765cb616e738

Former-commit-id: c4040dbce08fd0192e48602d98368311b1059bfd

root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/document-details/document-versions.get.js
@@ -8,7 +8,8 @@ function main()
    var documentDetails = AlfrescoUtil.getNodeDetails(model.nodeRef, model.site);
    if (documentDetails)
    {
-      model.allowNewVersionUpload = documentDetails.item.node.permissions.user["Write"] || false;
+      var userPermissions = documentDetails.item.node.permissions;
+      model.allowNewVersionUpload = (userPermissions["Write"] && userPermissions["Delete"]) || false;
       if (documentDetails.workingCopy && documentDetails.workingCopy.workingCopyVersion)
       {
          model.workingCopyVersion = documentDetails.workingCopy.workingCopyVersion;

commit 0496704d8c1473f61999f8cc291a1e98fa41cf9b

Fixes a bug introduced in r32649: Corrects the property the user permissions are loaded from.
git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@32865 7033f55c-6c29-4ad8-afba-765cb616e738

Former-commit-id: 2a35423cbf29870a06633822e49df0fdf563cbb2

root/projects/slingshot/config/alfresco/site-webscripts/org/alfresco/components/document-details/document-versions.get.js
@@ -8,7 +8,7 @@ function main()
    var documentDetails = AlfrescoUtil.getNodeDetails(model.nodeRef, model.site);
    if (documentDetails)
    {
-      var userPermissions = documentDetails.item.node.permissions;
+      var userPermissions = documentDetails.item.node.permissions.user;
       model.allowNewVersionUpload = (userPermissions["Write"] && userPermissions["Delete"]) || false;
       if (documentDetails.workingCopy && documentDetails.workingCopy.workingCopyVersion)
       {