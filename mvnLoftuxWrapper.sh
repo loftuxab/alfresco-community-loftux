9a1925fd369fa65fa3eb936ddc2143ef33d5dc8d
Peter Lofgren <peter@loftux.se>
2014-07-29 11:54:48 +0200

    Moved to use maven build as this is now default. Removed loftux ant wrapper scripts (they where no longer working due to too many changes in project). Updated pom files with correct version number. Added maven wrapper script that inserts correct build number in version.properties. Related to #1.

---
+++ mvnLoftuxWrapper.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+#Set the name and build number
+versionedition="Community by Loftux AB"
+buildnumber="5.0.a.LX66"
+
+# SCM Revision number -Fetch automatically
+
+scmpath=`git config --get remote.origin.url`
+scmrevision=`git describe --tags --always HEAD`
+echo
+echo "Loftux Maven Wrapper. Helper script for building Alfresco with maven."
+echo
+echo "version-edition: $versionedition"
+echo "build-number: $buildnumber"
+echo "scm-revision: $scmrevision"
+echo "scm-path: $scmpath"
+echo "maven command: $1"
+echo
+echo "Press control-c to stop this script."
+echo "Press any other key to continue."
+read KEY
+echo "Starting build..."
+
+mvn clean $1 -Dversion-edition="$versionedition" -Dbuild-number="$buildnumber" -Dscm-revision="$scmrevision" -Dscm-path="$scmpath"
