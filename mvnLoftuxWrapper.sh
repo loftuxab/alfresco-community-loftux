163a26a1e3c6c71bd1de9a0a20ae40b4f9a588e2
Peter Lofgren <peter@loftux.se>
2014-09-13 21:20:02 +0200

    Changing to 5.0.b.LX68-SNAPSHOT.

--- mvnLoftuxWrapper.sh
+++ mvnLoftuxWrapper.sh
@@ -3,7 +3,7 @@
 
 #Set the name and build number
 versionedition="Community by Loftux AB"
-buildnumber="5.0.b.LX67"
+buildnumber="5.0.b.LX68-SNAPSHOT"
 
 # SCM Revision number -Fetch automatically
 
