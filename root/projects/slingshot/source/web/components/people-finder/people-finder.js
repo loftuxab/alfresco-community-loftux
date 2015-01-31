3b47f2c727e5acfcd05467ca94e4e0911f2038bb
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 11:43:12 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    91958: Merged 5.0.N (5.0.1) to HEAD-BUG-FIX (5.1/Cloud)       91952: Merged V4.2-BUG-FIX (4.2.5) to 5.0.N (5.0.1)          91855: Merged DEV to V4.2-BUG-FIX (4.2.5)             91737: MNT-12889 : German, Dutch: "Add" button goes beyond the margin, when we invite the user to the site.                - Changes size element.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@94850 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: c9dcea6d42943209208264eb464adf1ad255441e

--- root/projects/slingshot/source/web/components/people-finder/people-finder.js
+++ root/projects/slingshot/source/web/components/people-finder/people-finder.js
@@ -576,7 +576,7 @@
          [
             { key: "avatar", label: "Avatar", sortable: false, formatter: this.fnRenderCellAvatar(), width: this.options.viewMode == Alfresco.PeopleFinder.VIEW_MODE_COMPACT ? 36 : 70 },
             { key: "person", label: "Description", sortable: false, formatter: this.fnRenderCellDescription() },
-            { key: "actions", label: "Actions", sortable: false, formatter: this.fnRenderCellActions(), width: 80 }
+            { key: "actions", label: "Actions", sortable: false, formatter: this.fnRenderCellActions(), width: 120 }
          ];
 
          // DataTable definition
