f6dce611e1eb2e306051bd359cfdbd7f606b69b2
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 19:45:11 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    90885: WEBDRONE-834 Implement Delete Blog methods    Added additional news Page and Blog page methods
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@95292 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/share-po/src/main/java/org/alfresco/po/share/wqs/WcmqsNewsPage.java
+++ projects/share-po/src/main/java/org/alfresco/po/share/wqs/WcmqsNewsPage.java
@@ -1,8 +1,5 @@
 package org.alfresco.po.share.wqs;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.alfresco.po.share.ShareLink;
 import org.alfresco.po.share.SharePage;
 import org.alfresco.webdrone.RenderTime;
@@ -15,6 +12,9 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The news page that is opened for a folder (Global Economy, Companies, Markets .... )
  * 
@@ -24,10 +24,34 @@ import org.openqa.selenium.WebElement;
 public class WcmqsNewsPage extends SharePage
 {
 
+    public static final String FTSE_1000 = "FTSE 100 rallies from seven-week low";
+    public static final String GLOBAL_CAR_INDUSTRY = "Global car industry";
+    public static final String FRESH_FLIGHT_TO_SWISS = "Fresh flight to Swiss franc as Europe's bond strains return";
+    public static final String HOUSE_PRICES = "House prices face rollercoaster ride";
+    public static final String EUROPE_DEPT_CONCERNS = "Europe dept concerns ease but bank fears remain";
+    public static final String MEDIA_CONSULT = "Media Consult new site coming out in September";
+    public static final String CHINA_EYES = "China eyes shake-up of bank holding";
+    public static final String MINICARDS_AVAILABLE = "Minicards are now available";
+    public static final String INVESTORS_FEAR = "Investors fear rising risk of US regional defaults";
+    public static final String OUR_NEW_BROCHURE = "Our new brochure is now available";
+
+    public static final String NEWS = "news";
+    public static final String GLOBAL = "global";
+    public static final String COMPANIES = "companies";
+    public static final String MARKETS = "markets";
+
+    private final String ARTICLE_4 = "article4.html";
+    private final String ARTICLE_3 = "article3.html";
+    private final String ARTICLE_2 = "article2.html";
+    private final String ARTICLE_1 = "article1.html";
+    private final String ARTICLE_6 = "article6.html";
+    private final String ARTICLE_5 = "article5.html";
+
     protected static String TITLES_NEWS = "//div[@id='left']//div[@class='interior-content']//a//.././/./.././a";
 
     @RenderWebElement
     private final By PAGE_LOGO = By.cssSelector("#logo>a");
+    private final By NEWS_MENU = By.cssSelector("a[href$='news/']");
 
     /**
      * Constructor.
@@ -173,7 +197,7 @@ public class WcmqsNewsPage extends SharePage
      * @param newsName - the of the news declared in share!
      * @return
      */
-    public void clickNewsTitle(String newsName)
+    public void clickNewsByName(String newsName)
     {
         try
         {
@@ -185,5 +209,50 @@ public class WcmqsNewsPage extends SharePage
         }
 
     }
+
+    /**
+     * Method to click a news title
+     *
+     * @param newsTitle - the of the news declared in share!
+     * @return
+     */
+    public void clickNewsByTitle(String newsTitle)
+    {
+        try
+        {
+            drone.findAndWait(By.xpath(String.format("//a[text()='%s']", newsTitle))).click();
+        }
+        catch (TimeoutException e)
+        {
+            throw new PageOperationException("Exceeded time to find news link. " + e.toString());
+        }
+
+    }
+
+    /**
+     * Method to navigate to news folders
+     *
+     * @param folderName - the Name of the folder from SHARE
+     * @return WcmqsNewsPage
+     */
+    public WcmqsNewsPage openNewsPageFolder(String folderName)
+    {
+        try
+        {
+            WebElement news = drone.findAndWait(NEWS_MENU);
+            drone.mouseOver(news);
+
+            drone.findAndWait(By.cssSelector(String.format("a[href$='/wcmqs/news/%s/']", folderName))).click();
+
+        }
+        catch (TimeoutException e)
+        {
+            throw new PageOperationException("Exceeded time to find news links. " + e.toString());
+        }
+
+        return new WcmqsNewsPage(drone);
+    }
+
+
 
 }
