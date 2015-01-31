71fbffd5b0caf0a4f2591de7978bfe1c52772657
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 21:01:56 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    92719: Merged 5.0.N (5.0.1) to HEAD-BUG-FIX (5.1/Cloud)       92709: WEBDRONE-864 Implement methods verifyNumberOfSearchResultsHeader, isLatestBlogArticlesDisplayed, getWcmqsSearchPagePagination in class WcmqsSearchPage
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@95559 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/share-po/src/main/java/org/alfresco/po/share/wqs/WcmqsSearchPage.java
+++ projects/share-po/src/main/java/org/alfresco/po/share/wqs/WcmqsSearchPage.java
@@ -1,12 +1,14 @@
 package org.alfresco.po.share.wqs;
 
-import org.alfresco.po.share.SharePage;
+import org.alfresco.po.share.site.document.PaginationForm;
 import org.alfresco.webdrone.RenderTime;
 import org.alfresco.webdrone.RenderWebElement;
 import org.alfresco.webdrone.WebDrone;
 import org.alfresco.webdrone.exception.PageException;
+import org.alfresco.webdrone.exception.PageOperationException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
@@ -15,65 +17,110 @@ import java.util.List;
 /**
  * Created by Lucian Tuca on 11/18/2014.
  */
-public class WcmqsSearchPage extends SharePage
+public class WcmqsSearchPage extends WcmqsAbstractPage
 {
-        @RenderWebElement
-        private final By TAG_SEARCH_RESULT_TITLES = By.cssSelector(".newslist-wrapper>li>h4>a");
+    @RenderWebElement
+    private final By TAG_SEARCH_RESULT_TITLES = By.cssSelector(".newslist-wrapper>li>h4>a");
 
-        /**
-         * Constructor.
-         *
-         * @param drone WebDriver to access page
-         */
-        public WcmqsSearchPage(WebDrone drone)
+    private final By NO_OF_SEARCH_RESULTS = By.cssSelector("p.intheader-paragraph");
+    private final By LATEST_BLOG_ARTICLES = By.cssSelector("div[id='right']>div[class='latest-news']");
+    private final By PAGINATION = By.xpath("//div[@class='pagination']");
+
+    /**
+     * Constructor.
+     * 
+     * @param drone WebDriver to access page
+     */
+    public WcmqsSearchPage(WebDrone drone)
+    {
+        super(drone);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public WcmqsSearchPage render(RenderTime timer)
+    {
+        webElementRender(timer);
+        return this;
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public WcmqsSearchPage render()
+    {
+        return render(new RenderTime(maxPageLoadingTime));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public WcmqsSearchPage render(final long time)
+    {
+        return render(new RenderTime(time));
+    }
+
+    /**
+     * Method to get tag search result titles
+     */
+    public ArrayList<String> getTagSearchResults()
+
+    {
+        ArrayList<String> results = new ArrayList<String>();
+        try
         {
-                super(drone);
+            List<WebElement> links = drone.findAndWaitForElements(TAG_SEARCH_RESULT_TITLES);
+            for (WebElement div : links)
+            {
+                results.add(div.getText());
+            }
+        }
+        catch (NoSuchElementException nse)
+        {
+            throw new PageException("Unable to access search results site data", nse);
         }
 
-        @SuppressWarnings("unchecked")
-        @Override
-        public WcmqsSearchPage render(RenderTime timer)
+        return results;
+    }
+
+    /**
+     * Method verifies the number of search results
+     */
+    public boolean verifyNumberOfSearchResultsHeader(int noOfResults, String searchedText)
+    {
+        String resultsText = String.format("Showing %d of %d results for \"%s\" within the website...", noOfResults, noOfResults, searchedText);
+        return resultsText.equals(drone.findAndWait(NO_OF_SEARCH_RESULTS).getText());
+    }
+
+    /**
+     * Method returns if the Latest Blog Articles block is displayed
+     */
+    public boolean isLatestBlogArticlesDisplayed()
+    {
+        try
         {
-                webElementRender(timer);
-                return this;
-
+            drone.findAndWait(LATEST_BLOG_ARTICLES);
+            return true;
         }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public WcmqsSearchPage render()
+        catch (TimeoutException e)
         {
-                return render(new RenderTime(maxPageLoadingTime));
+            throw new PageOperationException("Exceeded time to find Latest Blog Articles block. " + e.toString());
         }
+    }
 
-        @SuppressWarnings("unchecked")
-        @Override
-        public WcmqsSearchPage render(final long time)
+    /**
+     * Method returns the pagination text
+     * @return
+     */
+    public String getWcmqsSearchPagePagination()
+    {
+        try
         {
-                return render(new RenderTime(time));
+            return drone.findAndWait(PAGINATION).getText();
         }
-
-        /**
-         * Method to get tag search result titles
-         */
-        public ArrayList<String> getTagSearchResults()
-
+        catch (TimeoutException e)
         {
-                ArrayList<String> results = new ArrayList<String>();
-                try
-                {
-                        List<WebElement> links = drone.findAndWaitForElements(TAG_SEARCH_RESULT_TITLES);
-                        for (WebElement div : links)
-                        {
-                                results.add(div.getText());
-                        }
-                }
-                catch (NoSuchElementException nse)
-                {
-                        throw new PageException("Unable to access search results site data", nse);
-                }
-
-                return results;
+            throw new PageOperationException("Exceeded time to find Pagination. " + e.toString());
         }
+    }
 
 }
