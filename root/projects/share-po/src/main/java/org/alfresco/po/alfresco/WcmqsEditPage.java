1fcdc047df3e55347d2439322f3c6a868d5e1685
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 18:38:01 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@95188 7033f55c-6c29-4ad8-afba-765cb616e738
    Former-commit-id: f4c7392bf4519fa2e973cf5702b688a710e3f9d6

---
+++ root/projects/share-po/src/main/java/org/alfresco/po/alfresco/WcmqsEditPage.java
@@ -0,0 +1,162 @@
+package org.alfresco.po.alfresco;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.alfresco.webdrone.RenderElement.getVisibleRenderElement;
+
+import org.alfresco.po.share.SharePage;
+import org.alfresco.po.share.exception.ShareException;
+import org.alfresco.po.share.site.document.TinyMceEditor;
+import org.alfresco.webdrone.RenderTime;
+import org.alfresco.webdrone.WebDrone;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+
+public class WcmqsEditPage extends SharePage
+{
+    private Log logger = LogFactory.getLog(this.getClass());
+    private static final By NAME_INPUT = By.cssSelector("input[id='wef-panel-wefPanel_prop_cm_name']");
+    private static final By TITLE_INPUT = By.cssSelector("input[id='wef-panel-wefPanel_prop_cm_title']");
+    private static final By DESCRIPTION_INPUT = By.cssSelector("textarea[id='wef-panel-wefPanel_prop_cm_description']");
+    private static final String CONTENT_IFRAME = "wef-panel-wefPanel_prop_cm_content_ifr";
+    private static final By TEMPLATENAME_INPUT = By.cssSelector("input[id='wef-panel-wefPanel_prop_ws_templateName']");
+    private static final By SUBMIT_BUTTON = By.cssSelector("button[id='wef-panel-wefPanel-form-submit-button']");
+    private static final By CANCEL_BUTTON = By.cssSelector("button[id='wef-panel-wefPanel-form-cancel-button']");
+
+    private final TinyMceEditor contentTinyMceEditor;
+
+    /**
+     * Constructor.
+     */
+    public WcmqsEditPage(WebDrone drone)
+    {
+        super(drone);
+        contentTinyMceEditor = new TinyMceEditor(drone);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public WcmqsEditPage render(RenderTime renderTime)
+    {
+        elementRender(renderTime, getVisibleRenderElement(NAME_INPUT), getVisibleRenderElement(SUBMIT_BUTTON), getVisibleRenderElement(CANCEL_BUTTON));
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public WcmqsEditPage render()
+    {
+        return render(new RenderTime(maxPageLoadingTime));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public WcmqsEditPage render(final long time)
+    {
+        return render(new RenderTime(time));
+    }
+
+    /**
+     * Method for edit name
+     * 
+     * @param userName
+     * @return
+     */
+    public void editName(String newName)
+    {
+        drone.findAndWait(NAME_INPUT).clear();
+        drone.findAndWait(NAME_INPUT, SECONDS.convert(maxPageLoadingTime, MILLISECONDS)).sendKeys(newName);
+    }
+
+    /**
+     * Method for edit title
+     * 
+     * @param password
+     * @return
+     */
+    public void editTitle(String newTitle)
+    {
+        drone.findAndWait(TITLE_INPUT, SECONDS.convert(maxPageLoadingTime, MILLISECONDS)).sendKeys(newTitle);
+    }
+
+    /**
+     * Method for edit Description
+     * 
+     * @param password
+     * @return
+     */
+    public void editDescription(String newDescription)
+    {
+        drone.findAndWait(DESCRIPTION_INPUT, SECONDS.convert(maxPageLoadingTime, MILLISECONDS)).sendKeys(newDescription);
+    }
+
+    /**
+     * Method for edit Template Name
+     * 
+     * @param password
+     * @return
+     */
+    public void editTemplateName(String newTemplateName)
+    {
+        drone.findAndWait(TEMPLATENAME_INPUT, SECONDS.convert(maxPageLoadingTime, MILLISECONDS)).sendKeys(newTemplateName);
+    }
+
+    public WcmqsHomePage clickSubmitButton()
+    {
+        drone.findAndWait(SUBMIT_BUTTON).click();
+        return new WcmqsHomePage(drone);
+    }
+
+    public WcmqsHomePage clickCancelButton()
+    {
+        drone.findAndWait(CANCEL_BUTTON).click();
+        return new WcmqsHomePage(drone);
+    }
+
+    public WcmqsHomePage editArticle(WcmqsArticleDetails articleDetails)
+    {
+        logger.info("Edit and save the asticle");
+        if (articleDetails == null || StringUtils.isEmpty(articleDetails.getName()))
+        {
+            throw new UnsupportedOperationException("Article name cannot be blank");
+        }
+
+        editName(articleDetails.getName());
+        editDescription(articleDetails.getDescription());
+        editTitle(articleDetails.getTitle());
+        editTemplateName(articleDetails.getTemplateName());
+        insertTextInContent(articleDetails.getContent());
+        return clickSubmitButton();
+    }
+
+    /**
+     * Get TinyMCEEditor object to navigate TinyMCE functions.
+     * 
+     * @return
+     */
+    public TinyMceEditor getContentTinyMCEEditor()
+    {
+        contentTinyMceEditor.setTinyMce(CONTENT_IFRAME);
+        return contentTinyMceEditor;
+    }
+
+    /**
+     * Method for inserting text into the Reply form
+     * 
+     * @param txtLines
+     */
+    public void insertTextInContent(String txtLines)
+    {
+        try
+        {
+            contentTinyMceEditor.addContent(txtLines);
+        }
+        catch (TimeoutException toe)
+        {
+            throw new ShareException("Time out finding #tinymce content", toe);
+        }
+    }
+}
---
+++ root/projects/share-po/src/main/java/org/alfresco/po/alfresco/WcmqsHomePage.java
@@ -0,0 +1,161 @@
+package org.alfresco.po.alfresco;
+
+import static org.alfresco.webdrone.RenderElement.getVisibleRenderElement;
+
+import java.util.List;
+
+import org.alfresco.po.share.SharePage;
+import org.alfresco.webdrone.HtmlPage;
+import org.alfresco.webdrone.RenderTime;
+import org.alfresco.webdrone.WebDrone;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+
+public class WcmqsHomePage extends SharePage
+{
+    private static Log logger = LogFactory.getLog(WcmqsHomePage.class);
+    private final By PAGE_MENU = By.cssSelector("div[id='myslidemenu']");
+    private final By HOME_MENU = By.xpath("//div[@id='myslidemenu']//a[text()='Home']");
+    private final By NEWS_MENU = By.cssSelector("a[href$='news/']");
+    private final By PUBLICATIONS_MENU = By.cssSelector("a[href$='publications/']");
+    private final By BLOG_MENU = By.cssSelector("a[href$='blog/']");
+    private final By CONTACT_MENU = By.cssSelector("div.link-menu");
+    private final By FIRST_ARTICLE = By.cssSelector("div[id='left'] div.interior-content ul>li:nth-child(3)>h4>a");
+
+    // private final By BLOG_ARTICLE=By.cssSelector("div[id='left'] div.interior-content div.blog-entry:nth-child(2)>h2>a");
+    // private final By RIGHT_PANEL = By.cssSelector("div[id='right']");
+
+    private final By EDIT_LINK = By.cssSelector("a.alfresco-content-edit");
+    private final By CREATE_LINK = By.cssSelector("a.alfresco-content-new");
+    private final By DELETE_LINK = By.cssSelector("alfresco-content-delete");
+
+    public WcmqsHomePage(WebDrone drone)
+    {
+        super(drone);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public WcmqsHomePage render(RenderTime renderTime)
+    {
+        elementRender(renderTime, getVisibleRenderElement(PAGE_MENU), getVisibleRenderElement(CONTACT_MENU));
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public WcmqsHomePage render()
+    {
+        return render(new RenderTime(maxPageLoadingTime));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public WcmqsHomePage render(final long time)
+    {
+        return render(new RenderTime(time));
+    }
+
+    public WcmqsHomePage selectMenu(String menuOption)
+    {
+        WebElement webElement = null;
+        switch (menuOption.toLowerCase())
+        {
+            case "home":
+            {
+                webElement = drone.findAndWait(HOME_MENU);
+                break;
+            }
+            case "news":
+            {
+                webElement = drone.findAndWait(NEWS_MENU);
+                break;
+            }
+            case "publications":
+            {
+                webElement = drone.findAndWait(PUBLICATIONS_MENU);
+                break;
+            }
+            case "blog":
+            {
+                webElement = drone.findAndWait(BLOG_MENU);
+                break;
+            }
+
+        }
+        try
+        {
+            webElement.click();
+        }
+        catch (TimeoutException e)
+        {
+            throw new TimeoutException("Exceeded time to find and click " + menuOption + " menu. " + e.toString());
+        }
+        return new WcmqsHomePage(drone);
+    }
+
+    public HtmlPage selectFirstArticleFromLeftPanel()
+    {
+        logger.info("Selecting first article from left panel.");
+        List<WebElement> allArticles = drone.findAll(FIRST_ARTICLE);
+        try
+        {
+            allArticles.get(0).click();
+        }
+        catch (TimeoutException e)
+        {
+            throw new TimeoutException("Exceeded time to find and click first article. " + e.toString());
+        }
+
+        WebElement editLink = drone.find(EDIT_LINK);
+        if (!editLink.isDisplayed())
+        {
+            return new WcmqsLoginPage(drone);
+        }
+        else
+        {
+            return this;
+        }
+    }
+
+    public WcmqsEditPage clickEditButton()
+    {
+        try
+        {
+            drone.findAndWait(EDIT_LINK).click();
+            return new WcmqsEditPage(drone);
+        }
+        catch (TimeoutException e)
+        {
+            throw new TimeoutException("Exceeded time to find edit button. " + e.toString());
+        }
+    }
+
+    public void clickCreateButton()
+    {
+        try
+        {
+            drone.findAndWait(CREATE_LINK).click();
+        }
+        catch (TimeoutException e)
+        {
+            throw new TimeoutException("Exceeded time to find edit button. " + e.toString());
+        }
+    }
+
+    public void clickDeleteButton()
+    {
+        try
+        {
+            drone.findAndWait(DELETE_LINK).click();
+        }
+        catch (TimeoutException e)
+        {
+            throw new TimeoutException("Exceeded time to find edit button. " + e.toString());
+        }
+    }
+
+}
