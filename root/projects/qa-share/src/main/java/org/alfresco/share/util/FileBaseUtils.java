1976577d1cfcf082f6ac44346ac2781d8d4a73a2
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 19:45:01 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@95291 7033f55c-6c29-4ad8-afba-765cb616e738
    Former-commit-id: 896583b5d6f3f7107e5527081245504bcd53cbc1

---
+++ root/projects/qa-share/src/main/java/org/alfresco/share/util/FileBaseUtils.java
@@ -0,0 +1,35 @@
+package org.alfresco.share.util;
+
+import java.io.File;
+
+/**
+ * This file will handle actions related to Files, directories
+ * 
+ * @author Paul Brodner
+ */
+public class FileBaseUtils
+{
+    /**
+     * Create the full path of the File
+     * @param paths
+     * @return String
+     */
+    public static String combinePaths(String... paths)
+    {
+        if (paths.length == 0)
+        {
+            return "";
+        }
+
+        File combined = new File(paths[0]);
+
+        int i = 1;
+        while (i < paths.length)
+        {
+            combined = new File(combined, paths[i]);
+            ++i;
+        }
+
+        return combined.getPath();
+    }
+}
