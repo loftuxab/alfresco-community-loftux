a9a1a770615ec4e9d67fd6c86e6c8f8388050e6c
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 18:41:12 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    90403: QA-971 Implemented test cases for Editing items via AWE - from AONE-5613 to AONE-5618
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@95206 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/qa-share/src/test/java/org/alfresco/share/enterprise/wqs/web/awe/EditingItemsViaAWE.java
+++ projects/qa-share/src/test/java/org/alfresco/share/enterprise/wqs/web/awe/EditingItemsViaAWE.java
@@ -2,22 +2,17 @@ package org.alfresco.share.enterprise.wqs.web.awe;
 
 import static org.testng.Assert.assertTrue;
 
-import java.util.List;
-
-import org.alfresco.po.share.AbstractTest;
-import org.alfresco.po.share.Navigation;
+import org.alfresco.po.alfresco.WcmqsArticleDetails;
+import org.alfresco.po.alfresco.WcmqsEditPage;
+import org.alfresco.po.alfresco.WcmqsHomePage;
+import org.alfresco.po.alfresco.WcmqsLoginPage;
+import org.alfresco.po.alfresco.WcmqsNewsArticleDetails;
+import org.alfresco.po.alfresco.WcmqsNewsPage;
 import org.alfresco.po.share.SharePage;
-import org.alfresco.po.share.dashlet.MyCalendarDashlet;
 import org.alfresco.po.share.dashlet.SiteWebQuickStartDashlet;
 import org.alfresco.po.share.dashlet.WebQuickStartOptions;
 import org.alfresco.po.share.enums.Dashlets;
-import org.alfresco.po.share.site.CreateSitePage;
-import org.alfresco.po.share.site.CustomiseSiteDashboardPage;
 import org.alfresco.po.share.site.SiteDashboardPage;
-import org.alfresco.po.share.site.SiteNavigation;
-import org.alfresco.po.share.site.SiteType;
-import org.alfresco.po.share.util.SiteUtil;
-import org.alfresco.share.clustering.SiteDashboardClusterTests;
 import org.alfresco.share.site.document.ShareRefreshCopyToSites;
 import org.alfresco.share.util.AbstractUtils;
 import org.alfresco.share.util.ShareUser;
@@ -35,6 +30,7 @@ public class EditingItemsViaAWE extends AbstractUtils
 {
     private SharePage page;
     private String testName;
+    private String wqsURL;
 
     private static final Logger logger = Logger.getLogger(ShareRefreshCopyToSites.class);
 
@@ -43,107 +39,294 @@ public class EditingItemsViaAWE extends AbstractUtils
     public void setup() throws Exception
     {
         super.setup();
+        wqsURL = "http://localhost:8080/wcmqs";
         testName = this.getClass().getSimpleName();
+        logger.info("Start Tests from: " + testName);
     }
 
-
-    
-    @Test(groups = "DataPrepWQS")
-    public void dataPrep_AONE_5593() throws Exception
+    @Test(groups = { "DataPrepWQS" })
+    public void dataPrep_AONE() throws Exception
     {
+        String testUser = getUserNameForDomain(testName, DOMAIN_FREE);
+        String siteName = getSiteName(testName);
 
-        String testName = getTestName();
-        String user1 = getUserNameForDomain(testName + "-op", DOMAIN_HYBRID);
-        String[] userInfo1 = new String[] { user1 };
-        
-        String siteName = testName + "SiteName";
-        String siteURL = testName + "SiteURL";
+        // User
+        String[] testUserInfo = new String[] { testUser };
+        CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, testUserInfo);
 
-        // Create User1 (On-premise)
-        CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, userInfo1);
-        ShareUser.createSite(drone, siteName, "Public");
-        
-    }
-     
-    
-    
-    
-    /**
-     * AONE-5595:Creating web site in Share
-     */
+        // ---- Step 1 ----
+        // ---- Step Action -----
+        // WCM Quick Start is installed; - is not required to be executed automatically
 
-    @Test(groups = "WQS")
-    public void AONE_5595() throws Exception
-    {
-        // --- Step 1 ---
-        // --- Step action ---
-        // Click Create site link;
-        // --- Expected results ---
-        // Create Site window is opened;
+        // ---- Step 2 ----
+        // ---- Step Action -----
+        // Site "My Web Site" is created in Alfresco Share;
+        ShareUser.login(drone, ADMIN_USERNAME, ADMIN_PASSWORD);
+        ShareUser.createSite(drone, siteName, SITE_VISIBILITY_PUBLIC);
 
-        String testName = getTestName();
-        String user1 = getUserNameForDomain(testName + "-op", DOMAIN_HYBRID);
-        String[] userInfo1 = new String[] { user1 };
+        // ---- Step 3 ----
+        // ---- Step Action -----
+        // WCM Quick Start Site Data is imported;
+        SiteDashboardPage siteDashBoard = ShareUserDashboard.addDashlet(drone, siteName, Dashlets.WEB_QUICK_START);
 
-        String siteName = testName + "SiteName";
-        String siteURL = testName + "SiteURL";
-
-        // Create User1 (On-premise)
-        CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, userInfo1);
-        page = ShareUser.login(drone, user1, DEFAULT_PASSWORD);
-
-        CreateSitePage createSitePage = page.getNav().selectCreateSite().render();
-        assertTrue(createSitePage.isCreateSiteDialogDisplayed());
-
-        // --- Step 2 ---
-        // --- Step action ---
-        // Fill in mandatory fields:
-        // Name: My Web Site URL Name: MyWebSite Type: Collaboration site Visibility: Public
-        // --- Expected results ---
-        // Data is entered successfully;
-
-        createSitePage.setSiteName(siteName);
-        createSitePage.setSiteURL(siteURL);
-        createSitePage.selectSiteType(SiteType.COLLABORATION);
-        createSitePage.selectSiteVisibility(false, false);
-
-        Assert.assertEquals(createSitePage.getSiteName(), siteName);
-        Assert.assertEquals(createSitePage.getSiteUrl(), siteURL);
-        Assert.assertEquals(createSitePage.getSiteType().get(0), "Collaboration Site");
-
-        // --- Step 3 ---
-        // --- Step action ---
-        // Click OK button;
-        // --- Expected results ---
-        // Site is created, Site dashboard page is opened;
-
-        SiteDashboardPage siteDashboardPage = createSitePage.selectOk().render();
-        assertTrue(siteDashboardPage.isSiteTitle(siteName));
-
-        // --- Step 4 ---
-        // --- Step action ---
-        // Add "WCM Quick Start" dashlet to site dashboard
-        // --- Expected results ---
-        // Dashlet is added to dashboard;
-
-        CustomiseSiteDashboardPage customiseSiteDashboardPage = siteDashboardPage.getSiteNav().selectCustomizeDashboard().render();
-        siteDashboardPage = customiseSiteDashboardPage.addDashlet(Dashlets.WEB_QUICK_START, 1).render();
-
-        List<String> dashletTitles = siteDashboardPage.getTitlesList();
-        Assert.assertTrue(dashletTitles.contains("Web Quick Start"));
-
-        // --- Step 5 ---
-        // --- Step action ---
-        // Click 'Import Web Site Data' link on WCM Quick Start dashlet
-        // --- Expected results ---
-        // "Web Site data import successful" notification is dislpayed;
-
-        SiteWebQuickStartDashlet wqsDashlet = siteDashboardPage.getDashlet(SITE_WEB_QUICK_START_DASHLET).render();
+        SiteWebQuickStartDashlet wqsDashlet = siteDashBoard.getDashlet(SITE_WEB_QUICK_START_DASHLET).render();
         wqsDashlet.selectWebsiteDataOption(WebQuickStartOptions.FINANCE);
         wqsDashlet.clickImportButtton();
-
         assertTrue(wqsDashlet.isImportMessage());
 
     }
 
+    /** AONE-5618:Description field(submit editing) */
+    @Test(groups = { "WQS" })
+    public void AONE_5618() throws Exception
+    {
+        // ---- Step 4 ----
+        // ---- Step Action -----
+        // Edit blog post/article form is opened;
+        drone.navigateTo(wqsURL);
+        WcmqsHomePage wqsPage = new WcmqsHomePage(drone);
+        wqsPage.render();
+        WcmqsNewsArticleDetails article = wqsPage.selectFirstArticleFromLeftPanel().render();
+        WcmqsLoginPage loginPage = new WcmqsLoginPage(drone);
+        loginPage.render();
+        loginPage.login(ADMIN_USERNAME, ADMIN_PASSWORD);
+        article = WcmqsNewsArticleDetails.getCurrentNewsArticlePage(drone);
+
+        // ---- Step 5 ----
+        // ---- Step Action -----
+        // All mandatory fields are filled with correct information;
+        WcmqsEditPage editPage = article.clickEditButton().render();
+        WcmqsArticleDetails articleDeatils = editPage.getArticleDetails();
+        String shareName = "article3.html";
+        Assert.assertEquals(articleDeatils.getName(), shareName);
+
+        // ---- Step 1 ----
+        // ---- Step Action -----
+        // Change data in Description field;
+        // Data is entered successfully;
+        String newDescription = "new description-5618";
+        editPage.editDescription(newDescription);
+        // Assert.assertTrue(editPage.getArticleDetails().getDescription().contains(newDescription));
+
+        // ---- Step 2 ----
+        // ---- Step Action -----
+        // Click Submit button;
+        // Edit blog post/article form is closed, description data is changed;
+        editPage.clickSubmitButton();
+        WcmqsNewsPage newsPage = new WcmqsNewsPage(drone).render();
+        Assert.assertTrue(newsPage.getNewsDescrition(shareName).contains(newDescription));
+
+    }
+
+    /** AONE-5617:Description field(wildcards) */
+    @Test(groups = { "WQS" })
+    public void AONE_5617() throws Exception
+    {
+        // ---- Step 4 ----
+        // ---- Step Action -----
+        // Edit blog post/article form is opened;
+        drone.navigateTo(wqsURL);
+        WcmqsHomePage wqsPage = new WcmqsHomePage(drone);
+        wqsPage.render();
+        WcmqsNewsArticleDetails article = wqsPage.selectFirstArticleFromLeftPanel().render();
+        WcmqsLoginPage loginPage = new WcmqsLoginPage(drone);
+        loginPage.render();
+        loginPage.login(ADMIN_USERNAME, ADMIN_PASSWORD);
+        article = WcmqsNewsArticleDetails.getCurrentNewsArticlePage(drone);
+
+        // ---- Step 5 ----
+        // ---- Step Action -----
+        // All mandatory fields are filled with correct information;
+        WcmqsEditPage editPage = article.clickEditButton().render();
+        WcmqsArticleDetails articleDeatils = editPage.getArticleDetails();
+        String shareName = "article3.html";
+        Assert.assertEquals(articleDeatils.getName(), shareName);
+
+        // ---- Step 1 ----
+        // ---- Step Action -----
+        // Change data in Description field;
+        // Data is entered successfully;
+        String newDescription = "new description~!@#$%^&*(a)";
+        editPage.editDescription(newDescription);
+        // Assert.assertTrue(editPage.getArticleDetails().getDescription().contains(newDescription));
+
+        // ---- Step 2 ----
+        // ---- Step Action -----
+        // Click Submit button;
+        // Edit blog post/article form is closed, description data is changed;
+        editPage.clickSubmitButton();
+        WcmqsNewsPage newsPage = new WcmqsNewsPage(drone).render();
+        Assert.assertTrue(newsPage.getNewsDescrition(shareName).contains(newDescription));
+
+    }
+
+    /** AONE-5616:Description field(cancel editing) */
+    @Test(groups = { "WQS" })
+    public void AONE_5616() throws Exception
+    {
+        // ---- Step 4 ----
+        // ---- Step Action -----
+        // Edit blog post/article form is opened;
+        drone.navigateTo(wqsURL);
+        WcmqsHomePage wqsPage = new WcmqsHomePage(drone);
+        wqsPage.render();
+        WcmqsNewsArticleDetails article = wqsPage.selectFirstArticleFromLeftPanel().render();
+        WcmqsLoginPage loginPage = new WcmqsLoginPage(drone);
+        loginPage.render();
+        loginPage.login(ADMIN_USERNAME, ADMIN_PASSWORD);
+        article = WcmqsNewsArticleDetails.getCurrentNewsArticlePage(drone);
+
+        // ---- Step 5 ----
+        // ---- Step Action -----
+        // All mandatory fields are filled with correct information;
+        WcmqsEditPage editPage = article.clickEditButton().render();
+        WcmqsArticleDetails articleDeatils = editPage.getArticleDetails();
+        String expectedName = "article3.html";
+        Assert.assertEquals(articleDeatils.getName(), expectedName);
+        // ---- Step 1 ----
+        // ---- Step Action -----
+        // Change data in Description field;
+        // Data is entered successfully;
+        String newDescription = "new description-5616";
+        editPage.editDescription(newDescription);
+        // Assert.assertTrue(editPage.getArticleDetails().getDescription().contains(newDescription));
+
+        // ---- Step 2 ----
+        // ---- Step Action -----
+        // Click Cancel button;
+        // Edit blog post/article form is closed, description data isn't changed;
+        editPage.clickCancelButton();
+        article = WcmqsNewsArticleDetails.getCurrentNewsArticlePage(drone);
+        // the user is returned in the article page. To check if the changed are not present, edit again the blog post/article
+        editPage = article.clickEditButton().render();
+        Assert.assertFalse(editPage.getArticleDetails().getDescription().contains(newDescription));
+
+    }
+
+    /** AONE-5615:Editing Title field(positive test) */
+    @Test(groups = { "WQS" })
+    public void AONE_5615() throws Exception
+    {
+        // ---- Step 4 ----
+        // ---- Step Action -----
+        // Edit blog post/article form is opened;
+        drone.navigateTo(wqsURL);
+        WcmqsHomePage wqsPage = new WcmqsHomePage(drone);
+        wqsPage.render();
+        WcmqsNewsArticleDetails article = wqsPage.selectFirstArticleFromLeftPanel().render();
+        WcmqsLoginPage loginPage = new WcmqsLoginPage(drone);
+        loginPage.render();
+        loginPage.login(ADMIN_USERNAME, ADMIN_PASSWORD);
+        article = WcmqsNewsArticleDetails.getCurrentNewsArticlePage(drone);
+
+        // ---- Step 5 ----
+        // ---- Step Action -----
+        // All mandatory fields are filled with correct information;
+        WcmqsEditPage editPage = article.clickEditButton().render();
+        WcmqsArticleDetails articleDeatils = editPage.getArticleDetails();
+        String shareName = "article3.html";
+        Assert.assertEquals(articleDeatils.getName(), shareName);
+
+        // ---- Step 1 ----
+        // ---- Step Action -----
+        // Change some data in Title field;
+        // Data is changed successfully;
+        String newTitle = "title-edited";
+        editPage.editTitle(newTitle);
+        Assert.assertTrue(editPage.getArticleDetails().getTitle().contains(newTitle));
+
+        // ---- Step 2 ----
+        // ---- Step Action -----
+        // Click Sumbit button;
+        // Edit blog post/article form is closed, changes are saved;
+        editPage.clickSubmitButton();
+        WcmqsNewsPage newsPage = new WcmqsNewsPage(drone);
+        Assert.assertTrue(newsPage.getNewsTitle(shareName).contains(newTitle));
+
+    }
+
+    /** AONE-5614:Editing Title field(negative test) */
+    @Test(groups = { "WQS" })
+    public void AONE_5614() throws Exception
+    {
+        // ---- Step 4 ----
+        // ---- Step Action -----
+        // Edit blog post/article form is opened;
+        drone.navigateTo(wqsURL);
+        WcmqsHomePage wqsPage = new WcmqsHomePage(drone);
+        wqsPage.render();
+        WcmqsNewsArticleDetails article = wqsPage.selectFirstArticleFromLeftPanel().render();
+        WcmqsLoginPage loginPage = new WcmqsLoginPage(drone);
+        loginPage.render();
+        loginPage.login(ADMIN_USERNAME, ADMIN_PASSWORD);
+        article = WcmqsNewsArticleDetails.getCurrentNewsArticlePage(drone);
+
+        // ---- Step 5 ----
+        // ---- Step Action -----
+        // All mandatory fields are filled with correct information;
+        WcmqsEditPage editPage = article.clickEditButton().render();
+        WcmqsArticleDetails articleDeatils = editPage.getArticleDetails();
+        String expectedName = "article3.html";
+        Assert.assertEquals(articleDeatils.getName(), expectedName);
+
+        // ---- Step 1 ----
+        // ---- Step Action -----
+        // Change some data in Title field;
+        // Data is changed successfully;
+        String newTitle = "title-edited";
+        editPage.editTitle(newTitle);
+        Assert.assertTrue(editPage.getArticleDetails().getTitle().contains(newTitle));
+
+        // ---- Step 2 ----
+        // ---- Step Action -----
+        // Click Cancel button;
+        // Edit blog post/article form is closed, changes are not saved;
+        editPage.clickCancelButton();
+        article = WcmqsNewsArticleDetails.getCurrentNewsArticlePage(drone);
+        Assert.assertFalse(article.getTitleOfNewsArticle().contains(newTitle));
+
+    }
+
+    /** AONE-5613:Editing Content field(negative test) */
+    @Test(groups = { "WQS" })
+    public void AONE_5613() throws Exception
+    {
+
+        // ---- Step 4 ----
+        // ---- Step Action -----
+        // Edit blog post/article form is opened;
+        drone.navigateTo(wqsURL);
+        WcmqsHomePage wqsPage = new WcmqsHomePage(drone);
+        wqsPage.render();
+        WcmqsNewsArticleDetails article = wqsPage.selectFirstArticleFromLeftPanel().render();
+        WcmqsLoginPage loginPage = new WcmqsLoginPage(drone);
+        loginPage.render();
+        loginPage.login(ADMIN_USERNAME, ADMIN_PASSWORD);
+        article = WcmqsNewsArticleDetails.getCurrentNewsArticlePage(drone);
+
+        // ---- Step 5 ----
+        // ---- Step Action -----
+        // All mandatory fields are filled with correct information;
+        WcmqsEditPage editPage = article.clickEditButton().render();
+        WcmqsArticleDetails articleDeatils = editPage.getArticleDetails();
+        String expectedName = "article3.html";
+        Assert.assertEquals(articleDeatils.getName(), expectedName);
+
+        // ---- Step 1 ----
+        // ---- Step Action -----
+        // Change some data in Content field;
+        // Data is changed successfully;
+        String newContent = "content-edited";
+        editPage.insertTextInContent(newContent);
+        Assert.assertTrue(editPage.getContentTinyMCEEditor().getText().contains(newContent));
+
+        // ---- Step 2 ----
+        // ---- Step Action -----
+        // Click Cancel button;
+        // Edit blog post/article form is closed, changes are not saved;
+        editPage.clickCancelButton();
+        article = WcmqsNewsArticleDetails.getCurrentNewsArticlePage(drone);
+        Assert.assertFalse(article.getBodyOfNewsArticle().contains(newContent));
+
+    }
 }
