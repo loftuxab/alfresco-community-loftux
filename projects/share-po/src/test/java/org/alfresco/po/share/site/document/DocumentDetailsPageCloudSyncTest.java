8e9c1f4c895f58c5233fdc624e0f9a042d198fe3
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 20:44:59 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    92251: Merged 5.0.N (5.0.1) to HEAD-BUG-FIX (5.1/Cloud)       92104: WEBDRONE-835 added unit test
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@95494 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/share-po/src/test/java/org/alfresco/po/share/site/document/DocumentDetailsPageCloudSyncTest.java
+++ projects/share-po/src/test/java/org/alfresco/po/share/site/document/DocumentDetailsPageCloudSyncTest.java
@@ -7,8 +7,6 @@
  */
 package org.alfresco.po.share.site.document;
 
-import java.io.File;
-
 import org.alfresco.po.share.SharePage;
 import org.alfresco.po.share.site.SitePage;
 import org.alfresco.po.share.site.UploadFilePage;
@@ -23,9 +21,11 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 /**
  * Integration test to verify document CRUD is operating correctly.
- * 
+ *
  * @author Ranjith Manyam
  * @since 1.7
  */
@@ -33,83 +33,95 @@ import org.testng.annotations.Test;
 @Test(groups = "Hybrid")
 public class DocumentDetailsPageCloudSyncTest extends AbstractDocumentTest
 {
-    private String siteName1;
-    private File file1;
-    private File file2;
-    private DocumentLibraryPage documentLibPage;
-    private DocumentDetailsPage documentDetailsPage;
-    private DestinationAndAssigneePage destinationAndAssigneePage ;
+        private String siteName1;
+        private File file1;
+        private File file2;
+        private File file3;
+        private DocumentLibraryPage documentLibPage;
+        private DocumentDetailsPage documentDetailsPage;
+        private DestinationAndAssigneePage destinationAndAssigneePage;
 
-    /**
-     * Pre test setup of a dummy file to upload.
-     *
-     * @throws Exception
-     */
-    @BeforeClass
-    public void prepare() throws Exception
-    {
-        siteName1 = "Site-1" + System.currentTimeMillis();
-        file1 = SiteUtil.prepareFile("File-1"+System.currentTimeMillis());
-        file2 = SiteUtil.prepareFile("File-2"+System.currentTimeMillis());
+        /**
+         * Pre test setup of a dummy file to upload.
+         *
+         * @throws Exception
+         */
+        @BeforeClass
+        public void prepare() throws Exception
+        {
+                siteName1 = "Site-1" + System.currentTimeMillis();
+                file1 = SiteUtil.prepareFile("File-1" + System.currentTimeMillis());
+                file2 = SiteUtil.prepareFile("File-2" + System.currentTimeMillis());
+                file3 = SiteUtil.prepareFile("File-3" + System.currentTimeMillis());
 
-        loginAs(username, password);
+                loginAs(username, password);
 
-        signInToCloud(drone, cloudUserName, cloudUserPassword);
+                signInToCloud(drone, cloudUserName, cloudUserPassword);
 
-        //SiteUtil.createSite(drone, siteName2, "Public");
-        SiteUtil.createSite(drone, siteName1, "Public");
-        SitePage site = drone.getCurrentPage().render();
-        documentLibPage = site.getSiteNav().selectSiteDocumentLibrary().render();
-        UploadFilePage uploadForm = documentLibPage.getNavigation().selectFileUpload().render();
-        documentLibPage = uploadForm.uploadFile(file1.getCanonicalPath()).render();
-        uploadForm = documentLibPage.getNavigation().selectFileUpload().render();
-        documentLibPage = uploadForm.uploadFile(file2.getCanonicalPath()).render();
-    }
+                //SiteUtil.createSite(drone, siteName2, "Public");
+                SiteUtil.createSite(drone, siteName1, "Public");
+                SitePage site = drone.getCurrentPage().render();
+                documentLibPage = site.getSiteNav().selectSiteDocumentLibrary().render();
+                UploadFilePage uploadForm = documentLibPage.getNavigation().selectFileUpload().render();
+                documentLibPage = uploadForm.uploadFile(file1.getCanonicalPath()).render();
+                uploadForm = documentLibPage.getNavigation().selectFileUpload().render();
+                documentLibPage = uploadForm.uploadFile(file2.getCanonicalPath()).render();
+                uploadForm = documentLibPage.getNavigation().selectFileUpload().render();
+                documentLibPage = uploadForm.uploadFile(file3.getCanonicalPath()).render();
+        }
 
+        @AfterClass
+        public void tearDown()
+        {
+                //Disconnect Cloud Account
+                MyProfilePage myProfilePage = ((SharePage) drone.getCurrentPage()).getNav().selectMyProfile().render();
+                CloudSyncPage cloudSyncPage = myProfilePage.getProfileNav().selectCloudSyncPage().render();
+                cloudSyncPage.disconnectCloudAccount();
+                // Delete site
+                SiteUtil.deleteSite(drone, siteName1);
+                logout(drone);
+        }
 
-    @AfterClass
-    public void tearDown()
-    {
-        //Disconnect Cloud Account
-        MyProfilePage myProfilePage = ((SharePage) drone.getCurrentPage()).getNav().selectMyProfile().render();
-        CloudSyncPage cloudSyncPage = myProfilePage.getProfileNav().selectCloudSyncPage().render();
-        cloudSyncPage.disconnectCloudAccount();
-        // Delete site
-        SiteUtil.deleteSite(drone, siteName1);
-        logout(drone);
-    }
+        @Test
+        public void syncStatusBeforeSync() throws Exception
+        {
+                documentDetailsPage = documentLibPage.selectFile(file1.getName()).render();
+                Assert.assertFalse(documentDetailsPage.isRequestSyncIconDisplayed());
+                Assert.assertEquals(documentDetailsPage.getSyncStatus(), "");
+                Assert.assertEquals(documentDetailsPage.getLocationInCloud(), "");
 
-    @Test
-    public void syncStatusBeforeSync() throws Exception
-    {
-        documentDetailsPage = documentLibPage.selectFile(file1.getName()).render();
-        Assert.assertFalse(documentDetailsPage.isRequestSyncIconDisplayed());
-        Assert.assertEquals(documentDetailsPage.getSyncStatus(), "");
-        Assert.assertEquals(documentDetailsPage.getLocationInCloud(), "");
+        }
 
-    }
+        @Test(dependsOnMethods = "syncStatusBeforeSync")
+        public void syncStatusAfterSync()
+        {
+                destinationAndAssigneePage = documentDetailsPage.selectSyncToCloud().render();
+                documentDetailsPage = destinationAndAssigneePage.selectSubmitButtonToSync().render();
+                Assert.assertTrue(checkIfContentIsSynced(drone));
+        }
 
-    @Test (dependsOnMethods = "syncStatusBeforeSync")
-    public void syncStatusAfterSync()
-    {
-        destinationAndAssigneePage = documentDetailsPage.selectSyncToCloud().render();
-        documentDetailsPage = destinationAndAssigneePage.selectSubmitButtonToSync().render();
-        Assert.assertTrue(checkIfContentIsSynced(drone));
-    }
+        @Test(dependsOnMethods = "syncStatusAfterSync")
+        public void getLocationInCloud()
+        {
+                // Verify location in the cloud
+                Assert.assertEquals(documentDetailsPage.getLocationInCloud(), "premiernet.test > Auto Account's Home > Documents");
+        }
 
-    @Test (dependsOnMethods = "syncStatusAfterSync")
-    public void getLocationInCloud()
-    {
-        // Verify location in the cloud
-        Assert.assertEquals(documentDetailsPage.getLocationInCloud(), "premiernet.test > Auto Account's Home > Documents");
-    }
+        @Test(dependsOnMethods = "getLocationInCloud")
+        public void isRequestSyncIconDisplayed()
+        {
+                // Verify Request to Sync icon is displayed.
+                Assert.assertTrue(documentDetailsPage.isRequestSyncIconDisplayed());
+        }
 
-    @Test (dependsOnMethods = "getLocationInCloud")
-    public void isRequestSyncIconDisplayed()
-    {
-        // Verify Request to Sync icon is displayed.
-        Assert.assertTrue(documentDetailsPage.isRequestSyncIconDisplayed());
-    }
+        @Test(dependsOnMethods = "isRequestSyncIconDisplayed")
+        public void isSyncMessagePresent() throws Exception
+        {
+                documentLibPage = documentDetailsPage.getSiteNav().selectSiteDocumentLibrary().render();
+                destinationAndAssigneePage = documentLibPage.getFileDirectoryInfo(file3.getName()).selectSyncToCloud().render();
+                destinationAndAssigneePage.clickSyncButton();
+                Assert.assertTrue(documentLibPage.isSyncMessagePresent());
 
+        }
 
 }
