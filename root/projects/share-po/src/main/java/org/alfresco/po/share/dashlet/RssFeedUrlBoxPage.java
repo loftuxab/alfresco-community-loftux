be511ad4d144ddbbdf4c49cd318c64ab501898d2
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 17:59:37 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    89737: QA-964 - AddOnsRssFeedDashlet.getHeaderInfo()    - AddOnsRssFeedDashlet.isConfigurePresent()    - AddOnsRssFeedDashlet.getHeadlineLinksFromDashlet()    - add enum NrItems    - RssFeedUrlBoxPage.selectNrOfItemsToDisplay    - RssFeedUrlBoxPage.selectOpenLinkNewWindow    - ShareLink.openLink() - click on link without verifying the opened page
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@95154 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: e89833b877cca383a7b5717b5fe2c7fbbf5c5252

--- root/projects/share-po/src/main/java/org/alfresco/po/share/dashlet/RssFeedUrlBoxPage.java
+++ root/projects/share-po/src/main/java/org/alfresco/po/share/dashlet/RssFeedUrlBoxPage.java
@@ -1,35 +1,34 @@
 /*
  * Copyright (C) 2005-2013 Alfresco Software Limited.
- *
  * This file is part of Alfresco
- *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * Alfresco is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.alfresco.po.share.dashlet;
 
-import org.alfresco.po.share.SharePage;
-import org.alfresco.webdrone.RenderTime;
-import org.alfresco.webdrone.WebDrone;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.alfresco.webdrone.RenderElement.getVisibleRenderElement;
 
+import org.alfresco.po.share.SharePage;
+import org.alfresco.po.share.exception.ShareException;
+import org.alfresco.webdrone.RenderTime;
+import org.alfresco.webdrone.WebDrone;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 /**
  * Page object to reflect the rss feed url box web elements
- *
+ * 
  * @author Marina.Nenadovets
  */
 public class RssFeedUrlBoxPage extends SharePage
@@ -38,6 +37,24 @@ public class RssFeedUrlBoxPage extends SharePage
     private static final By CANCEL_BUTTON = By.cssSelector("button[id$='configDialog-cancel-button']");
     private static final By CLOSE_BUTTON = By.cssSelector(".container-close");
     private static final By URL_FIELD = By.cssSelector("input[id$='configDialog-url']");
+    private static final By CHK_OPEN_IN_NEW_WINDOW = By.cssSelector("input[id$='default-configDialog-new_window']");
+
+    public enum NrItems
+    {
+        ALL("All"), Five("5"), Ten("10"), Fifteen("15"), Twenty("20"), twentyFive("25");
+
+        private String label;
+
+        private NrItems(String label)
+        {
+            this.label = label;
+        }
+
+        public String getLabel()
+        {
+            return label;
+        }
+    }
 
     /**
      * Constructor.
@@ -96,6 +113,7 @@ public class RssFeedUrlBoxPage extends SharePage
 
     /**
      * Mimic fill URL field
+     * 
      * @param url
      */
     public void fillURL(String url)
@@ -117,5 +135,62 @@ public class RssFeedUrlBoxPage extends SharePage
         WebElement inputField = drone.findAndWait(selector);
         inputField.clear();
         inputField.sendKeys(text);
+    }
+
+    public void selectNrOfItemsToDisplay(NrItems items)
+    {
+        try
+        {
+            Select select = new Select(drone.findAndWait(By.cssSelector("select[id$='_default-configDialog-limit']")));
+            select.selectByValue(items.getLabel());
+        }
+        catch (TimeoutException te)
+        {
+            throw new ShareException("Unable to find drop-down");
+        }
+    }
+
+    public void selectOpenLinkNewWindow()
+    {
+        try
+        {
+            WebElement element = drone.findAndWait(CHK_OPEN_IN_NEW_WINDOW);
+            element.click();
+        }
+        catch (TimeoutException te)
+        {
+            throw new ShareException("Unable to find checkbox");
+        }
+    }
+    
+    public void clearUrlField()
+    {
+        try
+        {
+            WebElement element = drone.find(URL_FIELD);
+            element.clear();
+
+        }
+        catch (TimeoutException te)
+        {
+            throw new ShareException("Unable to find Url field");
+        } 
+    }
+
+    public String getValidationMessageFromUrlField(String text)
+    {
+        try
+        {
+            WebElement element = drone.find(URL_FIELD);
+            element.clear();
+            element.sendKeys(text);
+            clickOk();
+
+            return getValidationMessage(element);
+        }
+        catch (TimeoutException te)
+        {
+            throw new ShareException("Unable to find Url field");
+        }
     }
 }
