d8ae26525d1397c0a10611616a5510d22b95f2de
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 09:45:30 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.0/Cloud)    86234: Merged V4.2-BUG-FIX (4.2.4) to HEAD-BUG-FIX (5.0/Cloud)       86212: Merged V4.1-BUG-FIX (4.1.10) to V4.2-BUG-FIX (4.2.4)          86181: MNT-8530: Merged DEV to V4.1-BUG-FIX (4.1.10)             86094: MNT-8530: Solr OOM - java.lang.ref.Finalizer seems to be in a loop                - Map search of '*' with no field to 'ISNODE:T'. Add unit test.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@94531 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 634aef1a1b86d10a8496c171574e7233ed66b656

--- root/projects/data-model/source/test-java/org/alfresco/repo/search/impl/parsers/FTSTest.java
+++ root/projects/data-model/source/test-java/org/alfresco/repo/search/impl/parsers/FTSTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2010 Alfresco Software Limited.
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
  *
  * This file is part of Alfresco
  *
@@ -20,9 +20,18 @@ package org.alfresco.repo.search.impl.parsers;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import junit.framework.TestCase;
 
+import org.alfresco.repo.search.impl.querymodel.Column;
+import org.alfresco.repo.search.impl.querymodel.FunctionEvaluationContext;
+import org.alfresco.repo.search.impl.querymodel.QueryModelFactory;
+import org.alfresco.repo.search.impl.querymodel.Selector;
+import org.alfresco.repo.search.impl.querymodel.QueryOptions.Connective;
+import org.alfresco.repo.search.impl.querymodel.impl.lucene.LuceneQueryModelFactory;
+import org.alfresco.service.namespace.NamespaceService;
 import org.antlr.gunit.GrammarInfo;
 import org.antlr.gunit.gUnitLexer;
 import org.antlr.gunit.gUnitParser;
@@ -32,6 +41,7 @@ import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.tree.CommonTree;
+import org.antlr.runtime.tree.Tree;
 
 public class FTSTest extends TestCase
 {
@@ -99,6 +109,79 @@ public class FTSTest extends TestCase
         gUnitParser parser = new gUnitParser(tokens, grammarInfo);
         parser.gUnitDef(); // parse gunit script and save elements to grammarInfo
         return grammarInfo;
+    }
+
+    public void testMapLoneStar() throws Exception
+    {
+        final String ftsExpression = "* AND * AND * AND * AND * AND * AND * AND * AND * AND * AND *";
+        AlfrescoFunctionEvaluationContext functionContext = new AlfrescoFunctionEvaluationContext(null, null, NamespaceService.CONTENT_MODEL_1_0_URI);
+
+        Map<String, String> templates = new HashMap<String, String>();
+        String keywordsTemplate = "%(cm:name cm:title cm:description ia:whatEvent ia:descriptionEvent lnk:title lnk:description TEXT TAG)";
+        String keywordsKey = "keywords";
+        templates.put(keywordsKey, keywordsTemplate);
+
+        final FTSParser.Mode mode = FTSParser.Mode.DEFAULT_DISJUNCTION;
+        final Connective defaultFieldConnective = Connective.OR;
+
+        class TestMock extends FTSQueryParser.TestNodeBuilder
+        {
+            private void test(CommonTree initialNode, CommonTree replacedNode)
+            {
+                if (initialNode.getType() == FTSParser.TERM &&
+                        initialNode.getChildCount() == 1 &&
+                        initialNode.getChild(0).getType() == FTSParser.STAR)
+                {
+                    // input is the lone star
+                    Tree node = replacedNode;
+                    while (true)
+                    {
+                        if (node.getChildCount() == 1)
+                        {
+                            node = node.getChild(0);
+                            if (node.getType() == FTSParser.TERM)
+                            {
+                                assertEquals("Lone star should be mapped to " + FTSQueryParser.VALUE_REPLACELONESTAR, node.getChildCount(), 2);
+                                Tree child1 = node.getChild(0);
+                                assertEquals("Lone star should be mapped to " + FTSQueryParser.VALUE_REPLACELONESTAR, child1.getType(), FTSParser.ID);
+                                assertEquals("Lone star should be mapped to " + FTSQueryParser.VALUE_REPLACELONESTAR, child1.getText(), "T");
+                                Tree child2 = node.getChild(1);
+                                assertEquals("Lone star should be mapped to " + FTSQueryParser.VALUE_REPLACELONESTAR, child2.getType(), FTSParser.FIELD_REF);
+                                assertEquals("Lone star should be mapped to " + FTSQueryParser.VALUE_REPLACELONESTAR, child2.getChild(0).getText(), "ISNODE");
+                                // checking done
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            // wrong structure of the replaced node
+                            fail("Lone star should be mapped to " + FTSQueryParser.VALUE_REPLACELONESTAR);
+                        }
+                    }
+                }
+            }
+            
+            @Override
+            protected CommonTree build(CommonTree fieldReferenceNode, CommonTree argNode, QueryModelFactory factory, FunctionEvaluationContext functionEvaluationContext,
+                    Selector selector, Map<String, Column> columnMap, Map<String, CommonTree> templateTrees, String defaultField)
+            {
+                CommonTree testNode = super.build(fieldReferenceNode, argNode, factory, functionEvaluationContext, selector, columnMap, templateTrees, defaultField);
+                test(argNode, testNode);
+                return testNode;
+            }
+        }
+
+        FTSQueryParser.setTestNodeBuilder(new TestMock());
+        try
+        {
+            FTSQueryParser.buildFTS(ftsExpression, new LuceneQueryModelFactory(), functionContext, null, null,
+                    mode, defaultFieldConnective, templates, keywordsKey);
+        }
+        finally
+        {
+            // set default logic
+            FTSQueryParser.setTestNodeBuilder(new FTSQueryParser.TestNodeBuilder());
+        }
     }
 
 }
