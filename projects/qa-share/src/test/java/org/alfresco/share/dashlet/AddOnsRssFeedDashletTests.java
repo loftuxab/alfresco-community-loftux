fbe1b0460e7ee8798673fd700be73e225556b62c
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 18:03:29 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@95170 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ projects/qa-share/src/test/java/org/alfresco/share/dashlet/AddOnsRssFeedDashletTests.java
@@ -0,0 +1,331 @@
+package org.alfresco.share.dashlet;
+
+import java.util.List;
+import java.util.Set;
+
+import org.alfresco.po.share.ShareLink;
+import org.alfresco.po.share.dashlet.AddOnsRssFeedDashlet;
+import org.alfresco.po.share.dashlet.RssFeedUrlBoxPage;
+import org.alfresco.po.share.dashlet.RssFeedUrlBoxPage.NrItems;
+import org.alfresco.po.share.enums.Dashlets;
+import org.alfresco.share.util.AbstractUtils;
+import org.alfresco.share.util.ShareUser;
+import org.alfresco.share.util.ShareUserDashboard;
+import org.alfresco.share.util.api.CreateUserAPI;
+import org.alfresco.webdrone.testng.listener.FailedTestListener;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(FailedTestListener.class)
+public class AddOnsRssFeedDashletTests extends AbstractUtils
+{
+    private static Log logger = LogFactory.getLog(AddOnsRssFeedDashletTests.class);
+    String headerInfo = "Find, rate, and contribute Alfresco add-ons and extensions. Visit the Alfresco Add-ons Home Page";
+    String helpInfo = "This dashlet shows the latest news from Alfresco Add-ons. Click the edit icon on the dashlet to configure the feed.";
+
+    @Override
+    @BeforeClass(alwaysRun = true)
+    public void setup() throws Exception
+    {
+        super.setup();
+
+        testName = this.getClass().getSimpleName();
+        logger.info("Starting Tests: " + testName);
+    }
+
+    @Test(groups = { "DataPrepDashlets" })
+    public void dataPrep_2904() throws Exception
+    {
+        String testName = getTestName();
+        String testUser = getUserNameFreeDomain(testName);
+
+        // User
+        String[] testUserInfo = new String[] { testUser };
+        CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, testUserInfo);
+
+        // Login
+        ShareUser.login(drone, testUser, DEFAULT_PASSWORD);
+
+        // Alfresco Add-ons News Feed dashlet added to My Dashboard
+        ShareUserDashboard.addDashlet(drone, Dashlets.ALFRESCO_ADDONS_RSS_FEED).render();
+
+    }
+
+    @Test(groups = { "EnterpriseOnly" })
+    public void AONE_2904() throws Exception
+    {
+        String testName = getTestName();
+        String testUser = getUserNameFreeDomain(testName);
+
+        ShareUser.login(drone, testUser, DEFAULT_PASSWORD);
+
+        AddOnsRssFeedDashlet rssDashlet = ShareUserDashboard.getDashlet(drone, Dashlets.ALFRESCO_ADDONS_RSS_FEED).render();
+
+        String title = "";
+        int counter = 1;
+        int retryRefreshCount = 5;
+
+        while (counter <= retryRefreshCount)
+        {
+            title = rssDashlet.getTitle();
+            if (title.equals("Newest Add-ons"))
+            {
+                break;
+            }
+            else
+            {
+                logger.info("Wait a few seconds to load the RSS feed");
+                Thread.sleep(7000);
+                counter++;
+            }
+        }
+
+        // ---- Step 1 ----
+        // ---- Step action ---
+        // Verify info available on dashlet
+        // ---- Expected results ----
+        // The next info should be available:
+        // - Newest Add-ons title
+        // - Configure this dashlet icon
+        // - Display help for this dashlet icon
+        // - Find, rate, and contribute Alfresco add-ons and extensions. Visit the Alfresco Add-ons Home Page description
+        Assert.assertTrue(title.equals("Newest Add-ons"));
+        Assert.assertTrue(rssDashlet.getHeaderInfo().equals(headerInfo));
+        Assert.assertTrue(rssDashlet.isHelpIconDisplayed());
+        Assert.assertTrue(rssDashlet.isConfigurePresent());
+
+        // ---- Step 2 ----
+        // ---- Step action ---
+        // Click Configure this dashlet icon
+        // ---- Expected results ----
+        // Help pop-up displays:
+        // This dashlet shows the latest news from Alfresco Add-ons. Click the edit icon on the dashlet to configure the feed
+        RssFeedUrlBoxPage rssFeedUrlBoxPage = rssDashlet.clickConfigure().render();
+        Assert.assertNotNull(rssFeedUrlBoxPage);
+        rssFeedUrlBoxPage.clickClose();
+
+        // ---- Step 3 ----
+        // ---- Step action ---
+        // Click Display help for this dashlet icon
+        // ---- Expected results ----
+        // Configuring dashlet form displays
+        rssDashlet.clickOnHelpIcon();
+        Assert.assertTrue(rssDashlet.isBalloonDisplayed());
+        Assert.assertTrue(rssDashlet.getHelpBalloonMessage().equals(helpInfo));
+        rssDashlet.getHelpBalloonMessage();
+
+        // ---- Step 4 ----
+        // ---- Step action ---
+        // Click X
+        // ---- Expected results ----
+        // Help pop-up closed
+        rssDashlet.closeHelpBallon();
+
+        // ---- Step 5 ----
+        // ---- Step action ---
+        // Click all available links on the dahslet
+        // ---- Expected results ----
+        // All links lead to correct we pages
+        List<ShareLink> links = rssDashlet.getHeadlineLinksFromDashlet();
+        String firstRssDescription = links.get(0).getDescription();
+        links.get(0).openLink();
+        Assert.assertTrue(drone.getTitle().contains(firstRssDescription));
+
+    }
+
+    @Test(groups = { "DataPrepDashlets" })
+    public void dataPrep_2905() throws Exception
+    {
+        String testName = getTestName();
+        String testUser = getUserNameFreeDomain(testName);
+
+        // User
+        String[] testUserInfo = new String[] { testUser };
+        CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, testUserInfo);
+
+        // Login
+        ShareUser.login(drone, testUser, DEFAULT_PASSWORD);
+
+        // Alfresco Add-ons News Feed dashlet added to My Dashboard
+        ShareUserDashboard.addDashlet(drone, Dashlets.ALFRESCO_ADDONS_RSS_FEED).render();
+    }
+
+    @Test(groups = { "EnterpriseOnly" })
+    public void AONE_2905() throws Exception
+    {
+        String testName = getTestName();
+        String testUser = getUserNameFreeDomain(testName);
+        String rssUrl = "http://feeds.reuters.com/reuters/businessNews";
+
+        ShareUser.login(drone, testUser, DEFAULT_PASSWORD);
+
+        AddOnsRssFeedDashlet rssDashlet = ShareUserDashboard.getDashlet(drone, Dashlets.ALFRESCO_ADDONS_RSS_FEED).render();
+
+        // ---- Step 1 ----
+        // ---- Step action ---
+        // Click Configure this dashlet icon
+        // ---- Expected results ----
+        // Enter Feed URL form displays
+        RssFeedUrlBoxPage rssFeedUrlBoxPage = rssDashlet.clickConfigure().render();
+        Assert.assertNotNull(rssFeedUrlBoxPage);
+
+        // ---- Step 2 ----
+        // ---- Step action ---
+        // Fill in URL field;
+        // ---- Expected results ----
+        // Information is entered successfully;
+        rssFeedUrlBoxPage.fillURL(rssUrl);
+
+        // ---- Step 3 ----
+        // ---- Step action ---
+        // In Number of items to display drop-down menu choose any value (e.g. 5);
+        // ---- Expected results ----
+        // Information is chosen successfully;
+        rssFeedUrlBoxPage.selectNrOfItemsToDisplay(NrItems.Five);
+
+        // ---- Step 4 ----
+        // ---- Step action ---
+        // Activate Open links in new window check-box
+        // ---- Expected results ----
+        // Check-box is selected;
+        rssFeedUrlBoxPage.selectOpenLinkNewWindow();
+
+        // ---- Step 5 ----
+        // ---- Step action ---
+        // Press OK button;
+        // ---- Expected results ----
+        // My Dashboard page is displayed, feed information is changed;
+        rssFeedUrlBoxPage.clickOk();
+
+        // wait a few seconds to load the RSS Feed
+        rssDashlet.waitUntilLoadingDisappears();
+        // Thread.sleep(5);
+
+        // ---- Step 6 ----
+        // ---- Step action ---
+        // Click on any RSS news;
+        // ---- Expected results ----
+        // RSS news is opened in new window;
+        List<ShareLink> links = rssDashlet.getHeadlineLinksFromDashlet();
+        links.get(0).openLink();
+        Set<String> windowHandles = drone.getWindowHandles();
+        Assert.assertEquals(2, windowHandles.size());
+
+    }
+
+    @Test(groups = { "DataPrepDashlets" })
+    public void dataPrep_2906() throws Exception
+    {
+        String testName = getTestName();
+        String testUser = getUserNameFreeDomain(testName);
+
+        // User
+        String[] testUserInfo = new String[] { testUser };
+        CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, testUserInfo);
+
+        // Login
+        ShareUser.login(drone, testUser, DEFAULT_PASSWORD);
+
+        // Alfresco Add-ons News Feed dashlet added to My Dashboard
+        ShareUserDashboard.addDashlet(drone, Dashlets.ALFRESCO_ADDONS_RSS_FEED).render();
+    }
+
+    @Test(groups = { "EnterpriseOnly" })
+    public void AONE_2906() throws Exception
+    {
+        String testName = getTestName();
+        String testUser = getUserNameFreeDomain(testName);
+        ShareUser.login(drone, testUser, DEFAULT_PASSWORD);
+        String rssUrl = "http://feeds.reuters.com/reuters/businessNews";
+
+        AddOnsRssFeedDashlet rssDashlet = ShareUserDashboard.getDashlet(drone, Dashlets.ALFRESCO_ADDONS_RSS_FEED).render();
+
+        // ---- Step 1 ----
+        // ---- Step action ---
+        // Click Configure this dashlet icon
+        // ---- Expected results ----
+        // Enter Feed URL form displays
+        RssFeedUrlBoxPage rssFeedUrlBoxPage = rssDashlet.clickConfigure().render();
+        Assert.assertNotNull(rssFeedUrlBoxPage);
+
+        // ---- Step 2 ----
+        // ---- Step action ---
+        // Leave URL field empty
+        // ---- Expected results ----
+        // OK button disabled;
+        // For v4.2: OK button is enabled and "The value cannot be empty" message appears near the "URL" button;
+        String errorEmpty = rssFeedUrlBoxPage.getValidationMessageFromUrlField("");
+        Assert.assertTrue(errorEmpty.equals("The value cannot be empty."));
+
+        // ---- Step 3 ----
+        // ---- Step action ---
+        // Fill URL field with spaces
+        // ---- Expected results ----
+        // OK button disabled;
+        // For v4.2: OK button is enabled and "The value cannot be empty" message appears near the "URL" button;
+        String errorSpaces = rssFeedUrlBoxPage.getValidationMessageFromUrlField("                ");
+        Assert.assertTrue(errorSpaces.equals("The value cannot be empty."));
+
+        // ---- Step 4 ----
+        // ---- Step action ---
+        // Enter into URL filed special characters (~!@#$%^&*()_{}:|"<>?)
+        // ---- Expected results ----
+        // OK button disabled;
+        // For v4.2: OK button is enabled and "Field contains an error." message appears near the "URL" button
+        String errorSpecial = rssFeedUrlBoxPage.getValidationMessageFromUrlField("~!@#$%^&*()_{}:");
+        Assert.assertTrue(errorSpecial.equals("Field contains an error."));
+
+        // ---- Step 5 ----
+        // ---- Step action ---
+        // Enter info URL field more than 1024 character (without slashes and with http://)
+        // ---- Expected results ----
+        // OK button enabled;
+        String str = createString(1024);
+        rssFeedUrlBoxPage.fillURL("http://" + str);
+
+        // ---- Step 6 ----
+        // ---- Step action ---
+        // Press OK button;
+        // ---- Expected results ----
+        // Dashlet displays correctly
+        rssFeedUrlBoxPage.clickOk();
+        rssDashlet = ShareUserDashboard.getDashlet(drone, Dashlets.ALFRESCO_ADDONS_RSS_FEED).render();
+        Assert.assertTrue(rssDashlet.getHeaderInfo().equals(headerInfo));
+        Assert.assertTrue(rssDashlet.isHelpIconDisplayed());
+        Assert.assertTrue(rssDashlet.isConfigurePresent());
+        List<ShareLink> links = rssDashlet.getHeadlineLinksFromDashlet();
+        Assert.assertTrue(links.size() == 0);
+
+        // ---- Step 7 ----
+        // ---- Step action ---
+        // Click Configure this dashlet icon again
+        // ---- Expected results ----
+        // Enter RSS URL for displays
+        rssFeedUrlBoxPage = rssDashlet.clickConfigure().render();
+
+        // ---- Step 8 ----
+        // ---- Step action ---
+        // Enter something into URL field and click Cancel button
+        // ---- Expected results ----
+        // Dashlet displays. No changes were made.
+        rssFeedUrlBoxPage.fillURL(rssUrl);
+        rssFeedUrlBoxPage.clickCancel();
+        links = rssDashlet.getHeadlineLinksFromDashlet();
+        Assert.assertTrue(links.size() == 0);
+    }
+
+    private static String createString(int size)
+    {
+        StringBuilder o = new StringBuilder(size);
+        for (int i = 0; i < size; i++)
+        {
+            o.append("f");
+        }
+        return o.toString();
+    }
+
+}
---
+++ projects/qa-share/src/test/java/org/alfresco/share/mydashboard/MyDashboardViewTests.java
@@ -0,0 +1,115 @@
+package org.alfresco.share.mydashboard;
+
+import org.alfresco.po.share.dashlet.MyActivitiesDashlet;
+import org.alfresco.po.share.dashlet.MyDocumentsDashlet;
+import org.alfresco.po.share.dashlet.MySitesDashlet;
+import org.alfresco.po.share.dashlet.MyTasksDashlet;
+import org.alfresco.po.share.enums.Dashlets;
+import org.alfresco.po.share.util.FailedTestListener;
+import org.alfresco.share.util.AbstractUtils;
+import org.alfresco.share.util.ShareUser;
+import org.alfresco.share.util.ShareUserDashboard;
+import org.alfresco.share.util.api.CreateUserAPI;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(FailedTestListener.class)
+public class MyDashboardViewTests extends AbstractUtils
+{
+    private static Log logger = LogFactory.getLog(MyDashboardViewTests.class);
+
+    @Override
+    @BeforeClass(alwaysRun = true)
+    public void setup() throws Exception
+    {
+        super.setup();
+
+        testName = this.getClass().getSimpleName();
+        logger.info("Starting Tests: " + testName);
+    }
+
+    @Test(groups = { "EnterpriseOnly" })
+    public void AONE_2819() throws Exception
+    {
+        ShareUser.login(drone, ADMIN_USERNAME, ADMIN_PASSWORD);
+
+        MySitesDashlet mySites = ShareUserDashboard.getDashlet(drone, Dashlets.MY_SITES).render();
+        Assert.assertTrue(mySites.isHelpIconDisplayed());
+
+        MyActivitiesDashlet myActivities = ShareUserDashboard.getDashlet(drone, Dashlets.MY_ACTIVITIES).render();
+        Assert.assertTrue(myActivities.isHelpIconDisplayed());
+
+        MyTasksDashlet myTasks = ShareUserDashboard.getDashlet(drone, Dashlets.MY_TASKS).render();
+        Assert.assertTrue(myTasks.isHelpIconDisplayed());
+
+        MyDocumentsDashlet myDocuments = ShareUserDashboard.getDashlet(drone, Dashlets.MY_DOCUMENTS).render();
+        Assert.assertTrue(myDocuments.isHelpIconDisplayed());
+    }
+
+    @Test(groups = { "DataPrepDashlets" })
+    public void dataPrep_2820() throws Exception
+    {
+        String testName = getTestName();
+        String testUser = getUserNameFreeDomain(testName);
+
+        // Create User
+        String[] testUserInfo = new String[] { testUser };
+        CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, testUserInfo);
+    }
+
+    @Test(groups = { "EnterpriseOnly" })
+    public void AONE_2820() throws Exception
+    {
+        String testName = getTestName();
+        String testUser = getUserNameFreeDomain(testName);
+
+        ShareUser.login(drone, testUser, DEFAULT_PASSWORD);
+
+        // ---- Step 1 ----
+        // ---- Step action ---
+        // Modify that users dashboard somehow, ex. remove a dashlet.
+        // ---- Expected results ----
+        // The dashbord is modified.
+        ShareUserDashboard.removeDashletFromUserDashboard(drone, Dashlets.MY_DOCUMENTS);
+
+        MyDocumentsDashlet myDocuments = ShareUserDashboard.getDashlet(drone, Dashlets.MY_DOCUMENTS).render();
+
+        // ---- Step 2 ----
+        // ---- Step action ---
+        // Login as admin and delete the user "qwerty".
+        // ---- Expected results ----
+        // The user is removed.
+        ShareUser.login(drone, ADMIN_USERNAME, ADMIN_PASSWORD);
+        ShareUser.deleteUser(drone, testUser).render();
+
+        // ---- Step 3 ----
+        // ---- Step action ---
+        // Create a new user with the same username .
+        // ---- Expected results ----
+        // New user is created.
+        String[] testUserInfo = new String[] { testUser };
+        CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, testUserInfo);
+
+        // ---- Step 4 ----
+        // ---- Step action ---
+        // Verify that dashboards order is set to default.
+        // ---- Expected results ----
+        // Dashboards are default.
+        ShareUser.login(drone, testUser, DEFAULT_PASSWORD);
+        MySitesDashlet mySites = ShareUserDashboard.getDashlet(drone, Dashlets.MY_SITES).render();
+        Assert.assertTrue(mySites.isHelpIconDisplayed());
+
+        MyActivitiesDashlet myActivities = ShareUserDashboard.getDashlet(drone, Dashlets.MY_ACTIVITIES).render();
+        Assert.assertTrue(myActivities.isHelpIconDisplayed());
+
+        MyTasksDashlet myTasks = ShareUserDashboard.getDashlet(drone, Dashlets.MY_TASKS).render();
+        Assert.assertTrue(myTasks.isHelpIconDisplayed());
+
+        myDocuments = ShareUserDashboard.getDashlet(drone, Dashlets.MY_DOCUMENTS).render();
+        Assert.assertTrue(myDocuments.isHelpIconDisplayed());
+    }
+}
