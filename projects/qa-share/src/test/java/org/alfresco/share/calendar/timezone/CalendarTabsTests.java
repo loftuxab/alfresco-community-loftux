0c67ac421293232fbcb4a871799f2896fb087161
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 18:01:27 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    89786: QA-968 Adding tests for test cases AONE_672, AONE_682 and AONE_16620
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@95160 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/qa-share/src/test/java/org/alfresco/share/calendar/timezone/CalendarTabsTests.java
+++ projects/qa-share/src/test/java/org/alfresco/share/calendar/timezone/CalendarTabsTests.java
@@ -7,8 +7,8 @@ package org.alfresco.share.calendar.timezone;
  */
 
 import java.util.Map;
-import java.util.TimeZone;
 
+import org.alfresco.application.util.Application;
 import org.alfresco.po.share.site.SiteDashboardPage;
 import org.alfresco.po.share.site.SitePageType;
 import org.alfresco.po.share.site.calendar.CalendarPage;
@@ -19,13 +19,15 @@ import org.alfresco.share.util.CalendarUtil;
 import org.alfresco.share.util.ShareUser;
 import org.alfresco.share.util.ShareUserDashboard;
 import org.alfresco.share.util.api.CreateUserAPI;
+import org.alfresco.windows.application.MicorsoftOffice2010;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+import com.cobra.ldtp.Ldtp;
 
 @Listeners(FailedTestListener.class)
 public class CalendarTabsTests extends AbstractUtils
@@ -37,7 +39,10 @@ public class CalendarTabsTests extends AbstractUtils
 
     private String defaultTZ = "London";
     private String newTZ = "Bucharest";
-    
+
+    MicorsoftOffice2010 outlook = new MicorsoftOffice2010(Application.OUTLOOK, "2010");
+    private String sharePointPath;
+
     @Override
     @BeforeClass(alwaysRun = true)
     public void setup() throws Exception
@@ -49,13 +54,17 @@ public class CalendarTabsTests extends AbstractUtils
 
         logger.info("Start Tests in: " + testName);
 
+        Runtime.getRuntime().exec("taskkill /F /IM OUTLOOK.EXE");
+        sharePointPath = outlook.getSharePointPath();
+
         CalendarUtil.changeTimeZone(defaultTZ);
     }
-
 
     @AfterMethod(alwaysRun = true)
     public void teardownMEthod() throws Exception
     {
+        Runtime.getRuntime().exec("taskkill /F /IM OUTLOOK.EXE");
+        Runtime.getRuntime().exec("taskkill /F /IM CobraWinLDTP.EXE");
         CalendarUtil.changeTimeZone(defaultTZ);
     }
 
@@ -64,8 +73,8 @@ public class CalendarTabsTests extends AbstractUtils
     {
 
         // Create normal User
-       String[] testUser2 = new String[] { testUser };
-       CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, testUser2);
+        String[] testUser2 = new String[] { testUser };
+        CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, testUser2);
     }
 
     @Test(groups = { "Calendar", "EnterpriseOnly" })
@@ -659,6 +668,118 @@ public class CalendarTabsTests extends AbstractUtils
 
     }
 
+    /** AONE-16620:Calendar tabs. Recurrent */
+    @Test(groups = { "Calendar", "EnterpriseOnly" })
+    public void AONE_16620() throws Exception
+    {
+        boolean allDay = false;
+        String location = testName + " - Room";
+        String startDate = "2:30 PM";
+        String endDate = "5:25 PM";
+        String siteName = getSiteName(testName);
+
+        // MS Outlook 2010 is opened;
+        Ldtp l = outlook.openOfficeApplication();
+
+        // create new meeting workspace
+        outlook.operateOnCreateNewMeetingWorkspace(l, sharePointPath, siteName, location, testUser, DEFAULT_PASSWORD, true, false);
+
+        // Step 1
+        // Create any recurrent event, e.g.:
+        //
+        // Name: test-event;
+        // Start Date: 28/06/2013 14:30;
+        // End Date: 28/06/2013 17:25;
+        // Recurrence: Daily, Every 1 day;
+        // End after: 3 occurences.
+        Ldtp l1 = outlook.getAbstractUtil().setOnWindow(siteName);
+        l1.click("chkAlldayevent");
+        l1 = outlook.getAbstractUtil().setOnWindow(siteName);
+        l1.mouseLeftClick("btnRecurrence");
+        // set the recurrence
+        outlook.operateOnRecurrenceAppointment(l1, startDate, endDate, "3");
+
+        Ldtp after_rec = outlook.getAbstractUtil().setOnWindow(siteName);
+        after_rec.click("btnSave");
+
+        // User login.
+        ShareUser.login(drone, testUser);
+
+        // Step 2
+        // Open Calendar Day tab
+        // Calendar Day tab is opened
+        SiteDashboardPage siteDashBoard = ShareUser.openSiteDashboard(drone, siteName).render(maxWaitTime);
+        siteDashBoard.getSiteNav().selectCalendarPage();
+
+        Map<String, String> timeValues = CalendarUtil.addValuesToCurrentDate(0, 0, startDate, endDate, allDay);
+        String expectedStartDate = timeValues.get("startDateValue");
+        String expectedEndDate = timeValues.get("endDateValue");
+                        
+        Map<String, String> recuurrenceTimeValues = CalendarUtil.addValuesToCurrentDate(0, 2, startDate, endDate, allDay);
+        String expectedRecurrenceStartDate = recuurrenceTimeValues.get("startDateValue");
+        String expectedRecurrenceEndDate = recuurrenceTimeValues.get("endDateValue");
+
+        expectedRecurrenceStartDate = CalendarUtil.convertDateToNewTimeZone(expectedRecurrenceStartDate, defaultTZ, newTZ, allDay);
+        expectedRecurrenceEndDate = CalendarUtil.convertDateToNewTimeZone(expectedRecurrenceEndDate, defaultTZ, newTZ, allDay);
+        
+        String expectedRecurrence = "Occurs every day effective " + CalendarUtil.getDateInFormat(expectedRecurrenceStartDate, "MMM d, yyyy", allDay) + " until "
+                + CalendarUtil.getDateInFormat(expectedRecurrenceEndDate, "MMM d, yyyy", allDay) + " from "
+                + CalendarUtil.getDateInFormat(expectedRecurrenceStartDate, "h:mm a", allDay) + " to " + CalendarUtil.getDateInFormat(expectedRecurrenceEndDate, "h:mm a", allDay)
+                + " (EET)";
+
+        // Step 3
+        // Click on the event and check the event details.
+
+        // The following is displayed:
+        //
+        // Name: test-event;
+        // Start Date: <current day> <current month>, <current year> at 2:30 PM;
+        // End Date: <current day> <current month>, <current year> at 5:25 PM;
+        // Recurrence: Occurs every day effective <current day> <current month>, <current year> until <current day + 3 days> <current month>, <current year>
+        // from 12:30 PM to 3:25 PM (BST)
+        clickOnEventAndVerifyDate(CalendarPage.EventType.DAY_TAB_SINGLE_EVENT, siteName, expectedStartDate, expectedEndDate);
+        clickOnEventAndVerifyRecurrence(CalendarPage.EventType.DAY_TAB_SINGLE_EVENT, siteName, expectedRecurrence);
+        
+        clickOnEventAndVerifyDate(CalendarPage.EventType.WEEK_TAB_SINGLE_EVENT, siteName, expectedStartDate, expectedEndDate);
+        clickOnEventAndVerifyRecurrence(CalendarPage.EventType.WEEK_TAB_SINGLE_EVENT, siteName, expectedRecurrence);
+        
+        clickOnEventAndVerifyDate(CalendarPage.EventType.MONTH_TAB_SINGLE_EVENT, siteName, expectedStartDate, expectedEndDate);
+        clickOnEventAndVerifyRecurrence(CalendarPage.EventType.MONTH_TAB_SINGLE_EVENT, siteName, expectedRecurrence);
+        
+        clickOnEventAndVerifyDate(CalendarPage.EventType.AGENDA_TAB_SINGLE_EVENT, siteName, expectedStartDate, expectedEndDate);
+        clickOnEventAndVerifyRecurrence(CalendarPage.EventType.AGENDA_TAB_SINGLE_EVENT, siteName, expectedRecurrence);
+
+        ShareUser.logout(drone);
+
+        // Step 4
+        // On a server machine, open Site Dashboard - instead of accessing a server machine, the date time of the current machine is changed
+        CalendarUtil.changeTimeZone(newTZ);
+
+        ShareUser.login(drone, testUser);
+
+        // Calendar is opened
+        siteDashBoard = ShareUser.openSiteDashboard(drone, siteName).render(maxWaitTime);
+
+        // navigate to Calendar
+        siteDashBoard.getSiteNav().selectCalendarPage();
+
+        expectedStartDate = CalendarUtil.convertDateToNewTimeZone(expectedStartDate, defaultTZ, newTZ, allDay);
+        expectedEndDate = CalendarUtil.convertDateToNewTimeZone(expectedEndDate, defaultTZ, newTZ, allDay);
+
+        clickOnEventAndVerifyDate(CalendarPage.EventType.DAY_TAB_SINGLE_EVENT, siteName, expectedStartDate, expectedEndDate);
+        clickOnEventAndVerifyRecurrence(CalendarPage.EventType.DAY_TAB_SINGLE_EVENT, siteName, expectedRecurrence);
+        
+        clickOnEventAndVerifyDate(CalendarPage.EventType.WEEK_TAB_SINGLE_EVENT, siteName, expectedStartDate, expectedEndDate);
+        clickOnEventAndVerifyRecurrence(CalendarPage.EventType.WEEK_TAB_SINGLE_EVENT, siteName, expectedRecurrence);
+        
+        clickOnEventAndVerifyDate(CalendarPage.EventType.MONTH_TAB_SINGLE_EVENT, siteName, expectedStartDate, expectedEndDate);
+        clickOnEventAndVerifyRecurrence(CalendarPage.EventType.MONTH_TAB_SINGLE_EVENT, siteName, expectedRecurrence);
+        
+        clickOnEventAndVerifyDate(CalendarPage.EventType.AGENDA_TAB_SINGLE_EVENT, siteName, expectedStartDate, expectedEndDate);
+        clickOnEventAndVerifyRecurrence(CalendarPage.EventType.AGENDA_TAB_SINGLE_EVENT, siteName, expectedRecurrence);
+
+    }
+
     /***
      * @param eventType
      * @param eventName
@@ -691,6 +812,19 @@ public class CalendarTabsTests extends AbstractUtils
 
         Assert.assertEquals(actualStartDate, expectedStartDate);
         Assert.assertEquals(actualEndDate, expectedEndDate);
+    }
+
+    private void clickOnEventAndVerifyRecurrence(CalendarPage.EventType eventType, String eventName, String expectedRecurrence)
+    {
+        CalendarPage calendarPage = drone.getCurrentPage().render();
+
+        InformationEventForm eventInfo = calendarPage.clickOnEvent(eventType, eventName).render(maxWaitTime);
+
+        // get start and end date time from event information page
+        String actualRecurrence = eventInfo.getRecurrenceDetail();
+
+        Assert.assertEquals(actualRecurrence, expectedRecurrence);
+        calendarPage = eventInfo.closeInformationForm().render();
     }
 
 }
--- projects/qa-share/src/test/java/org/alfresco/share/calendar/timezone/SiteDashboardTests.java
+++ projects/qa-share/src/test/java/org/alfresco/share/calendar/timezone/SiteDashboardTests.java
@@ -8,6 +8,7 @@ package org.alfresco.share.calendar.timezone;
 
 import java.util.Map;
 
+import org.alfresco.application.util.Application;
 import org.alfresco.po.share.dashlet.SiteCalendarDashlet;
 import org.alfresco.po.share.enums.Dashlets;
 import org.alfresco.po.share.site.SiteDashboardPage;
@@ -19,13 +20,15 @@ import org.alfresco.share.util.CalendarUtil;
 import org.alfresco.share.util.ShareUser;
 import org.alfresco.share.util.ShareUserDashboard;
 import org.alfresco.share.util.api.CreateUserAPI;
+import org.alfresco.windows.application.MicorsoftOffice2010;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+import com.cobra.ldtp.Ldtp;
 
 @Listeners(FailedTestListener.class)
 public class SiteDashboardTests extends AbstractUtils
@@ -35,9 +38,12 @@ public class SiteDashboardTests extends AbstractUtils
     private String testName;
     private String testUser;
     private String siteName;
-    
+
     private String defaultTZ = "London";
     private String newTZ = "Bucharest";
+
+    MicorsoftOffice2010 outlook = new MicorsoftOffice2010(Application.OUTLOOK, "2010");
+    private String sharePointPath;
 
     @Override
     @BeforeClass(alwaysRun = true)
@@ -51,13 +57,19 @@ public class SiteDashboardTests extends AbstractUtils
 
         logger.info("Start Tests in: " + testName);
 
+        Runtime.getRuntime().exec("taskkill /F /IM OUTLOOK.EXE");
+        sharePointPath = outlook.getSharePointPath();
+
         CalendarUtil.changeTimeZone(defaultTZ);
     }
 
     @AfterMethod(alwaysRun = true)
     public void teardownMEthod() throws Exception
     {
-        CalendarUtil.changeTimeZone(defaultTZ);    }
+        CalendarUtil.changeTimeZone(defaultTZ);
+        Runtime.getRuntime().exec("taskkill /F /IM OUTLOOK.EXE");
+        Runtime.getRuntime().exec("taskkill /F /IM CobraWinLDTP.EXE");
+    }
 
     @Test(groups = { "DataPrepCalendar" })
     public void dataPrep_AONE_SiteDashboard() throws Exception
@@ -76,7 +88,6 @@ public class SiteDashboardTests extends AbstractUtils
         // Add Calendar dashlet
         ShareUserDashboard.addDashlet(drone, siteName, Dashlets.SITE_CALENDAR);
 
-        
         ShareUserDashboard.addPageToSite(drone, siteName, SitePageType.CALENDER);
 
     }
@@ -87,10 +98,10 @@ public class SiteDashboardTests extends AbstractUtils
         boolean allDay = false;
 
         ShareUser.login(drone, testUser);
-        
+
         // Calendar is opened
         SiteDashboardPage siteDashBoard = ShareUser.openSiteDashboard(drone, siteName).render(maxWaitTime);
-        
+
         // navigate to Calendar
         CalendarPage calendarPage = siteDashBoard.getSiteNav().selectCalendarPage();
 
@@ -434,7 +445,7 @@ public class SiteDashboardTests extends AbstractUtils
         ShareUser.logout(drone);
 
         CalendarUtil.changeTimeZone(newTZ);
-        
+
         ShareUser.login(drone, testUser);
 
         // Calendar is opened
@@ -557,6 +568,83 @@ public class SiteDashboardTests extends AbstractUtils
 
         eventDetail = convertedStartTime + " " + event1 + " (until: " + expectedEndDate + ")";
         Assert.assertTrue(siteCalendarDashlet.isEventsWithDetailDisplayed(eventDetail), "The " + eventDetail + " isn't correctly displayed on calendar");
+
+    }
+
+    /** AONE-672:Site Dashboard. Calendar dashlet. Recurrent */
+    @Test(groups = { "Calendar", "EnterpriseOnly" })
+    public void AONE_672() throws Exception
+    {
+        boolean allDay = false;
+        String location = testName + " - Room";
+        String startDate = "2:30 PM";
+        String endDate = "5:25 PM";
+
+        // MS Outlook 2010 is opened;
+        Ldtp l = outlook.openOfficeApplication();
+
+        // create new meeting workspace
+        outlook.operateOnCreateNewMeetingWorkspace(l, sharePointPath, siteName, location, testUser, DEFAULT_PASSWORD, true, false);
+
+        // Step 1
+        // Create any recurrent event, e.g.:
+        //
+        // Name: test-event;
+        // Start Date: 28/06/2013 14:30;
+        // End Date: 28/06/2013 17:25;
+        // Recurrence: Daily, Every 1 day;
+        // End after: 3 occurences.
+        Ldtp l1 = outlook.getAbstractUtil().setOnWindow(siteName);
+        l1.click("chkAlldayevent");
+        l1 = outlook.getAbstractUtil().setOnWindow(siteName);
+        l1.mouseLeftClick("btnRecurrence");
+        // set the recurrence
+        outlook.operateOnRecurrenceAppointment(l1, startDate, endDate, "3");
+
+        Ldtp after_rec = outlook.getAbstractUtil().setOnWindow(siteName);
+        after_rec.click("btnSave");
+
+        // User login.
+        ShareUser.login(drone, testUser);
+
+        // Step 2
+        // Open Site Dashboard
+        // Site Dashboard is opened
+        SiteDashboardPage siteDashBoard = ShareUser.openSiteDashboard(drone, siteName).render(maxWaitTime);
+
+        // Step 3
+        // Verify Calendar dashlet.
+        SiteCalendarDashlet siteCalendarDashlet = siteDashBoard.getDashlet("site-calendar").render();
+
+        Map<String, String> timeValues = CalendarUtil.addValuesToCurrentDate(0, 0, startDate, endDate, allDay);
+        String eventDetail = siteName + " (Repeating)";
+        String eventHeader = timeValues.get("endDayOfWeek") + ", " + timeValues.get("startDay") + " " + timeValues.get("startMonth") + ", "
+                + timeValues.get("startYear");
+
+        Assert.assertTrue(siteCalendarDashlet.isEventsDisplayed(siteName), "The " + siteName + " isn't correctly displayed on calendar");
+        Assert.assertTrue(siteCalendarDashlet.isEventsWithHeaderDisplayed(eventHeader), "The " + eventDetail + " isn't correctly displayed on calendar");
+        Assert.assertTrue(siteCalendarDashlet.isEventsWithDetailDisplayed(eventDetail), "The " + eventDetail + " isn't correctly displayed on calendar");
+        Assert.assertTrue(siteCalendarDashlet.isRepeating(siteName), "The " + siteName + " isn't cnot a repeating event");
+
+        ShareUser.logout(drone);
+
+        // Step 4
+        // On a server machine, open Site Dashboard - instead of accessing a server machine, the date time of the current machine is changed
+        CalendarUtil.changeTimeZone(newTZ);
+
+        ShareUser.login(drone, testUser);
+        
+        // Verify Site Dashboard is opened
+        siteDashBoard = ShareUser.openSiteDashboard(drone, siteName).render(maxWaitTime);
+
+        // Step 5
+        // Verify Calendar dashlet.
+        siteCalendarDashlet = siteDashBoard.getDashlet("site-calendar").render();
+
+        Assert.assertTrue(siteCalendarDashlet.isEventsDisplayed(siteName), "The " + siteName + " isn't correctly displayed on calendar");
+        Assert.assertTrue(siteCalendarDashlet.isEventsWithHeaderDisplayed(eventHeader), "The " + eventDetail + " isn't correctly displayed on calendar");
+        Assert.assertTrue(siteCalendarDashlet.isEventsWithDetailDisplayed(eventDetail), "The " + eventDetail + " isn't correctly displayed on calendar");
+        Assert.assertTrue(siteCalendarDashlet.isRepeating(siteName), "The " + siteName + " isn't cnot a repeating event");
 
     }
 
