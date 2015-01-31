d4c26636b241749fd441d766abeb7015ddcf8304
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 21:08:06 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    93121: Merged 5.0.N (5.0.1) to HEAD-BUG-FIX (5.1/Cloud)       93080: QA-1066 Format MyDashboardViewTests. Add render() to method removeDashletFromUserDashboard() from ShareUserDashboard
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@95594 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 978cb6a0a13feda4e6f3cb06805f39dbf425aa08

--- root/projects/qa-share/src/test/java/org/alfresco/share/mydashboard/MyDashboardViewTests.java
+++ root/projects/qa-share/src/test/java/org/alfresco/share/mydashboard/MyDashboardViewTests.java
@@ -32,26 +32,26 @@ public class MyDashboardViewTests extends AbstractUtils
         logger.info("Starting Tests: " + testName);
     }
 
-    @Test(groups = { "EnterpriseOnly" })
+    @Test(groups = { "EnterpriseOnly" }, timeOut = 100000 )
     public void AONE_2819() throws Exception
     {
         ShareUser.login(drone, ADMIN_USERNAME, ADMIN_PASSWORD);
 
         MySitesDashlet mySites = ShareUserDashboard.getDashlet(drone, Dashlets.MY_SITES).render();
-        Assert.assertTrue(mySites.isHelpIconDisplayed());
+        Assert.assertTrue(mySites.isHelpIconDisplayed(), "Help icon missing");
 
         MyActivitiesDashlet myActivities = ShareUserDashboard.getDashlet(drone, Dashlets.MY_ACTIVITIES).render();
-        Assert.assertTrue(myActivities.isHelpIconDisplayed());
+        Assert.assertTrue(myActivities.isHelpIconDisplayed(), "Help icon missing");
 
         MyTasksDashlet myTasks = ShareUserDashboard.getDashlet(drone, Dashlets.MY_TASKS).render();
-        Assert.assertTrue(myTasks.isHelpIconDisplayed());
+        Assert.assertTrue(myTasks.isHelpIconDisplayed(), "Help icon missing");
 
         MyDocumentsDashlet myDocuments = ShareUserDashboard.getDashlet(drone, Dashlets.MY_DOCUMENTS).render();
-        Assert.assertTrue(myDocuments.isHelpIconDisplayed());
+        Assert.assertTrue(myDocuments.isHelpIconDisplayed(), "Help icon missing");
     }
 
     @Test(groups = { "DataPrepDashlets" })
-    public void dataPrep_2820() throws Exception
+    public void dataPrep_AONE_2820() throws Exception
     {
         String testName = getTestName();
         String testUser = getUserNameFreeDomain(testName);
@@ -61,7 +61,7 @@ public class MyDashboardViewTests extends AbstractUtils
         CreateUserAPI.CreateActivateUser(drone, ADMIN_USERNAME, testUserInfo);
     }
 
-    @Test(groups = { "EnterpriseOnly" })
+    @Test(groups = { "EnterpriseOnly"}, timeOut = 280000  )
     public void AONE_2820() throws Exception
     {
         String testName = getTestName();
@@ -74,8 +74,9 @@ public class MyDashboardViewTests extends AbstractUtils
         // Modify that users dashboard somehow, ex. remove a dashlet.
         // ---- Expected results ----
         // The dashbord is modified.
-        ShareUserDashboard.removeDashletFromUserDashboard(drone, Dashlets.MY_DOCUMENTS);
-        
+        ShareUserDashboard.removeDashletFromUserDashboard(drone, Dashlets.MY_TASKS);
+        ShareUser.logout(drone);
+
         // ---- Step 2 ----
         // ---- Step action ---
         // Login as admin and delete the user "qwerty".
@@ -99,15 +100,15 @@ public class MyDashboardViewTests extends AbstractUtils
         // Dashboards are default.
         ShareUser.login(drone, testUser, DEFAULT_PASSWORD);
         MySitesDashlet mySites = ShareUserDashboard.getDashlet(drone, Dashlets.MY_SITES).render();
-        Assert.assertTrue(mySites.isHelpIconDisplayed());
+        Assert.assertTrue(mySites.isHelpIconDisplayed(), "Help icon missing");
 
         MyActivitiesDashlet myActivities = ShareUserDashboard.getDashlet(drone, Dashlets.MY_ACTIVITIES).render();
-        Assert.assertTrue(myActivities.isHelpIconDisplayed());
+        Assert.assertTrue(myActivities.isHelpIconDisplayed(), "Help icon missing");
 
         MyTasksDashlet myTasks = ShareUserDashboard.getDashlet(drone, Dashlets.MY_TASKS).render();
         Assert.assertTrue(myTasks.isHelpIconDisplayed());
 
         MyDocumentsDashlet myDocuments = ShareUserDashboard.getDashlet(drone, Dashlets.MY_DOCUMENTS).render();
-        Assert.assertTrue(myDocuments.isHelpIconDisplayed());
+        Assert.assertTrue(myDocuments.isHelpIconDisplayed(), "Help icon missing");
     }
 }
