fca49256fedd620bea6f2a63c1e9a786854bd89f
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-01-31 19:59:37 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@95370 7033f55c-6c29-4ad8-afba-765cb616e738
    Former-commit-id: 5592f9979b67b7d695410953275e91e5927cf182

--- root/projects/share-po/src/main/java/org/alfresco/po/share/wqs/WcmqsAllPublicationsPage.java
+++ root/projects/share-po/src/main/java/org/alfresco/po/share/wqs/WcmqsAllPublicationsPage.java
@@ -1,8 +1,5 @@
 package org.alfresco.po.share.wqs;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.alfresco.po.share.ShareLink;
 import org.alfresco.po.share.SharePage;
 import org.alfresco.webdrone.RenderTime;
@@ -13,12 +10,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.alfresco.webdrone.RenderElement.getVisibleRenderElement;
+
 public class WcmqsAllPublicationsPage extends SharePage
 {
     @RenderWebElement
     private final By PAGE_TITLE = By.cssSelector("div.interior-header > h2");
-    private final By PUBLICATIONS_TITLES = By.cssSelector(".portfolio-wrapper>li>h3>a");  
-    
+    private final By PUBLICATIONS_TITLES = By.cssSelector(".portfolio-wrapper>li>h3>a");
+    private final By KEY_PUBLICATIONS_SECTION = By.cssSelector(".interior-content>h3");
+    private final By PUBLICATION_PREVIEWS = By.cssSelector(".img-border");
+
     /**
      * Constructor.
      * 
@@ -33,7 +37,7 @@ public class WcmqsAllPublicationsPage extends SharePage
     @Override
     public WcmqsAllPublicationsPage render(RenderTime timer)
     {
-        webElementRender(timer);
+        elementRender(timer, getVisibleRenderElement(KEY_PUBLICATIONS_SECTION));
         return this;
     }
 
@@ -74,4 +78,35 @@ public class WcmqsAllPublicationsPage extends SharePage
 
         return folders;
     }
+
+    public WebElement getKeyPublicationsSection()
+    {
+        return drone.find(KEY_PUBLICATIONS_SECTION);
+    }
+
+    /**
+     * Method to get all titles from Publication Page
+     *
+     * @return List<ShareLink>
+     */
+    public List<ShareLink> getAllPublictionsImages()
+    {
+        List<ShareLink> folders = new ArrayList<ShareLink>();
+        try
+        {
+            List<WebElement> links = drone.findAll(PUBLICATION_PREVIEWS);
+            for (WebElement div : links)
+            {
+                folders.add(new ShareLink(div, drone));
+            }
+        }
+        catch (NoSuchElementException nse)
+        {
+            throw new PageException("Unable to access news site data", nse);
+        }
+
+        return folders;
+    }
+
+
 }
