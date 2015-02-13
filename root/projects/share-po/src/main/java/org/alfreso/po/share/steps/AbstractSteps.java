a45eb03058886fe8f453c0614d3eee1a2e0853f0
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-02-13 09:50:12 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@96990 7033f55c-6c29-4ad8-afba-765cb616e738
    Former-commit-id: de34dca153fe65bd5875ad0ba94e0e35f12b5a4a

---
+++ root/projects/share-po/src/main/java/org/alfreso/po/share/steps/AbstractSteps.java
@@ -0,0 +1,62 @@
+package org.alfreso.po.share.steps;
+
+import org.alfresco.po.share.SharePage;
+import org.alfresco.po.share.search.SearchKeys;
+import org.alfresco.webdrone.HtmlPage;
+import org.alfresco.webdrone.WebDrone;
+import org.alfresco.webdrone.exception.PageException;
+
+public class AbstractSteps
+{
+    // Constants
+    protected static final String SITE_VISIBILITY_PUBLIC = "public";
+    protected static final String SITE_VISIBILITY_PRIVATE = "private";
+    protected static final String SITE_VISIBILITY_MODERATED = "moderated";
+    protected static final String DOCLIB = "DocumentLibrary";
+    protected static final String REPO = "Repository";
+    protected static final String SITES = "Sites";
+    protected static final String DOCLIB_CONTAINER = "documentLibrary";
+    protected static final String MY_DASHBOARD = " Dashboard";
+    protected final static String SERACH_ZERO_CONTENT = "ZERO_RESULTS";
+    protected final static String NONE = "(None)";
+    protected final static String adminGroup = "ALFRESCO_ADMINISTRATORS";
+    protected static final String BASIC_SEARCH = SearchKeys.BASIC_SEARCH.getSearchKeys();
+    protected static final String ADV_FOLDER_SEARCH = SearchKeys.FOLDERS.getSearchKeys();
+    protected static final String ADV_CONTENT_SEARCH = SearchKeys.CONTENT.getSearchKeys();
+    protected static final String ADV_CRM_SEARCH = SearchKeys.CRM_SEARCH.getSearchKeys();
+    
+    /**
+     * Checks if driver is null, throws UnsupportedOperationException if so.
+     * 
+     * @param driver WebDrone Instance
+     * @throws UnsupportedOperationException if driver is null
+     */
+    protected static void checkIfDriverNull(WebDrone driver)
+    {
+        if (driver == null)
+        {
+            throw new UnsupportedOperationException("WebDrone is required");
+        }
+    }
+    
+    /**
+     * Checks if the current page is share page, throws PageException if not.
+     * 
+     * @param driver WebDrone Instance
+     * @return SharePage
+     * @throws PageException if the current page is not a share page
+     */
+    public static SharePage getSharePage(WebDrone driver)
+    {
+        checkIfDriverNull(driver);
+        try
+        {
+            HtmlPage generalPage = driver.getCurrentPage().render();
+            return (SharePage) generalPage;
+        }
+        catch (PageException pe)
+        {
+            throw new PageException("Can not cast to SharePage: Current URL: " + driver.getCurrentUrl());
+        }
+    }
+}
---
+++ root/projects/share-po/src/main/java/org/alfreso/po/share/steps/AdminTools.java
@@ -0,0 +1,21 @@
+package org.alfreso.po.share.steps;
+
+import org.alfresco.po.share.DashBoardPage;
+import org.alfresco.po.share.GroupsPage;
+import org.alfresco.webdrone.WebDrone;
+
+public class AdminTools
+{
+    /**
+     * @param driver WebDriver Instance
+     *            Navigate to Groups page
+     * @return Groups page
+     */
+
+    public static GroupsPage navigateToGroup(WebDrone driver)
+    {
+        DashBoardPage dashBoard = ShareUser.openUserDashboard(driver);
+        GroupsPage page = dashBoard.getNav().getGroupsPage();
+        return page;
+    }
+}
