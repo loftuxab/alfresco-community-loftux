d5502dece8d2113f4d9f9ddfc6a2d5a2184bbffd
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-02-16 19:13:03 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@97179 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/share-po/src/main/java/org/alfreso/po/share/steps/AdminActions.java
+++ projects/share-po/src/main/java/org/alfreso/po/share/steps/AdminActions.java
@@ -1,14 +1,18 @@
 package org.alfreso.po.share.steps;
 
+import java.util.List;
+
 import org.alfresco.po.share.DashBoardPage;
 import org.alfresco.po.share.GroupsPage;
+import org.alfresco.po.share.exception.UnexpectedSharePageException;
+import org.alfresco.po.share.site.document.UserProfile;
 import org.alfresco.webdrone.WebDrone;
 
 public class AdminActions extends DashBoardActions
 {
     /**
+     * Navigate to Groups page
      * @param driver WebDriver Instance
-     *            Navigate to Groups page
      * @return Groups page
      */
 
@@ -18,27 +22,67 @@ public class AdminActions extends DashBoardActions
         GroupsPage page = dashBoard.getNav().getGroupsPage();
         return page;
     }
+    
+    /**
+     * Returns GroupsPage
+     * @param driver WebDriver Instance
+     * @return GroupsPage 
+     * @throws UnexpectedSharePageException if not a Groups page instance
+     */
+    private GroupsPage getGroupsPage(WebDrone driver) throws UnexpectedSharePageException
+    {
+        try
+        {
+            GroupsPage page = (GroupsPage) getSharePage(driver);
+            return page;
+        }
+        catch(ClassCastException c)
+        {
+            throw new UnexpectedSharePageException(GroupsPage.class, c);
+        }        
+    }
 
     /**
-     * @param driver WebDriver Instance
-     *            This method is called when the user is on groups page
-     *            Click on browse button in Groups page
+     * Click on browse button in Groups page. 
+     * This method only proceeds when the user is on groups page
+     * 
+     * @param driver WebDriver Instance        
      * @return Groups page
      */
     public GroupsPage browseGroups(WebDrone driver)
     {
-        GroupsPage page = null;
+            GroupsPage page = getGroupsPage(driver);
+            page = page.clickBrowse().render();
+            return page;       
+    }
+    
+    /**
+     * Verify user is a member of group
+     * 
+     * @param driver WebDriver Instance
+     * @param fname- User's first name
+     * @param userName- check whether this user is in group
+     * @param groupName - Check whether user in this specific group Name
+     * @return Boolean
+     */
 
-        if (getSharePage(driver) instanceof GroupsPage)
-        {
-            // Right Page
-        }
-        else
-        {
-            page = navigateToGroup(driver);
-        }
+    public Boolean isUserGroupMember(WebDrone driver, String fName, String uName, String groupName)
+    {
+        GroupsPage page = browseGroups(driver);
+        GroupsPage groupspage = page.selectGroup(groupName).render();
+        List<UserProfile> userProfiles = groupspage.getMembersList();
 
-        GroupsPage groupsPage = page.clickBrowse().render();
-        return groupsPage;
+        for (UserProfile userProfile : userProfiles)
+        {
+            if (fName.equals(userProfile.getfName()))
+            {
+                // Verify user is present in the members list
+                if (userProfile.getUsername().contains(uName))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
