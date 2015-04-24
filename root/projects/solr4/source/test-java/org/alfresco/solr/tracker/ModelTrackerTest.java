d33e4677b7a3882e8483f9f0b1a3710d56f5dbda
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-04-24 11:04:46 +0000

    Merged HEAD-BUG-FIX (5.1/Cloud) to HEAD (5.1/Cloud)    102419: Build fix related R 102258:    Support for     ACE-1021 SOLR 4 - Sharding/Replication/Cluster/CloudACE-3827    SOLR 4 - Shard based on ACL hash
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@102546 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: d41b1fe6396c9d041fc496dad25d064b6591d75d

--- root/projects/solr4/source/test-java/org/alfresco/solr/tracker/ModelTrackerTest.java
+++ root/projects/solr4/source/test-java/org/alfresco/solr/tracker/ModelTrackerTest.java
@@ -94,6 +94,8 @@ public class ModelTrackerTest
         when(props.getProperty("alfresco.maxLiveSearchers", "2")).thenReturn("2");
         when(props.getProperty("enable.slave", "false")).thenReturn("false");
         when(props.getProperty("enable.master", "true")).thenReturn("true");
+        when(props.getProperty("acl.shard.count", "0")).thenReturn("0");
+        when(props.getProperty("acl.shard.instance", "0")).thenReturn("0");
         when(this.srv.getTrackerStats()).thenReturn(trackerStats);
 
         // TODO: create test folder for model sync?
