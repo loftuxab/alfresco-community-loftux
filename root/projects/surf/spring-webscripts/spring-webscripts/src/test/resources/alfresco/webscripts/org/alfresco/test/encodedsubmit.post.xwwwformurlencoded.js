097df3e9d5a3d8c4c0650ca092a60639ff13d1c0
slanglois <slanglois@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-04-30 10:49:22 +0000

    Merge HEAD-BUG-FIX (5.1) to HEAD (5.1/Cloud40)    102517: Merge RA-SPRINT3 to HEAD-BUG-FIX (5.1)       101660: RA-118: Move surf webscripts (and core) projects to common code base and move remaining sub projects to share code base.
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@103267 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 039bd64514b5433d2a196495347fcfbb753cb9cc

--- root/projects/surf/spring-webscripts/spring-webscripts/src/test/resources/alfresco/webscripts/org/alfresco/test/encodedsubmit.post.xwwwformurlencoded.js
+++
@@ -1,4 +0,0 @@
-//FIXME URL-encoded post of forms data is not yet working.
-
-model.p1 = decodedparams["param1"];
-model.p2 = decodedparams["param2"];
--- root/projects/surf/spring-webscripts/spring-webscripts/src/test/resources/alfresco/webscripts/org/alfresco/test/jsonecho.post.json.js
+++
@@ -1,1 +0,0 @@
-model.postCode = jsonUtils.toObject(json).postCode;
