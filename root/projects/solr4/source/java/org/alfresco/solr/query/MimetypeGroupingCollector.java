1a65ce489dc616c37ac1f7182a2dfd1c918e5552
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-06-08 09:21:57 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@105698 7033f55c-6c29-4ad8-afba-765cb616e738
    Former-commit-id: 946790bf01e336a8cca98f5da37d8b422faafccf

---
+++ root/projects/solr4/source/java/org/alfresco/solr/query/MimetypeGroupingCollector.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.solr.query;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.alfresco.solr.AlfrescoSolrDataModel;
+import org.alfresco.solr.AlfrescoSolrDataModel.FieldUse;
+import org.apache.lucene.index.SortedDocValues;
+import org.apache.lucene.util.Counter;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.handler.component.ResponseBuilder;
+import org.apache.solr.schema.SchemaField;
+import org.apache.solr.search.DelegatingCollector;
+
+/**
+ * @author Andy
+ *
+ */
+public class MimetypeGroupingCollector extends DelegatingCollector
+{
+    HashMap<String, Counter> counters = new HashMap<String, Counter>();
+    ResponseBuilder rb;
+    /**
+     * @param rb
+     */
+    public MimetypeGroupingCollector(ResponseBuilder rb)
+    {
+        this.rb = rb;
+    }
+    
+    public void collect(int doc) throws IOException 
+    {
+        String schemaFieldName = AlfrescoSolrDataModel.getInstance().mapProperty("content.mimetype", FieldUse.FACET, rb.req);
+        SchemaField schemaField = rb.req.getSchema().getFieldOrNull(schemaFieldName);
+        if(schemaField != null)
+        {
+            SortedDocValues sortedDocValues = context.reader().getSortedDocValues(schemaFieldName);
+            
+            if(sortedDocValues != null)
+            {
+                int ordinal = sortedDocValues.getOrd(doc);
+                if(ordinal > -1)
+                {
+                   String value = (String)schemaField.getType().toObject(schemaField, sortedDocValues.lookupOrd(ordinal));
+                   String group;
+                   int index = value.indexOf('/');
+                   if(index == -1)
+                   {
+                       group = value;
+                   }
+                   else
+                   {
+                       group = value.substring(0, index);
+                   }
+                   
+                   Counter counter = counters.get(group);
+                   if(counter == null)
+                   {
+                       counter = Counter.newCounter();
+                       counters.put(group, counter);
+                   }
+                   counter.addAndGet(1);
+                }
+            }
+        }
+       
+        delegate.collect(doc);
+    }
+
+    public void finish() throws IOException 
+    {
+        NamedList<Object> analytics = new NamedList<>();
+        rb.rsp.add("analytics", analytics);
+        NamedList<Object> map = new NamedList<>();
+        analytics.add("groupings", map);
+        for(String key : counters.keySet())
+        {
+            Counter counter = counters.get(key);
+            map.add(key, counter.get());
+        }
+        
+        if(this.delegate instanceof DelegatingCollector) {
+            ((DelegatingCollector)this.delegate).finish();
+        }
+    }
+}
