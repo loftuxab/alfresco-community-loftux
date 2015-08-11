930257ad58e2865551c568b12fa7b1a7953029c0
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-08-11 08:03:54 +0000

    Merged 5.0.N (5.0.3) to HEAD (5.1)    109838: Merged DEV to 5.0.N (5.0.3)       108444 : MNT-14322 - Do not use wildcard on numeral fields       109664 : MNT-14322 - added a test for the scenarios described in the issue
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD@109875 7033f55c-6c29-4ad8-afba-765cb616e738
    
    Former-commit-id: 00ccefe4150b4a359a2ce91ac1de22af6e01ed53

--- root/projects/solr4/source/java/org/alfresco/solr/AlfrescoCoreAdminTester.java
+++ root/projects/solr4/source/java/org/alfresco/solr/AlfrescoCoreAdminTester.java
@@ -1741,6 +1741,11 @@ public class AlfrescoCoreAdminTester
                         folder00QName, date00);
             check_order(rsp, core, dataModel);
 
+            addTrieTypeTestData(core, dataModel, folder00NodeRef, rootNodeRef, baseFolderNodeRef, baseFolderQName,
+                    folder00QName, date00);
+            checkTrieFields(rsp, core, dataModel);
+
+
             // remove core
 
             if (remove)
@@ -1753,6 +1758,93 @@ public class AlfrescoCoreAdminTester
         {
             e.printStackTrace();
         }
+    }
+
+    private void checkTrieFields(SolrQueryResponse rsp, SolrCore core,
+            AlfrescoSolrDataModel dataModel) throws IOException
+    {
+        NamedList<Object> report = new SimpleOrderedMap<Object>();
+        rsp.add("trie fields query", report);
+
+        
+        //See MNT-14322 for the origin of the numbers
+        testQueryByHandler(report, core, "/cmis",
+                    "SELECT * FROM cmistest:extendedContent WHERE cmistest:singleInteger = 98218", 0, null, null, null,
+                    null, null, (String) null);
+        
+        testQueryByHandler(report, core, "/cmis",
+                "SELECT * FROM cmistest:extendedContent WHERE cmistest:singleInteger = 98198", 1, null, null, null,
+                null, null, (String) null);
+        
+        testQueryByHandler(report, core, "/cmis",
+                "SELECT * FROM cmistest:extendedContent WHERE cmistest:singleInteger = 98200", 1, null, null, null,
+                null, null, (String) null);        
+        
+        
+        testQueryByHandler(report, core, "/cmis",
+                "SELECT * FROM cmistest:extendedContent WHERE cmistest:singleLong = 3956650", 1, null, null, null,
+                null, null, (String) null);
+        
+        testQueryByHandler(report, core, "/cmis",
+                "SELECT * FROM cmistest:extendedContent WHERE cmistest:singleLong = 3956651", 1, null, null, null,
+                null, null, (String) null);
+        
+    }
+
+    private void addTrieTypeTestData(SolrCore core, AlfrescoSolrDataModel dataModel,
+            NodeRef folder00NodeRef, NodeRef rootNodeRef, NodeRef baseFolderNodeRef,
+            QName baseFolderQName, QName folder00QName, Date date00) throws IOException
+    {
+        HashMap<QName, PropertyValue> content00Properties = new HashMap<QName, PropertyValue>();
+        MLTextPropertyValue desc00 = new MLTextPropertyValue();
+        desc00.addValue(Locale.ENGLISH, "Trie test1");
+        content00Properties.put(ContentModel.PROP_DESCRIPTION, desc00);
+        content00Properties.put(ContentModel.PROP_TITLE, desc00);
+        content00Properties.put(ContentModel.PROP_NAME, new StringPropertyValue("Trie test1"));
+        content00Properties.put(ContentModel.PROP_CREATED,
+                    new StringPropertyValue(DefaultTypeConverter.INSTANCE.convert(String.class, date00)));
+
+        StringPropertyValue firstIntValue = new StringPropertyValue("98198");
+        content00Properties.put(singleInteger,firstIntValue);
+        
+        StringPropertyValue firstLongValue = new StringPropertyValue("3956650");
+        content00Properties.put(singleLong,firstLongValue);
+        
+        NodeRef content00NodeRef = new NodeRef(new StoreRef("workspace", "SpacesStore"), createGUID());
+        QName content00QName = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "Trie test1");
+        ChildAssociationRef content00CAR = new ChildAssociationRef(ContentModel.ASSOC_CONTAINS, folder00NodeRef,
+                    content00QName, content00NodeRef, true, 0);
+        addNode(core, dataModel, 1, 300, 1, extendedContent, new QName[] { ContentModel.ASPECT_OWNABLE,
+                    ContentModel.ASPECT_TITLED }, content00Properties, null, "andy",
+                    new ChildAssociationRef[] { content00CAR }, new NodeRef[] { baseFolderNodeRef, rootNodeRef,
+                                folder00NodeRef }, new String[] { "/" + baseFolderQName.toString() + "/"
+                                + folder00QName.toString() + "/" + content00QName.toString() }, content00NodeRef, true);
+        
+        HashMap<QName, PropertyValue> content01Properties = new HashMap<QName, PropertyValue>();
+        MLTextPropertyValue desc01 = new MLTextPropertyValue();
+        desc01.addValue(Locale.ENGLISH, "Trie test2");
+        content01Properties.put(ContentModel.PROP_DESCRIPTION, desc01);
+        content01Properties.put(ContentModel.PROP_TITLE, desc01);
+        content01Properties.put(ContentModel.PROP_NAME, new StringPropertyValue("Trie test2"));
+        content01Properties.put(ContentModel.PROP_CREATED,
+                    new StringPropertyValue(DefaultTypeConverter.INSTANCE.convert(String.class, date00)));
+
+        StringPropertyValue secondIntValue = new StringPropertyValue("98200");
+        content01Properties.put(singleInteger,secondIntValue);
+        
+        StringPropertyValue secondLongValue = new StringPropertyValue("3956651");
+        content01Properties.put(singleLong,secondLongValue);
+        
+        NodeRef content01NodeRef = new NodeRef(new StoreRef("workspace", "SpacesStore"), createGUID());
+        QName content01QName = QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "Trie test1");
+        ChildAssociationRef content01CAR = new ChildAssociationRef(ContentModel.ASSOC_CONTAINS, folder00NodeRef,
+                    content01QName, content01NodeRef, true, 0);
+        addNode(core, dataModel, 1, 301, 1, extendedContent, new QName[] { ContentModel.ASPECT_OWNABLE,
+                    ContentModel.ASPECT_TITLED }, content01Properties, null, "andy",
+                    new ChildAssociationRef[] { content01CAR }, new NodeRef[] { baseFolderNodeRef, rootNodeRef,
+                                folder00NodeRef }, new String[] { "/" + baseFolderQName.toString() + "/"
+                                + folder00QName.toString() + "/" + content01QName.toString() }, content01NodeRef, true);
+        
     }
 
     /**
