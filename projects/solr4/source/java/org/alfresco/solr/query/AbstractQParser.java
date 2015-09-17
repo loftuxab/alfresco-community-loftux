acf3dc5f9eb6d0adbc77213308b328097a83e39f
ahind <ahind@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-09-17 08:53:45 +0000

    Fix for     ACE-4133    SOLR 4 - sharded - Categories and tags are broken
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@112254 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/solr4/source/java/org/alfresco/solr/query/AbstractQParser.java
+++ projects/solr4/source/java/org/alfresco/solr/query/AbstractQParser.java
@@ -53,6 +53,7 @@ import org.alfresco.solr.AlfrescoSolrDataModel.FieldUse;
 import org.alfresco.solr.AlfrescoSolrDataModel.IndexedField;
 import org.alfresco.util.Pair;
 import org.apache.commons.io.IOUtils;
+import org.apache.lucene.util.ArrayUtil;
 import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.params.SolrParams;
@@ -594,6 +595,11 @@ public abstract class AbstractQParser extends QParser implements QueryConstants
 
         int start = startS != null ? Integer.parseInt(startS) : 0;
         int rows = rowsS != null ? Integer.parseInt(rowsS) : 10;
+        // Avoid +1 in SOLR code which produces null:java.lang.NegativeArraySizeException at at org.apache.lucene.util.PriorityQueue.<init>(PriorityQueue.java:56)
+        if(rows >  1000000)
+        {
+            rows = 1000000;
+        }
 
         // Fix sort fields here
         if(sortStr != null)
