37b1134331ee47cb5af0705a95fa4cad5f0bd8f5
adavis <adavis@7033f55c-6c29-4ad8-afba-765cb616e738>
2016-06-03 16:40:56 +0000

    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@127808 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/solr4/source/java/org/alfresco/solr/component/AsyncBuildSuggestComponent.java
+++ projects/solr4/source/java/org/alfresco/solr/component/AsyncBuildSuggestComponent.java
@@ -1,30 +1,5 @@
 package org.alfresco.solr.component;
 
-/*
- * Copyright (C) 2005-2014 Alfresco Software Limited.
- *
- * This file is part of Alfresco
- *
- * Alfresco is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Alfresco is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
- * 
- * 
- * This file is based upon code copied from the Solr project and modified.
- * The original code is licensed to the Apache Software Foundation. Please see:
- * 
- *   http://lucene.apache.org/solr/
- *   http://www.apache.org/licenses/LICENSE-2.0
- */
 
 import java.io.File;
 import java.io.IOException;
