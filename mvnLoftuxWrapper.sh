f25958a1d2c9786632c9674d753015b26a0997d5
Peter Löfgren <peter@loftux.se>
2017-02-22 11:17:27 +0100

    Creating LX96

--- mvnLoftuxWrapper.sh
+++ mvnLoftuxWrapper.sh
@@ -2,7 +2,7 @@
 
 #Set the name and build number
 versionedition="Community by Loftux AB"
-buildnumber="LX95"
+buildnumber="LX96"
 buildAlfresco="5.2-SNAPSHOT"
 
 # Add url in format id::layout::url, see http://maven.apache.org/plugins/maven-deploy-plugin/deploy-mojo.html
