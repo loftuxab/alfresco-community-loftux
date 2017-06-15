916f1c97245c12f89b3fd46a71f64289a209c457
arebegea <arebegea@7033f55c-6c29-4ad8-afba-765cb616e738>
2017-06-15 11:16:46 +0000

    Merged 5.2.N (5.2.2) to HEAD (5.2)    137065 gjames: Merged searchrep (5.2.1) to 5.2.N (5.2.1)       136886 ahind: SEARCH-339    Interval       - merged to SOLR 4
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@137580 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/solr4/source/java/org/alfresco/solr/component/RewriteFacetCountsComponent.java
+++ projects/solr4/source/java/org/alfresco/solr/component/RewriteFacetCountsComponent.java
@@ -77,10 +77,10 @@ public class RewriteFacetCountsComponent extends SearchComponent
         rewrite(rb, "_field_mappings_", "facet_counts", "facet_fields");
         rewrite(rb, "_date_mappings_", "facet_counts", "facet_dates");
         rewrite(rb, "_range_mappings_", "facet_counts", "facet_ranges");
+        rewrite(rb, "_interval_mappings_", "facet_counts", "facet_intervals");
         
         rewrite(rb, "_pivot_mappings_", "facet_counts", "facet_pivot");
         rewritePivotFields(rb, "facet_counts", "facet_pivot");
-        // TODO: rewrite(rb, "_interval_mappings_", "facet_counts", "facet_fields");
         
         rewrite(rb, "_stats_field_mappings_", "stats", "stats_fields");
         
