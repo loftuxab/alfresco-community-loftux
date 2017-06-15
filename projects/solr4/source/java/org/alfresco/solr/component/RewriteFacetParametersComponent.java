019564f5d6beaac6d2e348e4653283dc1e652cb4
arebegea <arebegea@7033f55c-6c29-4ad8-afba-765cb616e738>
2017-06-15 11:17:07 +0000

    Merged 5.2.N (5.2.2) to HEAD (5.2)    137068 gjames: Merged searchrep (5.2.1) to 5.2.N (5.2.1)       136889 ahind: SEARCH-406    Facet rewrite is specific to {!afts .... it should be more general        - merged to SOLR 4
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root@137583 7033f55c-6c29-4ad8-afba-765cb616e738

--- projects/solr4/source/java/org/alfresco/solr/component/RewriteFacetParametersComponent.java
+++ projects/solr4/source/java/org/alfresco/solr/component/RewriteFacetParametersComponent.java
@@ -392,10 +392,13 @@ public class RewriteFacetParametersComponent extends SearchComponent
             for(String facetFields : facetFieldsOrig)
             {
                 StringBuilder commaSeparated = new StringBuilder();
+                StringBuilder mapping = new StringBuilder();
+                StringBuilder unmapped = new StringBuilder();
                 String[] fields = parseFacetField(facetFields);
                 
                 for(String field : fields)
                 {
+                	String prefix = "";
                     field = field.trim();
                     
                     if(field.endsWith("()"))
@@ -404,14 +407,27 @@ public class RewriteFacetParametersComponent extends SearchComponent
                         continue;
                     }
                     
+                    if(field.startsWith("{!"))
+                    {
+                    	int index = field.indexOf("}");
+                    	if((index > 0) && (index < (field.length() - 1)))
+                    	{
+                    		prefix = field.substring(0, index+1);
+                    		field = field.substring(index+1);
+                    	}
+                    }
                     
                     if(req.getSchema().getFieldOrNull(field) != null)
                     {
                         if(commaSeparated.length() > 0)
                         {
                             commaSeparated.append(",");
+                            mapping.append(",");
+                            unmapped.append(",");
                         }
-                        commaSeparated.append(field);
+                        commaSeparated.append(prefix).append(field);
+                        mapping.append(field);
+                        unmapped.append(field);
                     }
                     else
                     {
@@ -420,13 +436,17 @@ public class RewriteFacetParametersComponent extends SearchComponent
                         if(commaSeparated.length() > 0)
                         {
                             commaSeparated.append(",");
+                            mapping.append(",");
+                            unmapped.append(",");
                         }
-                        commaSeparated.append(mappedField);
+                        commaSeparated.append(prefix).append(mappedField);
+                        mapping.append(mappedField);
+                        unmapped.append(field);
                     }
                 }
                 if(!facetFields.equals(commaSeparated.toString()))
                 {
-                    fieldMappings.put(facetFields, commaSeparated.toString());
+                    fieldMappings.put(unmapped.toString(), mapping.toString());
                 }
                 if(commaSeparated.length() > 0)
                 {
